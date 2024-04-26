9ceca225fd546b315b712397c24dfa44a5405f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 16:03:54 +0000

    Add package ncmlistdownloader/1.0.1.24042601/ncmlistdownloader-1.0.1.24042601-py3-none-any.whl

---
+++ packages/ncmlistdownloader/ncmlistdownloader-1.0.1.24042601-py3-none-any.whl/ncmlistdownloader/__init__.py
@@ -0,0 +1,43 @@
+'''
+ncmlistdownloader/__init__.py
+Core.Ver.1.0.2.240426
+Author: CooooldWind_
+'''
+from pathlib import Path
+from ncmlistdownloader.playlist import *
+from ncmlistdownloader.common import *
+from ncmlistdownloader.common.global_args import *
+
+def main():
+    for i in CMD_START_WORDS:
+        print(i)
+    print(f'[*]{CORE_VERSION}')
+    id = str(input("ID: "))
+    p = Playlist(id)
+    p.get_info()
+    print("Playlist info-reading succeed.")
+    d = str(input("Dir: "))
+    if d == '':
+        d = str(Path.home()) + '/Downloads/ncmld_downloads/'
+    fnf = str(input("Filename format: "))
+    if fnf == '':
+        fnf = '$title$ - $artist$'
+    if d[-1] != '/' and d[-1] != '\\':
+        d += '/'
+    d = d.replace('\\', '/')
+    auto_mkdir(d)
+    for i in p.track:
+        i.filename_format = d + fnf
+    p.get_detail_info()
+    for i in p.track:
+        music_filename = i.song_download()
+        if music_filename == -1:
+            print(i.title + ' cannot download.')
+            continue
+        i.cover_download()
+        i.lyric_get()
+        i.attribute_write()
+        i.cover_write()
+        i.lyric_write()
+        print(i.title + ' succeed.')
+    print('Succeed. Files at:', d)
