4aeff8ee7cab3c4f95f11b5ac5cbb0ec8663bb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:44 +0000

    Add package ashpokealan10/1.0.1/ashpokealan10-1.0.1-py3-none-any.whl

---
+++ packages/ashpokealan10/ashpokealan10-1.0.1-py3-none-any.whl/ashpokealan10/alan.py
@@ -0,0 +1,50 @@
+def getPokemon(self):
+    return self._pokemon
+
+
+def getNumber(self):
+    return self._number
+
+
+def getName(self):
+    return self._name
+
+
+def getType1(self):
+    return self._type1
+
+
+def getType2(self):
+    return self._type2
+
+
+def getHP(self):
+    return self._hp
+
+
+def getSpeed(self):
+    return self._speed
+
+
+def getGeneration(self):
+    return self._generation
+
+
+def getLegendary(self):
+    return self._legendary
+
+
+def getDefense(self):
+    return self._defense
+
+
+def getAttack(self):
+    return self._attack
+
+
+def getSp(self):
+    return self._sp
+
+
+def getSp1(self):
+    return self._sp1
