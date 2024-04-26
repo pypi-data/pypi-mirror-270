1838d97914e80ee2a57d1d1cc28079842619500b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:47 +0000

    Add package pms-onpremise-encoder/0.0.1/pms-onpremise-encoder-0.0.1.tar.gz

---
+++ packages/pms-onpremise-encoder/pms-onpremise-encoder-0.0.1.tar.gz/pms-onpremise-encoder-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pms-onpremise-encoder',
+    version='0.0.1',
+    description='',
+    author='Heeyong Kwon',
+    author_email='heeyong.kwon@4by4inc.com',
+    url='',
+    install_requires=['ffmpeg-python', 'pms-inference-engine', 'loguru', 'numpy', 'python-dotenv', 'redis', 'PyMySQL', ],
+    packages=find_packages(exclude=[]),
+    keywords=['pms-encoder'],
+    python_requires='>=3.10',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
