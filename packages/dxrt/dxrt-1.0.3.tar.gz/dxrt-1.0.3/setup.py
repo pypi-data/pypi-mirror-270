31f2b0648cc0447af6437265e8478b3ce1b0c841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:51 +0000

    Add package dxrt/1.0.3/dxrt-1.0.3.tar.gz

---
+++ packages/dxrt/dxrt-1.0.3.tar.gz/dxrt-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='dxrt',
+   version="1.0.3",
+   author="Gael Peña",
+   author_email="gaelpeje1@gmail.com",
+   description="Libreria para generar un pokemon aleatorio",
+   packages=find_packages(),
+   package_data={'dxrt' : ["pokemon.csv"]},
+   install_requires=[
+          'pandas',
+          ],
+)
