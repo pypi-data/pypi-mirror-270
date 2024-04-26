0da4af42a5ec5d8f28a50d0c4b16851fed182aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:06 +0000

    Add package pixel-font-builder/0.0.21/pixel_font_builder-0.0.21-py3-none-any.whl

---
+++ packages/pixel-font-builder/pixel_font_builder-0.0.21-py3-none-any.whl/pixel_font_builder/font.py
@@ -0,0 +1,114 @@
+import datetime
+import os
+
+import bdffont
+import fontTools.fontBuilder
+import fontTools.ttLib
+import pcffont
+
+from pixel_font_builder import os2, opentype, bdf, pcf
+from pixel_font_builder.glyph import Glyph
+from pixel_font_builder.info import MetaInfo, LayoutHeader
+
+
+class FontBuilder:
+    def __init__(self, size: int):
+        self.size = size
+        self.created_time: datetime.datetime | None = None
+        self.modified_time: datetime.datetime | None = None
+
+        self.meta_info = MetaInfo()
+        self.horizontal_header = LayoutHeader()
+        self.vertical_header = LayoutHeader()
+        self.os2_config = os2.Config()
+        self.character_mapping: dict[int, str] = {}
+        self.glyphs: list[Glyph] = []
+        self.opentype_config = opentype.Config()
+        self.bdf_config = bdf.Config()
+        self.pcf_config = pcf.Config()
+
+    def prepare_glyphs(self) -> tuple[list[str], dict[str, Glyph]]:
+        glyph_order = ['.notdef']
+        name_to_glyph = {}
+
+        for glyph in self.glyphs:
+            assert glyph.name not in name_to_glyph, f"Duplicate glyphs: '{glyph.name}'"
+            if glyph.name != '.notdef':
+                glyph_order.append(glyph.name)
+            name_to_glyph[glyph.name] = glyph
+
+        assert '.notdef' in name_to_glyph, "Need to provide a glyph named '.notdef'"
+
+        for code_point, glyph_name in self.character_mapping.items():
+            assert code_point >= 0, 'Code points must >= 0'
+            assert glyph_name in name_to_glyph, f"Missing glyph: '{glyph_name}'"
+
+        return glyph_order, name_to_glyph
+
+    def to_xtf_builder(self, is_ttf: bool, flavor: opentype.Flavor = None) -> fontTools.fontBuilder.FontBuilder:
+        return opentype.create_builder(self, is_ttf, flavor)
+
+    def to_otf_builder(self, flavor: opentype.Flavor = None) -> fontTools.fontBuilder.FontBuilder:
+        return self.to_xtf_builder(False, flavor)
+
+    def save_otf(
+            self,
+            file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
+            flavor: opentype.Flavor = None,
+    ):
+        self.to_otf_builder(flavor).save(file_path)
+
+    def to_ttf_builder(self, flavor: opentype.Flavor = None) -> fontTools.fontBuilder.FontBuilder:
+        return self.to_xtf_builder(True, flavor)
+
+    def save_ttf(
+            self,
+            file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
+            flavor: opentype.Flavor = None,
+    ):
+        self.to_ttf_builder(flavor).save(file_path)
+
+    def to_bdf_builder(self) -> bdffont.BdfFont:
+        return bdf.create_font(self)
+
+    def save_bdf(self, file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]):
+        self.to_bdf_builder().save(file_path)
+
+    def to_pcf_builder(self) -> pcffont.PcfFont:
+        return pcf.create_font(self)
+
+    def save_pcf(self, file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes]):
+        self.to_pcf_builder().save(file_path)
+
+
+class FontCollectionBuilder:
+    def __init__(self, font_builders: list[FontBuilder] = None):
+        if font_builders is None:
+            font_builders = []
+        self.font_builders = font_builders
+
+    def _to_xtf_builders(self, is_ttf: bool) -> list[fontTools.fontBuilder.FontBuilder]:
+        return [font_builder.to_xtf_builder(is_ttf) for font_builder in self.font_builders]
+
+    def to_xtc_builder(self, is_ttf: bool) -> fontTools.ttLib.TTCollection:
+        return opentype.create_collection_builder(self._to_xtf_builders(is_ttf))
+
+    def to_otc_builder(self) -> fontTools.ttLib.TTCollection:
+        return self.to_xtc_builder(False)
+
+    def save_otc(
+            self,
+            file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
+            share_tables: bool = True,
+    ):
+        self.to_otc_builder().save(file_path, share_tables)
+
+    def to_ttc_builder(self) -> fontTools.ttLib.TTCollection:
+        return self.to_xtc_builder(True)
+
+    def save_ttc(
+            self,
+            file_path: str | bytes | os.PathLike[str] | os.PathLike[bytes],
+            share_tables: bool = True,
+    ):
+        self.to_ttc_builder().save(file_path, share_tables)
---
+++ packages/pixel-font-builder/pixel_font_builder-0.0.21-py3-none-any.whl/pixel_font_builder/pcf.py
@@ -0,0 +1,124 @@
+import logging
+import math
+
+from pcffont import PcfFont, PcfMetric, PcfProperties, PcfAccelerators, PcfMetrics, PcfBitmaps, PcfBdfEncodings, PcfScalableWidths, PcfGlyphNames
+
+import pixel_font_builder
+from pixel_font_builder.info import SerifMode, WidthMode
+
+logger = logging.getLogger('pixel_font_builder.pcf')
+
+
+class Config:
+    def __init__(
+            self,
+            resolution_x: int = 75,
+            resolution_y: int = 75,
+    ):
+        self.resolution_x = resolution_x
+        self.resolution_y = resolution_y
+
+
+def create_font(context: 'pixel_font_builder.FontBuilder') -> PcfFont:
+    config = context.pcf_config
+    font_size = context.size
+    meta_info = context.meta_info
+    horizontal_header = context.horizontal_header
+    os2_config = context.os2_config
+    character_mapping = context.character_mapping
+    _, name_to_glyph = context.prepare_glyphs()
+
+    logger.debug("Create 'PcfFont': %s", meta_info.family_name)
+    font = PcfFont()
+
+    logger.debug("Setup 'Glyphs'")
+    bdf_encodings = PcfBdfEncodings()
+    glyph_names = PcfGlyphNames()
+    metrics = PcfMetrics()
+    scalable_widths = PcfScalableWidths()
+    bitmaps = PcfBitmaps()
+
+    min_bounds = None
+    max_bounds = None
+    bdf_encodings.default_char = 0xFFFE
+    character_mapping[bdf_encodings.default_char] = '.notdef'
+    for code_point, glyph_name in sorted(character_mapping.items()):
+        if code_point > 0xFFFF:
+            break
+        glyph = name_to_glyph[glyph_name]
+        logger.debug("Add 'Encoding': %04X", code_point)
+        bdf_encodings[code_point] = len(glyph_names)
+        glyph_names.append(f'U+{code_point:04X}')
+        metric = PcfMetric(
+            left_side_bearing=glyph.horizontal_origin_x,
+            right_side_bearing=glyph.horizontal_origin_x + glyph.width,
+            character_width=glyph.advance_width,
+            ascent=glyph.horizontal_origin_y + glyph.height,
+            descent=-glyph.horizontal_origin_y,
+        )
+        if min_bounds is None or (metric.left_side_bearing + metric.right_side_bearing) < (min_bounds.left_side_bearing + min_bounds.right_side_bearing):
+            min_bounds = metric
+        if max_bounds is None or (metric.left_side_bearing + metric.right_side_bearing) > (max_bounds.left_side_bearing + max_bounds.right_side_bearing):
+            max_bounds = metric
+        metrics.append(metric)
+        scalable_widths.append(math.ceil((glyph.advance_width / font_size) * (75 / config.resolution_x) * 1000))
+        bitmaps.append(glyph.data)
+
+    font.bdf_encodings = bdf_encodings
+    font.glyph_names = glyph_names
+    font.metrics = metrics
+    font.ink_metrics = metrics
+    font.scalable_widths = scalable_widths
+    font.bitmaps = bitmaps
+
+    logger.debug("Setup 'Accelerators'")
+    accelerators = PcfAccelerators()
+    accelerators.ink_inside = True
+    accelerators.ink_metrics = True
+    accelerators.font_ascent = horizontal_header.ascent
+    accelerators.font_descent = -horizontal_header.descent
+    accelerators.min_bounds = min_bounds
+    accelerators.max_bounds = max_bounds
+    accelerators.ink_min_bounds = min_bounds
+    accelerators.ink_max_bounds = max_bounds
+    font.accelerators = accelerators
+    font.bdf_accelerators = accelerators
+
+    logger.debug("Setup 'Properties'")
+    properties = PcfProperties()
+    properties.foundry = meta_info.manufacturer
+    properties.family_name = meta_info.family_name
+    properties.weight_name = meta_info.style_name
+    properties.slant = 'R'
+    properties.setwidth_name = 'Normal'
+    if meta_info.serif_mode == SerifMode.SERIF:
+        properties.add_style_name = 'Serif'
+    elif meta_info.serif_mode == SerifMode.SANS_SERIF:
+        properties.add_style_name = 'Sans Serif'
+    else:
+        properties.add_style_name = meta_info.serif_mode
+    properties.pixel_size = font_size
+    properties.point_size = font_size * 10
+    properties.resolution_x = config.resolution_x
+    properties.resolution_y = config.resolution_y
+    if meta_info.width_mode == WidthMode.MONOSPACED:
+        properties.spacing = 'M'
+    elif meta_info.width_mode == WidthMode.DUOSPACED:
+        properties.spacing = 'D'
+    elif meta_info.width_mode == WidthMode.PROPORTIONAL:
+        properties.spacing = 'P'
+    else:
+        properties.spacing = meta_info.width_mode
+    properties.average_width = round(sum([metric.character_width * 10 for metric in font.metrics]) / len(font.metrics))
+    properties.charset_registry = 'ISO10646'
+    properties.charset_encoding = '1'
+    properties.generate_xlfd()
+    properties.x_height = os2_config.x_height
+    properties.cap_height = os2_config.cap_height
+    properties.font_version = meta_info.version
+    properties.copyright = meta_info.copyright_info
+    properties['LICENSE'] = meta_info.license_info
+    font.properties = properties
+
+    logger.debug("Create 'PcfFont' finished")
+    return font
