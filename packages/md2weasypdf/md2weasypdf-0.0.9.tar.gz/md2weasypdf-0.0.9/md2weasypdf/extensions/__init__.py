b4fd99f012105744db99b0a0839f14b632c46b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 17:03:09 +0000

    Add package md2weasypdf/0.0.9/md2weasypdf-0.0.9.tar.gz

---
+++ packages/md2weasypdf/md2weasypdf-0.0.9.tar.gz/md2weasypdf-0.0.9/md2weasypdf/extensions/__init__.py
@@ -0,0 +1,9 @@
+from markdown.extensions.abbr import AbbrExtension
+from markdown.extensions.tables import TableExtension
+
+from .checkbox import CheckboxExtension
+from .footnotes import FootnoteExtension
+from .subscript import SubscriptExtension
+from .textbox import TextboxExtension
+from .toa import ToaExtension
+from .toc import TocExtension
