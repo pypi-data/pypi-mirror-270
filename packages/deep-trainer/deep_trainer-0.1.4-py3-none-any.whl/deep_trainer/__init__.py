0f7500b3f46bd35b2fb5f25689a0a5ee92cc564f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 17:03:11 +0000

    Add package deep-trainer/0.1.4/deep_trainer-0.1.4-py3-none-any.whl

---
+++ packages/deep-trainer/deep_trainer-0.1.4-py3-none-any.whl/deep_trainer/__init__.py
@@ -0,0 +1,10 @@
+"""Deep Trainer
+
+Helps you train your deep learning models
+"""
+
+from .pytorch.metric import Metric
+from .pytorch.logging import TrainLogger
+from .pytorch.trainer import PytorchTrainer
+
+__version__ = "0.1.4"
