0b9019f7afb048cf76393af0fd043392e348dd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 16:03:15 +0000

    Add package hello-world-suraj-test/1.2.0/hello_world_suraj_test-1.2.0.tar.gz

---
+++ packages/hello-world-suraj-test/hello_world_suraj_test-1.2.0.tar.gz/hello_world_suraj_test-1.2.0/hello_world.py
@@ -0,0 +1,8 @@
+import datetime
+
+def test():
+    print("hello world")
+    print(datetime.now().date())
+    
+if __name__ == "__main__":
+    test()
---
+++ packages/hello-world-suraj-test/hello_world_suraj_test-1.2.0.tar.gz/hello_world_suraj_test-1.2.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='hello_world_suraj_test',
+    version='1.2.0',
+    scripts=['hello_world.py'],
+)
