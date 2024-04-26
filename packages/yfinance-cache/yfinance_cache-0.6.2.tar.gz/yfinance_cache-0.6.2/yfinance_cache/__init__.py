43c9d60ed7707fef863a483957782926cb570f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:08 +0000

    Add package yfinance-cache/0.6.2/yfinance_cache-0.6.2.tar.gz

---
+++ packages/yfinance-cache/yfinance_cache-0.6.2.tar.gz/yfinance_cache-0.6.2/yfinance_cache/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from .yfc_dat import Period, Interval, AmbiguousComparisonException
+from .yfc_ticker import Ticker, verify_cached_tickers_prices
+from .yfc_multi import download
+from .yfc_logging import EnableLogging, DisableLogging
+from .yfc_cache_manager import _option_manager as options
+
+from .yfc_upgrade import _init_options
+_init_options()
+
