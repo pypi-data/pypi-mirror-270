51bf25e86f03a55870cdc9ea4771fe18d4652241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 14:03:08 +0000

    Add package xl-database/0.6.5/xl_database-0.6.5.tar.gz

---
+++ packages/xl-database/xl_database-0.6.5.tar.gz/xl_database-0.6.5/src/xl_database/__init__.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData, create_engine, func, Table
+
+
+class Database(SQLAlchemy):
+    def init(self, url):
+        engine = create_engine(url)
+        metadata = MetaData(bind=engine)
+        return engine, metadata
+
+    def mount(self, app):
+        super().init_app(app)
+
+
+db = Database()
+session = db.session
