18e8ac5e586a85844dbde98f4e2f2ac140ccc9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:25 +0000

    Add package uac-api/0.3.0/uac-api-0.3.0.tar.gz

---
+++ packages/uac-api/uac-api-0.3.0.tar.gz/uac-api-0.3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from uac_api import __version__
+version = __version__
+
+def main():
+    with open('README.md', 'r') as readme:
+        long_description = readme.read()
+    setup(
+        name='uac-api',
+        version=version,
+        author_email="huseyin.gomleksizoglu@stonebranch.com",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            "requests"
+        ],
+        author='Stonebranch',
+        description='A Python wrapper for the Stonebranch UAC API',
+        python_requires='>=3.7',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        long_description=long_description,
+        long_description_content_type="text/markdown"
+    )
+
+
+if __name__ == '__main__':
+    main()
