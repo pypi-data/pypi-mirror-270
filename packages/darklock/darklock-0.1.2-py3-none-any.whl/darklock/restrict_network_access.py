c39d5a672eaf834a83e7c78db432206318437033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:07:34 +0000

    Add package darklock/0.1.2/darklock-0.1.2-py3-none-any.whl

---
+++ packages/darklock/darklock-0.1.2-py3-none-any.whl/darklock/restrict_network_access.py
@@ -0,0 +1,37 @@
+import socket
+import logging
+from darklock.no_internet_socket import NoInternetSocket
+from darklock.singleton import Singleton
+
+
+class RestrictNetworkAccess(metaclass=Singleton):
+    def __init__(self):
+        self.__init_logger()
+        self.original_socket = socket.socket
+        self.original_socket_type = socket.SocketType
+
+    def __init_logger(self):
+        """
+        Initializes the logger with a file handler and a specific format.
+        """
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        # handler = logging.FileHandler('network_access_attempts.log')
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)  # Set the handler's level to INFO
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
+
+    def activate(self):
+        self.logger.info("Installing network restrictions.")
+        self.original_socket = socket.socket
+        self.original_socket_type = socket.SocketType
+        socket.socket = NoInternetSocket
+        socket.SocketType = NoInternetSocket
+
+    def deactivate(self):
+        self.logger.info("Uninstalling network restrictions.")
+        socket.socket = self.original_socket
+        socket.SocketType = self.original_socket_type
