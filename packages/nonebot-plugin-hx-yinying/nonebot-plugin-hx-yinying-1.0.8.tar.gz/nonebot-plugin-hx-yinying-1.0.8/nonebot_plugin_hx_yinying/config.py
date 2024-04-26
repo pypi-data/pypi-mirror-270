07077d9cb54538195655b1f5c687c3f3160070cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:57 +0000

    Add package nonebot-plugin-hx-yinying/1.0.8/nonebot-plugin-hx-yinying-1.0.8.tar.gz

---
+++ packages/nonebot-plugin-hx-yinying/nonebot-plugin-hx-yinying-1.0.8.tar.gz/nonebot-plugin-hx-yinying-1.0.8/nonebot_plugin_hx_yinying/config.py
@@ -0,0 +1,16 @@
+__author__ = "HuanXin"
+from typing import Optional
+
+import nonebot
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    # 插件版本号勿动！！！！
+    hx_version: Optional[str] = "1.0.8"
+    # 你的appid
+    yinying_appid: Optional[str] = None
+    yinying_token: Optional[str] = None
+    hx_path: Optional[str] = None
+
+global_config = nonebot.get_driver().config
