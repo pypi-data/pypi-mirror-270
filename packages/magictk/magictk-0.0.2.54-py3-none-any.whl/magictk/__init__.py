f9f579325b807a6a8c50bcdbb01877a6a5c32cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 14:03:16 +0000

    Add package magictk/0.0.2.54/magictk-0.0.2.54-py3-none-any.whl

---
+++ packages/magictk/magictk-0.0.2.54-py3-none-any.whl/magictk/__init__.py
@@ -0,0 +1,11 @@
+from magictk.window import Window
+from magictk.basicwindow import Tk
+from magictk.button import Button, ButtonFill
+from magictk.progressbar import ProgressBar
+from magictk.checkbox import Checkbox, RadioGroup
+from magictk.submenu import Menu, MenuObjs
+from magictk.select import Select
+from magictk.frame import Frame, Container
+from magictk.entry import Entry
+from magictk.scrollbar import ScrollBar
+from magictk.icon import Icon
