0352603fc7145f6b2089a3457f6ebf7b0d546a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:04:00 +0000

    Add package bento-tools/2.1/bento_tools-2.1-py3-none-any.whl

---
+++ packages/bento-tools/bento_tools-2.1-py3-none-any.whl/bento/_constants.py
@@ -0,0 +1,3 @@
+PATTERN_COLORS = ["#17becf", "#1f77b4", "#7f7f7f", "#ff7f0e", "#d62728"]
+PATTERN_NAMES = ["cell_edge", "cytoplasmic", "none", "nuclear", "nuclear_edge"]
+PATTERN_PROBS = [f"{p}_p" for p in PATTERN_NAMES]
