60054bfbf09884a8bbf4a99a2fa08cf03405c367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 16:04:06 +0000

    Add package smartieee/0.39/smartieee-0.39.tar.gz

---
+++ packages/smartieee/smartieee-0.39.tar.gz/smartieee-0.39/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smartieee',
+    version='0.39',
+    description='A library that allows to use binary calculations such as IEEE754 standard or binary integer/float numbers',
+    long_description='For documentation go to: https://github.com/MarcinPec/smartieee',
+    author='MarcinPec',
+    url='https://github.com/MarcinPec/smartieee',
+    packages=find_packages(),
+)
