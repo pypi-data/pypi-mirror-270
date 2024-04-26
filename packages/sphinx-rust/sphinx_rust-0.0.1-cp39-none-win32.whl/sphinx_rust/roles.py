b3dd34ab75c7851b58202babff83f771ae677254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:04:02 +0000

    Add package sphinx-rust/0.0.1/sphinx_rust-0.0.1-cp39-none-win32.whl

---
+++ packages/sphinx-rust/sphinx_rust-0.0.1-cp39-none-win32.whl/sphinx_rust/roles.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from sphinx.roles import XRefRole
+
+
+class RustCrateXRefRole(XRefRole):
+    """Role to cross-reference a Rust crate."""
+
+
+class RustModuleXRefRole(XRefRole):
+    """Role to cross-reference a Rust module."""
+
+
+class RustStructXRefRole(XRefRole):
+    """Role to cross-reference a Rust struct."""
+
+
+class RustEnumXRefRole(XRefRole):
+    """Role to cross-reference a Rust enum."""
