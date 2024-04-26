9172ce09524ab6e16815d9fb45a3bb27cbfa582e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:04:05 +0000

    Add package armis/1.0.21/armis-1.0.21.tar.gz

---
+++ packages/armis/armis-1.0.21.tar.gz/armis-1.0.21/tests/test_integrations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# ruff: noqa: F811,SLF001,D103,S311
+import random
+
+import pytest
+import tenacity
+from test_fixture import armis_object
+
+
+def test_get_integrations_count(armis_object):
+    x = armis_object.get_integrations_count()
+    assert x > 0
+
+
+@pytest.fixture(scope="module")
+def integrations(armis_object):
+    return armis_object.get_integrations()
+
+
+def test_get_integration(integrations, armis_object):
+    for _ in range(5):
+        randomintegrationid = random.choice(list(integrations.keys()))
+        randomintegration = armis_object.get_integration(randomintegrationid)
+        assert randomintegrationid == randomintegration["id"]
+
+
+def test_get_integration_invalid(armis_object, integrations):
+    integration_ids = sorted(list(integrations.keys()))
+
+    # let's make up an integration_id that does not exist, i.e. not in
+    # this set:
+    # 0 >= valid integrations_ids >= max(list of integration_ids)
+
+    integrationid_invalid = random.randint(
+        integration_ids[-1] * 2,
+        integration_ids[-1] * 2222,
+    )
+
+    integration_invalid = armis_object.get_integration(integrationid_invalid)
+    assert len(integration_invalid) == 0
