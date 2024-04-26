8ec2282467db06d48cc182a6d9faea8a7bf4badd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:21 +0000

    Add package edgeiq/2.3.1/edgeiq-2.3.1-cp39-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-2.3.1-cp39-none-macosx_10_9_x86_64.whl/edgeiq/_globals.pyi
@@ -0,0 +1,9 @@
+from _typeshed import Incomplete
+
+DEVICE: bool
+DEVICE_MODE: str | None
+DEV_HOST: bool
+DEVICE_UUID: str | None
+USERNAME: str | None
+SYSTEM_ID: str | None
+random_number_generator: Incomplete
