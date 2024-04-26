d05d1501b14e5f7ac5f97e1f890f8bba50df395e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:04:00 +0000

    Add package poke-frm-donpolloloco/1.0.2/poke_frm_donpolloloco-1.0.2.tar.gz

---
+++ packages/poke-frm-donpolloloco/poke_frm_donpolloloco-1.0.2.tar.gz/poke_frm_donpolloloco-1.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='poke_frm_donpolloloco',
+    version='1.0.2',
+    author= "FernandoRoldan",
+    author_email="roldanf661@gmail.com",
+    description="LIBRERIA QUE GENRA UN POKEMON DE DONPOLLOLO, ES DECIR UN POKEMON ALEATORIO",
+    packages=["POKE_DONPOLLO"],
+    package_data= {'POKE_DONPOLLO': ['pokemon.csv']},
+    install_requires = ['pandas']
+)
