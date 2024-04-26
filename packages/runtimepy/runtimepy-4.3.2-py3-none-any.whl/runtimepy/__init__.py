c53c7abb2f4051d02c7e6760af63b4a21d5c5885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 05:02:49 +0000

    Add package runtimepy/4.3.2/runtimepy-4.3.2-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-4.3.2-py3-none-any.whl/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=6ec64992e5ab710e4c5fd007b9455747
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "4.3.2"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
---
+++ packages/runtimepy/runtimepy-4.3.2-py3-none-any.whl/runtimepy/net/server/app/__init__.py
@@ -0,0 +1,30 @@
+"""
+A module implementing application methods for this package's server interface.
+"""
+
+# built-in
+from importlib import import_module as _import_module
+
+# internal
+from runtimepy.net.arbiter.info import AppInfo
+from runtimepy.net.server import RuntimepyServerConnection
+from runtimepy.net.server.app.create import config_param, create_app
+from runtimepy.util import import_str_and_item
+
+
+async def setup(app: AppInfo) -> int:
+    """Perform server application setup steps."""
+
+    # Set default application.
+    module, method = import_str_and_item(
+        config_param(
+            app,
+            "html_method",
+            "runtimepy.net.server.app.env.channel_environments",
+        )
+    )
+    RuntimepyServerConnection.default_app = create_app(
+        app, getattr(_import_module(module), method)
+    )
+
+    return 0
