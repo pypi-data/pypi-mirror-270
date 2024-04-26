7b722f0dfcf08f82bddcfa241dc9c6083d252f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:22 +0000

    Add package kitab/0.0.7/kitab-0.0.7.tar.gz

---
+++ packages/kitab/kitab-0.0.7.tar.gz/kitab-0.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Add install requirements
+setup(
+    author="Alexander Shahramanyan",
+    description="A package for book recommendation.",
+    name="kitab",
+    packages=find_packages(include=["kitab", "kitab.*"]),
+    version="0.0.7",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['pandas', 'sentence-transformers>=2.6', 'psycopg2-binary', 'pgvector', 'python-dotenv'],
+    python_requires=">=3.7",
+)
