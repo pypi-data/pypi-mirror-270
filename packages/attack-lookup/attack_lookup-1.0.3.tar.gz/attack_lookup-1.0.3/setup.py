5598fdd471e9a96635484849c3eb8e9725e6a0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 14:02:54 +0000

    Add package attack-lookup/1.0.3/attack_lookup-1.0.3.tar.gz

---
+++ packages/attack-lookup/attack_lookup-1.0.3.tar.gz/attack_lookup-1.0.3/setup.py
@@ -0,0 +1,39 @@
+# https://github.com/realpython/reader/blob/master/setup.py
+
+import pathlib
+
+from setuptools import setup
+
+base_dir = pathlib.Path(__file__).resolve().parent
+
+with open(base_dir / "requirements.txt", "r") as f:
+    deps = [x.strip() for x in f.readlines()]
+
+with open(base_dir / "README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="attack-lookup",
+    version="1.0.3",
+    description="MITRE ATT&CK Lookup Tool",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/curated-intel/attack-lookup",
+    author="Zander Work",
+    author_email="pypi@zanderwork.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8"
+    ],
+    packages=["attack_lookup"],
+    python_requires=">=3.6",
+    include_package_data=True,
+    install_requires=deps,
+    entry_points={
+        "console_scripts": [
+            "attack-lookup=attack_lookup.__main__:main"
+        ]
+    }
+)
