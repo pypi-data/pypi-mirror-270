db9f3233f35c4b548195cdeb16f7d465576251a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:11 +0000

    Add package draggable-charts/1.1.0/draggable-charts-1.1.0.tar.gz

---
+++ packages/draggable-charts/draggable-charts-1.1.0.tar.gz/draggable-charts-1.1.0/setup.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="draggable-charts",
+    version="1.1.0",
+    author="Brayan Munoz",
+    author_email="balexander.munoz@udea.edu.co",
+    description="A Streamlit component that displays a line chart with draggable points. Users can click and drag points on the chart to adjust their values. The updated data of the chart is returned.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/balexandermunoz/draggable-charts",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.39.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    }
+)
