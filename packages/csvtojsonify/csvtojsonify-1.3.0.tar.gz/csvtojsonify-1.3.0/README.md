0b46d653638edd148496282b6e008965602ea4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:04:11 +0000

    Add package csvtojsonify/1.3.0/csvtojsonify-1.3.0.tar.gz

---
+++ packages/csvtojsonify/csvtojsonify-1.3.0.tar.gz/csvtojsonify-1.3.0/README.md
@@ -0,0 +1,14 @@
+# csvtojsonify
+
+A simple tool to convert CSV files to JSON format.
+
+## Usage
+
+ Simply run `csvtojsonify` it will prompt you to enter input CSV file with .csv file extension and output JSON file with .json file extension.
+
+##Sample Run
+(venv-3.12) % python3 csvtojsonify.py 
+Enter the input CSV file name: somefile.csv
+Enter the output JSON file name: results.json
+Conversion successful! The new JSON file is results.json
+(venv-3.12) % 
---
+++ packages/csvtojsonify/csvtojsonify-1.3.0.tar.gz/csvtojsonify-1.3.0/csvtojsonify.py
@@ -0,0 +1,35 @@
+# csvtojsonify.py
+
+import csv
+import json
+
+def convert_csv_to_json(csv_file, json_file):
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        data = list(reader)
+
+    with open(json_file, 'w') as jsonfile:
+        json.dump(data, jsonfile)
+
+def main():
+    # Get the input and output file names from the user
+    csv_file = input("Enter the input CSV file name: ")
+    json_file = input("Enter the output JSON file name: ")
+
+    # Check if input file name ends with ".csv"
+    if not csv_file.endswith(".csv"):
+        print("Error: Input file name must end with '.csv'")
+        return
+
+    # Check if output file name ends with ".json"
+    if not json_file.endswith(".json"):
+        print("Error: Output file name must end with '.json'")
+        return
+
+    # Call the conversion function
+    convert_csv_to_json(csv_file, json_file)
+
+    print("Conversion successful! The new JSON file is", json_file)
+
+if __name__ == "__main__":
+    main()
