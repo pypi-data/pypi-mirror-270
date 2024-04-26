b332f61d87d738508b80c7282cc7346ae64947d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:59 +0000

    Add package pymoises/1.0.15/pymoises-1.0.15.tar.gz

---
+++ packages/pymoises/pymoises-1.0.15.tar.gz/pymoises-1.0.15/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="pymoises",
+    version="1.0.15",
+    description="Callings from moises endpoints...",
+    author="Platimex",
+    packages=[
+        "pymoises", 
+        "pymoises.dto", 
+        "pymoises.dto.input", 
+        "pymoises.dto.input.customer",
+        "pymoises.dto.input.transaction",
+        "pymoises.dto.output",
+        "pymoises.dto.output.customer",
+        "pymoises.dto.output.transaction",
+        "pymoises.dto.output.psps"
+        ],
+    install_requires=['requests', 'marshmallow'],
+)
