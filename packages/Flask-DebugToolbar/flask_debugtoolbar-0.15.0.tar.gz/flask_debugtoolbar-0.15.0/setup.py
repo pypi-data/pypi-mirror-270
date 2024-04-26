659bac6b0c6a095399ef5d2e095a2796b855d52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:09 +0000

    Add package Flask-DebugToolbar/0.15.0/flask_debugtoolbar-0.15.0.tar.gz

---
+++ packages/Flask-DebugToolbar/flask_debugtoolbar-0.15.0.tar.gz/flask_debugtoolbar-0.15.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="Flask-DebugToolbar",
+    install_requires=[
+        'Flask>=2.2.0',
+        'Blinker',
+        'itsdangerous',
+        'werkzeug',
+        'MarkupSafe',
+        'packaging',
+    ],
+     extras_require={
+        "docs": [
+            'Sphinx>=1.2.2',
+            'Pallets-Sphinx-Themes',
+        ],
+     }
+)
