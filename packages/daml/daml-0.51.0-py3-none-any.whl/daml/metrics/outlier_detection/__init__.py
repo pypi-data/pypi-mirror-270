4906418995ec8ff2e32fecea6a61886aba176dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:37 +0000

    Add package daml/0.51.0/daml-0.51.0-py3-none-any.whl

---
+++ packages/daml/daml-0.51.0-py3-none-any.whl/daml/metrics/outlier_detection/__init__.py
@@ -0,0 +1,21 @@
+from daml._internal.metrics.od import (
+    OD_AE,
+    OD_AEGMM,
+    OD_LLR,
+    OD_VAE,
+    OD_VAEGMM,
+    OutlierType,
+    Threshold,
+    ThresholdType,
+)
+
+__all__ = [
+    "OD_AE",
+    "OD_AEGMM",
+    "OD_LLR",
+    "OD_VAE",
+    "OD_VAEGMM",
+    "OutlierType",
+    "Threshold",
+    "ThresholdType",
+]
