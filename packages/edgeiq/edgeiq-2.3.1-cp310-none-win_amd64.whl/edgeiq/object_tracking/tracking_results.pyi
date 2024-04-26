f6a1e228965772cca82ee3f3842c85866ab46a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:19 +0000

    Add package edgeiq/2.3.1/edgeiq-2.3.1-cp310-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-2.3.1-cp310-none-win_amd64.whl/edgeiq/object_tracking/tracking_results.pyi
@@ -0,0 +1,13 @@
+from _typeshed import Incomplete
+from edgeiq import object_detection as object_detection
+from edgeiq.tools import to_json_serializable as to_json_serializable
+from typing import Dict, TypeVar
+
+T = TypeVar('T')
+
+class TrackingResults(Dict[int, T]):
+    def __init__(self, objects: Dict[int, T], tracking_algorithm: str) -> None: ...
+    @property
+    def tracking_algorithm(self): ...
+
+RESULT_TYPE: Incomplete
---
+++ packages/edgeiq/edgeiq-2.3.1-cp310-none-win_amd64.whl/edgeiq/pyarmor_runtime_006224/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.0 (group), 006224, 2024-04-24T22:34:18.671868
+from .pyarmor_runtime import __pyarmor__
