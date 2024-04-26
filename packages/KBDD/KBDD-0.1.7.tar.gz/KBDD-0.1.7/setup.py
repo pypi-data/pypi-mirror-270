25c6ea558dff04b7c30ed2dd13129ecb1a2993e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:08 +0000

    Add package KBDD/0.1.7/KBDD-0.1.7.tar.gz

---
+++ packages/KBDD/KBDD-0.1.7.tar.gz/KBDD-0.1.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='KBDD',
+    version='0.1.7',
+    packages=setuptools.find_packages(),
+    description='A package for KBDD: a knowledge-based and data-driven method for genetic network construction',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Chen-Po Liao',
+    author_email='liaochenpo@gmail.com',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>=3.6',
+)
