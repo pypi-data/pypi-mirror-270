02d782460cdb4bfc7bd952139ce724e611ed20fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 05:02:53 +0000

    Add package indexify-extractor-sdk/0.0.48/indexify_extractor_sdk-0.0.48.tar.gz

---
+++ packages/indexify-extractor-sdk/indexify_extractor_sdk-0.0.48.tar.gz/indexify_extractor_sdk-0.0.48/indexify_extractor_sdk/__init__.py
@@ -0,0 +1,19 @@
+from .base_extractor import Content, Extractor, Feature, EmbeddingSchema, load_extractor
+import os
+import sys
+
+sys.path.append(".")
+
+extractors_path = os.path.join(os.path.expanduser("~"), ".indexify-extractors")
+if os.path.exists(extractors_path):
+    sys.path.append(extractors_path)
+
+
+
+__all__ = [
+    "Content",
+    "EmbeddingSchema",
+    "Extractor",
+    "Feature",
+    "load_extractor",
+]
