5202f10ff47c067b7ec38fe6000f42824a9c71e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 05:02:53 +0000

    Add package micropython-stubber/1.20.0/micropython_stubber-1.20.0-py3-none-any.whl

---
+++ packages/micropython-stubber/micropython_stubber-1.20.0-py3-none-any.whl/mpflash/mpflash/cli_main.py
@@ -0,0 +1,26 @@
+"""mpflash is a CLI to download and flash MicroPython firmware to various boards."""
+
+# import rich_click as click
+
+from .cli_download import cli_download
+from .cli_flash import cli_flash_board
+from .cli_group import cli
+from .cli_list import cli_list_mcus
+
+# from loguru import logger as log
+
+
+def mpflash():
+    cli.add_command(cli_flash_board)
+    cli.add_command(cli_list_mcus)
+    cli.add_command(cli_download)
+    # cli(auto_envvar_prefix="MPFLASH")
+    try:
+        exit(cli())
+    except AttributeError as e:
+        print(f"Error: {e}")
+        exit(-1)
+
+
+if __name__ == "__main__":
+    mpflash()
