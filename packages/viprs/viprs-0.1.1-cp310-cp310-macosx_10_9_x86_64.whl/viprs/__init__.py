f1d3c3b2238de5217ae51367fd7c16505b39aed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:09 +0000

    Add package viprs/0.1.1/viprs-0.1.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/viprs/viprs-0.1.1-cp310-cp310-macosx_10_9_x86_64.whl/viprs/__init__.py
@@ -0,0 +1,9 @@
+
+from .model.VIPRS import VIPRS
+from .model.VIPRSMix import VIPRSMix
+from .model.gridsearch.VIPRSGridSearch import VIPRSGridSearch
+from .model.gridsearch.HyperparameterGrid import HyperparameterGrid
+from .utils.data_utils import *
+
+__version__ = '0.1.1'
+__release_date__ = 'April 2024'
