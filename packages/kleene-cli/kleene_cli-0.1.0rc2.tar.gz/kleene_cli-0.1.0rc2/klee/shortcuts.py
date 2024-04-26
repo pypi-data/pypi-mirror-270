ea451d2adf7ddf444eb7b09ad22d8c33cfe6efed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 16:04:10 +0000

    Add package kleene-cli/0.1.0rc2/kleene_cli-0.1.0rc2.tar.gz

---
+++ packages/kleene-cli/kleene_cli-0.1.0rc2.tar.gz/kleene_cli-0.1.0rc2/klee/shortcuts.py
@@ -0,0 +1,16 @@
+SHORTCUTS = {
+    # Format: (<shortcut name>, <source command>)
+    "build": "image build",
+    "create": "container create",
+    "exec": "container exec",
+    "start": "container start",
+    "stop": "container stop",
+    "restart": "container restart",
+    "lsc": "container ls",
+    "lsi": "image ls",
+    "lsn": "network ls",
+    "lsv": "volume ls",
+    "rmc": "container rm",
+    "rmi": "image rm",
+    "run": "container run",
+}
