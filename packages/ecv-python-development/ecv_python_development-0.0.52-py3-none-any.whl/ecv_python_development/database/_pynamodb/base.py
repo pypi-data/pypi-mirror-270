ed6998e20d71723b63530a2930c07a3c81eb7167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:04:37 +0000

    Add package ecv-python-development/0.0.52/ecv_python_development-0.0.52-py3-none-any.whl

---
+++ packages/ecv-python-development/ecv_python_development-0.0.52-py3-none-any.whl/ecv_python_development/database/_pynamodb/base.py
@@ -0,0 +1,23 @@
+import os
+
+from pynamodb.attributes import (
+    BooleanAttribute,
+    MapAttribute,
+    NumberAttribute,
+    UnicodeAttribute,
+)
+from pynamodb.models import Model as PynamoDBModel
+
+PYDANTIC_PYNAMO_MAPPING = {
+    str: UnicodeAttribute,
+    int: NumberAttribute,
+    float: NumberAttribute,
+    bool: BooleanAttribute,
+    dict: MapAttribute,
+}
+
+
+class PynamoDBBaseModel(PynamoDBModel):
+    class Meta:
+        table_name = ""
+        region = os.environ.get("AWS_REGION", "us-east-1")
