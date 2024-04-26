3a84eaff8d90cd17385aeeaccc6648cf93bbc89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:39 +0000

    Add package pure-cv/0.1.9/pure_cv-0.1.9.tar.gz

---
+++ packages/pure-cv/pure_cv-0.1.9.tar.gz/pure_cv-0.1.9/src/pure_cv/__init__.pyi
@@ -0,0 +1,23 @@
+from .types import Rotation
+from .opencv import imread, grayscale, threshold, dilate, erode, cvtColor, imdecode
+try:
+  from .plot import show
+except ImportError:
+  ...
+from .encoding import b64encode, b64decode, encode, decode
+from .colors import mod_color
+from .misc import black
+from .rescaling import descale_h, rescale_h
+from .rotation import rotate
+from . import draw
+
+__all__ = [
+  'imread', 'grayscale', 'threshold', 'dilate', 'erode', 'cvtColor', 'imdecode', 
+  'show',
+  'b64encode', 'b64decode', 'encode', 'decode',
+  'mod_color',
+  'black',
+  'descale_h', 'rescale_h',
+  'rotate', 'Rotation',
+  'draw'
+]
