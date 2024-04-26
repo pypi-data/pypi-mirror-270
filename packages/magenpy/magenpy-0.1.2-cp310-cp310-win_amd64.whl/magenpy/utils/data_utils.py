f8d6ed7934914236b465d28090cb32f2ce2ca97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:03:54 +0000

    Add package magenpy/0.1.2/magenpy-0.1.2-cp310-cp310-win_amd64.whl

---
+++ packages/magenpy/magenpy-0.1.2-cp310-cp310-win_amd64.whl/magenpy/utils/data_utils.py
@@ -0,0 +1,20 @@
+import os.path as osp
+
+
+def tgp_eur_data_path():
+    """
+    :return: The path of the attached 1000G genotype data for
+    European samples (N=378) and a subset of chromosome 22 (p=15938)
+    """
+    return osp.join(osp.dirname(osp.dirname(__file__)), 'data/1000G_eur_chr22')
+
+
+def ukb_height_sumstats_path():
+    """
+    :return: The path of the attached GWAS summary statistics file
+    for standing height. The file contains summary statistics for
+    HapMap3 variants on CHR22 and is a snapshot of the summary statistics
+    published on the fastGWA database:
+    https://yanglab.westlake.edu.cn/data/fastgwa_data/UKB/50.v1.1.fastGWA.gz
+    """
+    return osp.join(osp.dirname(osp.dirname(__file__)), 'data/ukb_height_chr22.fastGWA.gz')
