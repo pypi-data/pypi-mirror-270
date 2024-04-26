36993c29a7cb5e849a8143e03b4dd1f918b6104d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:07 +0000

    Add package stratis-cli/3.6.2/stratis_cli-3.6.2-py3-none-any.whl

---
+++ packages/stratis-cli/stratis_cli-3.6.2-py3-none-any.whl/stratis_cli/_actions/_utils.py
@@ -0,0 +1,226 @@
+# Copyright 2020 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Miscellaneous functions.
+"""
+
+# isort: STDLIB
+import json
+from uuid import UUID
+
+from .._constants import Clevis, PoolIdType
+from .._stratisd_constants import (
+    CLEVIS_KEY_TANG_TRUST_URL,
+    CLEVIS_KEY_THP,
+    CLEVIS_KEY_URL,
+    CLEVIS_PIN_TANG,
+    CLEVIS_PIN_TPM2,
+)
+
+
+class ClevisInfo:
+    """
+    Store a representation of Clevis encryption info
+    """
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self, pin, config):
+        """
+        Initialize clevis information.
+
+        :param str pin: the Clevis "pin"
+        :param dict config: the JSON config corresponding to the pin
+        """
+        self.pin = pin
+        self.config = config
+
+    @staticmethod
+    def get_info_from_namespace(namespace):
+        """
+        Get clevis info, if any, from the namespace.
+
+        :param namespace: namespace set up by the parser
+
+        :returns: clevis info or None
+        :rtype: ClevisInfo or NoneType
+        """
+        clevis_info = None
+        if namespace.clevis is not None:
+            if namespace.clevis in (Clevis.NBDE, Clevis.TANG):
+                assert namespace.tang_url is not None
+
+                assert namespace.trust_url or namespace.thumbprint is not None
+
+                clevis_config = {CLEVIS_KEY_URL: namespace.tang_url}
+                if namespace.trust_url:
+                    clevis_config[CLEVIS_KEY_TANG_TRUST_URL] = True
+                else:
+                    assert namespace.thumbprint is not None
+                    clevis_config[CLEVIS_KEY_THP] = namespace.thumbprint
+
+                clevis_info = ClevisInfo(CLEVIS_PIN_TANG, clevis_config)
+
+            elif namespace.clevis is Clevis.TPM2:
+                clevis_info = ClevisInfo(CLEVIS_PIN_TPM2, {})
+
+            else:
+                raise AssertionError(
+                    f"unexpected value {namespace.clevis} for clevis option"
+                )  # pragma: no cover
+
+        return clevis_info
+
+
+class EncryptionInfo:  # pylint: disable=too-few-public-methods
+    """
+    Generic information about a single encryption method.
+    """
+
+    def __init__(self, info):
+        """
+        Initializer.
+        :param info: info about an encryption method, as a dbus-python type
+        """
+        (consistent, info) = info
+        if consistent:
+            (encrypted, value) = info
+            self.value = value if encrypted else None
+        else:
+            # No tests that generate inconsistent encryption information
+            self.error = str(info)  # pragma: no cover
+
+    def consistent(self):
+        """
+        True if consistent, otherwise False.
+        """
+        return not hasattr(self, "error")
+
+
+class EncryptionInfoClevis(EncryptionInfo):  # pylint: disable=too-few-public-methods
+    """
+    Encryption info for Clevis
+    """
+
+    def __init__(self, info):
+        super().__init__(info)
+
+        # We don't test with Clevis for coverage
+        if hasattr(self, "value"):  # pragma: no cover
+            value = self.value
+            if value is not None:
+                (pin, config) = value  # pyright: ignore [ reportGeneralTypeIssues ]
+                self.value = ClevisInfo(str(pin), json.loads(str(config)))
+
+
+class EncryptionInfoKeyDescription(
+    EncryptionInfo
+):  # pylint: disable=too-few-public-methods
+    """
+    Encryption info for kernel keyring
+    """
+
+    def __init__(self, info):
+        super().__init__(info)
+
+        # Our listing code excludes creating an object of this class without
+        # it being consistent and set.
+        if hasattr(self, "value"):  # pragma: no cover
+            value = self.value
+            if value is not None:
+                self.value = str(value)
+
+
+class Device:  # pylint: disable=too-few-public-methods
+    """
+    A representation of a device in a stopped pool.
+    """
+
+    def __init__(self, mapping):
+        self.uuid = UUID(mapping["uuid"])
+        self.devnode = str(mapping["devnode"])
+
+
+class StoppedPool:  # pylint: disable=too-few-public-methods
+    """
+    A representation of a single stopped pool.
+    """
+
+    def __init__(self, pool_info):
+        """
+        Initializer.
+        :param pool_info: a D-Bus structure
+        """
+
+        self.devs = [Device(info) for info in pool_info["devs"]]
+
+        clevis_info = pool_info.get("clevis_info")
+        self.clevis_info = (
+            None if clevis_info is None else EncryptionInfoClevis(clevis_info)
+        )
+
+        key_description = pool_info.get("key_description")
+        self.key_description = (
+            None
+            if key_description is None
+            else EncryptionInfoKeyDescription(key_description)
+        )
+
+        name = pool_info.get("name")
+        self.name = None if name is None else str(name)
+
+
+class PoolSelector:
+    """
+    Methods to help locate a pool by one of its identifiers.
+    """
+
+    def __init__(self, pool_id_type, value):
+        """
+        Initializer.
+
+        :param PoolIdType pool_id_type: the id type
+        :param object value: the value, determined by the type
+        """
+        self.pool_id_type = pool_id_type
+        self.value = value
+
+    def managed_objects_key(self):
+        """
+        Get the key for searching GetManagedObjects result.
+        :rtype: dict of str * object
+        :returns: a dict containing a correct configuration for pools() method
+        """
+        return (
+            {"Uuid": self.value.hex}
+            if self.pool_id_type is PoolIdType.UUID
+            else {"Name": self.value}
+        )
+
+    def stopped_pools_func(self):
+        """
+        Get a function appropriate for searching StoppedPools D-Bus property.
+        :returns: a function for selecting from StoppedPools items
+        :rtype: (str * (dict of (str * object))) -> bool
+        """
+        return (
+            (lambda uuid, _info: uuid == self.value.hex)
+            if self.pool_id_type is PoolIdType.UUID
+            else (lambda _uuid, info: info.get("name") == self.value)
+        )
+
+    def __str__(self):
+        pool_id_type_str = "UUID" if self.pool_id_type is PoolIdType.UUID else "name"
+        return f"pool with {pool_id_type_str} {self.value}"
---
+++ packages/stratis-cli/stratis_cli-3.6.2-py3-none-any.whl/stratis_cli/_parser/_pool.py
@@ -0,0 +1,443 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Definition of pool actions to display in the CLI.
+"""
+
+# isort: STDLIB
+from argparse import ArgumentTypeError
+from uuid import UUID
+
+from .._actions import BindActions, PoolActions
+from .._constants import Clevis, EncryptionMethod, YesOrNo
+from .._error_codes import PoolErrorCode
+from ._bind import BIND_SUBCMDS, REBIND_SUBCMDS
+from ._debug import POOL_DEBUG_SUBCMDS
+
+
+def _ensure_nat(arg):
+    """
+    Raise error if argument is not an natural number.
+    """
+    try:
+        result = int(arg)
+    except Exception as err:
+        raise ArgumentTypeError(f"Argument {arg} is not a natural number.") from err
+
+    if result < 0:
+        raise ArgumentTypeError(f"Argument {arg} is not a natural number.")
+    return result
+
+
+POOL_SUBCMDS = [
+    (
+        "create",
+        {
+            "help": "Create a pool",
+            "groups": [
+                (
+                    "clevis",
+                    {
+                        "description": "Arguments controlling creation with Clevis encryption",
+                        "args": [
+                            (
+                                "--clevis",
+                                {
+                                    "type": Clevis,
+                                    "help": ("Specification for binding with Clevis."),
+                                    "dest": "clevis",
+                                    "choices": list(Clevis),
+                                },
+                            ),
+                            (
+                                "--tang-url",
+                                {
+                                    "type": str,
+                                    "help": (
+                                        "URL of Clevis tang server "
+                                        "(--clevis=[tang|nbde] must be set)"
+                                    ),
+                                    "dest": "tang_url",
+                                },
+                            ),
+                        ],
+                        "mut_ex_args": [
+                            (
+                                False,
+                                [
+                                    (
+                                        "--trust-url",
+                                        {
+                                            "action": "store_true",
+                                            "help": (
+                                                "Omit verification of tang "
+                                                "server credentials "
+                                                "(--tang-url option must be "
+                                                "set)"
+                                            ),
+                                            "dest": "trust_url",
+                                        },
+                                    ),
+                                    (
+                                        "--thumbprint",
+                                        {
+                                            "help": (
+                                                "Thumbprint of tang server "
+                                                "(--tang-url option must be "
+                                                "set)"
+                                            ),
+                                            "dest": "thumbprint",
+                                        },
+                                    ),
+                                ],
+                            )
+                        ],
+                    },
+                )
+            ],
+            "args": [
+                ("pool_name", {"help": "Name of new pool"}),
+                (
+                    "blockdevs",
+                    {"help": "Create the pool using these block devs", "nargs": "+"},
+                ),
+                (
+                    "--key-desc",
+                    {
+                        "type": str,
+                        "help": (
+                            "Key description of key in kernel keyring to use "
+                            "for encryption"
+                        ),
+                        "dest": "key_desc",
+                    },
+                ),
+                (
+                    "--no-overprovision",
+                    {
+                        "action": "store_true",
+                        "help": (
+                            "Do not allow the sum of the logical size of the "
+                            "pool's filesystems to exceed the size of the "
+                            "pool's data area."
+                        ),
+                        "dest": "no_overprovision",
+                    },
+                ),
+            ],
+            "func": PoolActions.create_pool,
+        },
+    ),
+    (
+        "stop",
+        {
+            "help": (
+                "Stop a pool. Tear down the pool's storage stack "
+                "but do not erase any metadata."
+            ),
+            "mut_ex_args": [
+                (
+                    True,
+                    [
+                        (
+                            "--uuid",
+                            {
+                                "type": UUID,
+                                "help": "UUID of the pool to stop",
+                            },
+                        ),
+                        (
+                            "--name",
+                            {"help": "name of the pool to stop"},
+                        ),
+                    ],
+                )
+            ],
+            "func": PoolActions.stop_pool,
+        },
+    ),
+    (
+        "start",
+        {
+            "help": "Start a pool.",
+            "mut_ex_args": [
+                (
+                    True,
+                    [
+                        (
+                            "--uuid",
+                            {
+                                "type": UUID,
+                                "help": "UUID of the pool to start",
+                            },
+                        ),
+                        (
+                            "--name",
+                            {"help": "name of the pool to start"},
+                        ),
+                    ],
+                )
+            ],
+            "args": [
+                (
+                    "--unlock-method",
+                    {
+                        "dest": "unlock_method",
+                        "choices": list(EncryptionMethod),
+                        "help": "Method to use to unlock the pool if encrypted.",
+                        "type": EncryptionMethod,
+                    },
+                ),
+            ],
+            "func": PoolActions.start_pool,
+        },
+    ),
+    (
+        "init-cache",
+        {
+            "help": "Initialize the cache with block devices",
+            "args": [
+                (
+                    "pool_name",
+                    {
+                        "help": ("Name of the pool for which to initialize the cache"),
+                    },
+                ),
+                (
+                    "blockdevs",
+                    {
+                        "help": "Initialize the pool cache using these block devs",
+                        "nargs": "+",
+                    },
+                ),
+            ],
+            "func": PoolActions.init_cache,
+        },
+    ),
+    (
+        "list",
+        {
+            "help": "List pools",
+            "description": "List Stratis pools",
+            "mut_ex_args": [
+                (
+                    False,
+                    [
+                        (
+                            "--uuid",
+                            {
+                                "type": UUID,
+                                "help": "UUID of pool to list",
+                            },
+                        ),
+                        (
+                            "--name",
+                            {
+                                "help": "name of pool to list",
+                            },
+                        ),
+                    ],
+                ),
+            ],
+            "args": [
+                (
+                    "--stopped",
+                    {
+                        "action": "store_true",
+                        "help": "Display information about stopped pools only.",
+                    },
+                ),
+            ],
+            "func": PoolActions.list_pools,
+        },
+    ),
+    (
+        "destroy",
+        {
+            "help": "Destroy a pool",
+            "args": [("pool_name", {"help": "pool name"})],
+            "func": PoolActions.destroy_pool,
+        },
+    ),
+    (
+        "rename",
+        {
+            "help": "Rename a pool",
+            "args": [
+                ("current", {"help": "Current pool name"}),
+                ("new", {"help": "New pool name"}),
+            ],
+            "func": PoolActions.rename_pool,
+        },
+    ),
+    (
+        "add-data",
+        {
+            "help": "Add one or more blockdevs to an existing pool for use as data storage",
+            "args": [
+                ("pool_name", {"help": "Pool name"}),
+                (
+                    "blockdevs",
+                    {
+                        "help": "Block devices to add to the pool",
+                        "metavar": "blockdev",
+                        "nargs": "+",
+                    },
+                ),
+            ],
+            "func": PoolActions.add_data_devices,
+        },
+    ),
+    (
+        "add-cache",
+        {
+            "help": "Add one or more blockdevs to an existing pool for use as cache",
+            "args": [
+                ("pool_name", {"help": "Pool name"}),
+                (
+                    "blockdevs",
+                    {
+                        "help": "Block devices to add to the pool as cache",
+                        "metavar": "blockdev",
+                        "nargs": "+",
+                    },
+                ),
+            ],
+            "func": PoolActions.add_cache_devices,
+        },
+    ),
+    (
+        "extend-data",
+        {
+            "help": (
+                "Extend the pool's data capacity with additional storage "
+                "space offered by its component data devices through, e.g., "
+                "expansion of a component RAID device."
+            ),
+            "args": [
+                ("pool_name", {"help": "Pool name"}),
+                (
+                    "--device-uuid",
+                    {
+                        "action": "extend",
+                        "dest": "device_uuid",
+                        "nargs": "*",
+                        "type": UUID,
+                        "default": [],
+                        "help": (
+                            "UUID of device to use; may be specified multiple "
+                            "times. If no devices are specified then all "
+                            "devices belonging to the pool that appear to have "
+                            "increased in size will be used."
+                        ),
+                    },
+                ),
+            ],
+            "func": PoolActions.extend_data,
+        },
+    ),
+    (
+        "bind",
+        {
+            "help": "Bind the given pool with an additional encryption facility",
+            "subcmds": BIND_SUBCMDS,
+        },
+    ),
+    (
+        "rebind",
+        {
+            "help": (
+                "Rebind the given pool with a currently in use encryption "
+                "facility but new credentials"
+            ),
+            "subcmds": REBIND_SUBCMDS,
+        },
+    ),
+    (
+        "unbind",
+        {
+            "help": "Unbind the given pool, removing support for encryption with Clevis",
+            "args": [
+                (
+                    "method",
+                    {
+                        "choices": list(EncryptionMethod),
+                        "help": "Encryption method to unbind",
+                        "type": EncryptionMethod,
+                    },
+                ),
+                ("pool_name", {"help": "Pool name"}),
+            ],
+            "func": BindActions.unbind,
+        },
+    ),
+    (
+        "set-fs-limit",
+        {
+            "help": "Set the maximum number of filesystems the pool can support.",
+            "args": [
+                ("pool_name", {"help": "Pool name"}),
+                (
+                    "amount",
+                    {
+                        "type": _ensure_nat,
+                        "help": "Number of filesystems.",
+                    },
+                ),
+            ],
+            "func": PoolActions.set_fs_limit,
+        },
+    ),
+    (
+        "overprovision",
+        {
+            "help": "Specify whether or not to allow overprovisioning for the pool.",
+            "args": [
+                ("pool_name", {"help": "Pool name"}),
+                (
+                    "decision",
+                    {
+                        "help": "yes to allow overprovisioning, otherwise no",
+                        "choices": list(YesOrNo),
+                        "type": YesOrNo,
+                    },
+                ),
+            ],
+            "func": PoolActions.set_overprovisioning_mode,
+        },
+    ),
+    (
+        "explain",
+        {
+            "help": "Explain pool alert codes",
+            "args": [
+                (
+                    "code",
+                    {
+                        "choices": [str(x) for x in PoolErrorCode.codes()],
+                        "help": "Error code to explain",
+                    },
+                )
+            ],
+            "func": PoolActions.explain_code,
+        },
+    ),
+    (
+        "debug",
+        {
+            "help": ("Miscellaneous pool-level debug commands"),
+            "subcmds": POOL_DEBUG_SUBCMDS,
+        },
+    ),
+]
