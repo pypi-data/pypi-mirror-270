09c02c71e1ea52f05ea4a400136f6b3a7c95fc39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 16:04:14 +0000

    Add package django-prose-editor/0.3.4/django_prose_editor-0.3.4.tar.gz

---
+++ packages/django-prose-editor/django_prose_editor-0.3.4.tar.gz/django_prose_editor-0.3.4/django_prose_editor/static/django_prose_editor/init.js
@@ -0,0 +1,26 @@
+/* global DjangoProseEditor */
+
+const marker = "data-django-prose-editor"
+
+function initializeDjangoProseEditor(container) {
+  for (let el of container.querySelectorAll(`[${marker}]`)) {
+    if (!el.id.includes("__prefix__")) {
+      DjangoProseEditor.createEditor(el, JSON.parse(el.getAttribute(marker)))
+      el.removeAttribute(marker)
+    }
+  }
+}
+
+function initializeDjangoInlines() {
+  let o
+  if ((o = window.django) && (o = o.jQuery)) {
+    o(document).on("formset:added", (e) => {
+      initializeDjangoProseEditor(e.target)
+    })
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeDjangoProseEditor(document)
+  initializeDjangoInlines()
+})
