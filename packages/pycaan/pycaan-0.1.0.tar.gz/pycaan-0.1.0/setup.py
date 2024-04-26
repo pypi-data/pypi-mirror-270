3244812c565edcc04275cd33f66cf57fbaa8f44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:07 +0000

    Add package pycaan/0.1.0/pycaan-0.1.0.tar.gz

---
+++ packages/pycaan/pycaan-0.1.0.tar.gz/pycaan-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="pycaan",
+    version="0.1.0",
+    description="Python calcium imaging analysis (PyCaAn)",
+    author="Guillaume Etter",
+    author_email="etterguillaume@gmail.com",
+    # url='',
+    # packages=['numpy'],
+    py_modules=["functions", "analysis"],
+)
