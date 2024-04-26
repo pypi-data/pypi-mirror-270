10b9837c158e3443fd00bd5954b7e1d11ace7196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:03:38 +0000

    Add package pytgpt-bot/0.0.9/pytgpt_bot-0.0.9.tar.gz

---
+++ packages/pytgpt-bot/pytgpt_bot-0.0.9.tar.gz/pytgpt_bot-0.0.9/pytgpt_bot/utils.py
@@ -0,0 +1,29 @@
+from os import makedirs
+from pytgpt.utils import api_static_dir
+
+from pytgpt.opengpt import OPENGPT
+from pytgpt.koboldai import KOBOLDAI
+from pytgpt.phind import PHIND
+from pytgpt.llama2 import LLAMA2
+from pytgpt.blackboxai import BLACKBOXAI
+from pytgpt.perplexity import PERPLEXITY
+from pytgpt.yepchat import YEPCHAT
+from pytgpt.auto import AUTO
+
+provider_map: dict[str, object] = {
+    "opengpt": OPENGPT,
+    "koboldai": KOBOLDAI,
+    "phind": PHIND,
+    "llama2": LLAMA2,
+    "blackboxai": BLACKBOXAI,
+    "perplexity": PERPLEXITY,
+    "yepchat": YEPCHAT,
+    "auto": AUTO,
+}
+
+provider_keys: list = list(provider_map.keys())
+
+bot_dir = api_static_dir / "bots"
+path_to_db = bot_dir / "telegram.db"
+
+makedirs(bot_dir, exist_ok=True)
