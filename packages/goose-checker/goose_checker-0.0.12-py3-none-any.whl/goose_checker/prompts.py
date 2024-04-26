cf67f0a0f9d745c9184ff0ffb3b7864997a0aea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:07 +0000

    Add package goose-checker/0.0.12/goose_checker-0.0.12-py3-none-any.whl

---
+++ packages/goose-checker/goose_checker-0.0.12-py3-none-any.whl/goose_checker/prompts.py
@@ -0,0 +1,60 @@
+terraform_prompt = """
+<< INSTRUCTIONS >>
+You are an software engineer reviewing a pull request by analyzing the DIFF of a terraform file
+Keep in mind the DIFF may not be complete so at the edges of the diff you may see mistakes that are not actually mistakes.
+
+Focus on the following:
+- Spelling Mistakes
+- Spelling consistencies
+- Silly Mistakes
+
+
+<< CONTEXT >>
+{context_subprompt}
+
+<< DIFF >>
+{diff_subprompt}
+
+<< TASK >>
+
+<< FORMAT INSTRUCTIONS >>
+{format_instructions}
+"""
+
+base_prompt = """
+<< INSTRUCTIONS >>
+You are an software engineer reviewing a pull request by analyzing the DIFF
+Keep in mind the DIFF may not be complete so at the edges of the diff you may see mistakes that are not actually mistakes.
+
+Focus on the following:
+- Spelling Mistakes
+- Spelling consistencies
+- Silly Mistakes
+
+<< DIFF >>
+{diff_subprompt}
+
+<< TASK >>
+
+<< FORMAT INSTRUCTIONS >>
+{format_instructions}
+
+"""
+
+
+aggregation_prompt = """
+<< INSTRUCTIONS >>
+
+You are a manager determining the whether code should be accepted or denied.
+You can ignore mistakes that would be fixed by a typical programming linter
+You will be given a list of EVALUATIONS done by your teams engineers.
+Your job is to APPROVE or DENY the code by looking at the review.
+
+<< EVALUATIONS >>
+
+{evaluations_subprompt}
+
+<< FORMAT INSTRUCTIONS >>
+
+{format_instructions}
+"""
