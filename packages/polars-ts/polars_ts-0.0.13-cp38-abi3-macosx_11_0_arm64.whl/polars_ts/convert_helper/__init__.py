dc5495e2f5d8b86ec1214a2dbb600656e1f21b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 14:02:54 +0000

    Add package polars-ts/0.0.13/polars_ts-0.0.13-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/polars-ts/polars_ts-0.0.13-cp38-abi3-macosx_11_0_arm64.whl/polars_ts/convert_helper/__init__.py
@@ -0,0 +1,192 @@
+import polars as pl
+
+from ..types import FrameType
+from ..sf_helper import column_name_unique_over
+
+
+def impl_construct_closure(
+    conv_df: FrameType,
+    max_iterations: int,
+) -> FrameType:
+    # close under identity, inverse and transitive
+    cf = pl.col("conversion_factor")
+
+    inverses_df = conv_df.with_columns(
+        base="target", target="base", conversion_factor=1 / cf
+    )
+
+    known_units = pl.concat(
+        [conv_df.select("base"), conv_df.select(base="target")]
+    ).unique()
+
+    # unusual construction to align schema of identities_df with conv_df
+    identities_df = (
+        conv_df.clear()
+        .join(known_units, on="base", how="outer_coalesce")
+        .with_columns(
+            target="base",
+            conversion_factor=1.0,
+        )
+    )
+
+    partial = (
+        pl.concat([conv_df, inverses_df, identities_df])
+        .with_columns(
+            pl.col("base").cast(pl.String).str.to_lowercase(),
+            pl.col("target").cast(pl.String).str.to_lowercase(),
+            pl.exclude("base", "target", "conversion_factor").forward_fill(),
+        )
+        .lazy()
+        .collect()
+    )
+
+    added_rows = len(partial)
+    while (added_rows > 0) and (max_iterations != 0):
+        old_rows = len(partial)
+        max_iterations = max_iterations - 1
+
+        partial = (
+            partial.join(partial, left_on="target", right_on="base", how="inner")
+            .select(
+                "base",
+                target="target_right",
+                conversion_factor=cf * pl.col("conversion_factor_right"),
+            )
+            .unique(subset=["base", "target"], keep="first")
+            .sort("base", "target")
+        )
+
+        added_rows = len(partial) - old_rows
+
+    partial = partial.with_columns(pl.col(pl.String).cast(pl.Categorical))
+
+    result: FrameType = (
+        partial.lazy()
+        if isinstance(conv_df, pl.LazyFrame)
+        else partial.lazy().collect()
+    )
+
+    return result
+
+
+def _split_multi_unit(u: pl.Expr, field_name: str) -> pl.Expr:
+    return (
+        u.cast(pl.String)
+        .str.split_exact("/", 1)
+        .struct.rename_fields([f"{field_name}_numerator", f"{field_name}_denominator"])
+        .alias(field_name)
+    )
+
+
+def _impl_convert_multi_dim(
+    df: FrameType,
+    target_unit: pl.Expr,
+    conversion_matrix: FrameType,
+    value: pl.Expr,
+    value_unit: pl.Expr,
+) -> FrameType:
+    value_unit_name = value_unit.meta.output_name()
+    target_unit_name = target_unit.meta.output_name()
+
+    value_unit_prefix = column_name_unique_over(value_unit_name, df)
+    target_unit_prefix = column_name_unique_over(target_unit_name, df)
+
+    value_unit_list = _split_multi_unit(value_unit, value_unit_prefix)
+    target_unit_list = _split_multi_unit(target_unit, target_unit_prefix)
+
+    result = (
+        df.with_columns(
+            target_unit_list,
+            value_unit_list,
+        )
+        .unnest(target_unit_prefix, value_unit_prefix)
+        .with_columns(
+            pl.col(f"^{target_unit_prefix}.*$", f"^{value_unit_prefix}.*$").cast(
+                pl.Categorical
+            )
+        )
+        .pipe(
+            _impl_convert_single_dim,
+            pl.col(f"{target_unit_prefix}_numerator"),
+            conversion_matrix,
+            value,
+            value_unit=pl.col(f"{value_unit_prefix}_numerator"),
+            invert=False,
+        )
+        .pipe(
+            _impl_convert_single_dim,
+            pl.col(f"{target_unit_prefix}_denominator"),
+            conversion_matrix,
+            value,
+            value_unit=pl.col(f"{value_unit_prefix}_denominator"),
+            invert=True,
+        )
+        .with_columns(
+            pl.concat_str(
+                [f"{value_unit_prefix}_numerator", f"{value_unit_prefix}_denominator"],
+                separator="/",
+            ).alias(value_unit_name)
+        )
+        .select(pl.exclude(f"^{target_unit_prefix}.*$", f"^{value_unit_prefix}.*$"))
+    )
+
+    return result
+
+
+def _impl_convert_single_dim(
+    df: FrameType,
+    target_unit: pl.Expr,
+    conversion_matrix: FrameType,
+    value: pl.Expr,
+    value_unit: pl.Expr,
+    invert: bool,
+) -> FrameType:
+    value_unit_name = value_unit.meta.output_name()
+    target_unit_name = target_unit.meta.output_name()
+
+    conversion_join_cols = ["target", "base"] if invert else ["base", "target"]
+    working_columns = []
+
+    if target_unit_name not in df.columns:
+        df = df.with_columns(target_unit.alias(target_unit_name))
+        working_columns.append(target_unit_name)
+
+    result = (
+        df.join(
+            conversion_matrix,
+            left_on=[value_unit_name, target_unit_name],
+            right_on=conversion_join_cols,
+            how="left",
+        )
+        .with_columns(
+            (value * pl.coalesce("conversion_factor", 1.0)).alias(
+                value.meta.output_name()
+            ),
+            pl.when(pl.col("conversion_factor").is_null())
+            .then(value_unit_name)
+            .otherwise(target_unit_name),
+        )
+        .select(pl.exclude("conversion_factor", *working_columns))
+    )
+
+    return result
+
+
+def impl_convert(
+    df: FrameType,
+    target_unit: pl.Expr,
+    conversion_matrix: FrameType,
+    value: pl.Expr,
+    value_unit: pl.Expr,
+    is_multi_dim: bool,
+) -> FrameType:
+    if is_multi_dim:
+        result = _impl_convert_multi_dim(
+            df, target_unit, conversion_matrix, value, value_unit
+        )
+    else:
+        result = _impl_convert_single_dim(
+            df, target_unit, conversion_matrix, value, value_unit, invert=False
+        )
+
+    return result
