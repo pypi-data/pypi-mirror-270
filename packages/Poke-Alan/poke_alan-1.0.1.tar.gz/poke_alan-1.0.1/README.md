777a2f1e8a37bc9209dde3a3863cff783139b4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:03 +0000

    Add package Poke-Alan/1.0.1/poke_alan-1.0.1.tar.gz

---
+++ packages/Poke-Alan/poke_alan-1.0.1.tar.gz/poke_alan-1.0.1/README.md
@@ -0,0 +1,5 @@
+# Pokeapi
+Es una libreria para generar un pokemon aleatorio.
+
+## Instalación
+Para instalar la libreria tienes que usar `pip install pokapi`
---
+++ packages/Poke-Alan/poke_alan-1.0.1.tar.gz/poke_alan-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name='Poke-Alan',
+    version='1.0.1',
+    author="Alan Suarez",
+    author_email="202224129_bautista@tesch.edu.mx",
+    description="Libreria para generar un pokemon aleatorio",
+    packages=['poke-base'],
+    package_data={"poke-base": ["pokemon.csv"]},
+    install_requires=[
+        "pandas"
+    ]
+)
