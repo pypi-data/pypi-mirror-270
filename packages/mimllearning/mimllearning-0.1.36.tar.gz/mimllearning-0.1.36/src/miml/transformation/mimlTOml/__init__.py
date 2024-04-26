9027e848fec10a237611dddf3544148978632bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:41 +0000

    Add package mimllearning/0.1.36/mimllearning-0.1.36.tar.gz

---
+++ packages/mimllearning/mimllearning-0.1.36.tar.gz/mimllearning-0.1.36/src/miml/transformation/mimlTOml/__init__.py
@@ -0,0 +1,5 @@
+
+from .miml_to_ml_transformation import MIMLtoMLTransformation
+from .arithmetic import ArithmeticTransformation
+from .geometric import GeometricTransformation
+from .minmax import MinMaxTransformation
