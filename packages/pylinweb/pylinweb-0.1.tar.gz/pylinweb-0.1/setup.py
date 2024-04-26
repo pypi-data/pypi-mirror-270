5e16fa0f43e8328ca46bc16e921f39bf12face5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 04:04:09 +0000

    Add package pylinweb/0.1/pylinweb-0.1.tar.gz

---
+++ packages/pylinweb/pylinweb-0.1.tar.gz/pylinweb-0.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+setup(
+    name='pylinweb',
+    version='0.1',
+    description='A simple functional test library using Selenium and Chrome driver',
+    author='Linda Lopez',
+    packages=find_packages(),
+    include_package_data=True, #Esto hace que setuptools lea el archivo MANIFEST.in
+    package_data={
+        'pylinweb': ['app/*'],
+    },
+    install_requires=[
+        "allure-behave==2.13.4",
+        "allure-python-commons==2.13.4",
+        "appium-python-client==4.0.0",
+        "behave==1.2.6",
+        "behave-html-formatter==0.9.10",
+        "behave2cucumber==1.0.3",
+        "docxcompose==1.4.0",
+        "docxtpl==0.16.8",
+        "playwright==1.42.0",
+        "psutil==5.9.2",
+        "PyPDF2==3.0.1",
+        "python-docx==1.1.0",
+        "pycparser==2.21",
+        "screeninfo==0.8",
+        "selenium==4.1.3",
+        "webdriver-manager==4.0.1",
+        "webdrivermanager==0.10.0",
+        "keyboard==0.13.5"
+    ],
+    entry_points={
+        'console_scripts': [
+            'pylinweb=pylinweb.main:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'
+    ],
+)
