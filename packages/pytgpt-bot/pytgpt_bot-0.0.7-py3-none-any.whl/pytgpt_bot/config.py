64cae8571969893b1759a28c5ef7a318d2823fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 14:03:16 +0000

    Add package pytgpt-bot/0.0.7/pytgpt_bot-0.0.7-py3-none-any.whl

---
+++ packages/pytgpt-bot/pytgpt_bot-0.0.7-py3-none-any.whl/pytgpt_bot/config.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+from os import environ
+from os import makedirs
+from pytgpt.utils import api_static_dir
+
+load_dotenv()
+
+assert environ.get("token"), "Export bot's token to the environment 😠"
+
+bot_token: str = environ.get("token")
+provider: str = environ.get("provider", "auto")
+admin_id: int = int(environ.get("admin-id", 1234567))
+max_tokens: int = int(environ.get("max-tokens", 600))
+timeout: int = int(environ.get("timeout", 30))
+loglevel: int = int(environ.get("loglevel", 20))
+logfile = environ.get("logfile")
+
+bot_dir = api_static_dir / "bots"
+path_to_db = bot_dir / "telegram.db"
+
+makedirs(bot_dir, exist_ok=True)
