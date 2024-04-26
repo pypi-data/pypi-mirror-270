ffde01dcba538267704026c4473cb972a372411a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 16:03:28 +0000

    Add package gingerai/0.1.29/gingerai-0.1.29.tar.gz

---
+++ packages/gingerai/gingerai-0.1.29.tar.gz/gingerai-0.1.29/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gingerai",
+    version="0.1.29",
+    description="Ginger AI Python Library",
+    url="https://github.com/ginger-ai",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchsummary",
+        "numpy",
+        "requests",
+        "tqdm",
+        "grequests",
+        "joblib",
+        "io",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
