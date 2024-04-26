5aa44f27d81177e4b17bfd39b82de87362761ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 05:02:55 +0000

    Add package nba_live/0.3.0/nba_live-0.3.0.tar.gz

---
+++ packages/nba_live/nba_live-0.3.0.tar.gz/nba_live-0.3.0/nba_live/nbalive.py
@@ -0,0 +1,28 @@
+from nba_live.getgamedata import GetGameData
+from nba_live.getboxscore import GetBoxScore
+from nba_live.datastream import dataStream
+from multiprocessing import Process, Queue
+import pandas as pd
+import time
+
+class NBALive():
+    def __init__(self):
+        self.game_data = GetGameData()
+        self.last_message = pd.DataFrame()
+        self.queue = Queue()
+        self.get_stats_process = Process(target=dataStream, args=(self.queue, self.game_data))
+
+    def start(self):
+        self.get_stats_process.start()
+
+    def getStats(self):
+        if not self.queue.empty():
+            self.last_message = self.queue.get()
+        return self.last_message
+    
+    def getPlayerStats(self, player_id):
+        for index, row in self.last_message.iterrows():
+            for player in row.homeTeamPlayers:
+                if player.personId == player_id:
+                    return player
+        return pd.DataFrame()
