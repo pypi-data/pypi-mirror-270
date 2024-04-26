35ff454536dbb3268d2ecb0a0fb5409a39e451c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:27 +0000

    Add package tlcpack-nightly-cu102/0.13.dev0/tlcpack-nightly-cu102-0.13.dev0.tar.gz

---
+++ packages/tlcpack-nightly-cu102/tlcpack-nightly-cu102-0.13.dev0.tar.gz/tlcpack-nightly-cu102-0.13.dev0/README.md
@@ -0,0 +1,3 @@
+## Installation
+You can install the package using pip:
+pip3 install tlcpack-nightly-cu102
---
+++ packages/tlcpack-nightly-cu102/tlcpack-nightly-cu102-0.13.dev0.tar.gz/tlcpack-nightly-cu102-0.13.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tlcpack-nightly-cu102',
+    version='0.13.dev',
+    description='Testing',
+    author='Virendra Pawar',
+    author_email='vpawar1147@gmail.com',
+)
