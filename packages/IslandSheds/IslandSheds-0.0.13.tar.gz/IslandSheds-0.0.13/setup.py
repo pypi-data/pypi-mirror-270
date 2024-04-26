a3c13f5a178a30617b024495f8cb8a201dda413e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 15:04:10 +0000

    Add package IslandSheds/0.0.13/IslandSheds-0.0.13.tar.gz

---
+++ packages/IslandSheds/IslandSheds-0.0.13.tar.gz/IslandSheds-0.0.13/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.13'
+DESCRIPTION = 'A Caribbean watershed shapefile downloader package.'
+LONG_DESCRIPTION = 'Python package for downloading country-specific Caribbean watershed data.'
+
+setup(
+    name='IslandSheds',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        'geopandas',
+        'requests'
+    ],
+    author='Kent Thomas',  # Placeholder for author name
+    author_email='kthomas.wsrn@gmail.com',  # Placeholder for author email
+    description=DESCRIPTION,
+    long_description_content_type= "text/markdown",
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/kgdthomas/IslandSheds',  # Placeholder for GitHub repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
