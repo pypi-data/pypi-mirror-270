cbd3e1d62238888ed47b72cd53620b42b86912f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 05:02:58 +0000

    Add package supamodel/0.5.5/supamodel-0.5.5.tar.gz

---
+++ packages/supamodel/supamodel-0.5.5.tar.gz/supamodel-0.5.5/supamodel/__init__.py
@@ -0,0 +1,10 @@
+from supamodel.trading.tokens import BaseModel, OhlcvData, Overview, TokenData
+
+___all__ = [
+    [
+        "TokenData",
+        "BaseModel",
+        "Overview",
+        "OhlcvData",
+    ]
+]
