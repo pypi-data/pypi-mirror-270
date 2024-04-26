04333bacecb4654bc388a1ff4a177c6fcb99dc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-26 03:03:04 +0000

    Add package simple-token-bucket/0.2.0/simple_token_bucket-0.2.0-py3-none-any.whl

---
+++ packages/simple-token-bucket/simple_token_bucket-0.2.0-py3-none-any.whl/simple_token_bucket/backends/redis.py
@@ -0,0 +1,27 @@
+from typing import Tuple
+from . import Backend
+import redis
+
+
+class RedisBackend(Backend):
+    @classmethod
+    def from_url(cls, redis_url: str):
+        client = redis.StrictRedis.from_url(redis_url)
+        return RedisBackend(client)
+
+    def __init__(self, redis_client: redis.Redis):
+        self._client = redis_client
+
+    def get_token(self, *, bucket_name: str, bucket_size: int, refresh_interval: int) -> Tuple[int, int]:
+        pipeline = self._client.pipeline()
+        pipeline.set(bucket_name, bucket_size, nx=True, ex=refresh_interval)
+        pipeline.ttl(bucket_name)
+        pipeline.get(bucket_name)
+        pipeline.decr(bucket_name)
+        _, ttl, available_tokens, _ = pipeline.execute()
+
+        if ttl < 0:
+            # some keys can be created by the decr command, those keys do not have a TTL
+            self._client.expire(bucket_name, refresh_interval)
+            return int(bucket_size), refresh_interval
+        return int(available_tokens), ttl
