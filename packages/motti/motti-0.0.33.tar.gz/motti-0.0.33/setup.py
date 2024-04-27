d883636d7739e76f3368540fbbb11f36498eff66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:04:06 +0000

    Add package motti/0.0.33/motti-0.0.33.tar.gz

---
+++ packages/motti/motti-0.0.33.tar.gz/motti-0.0.33/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="motti",
+    version="0.0.33",
+    author="lup1n",
+    author_email="780966523@qq.com",
+    description="some util functions",
+    packages=find_packages(),
+)
