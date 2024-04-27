3b47a66d89b7f63b6468bcb85178501cd69d65b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 14:03:18 +0000

    Add package eznf/0.59.2/eznf-0.59.2.tar.gz

---
+++ packages/eznf/eznf-0.59.2.tar.gz/eznf-0.59.2/tests/test_modeler.py
@@ -0,0 +1,14 @@
+from eznf.modeler import Modeler
+
+
+def test_variable_addition():
+    Z = Modeler()
+    Z.add_var("x", "x")
+    assert Z.v("x") == 1
+
+
+def test_qbf_basic_encoding():
+    Z = Modeler()
+    Z.add_existential_var("x")
+    Z.add_universal_var("y")
+    assert Z.v("y") == 2
