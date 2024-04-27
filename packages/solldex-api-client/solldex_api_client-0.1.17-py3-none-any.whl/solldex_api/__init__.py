662cc0aeadecfdc6a619754fc3762955c6facc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:45 +0000

    Add package solldex-api-client/0.1.17/solldex_api_client-0.1.17-py3-none-any.whl

---
+++ packages/solldex-api-client/solldex_api_client-0.1.17-py3-none-any.whl/solldex_api/__init__.py
@@ -0,0 +1,13 @@
+from .solldex_api import SolldexAPI
+from .solldex_models import (
+    RecepcionarLoteParams,
+    ConsultaLoteParams,
+    ConsultaRpsParams,
+    ConsultaNfseParams,
+    CancelaNfseParams,
+    ConsultaUrlVisualizacaoNfseParams,
+    Tomador,
+    Prestador,
+    Servico,
+    Endereco
+)
