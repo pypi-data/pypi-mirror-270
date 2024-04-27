a8838c2c4e86a05619d1ec7af1b79f2bb479c797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:53 +0000

    Add package HeraData/0.1.3/HeraData-0.1.3.tar.gz

---
+++ packages/HeraData/HeraData-0.1.3.tar.gz/HeraData-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HeraData',
+    version='0.1.3',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author='369',
+    author_email='luck.yangbo@gmail.com',
+    description='A data storage library',
+    packages=find_packages(),
+    install_requires=[
+        'elasticsearch',
+    ],
+    python_requires='>=3.6',
+)
