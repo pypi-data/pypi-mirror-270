0424306e97166a26ccde368eda1f73b2f8a364cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:43 +0000

    Add package mlsauce/0.15.3/mlsauce-0.15.3.tar.gz

---
+++ packages/mlsauce/mlsauce-0.15.3.tar.gz/mlsauce-0.15.3/mlsauce/lasso/fallbacksetup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+setup(ext_modules=cythonize(os.path.join(dir_path, "_lassoc.pyx"), 
+                            compiler_directives={'language_level' : "3"}))
---
+++ packages/mlsauce/mlsauce-0.15.3.tar.gz/mlsauce-0.15.3/mlsauce/utils/misc/__init__.py
@@ -0,0 +1,4 @@
+from .misc import cluster, cythonize_file, merge_two_dicts, flatten, is_float, is_factor
+
+
+__all__ = ["cluster", "cythonize_file", "merge_two_dicts", "flatten", "is_float", "is_factor"]
