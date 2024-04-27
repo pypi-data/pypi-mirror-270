0e9e1586ccf3aa971079146698ff7535afede3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:38 +0000

    Add package sharded-google-photos/0.2.3/sharded_google_photos-0.2.3-py3-none-any.whl

---
+++ packages/sharded-google-photos/sharded_google_photos-0.2.3-py3-none-any.whl/sharded_google_photos/backup/gphotos_uploader.py
@@ -0,0 +1,17 @@
+from sharded_google_photos.shared.gphotos_client import GPhotosClient
+
+
+class GPhotosUploader:
+    def __init__(self, gphoto_client: GPhotosClient):
+        self.gphoto_client = gphoto_client
+
+    def upload_photos(self, file_paths: list[str], file_names: list[str]):
+        upload_tokens = []
+
+        for file_path, file_name in zip(file_paths, file_names):
+            upload_token = self.gphoto_client.media_items().upload_photo_in_chunks(
+                file_path, file_name
+            )
+            upload_tokens.append(upload_token)
+
+        return upload_tokens
---
+++ packages/sharded-google-photos/sharded_google_photos-0.2.3-py3-none-any.whl/sharded_google_photos/backup/group_diffs_with_metadata.py
@@ -0,0 +1,23 @@
+from .models import DiffWithMetadata, GroupedDiffs
+
+
+def group_diffs_with_metadata(diffs: list[DiffWithMetadata]) -> GroupedDiffs:
+    """Splits the diffs based on its album and the modifications to them.
+
+    Refer to its test cases for its sample usages.
+    """
+    result = {}
+
+    for diff in diffs:
+        modifier = diff["modifier"]
+        album_title = diff["album_title"]
+
+        if album_title not in result:
+            result[album_title] = {}
+
+        if modifier not in result[album_title]:
+            result[album_title][modifier] = []
+
+        result[album_title][modifier].append(diff)
+
+    return result
