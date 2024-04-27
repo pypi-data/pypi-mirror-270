d1c3f3f29797077a2866cb36c365099887611f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:47 +0000

    Add package pyslth/0.1.4/pyslth-0.1.4.tar.gz

---
+++ packages/pyslth/pyslth-0.1.4.tar.gz/pyslth-0.1.4/slth/utils.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+
+def build_url(request, path=None):
+    if path and path.startswith('http'):
+        return path
+    elif request:
+        return '{}{}'.format(settings.SITE_URL, path or request.path)
+    return ''
+
+def absolute_url(request, *querystrings):
+    url = build_url(request)
+    if url:
+        querstring = request.META.get('QUERY_STRING')
+        url = append_url(url, querstring)
+        for querystring in querystrings:
+            url = append_url(url, querystring)
+    return url
+
+def append_url(url, *querystrings):
+    params = {}
+    if '?' in url:
+        url, querystring = url.split('?')
+        if querystring:
+            for param in querystring.split('&'):
+                k, v = param.split('=')
+                params[k] = v
+    else:
+        url, querystring = url, ''
+    for querystring in querystrings:
+        if querystring:
+            for param in querystring.split('&'):
+                k, v = param.split('=')
+                params[k] = v
+    if params:
+        url = '{}?{}'.format(url, '&'.join([f'{k}={v}' for k, v in params.items()]))
+    return url
