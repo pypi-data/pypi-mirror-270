5a89fdbde5b703128a85776a3c2b1cb083df9b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:04:13 +0000

    Add package SigProfilerMatrixGenerator/1.2.26/SigProfilerMatrixGenerator-1.2.26-py3-none-any.whl

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.26-py3-none-any.whl/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.26'
+version = '1.2.26'
+Update = 'v1.2.26: Update Scipy dependency for python>3.9 and removed duplicate plotSV code.'
+
+	
