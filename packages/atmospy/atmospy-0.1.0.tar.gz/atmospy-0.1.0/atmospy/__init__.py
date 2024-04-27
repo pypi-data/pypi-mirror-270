aa590a8be3a3f9558f81d832da5b36ab49b9564a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:03:44 +0000

    Add package atmospy/0.1.0/atmospy-0.1.0.tar.gz

---
+++ packages/atmospy/atmospy-0.1.0.tar.gz/atmospy-0.1.0/atmospy/__init__.py
@@ -0,0 +1,20 @@
+from importlib.metadata import version
+
+# import warnings
+# import pandas as pd
+# import numpy as np
+# import math
+# import os
+
+from .utils import *
+from .rcmod import *
+from .relational import *
+from .trends import *
+from .rose import *
+
+# Capture the original matplotlib rcParams
+import matplotlib as mpl
+_orig_rc_params = mpl.rcParams.copy()
+
+# Determine the atmospy version
+__version__ = version('atmospy')
