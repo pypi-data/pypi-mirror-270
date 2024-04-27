97740055fdaa9dc7872e451969bfe109e5b920eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 14:03:35 +0000

    Add package mh-moe/0.0.1/mh_moe-0.0.1.tar.gz

---
+++ packages/mh-moe/mh_moe-0.0.1.tar.gz/mh_moe-0.0.1/README.md
@@ -0,0 +1,7 @@
+[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)
+
+# Multi-Head Mixture of Experts (MHMoE)
+
+MH-MoE to collectively attend to information from various representation
+spaces within different experts to deepen context understanding while significantly enhancing expert activation. 
+
---
+++ packages/mh-moe/mh_moe-0.0.1.tar.gz/mh_moe-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mh_moe']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['einops', 'swarms', 'torch', 'zetascale']
+
+setup_kwargs = {
+    'name': 'mh-moe',
+    'version': '0.0.1',
+    'description': 'Paper - Pytorch',
+    'long_description': '[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)\n\n# Multi-Head Mixture of Experts (MHMoE)\n\nMH-MoE to collectively attend to information from various representation\nspaces within different experts to deepen context understanding while significantly enhancing expert activation. \n\n',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/MHMoE',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
