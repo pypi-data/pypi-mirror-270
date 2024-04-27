174c5609ef376b7294f480aff0c66879fdd8934e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:26 +0000

    Add package runtimepy/4.3.5/runtimepy-4.3.5-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-4.3.5-py3-none-any.whl/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=c1c1f345366e6922123e7e5b5060de2b
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "4.3.5"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
