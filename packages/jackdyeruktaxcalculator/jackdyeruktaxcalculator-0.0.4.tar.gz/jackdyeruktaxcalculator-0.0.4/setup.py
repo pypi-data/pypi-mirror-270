c20eeac212fb4ccf2ea3618ff87814b0f2c1a38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:36 +0000

    Add package jackdyeruktaxcalculator/0.0.4/jackdyeruktaxcalculator-0.0.4.tar.gz

---
+++ packages/jackdyeruktaxcalculator/jackdyeruktaxcalculator-0.0.4.tar.gz/jackdyeruktaxcalculator-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jackdyeruktaxcalculator",
+    version="0.0.4",
+    author="Jack Dyer",
+    author_email="jack.dyer387@gmail.com",
+    description="UK Tax Calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
