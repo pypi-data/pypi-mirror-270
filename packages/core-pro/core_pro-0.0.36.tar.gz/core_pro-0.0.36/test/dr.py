dbae59ebaed7a41e99e0a209d97c49b9f66da2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:40 +0000

    Add package core-pro/0.0.36/core_pro-0.0.36.tar.gz

---
+++ packages/core-pro/core_pro-0.0.36.tar.gz/core_pro-0.0.36/test/dr.py
@@ -0,0 +1,20 @@
+from src.core_pro import Drive
+
+
+url = '127h31Zzw-mHnE_-vGEWc3ikKhs1MgQAf'
+a = Drive().get_file_info(url)
+# url = '1SU-YjuibMc1xO4KzDdVTSlODhLHrFXlx'
+# a = Drive().get_file_info(url)
+
+# path = '/home/kevin/Downloads/1.csv'
+# folder_id = '1Ez8dNFmLQp936xkQlr1Ke1xW1WgJcvzD'
+# drive = Drive()
+# file_id = drive.upload(path, 'test.jpg', folder_id=folder_id)
+# drive.share_file(file_id, email='xuankhang.do@shopee.com')
+
+# url = '1FseQXxGUyHy4GfVgvvzvDWERhjcctxtsKl2txuVdg7z0QexxZ-PRe8afhvV87AvGJ51PAzzy'
+# url = '1t_fKO-N7CrVkhLYkG-Ouu9-Gupc-RiZ8L5ETUWQel9-BMe2EhL6z--H1BXkljAEM8ccqszXN'
+# a = Drive().search_files(url)
+# b = Drive().remove_file('1N49jLYrhK3rd1TfHKlTgaQQt-QT0fFdk')
+
+# Drive().empty_trash()
