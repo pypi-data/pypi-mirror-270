4e584beadcbee567da6b78b7e7d7a72c52e3e786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:36 +0000

    Add package makex/20240401/makex-20240401.tar.gz

---
+++ packages/makex/makex-20240401.tar.gz/makex-20240401/python/makex/patterns_test.py
@@ -0,0 +1,24 @@
+from makex.patterns import (
+    combine_patterns,
+    make_glob_pattern,
+)
+
+
+def test():
+    pattern = combine_patterns(
+        [
+            r"\.hgignore",
+            r"\.p4ignore",
+            make_glob_pattern("**/*.py"),
+            make_glob_pattern("*.py"),
+        ]
+    )
+
+    print(pattern.match(".hgignore"))
+    assert not pattern.match("1hgignore")
+    assert pattern.match(".hgignore")
+    assert pattern.match(".p4ignore")
+    assert not pattern.match("hgignore")
+    assert pattern.match("test/test.py")
+    assert pattern.match("/test/test.py")
+    assert pattern.match("test.py")
