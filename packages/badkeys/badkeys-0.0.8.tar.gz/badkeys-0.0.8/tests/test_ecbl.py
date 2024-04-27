5872dee126b5354b9b2fd4a6990717bb6d00da38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:14 +0000

    Add package badkeys/0.0.8/badkeys-0.0.8.tar.gz

---
+++ packages/badkeys/badkeys-0.0.8.tar.gz/badkeys-0.0.8/tests/test_ecbl.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+
+import badkeys
+
+TDPATH = f"{os.path.dirname(__file__)}/data/"
+
+
+class TestEcbl(unittest.TestCase):
+    @unittest.skipUnless(os.environ.get("RUNBLTESTS"), "Not running blocklist tests")
+    def test_ecbl(self):
+        with open(f"{TDPATH}ec-p256-rfc-example.key") as f:
+            key = f.read()
+        r = badkeys.checkpubkey(key, checks=["blocklist"])
+        self.assertTrue("blocklist" in r["results"])
+        with open(f"{TDPATH}ed25519-rfc-example.key") as f:
+            key = f.read()
+        r = badkeys.checkpubkey(key, checks=["blocklist"])
+        self.assertTrue("blocklist" in r["results"])
+        with open(f"{TDPATH}x448-ok.key") as f:
+            key = f.read()
+        r = badkeys.checkpubkey(key, checks=["blocklist"])
+        self.assertFalse(r["results"])
+
+
+if __name__ == "__main__":
+    unittest.main()
