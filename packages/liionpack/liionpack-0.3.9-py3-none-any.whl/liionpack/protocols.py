a20ff5196538c4eda8478886a2ae85dfc5291754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:04:08 +0000

    Add package liionpack/0.3.9/liionpack-0.3.9-py3-none-any.whl

---
+++ packages/liionpack/liionpack-0.3.9-py3-none-any.whl/liionpack/protocols.py
@@ -0,0 +1,47 @@
+#
+# Experimental protocol
+#
+
+import numpy as np
+
+
+def generate_protocol_from_experiment(experiment, flatten=True):
+    """
+
+    Args:
+        experiment (pybamm.Experiment):
+            The experiment to generate the protocol from.
+        flatten (bool):
+            Default is True: return all steps in one list otherwise return a
+            list of lists for each operating command.
+
+    Returns:
+        protocol (list):
+            a sequence of terminal currents to apply at each timestep
+
+    """
+    protocol = []
+    for i, step in enumerate(experiment.operating_conditions_steps):
+        proto = []
+        t = step.duration
+        dt = step.period
+        typ = step.type
+        if typ not in ["current"]:
+            raise ValueError("Only constant current operations are supported")
+        else:
+            if typ == "current":
+                if not step.is_drive_cycle:
+                    I = step.value
+                    proto.extend([I] * int(np.round(t, 5) / np.round(dt, 5)))
+                    if i == 0:
+                        # Include initial state when not drive cycle, first op
+                        proto = [proto[0]] + proto
+                else:
+                    proto.extend(step.value.y.tolist())
+
+        if flatten:
+            protocol.extend(proto)
+        else:
+            protocol.append(proto)
+
+    return protocol
