1cba06fd6cd846d088063a0a0f06ca8493e47844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 14:03:38 +0000

    Add package sdmc-adhoc-processing/0.1.47/sdmc_adhoc_processing-0.1.47.tar.gz

---
+++ packages/sdmc-adhoc-processing/sdmc_adhoc_processing-0.1.47.tar.gz/sdmc_adhoc_processing-0.1.47/src/sdmc_adhoc_processing/__init__.py
@@ -0,0 +1,11 @@
+# from sdmc_adhoc_processing.generate_dict import *
+# # import argparse
+#
+# def hello_world():
+#     print("this is a test! seems to be working")
+#
+# def gen_data_dict():
+#     if os.path.exists(output_dir + data_dict_fname):
+#         edit_existing_dict()
+#     else:
+#         build_brand_new_dict()
