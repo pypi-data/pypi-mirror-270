4ad447d96f50db03d17574fb7eff2b4a6f7dd561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:33 +0000

    Add package sttpg-qats/1.0.0/sttpg_qats-1.0.0.tar.gz

---
+++ packages/sttpg-qats/sttpg_qats-1.0.0.tar.gz/sttpg_qats-1.0.0/setup.py
@@ -0,0 +1,62 @@
+import __future__
+import abc
+import contextlib
+import copy
+import dataclasses
+import logging
+import math
+import pathlib
+import pdb
+import platform
+import sys
+import typing
+import uuid
+from datetime import time
+import re
+
+import matplotlib as matplotlib
+import numpy
+import pandas as pandas
+import psycopg2 as psycopg2
+import scipy as scipy
+import sqlalchemy as sqlalchemy
+import toml as toml
+from setuptools import setup, find_packages
+
+setup(
+    name='sttpg_qats',
+    version='1.0.0',
+    author='',
+    author_email='',
+    description='sttpg package used in QATS',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'matplotlib',
+        're',
+        'math',
+        'typing',
+        'abc',
+        'dataclasses',
+        'uuid',
+        'sqlalchemy',
+        'psycopg2',
+        'copy',
+        'logging',
+        'time',
+        'pathlib',
+        'sys',
+        'platform',
+        'contextlib',
+        'toml',
+    ],
+    classifiers=[
+        # Se https://pypi.org/classifiers/ for en liste over klassifikatorer
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
