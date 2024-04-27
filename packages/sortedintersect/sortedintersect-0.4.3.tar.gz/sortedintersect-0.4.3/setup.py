8b0a9480b0a3bacc50c67ec9d403b4e3d9393fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:03:45 +0000

    Add package sortedintersect/0.4.3/sortedintersect-0.4.3.tar.gz

---
+++ packages/sortedintersect/sortedintersect-0.4.3.tar.gz/sortedintersect-0.4.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages, Extension
+
+ext_modules = [
+    Extension("sortedintersect.sintersect",
+              ["sortedintersect/sintersect.pyx"],
+              language="c++",
+              extra_compile_args=["-std=c++17"])
+]
+
+setup(
+    version='0.4.3',
+    name='sortedintersect',
+    description="Interval intersection for sorted query and target intervals",
+    author="Kez Cleal",
+    author_email="clealk@cardiff.ac.uk",
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    package_data={"": ['*.pyx', '*.hpp']},
+    include_package_data=False
+)
