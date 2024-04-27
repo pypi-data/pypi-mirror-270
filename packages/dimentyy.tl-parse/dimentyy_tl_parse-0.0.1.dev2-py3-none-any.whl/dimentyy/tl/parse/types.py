d4372d3ee624e474628cc4c929fd960680c15468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:04:06 +0000

    Add package dimentyy.tl-parse/0.0.1.dev2/dimentyy_tl_parse-0.0.1.dev2-py3-none-any.whl

---
+++ packages/dimentyy.tl-parse/dimentyy_tl_parse-0.0.1.dev2-py3-none-any.whl/dimentyy/tl/parse/types.py
@@ -0,0 +1,6 @@
+from telethon.tl.types import (MessageEntityBold, MessageEntityItalic, MessageEntityUnderline,
+                               MessageEntityCode, MessageEntitySpoiler, MessageEntityBlockquote,
+                               MessageEntityStrike)
+
+plain_entities = {MessageEntityBold, MessageEntityItalic, MessageEntityCode, MessageEntityStrike,
+                  MessageEntityUnderline, MessageEntityBlockquote, MessageEntitySpoiler}
