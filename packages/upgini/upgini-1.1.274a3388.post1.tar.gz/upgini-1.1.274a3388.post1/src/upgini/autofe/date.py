b18c579b53ab0bb30bfc516dda2f30d4fa561f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:45 +0000

    Add package upgini/1.1.274a3388.post1/upgini-1.1.274a3388.post1.tar.gz

---
+++ packages/upgini/upgini-1.1.274a3388.post1.tar.gz/upgini-1.1.274a3388.post1/src/upgini/autofe/date.py
@@ -0,0 +1,125 @@
+from typing import Any, List, Optional, Union
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel
+
+from upgini.autofe.operand import MultiOperand, Operand, PandasOperand
+
+
+class DateDiffMixin(BaseModel):
+    diff_unit: str = "D"
+    left_unit: Optional[str] = None
+    right_unit: Optional[str] = None
+
+    def _convert_to_date(
+        self, x: Union[pd.DataFrame, pd.Series], unit: Optional[str]
+    ) -> Union[pd.DataFrame, pd.Series]:
+        if isinstance(x, pd.DataFrame):
+            return x.apply(lambda y: self._convert_to_date(y, unit), axis=1)
+
+        return pd.to_datetime(x, unit=unit)
+
+
+class DateDiff(PandasOperand, DateDiffMixin):
+    name = "date_diff"
+    is_binary = True
+    has_symmetry_importance = True
+    common_type = "date_diff"
+
+    def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
+        left = self._convert_to_date(left, self.left_unit)
+        right = self._convert_to_date(right, self.right_unit)
+        return self.__replace_negative((left - right) / np.timedelta64(1, self.diff_unit))
+
+    def __replace_negative(self, x: Union[pd.DataFrame, pd.Series]):
+        x[x < 0] = None
+        return x
+
+
+class DateDiffType2(PandasOperand, DateDiffMixin):
+    name = "date_diff_type2"
+    is_binary = True
+    has_symmetry_importance = True
+    common_type = "date_diff"
+
+    def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
+        left = self._convert_to_date(left, self.left_unit)
+        right = self._convert_to_date(right, self.right_unit)
+        future = right + (left.dt.year - right.dt.year).apply(
+            lambda y: np.datetime64("NaT") if np.isnan(y) else pd.tseries.offsets.DateOffset(years=y)
+        )
+        before = future[future < left]
+        future[future < left] = before + pd.tseries.offsets.DateOffset(years=1)
+        diff = (future - left) / np.timedelta64(1, self.diff_unit)
+
+        return diff
+
+
+_ext_aggregations = {"nunique": (lambda x: len(np.unique(x)), 0), "count": (len, 0)}
+
+
+class DateListDiff(PandasOperand, DateDiffMixin):
+    is_binary = True
+    has_symmetry_importance = True
+    aggregation: str
+    common_type = "date_diff_list"
+
+    def __init__(self, **data: Any) -> None:
+        if "name" not in data:
+            data["name"] = f"date_diff_{data.get('aggregation')}"
+        super().__init__(**data)
+
+    def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
+        left = self._convert_to_date(left, self.left_unit)
+        right = right.apply(lambda x: pd.arrays.DatetimeArray(self._convert_to_date(x, self.right_unit)))
+
+        return pd.Series(left - right.values).apply(lambda x: self._agg(self._diff(x)))
+
+    def _diff(self, x):
+        x = x / np.timedelta64(1, self.diff_unit)
+        return x[x > 0]
+
+    def _agg(self, x):
+        method = getattr(np, self.aggregation, None)
+        default = np.nan
+        if method is None and self.aggregation in _ext_aggregations:
+            method, default = _ext_aggregations[self.aggregation]
+        elif not callable(method):
+            raise ValueError(f"Unsupported aggregation: {self.aggregation}")
+
+        return method(x) if len(x) > 0 else default
+
+    def make_multi_operand(self, operands: List[Operand]) -> MultiOperand | None:
+        return DateListDiffMulti(children=operands, aggregation="")
+
+
+class DateListDiffBounded(DateListDiff):
+    lower_bound: Optional[int]
+    upper_bound: Optional[int]
+
+    def __init__(self, **data: Any) -> None:
+        if "name" not in data:
+            lower_bound = data.get("lower_bound")
+            upper_bound = data.get("upper_bound")
+            components = [
+                "date_diff",
+                data.get("diff_unit"),
+                str(lower_bound if lower_bound is not None else "minusinf"),
+                str(upper_bound if upper_bound is not None else "plusinf"),
+            ]
+            components.append(data.get("aggregation"))
+            data["name"] = "_".join(components)
+        super().__init__(**data)
+
+    def _agg(self, x):
+        x = x[(x >= (self.lower_bound or -np.inf)) & (x < (self.upper_bound or np.inf))]
+        return super()._agg(x)
+
+
+class DateListDiffMulti(DateListDiff, MultiOperand):
+    def calculate_binary(self, left: pd.Series, right: pd.Series) -> pd.Series:
+        left = self._convert_to_date(left, self.left_unit)
+        right = right.apply(lambda x: pd.arrays.DatetimeArray(self._convert_to_date(x, self.right_unit)))
+
+        diff = pd.Series(left - right.values).apply(self._diff)
+        return diff.apply(lambda x: [c._agg(x) for c in self.children])
