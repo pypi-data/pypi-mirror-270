7614391bac0e561bdb8bf70885ddccd6427a2a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:43 +0000

    Add package showCV/1.0/showCV-1.0.tar.gz

---
+++ packages/showCV/showCV-1.0.tar.gz/showCV-1.0/setup.py
@@ -0,0 +1,37 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+VERSION = '1.0'
+DESCRIPTION = 'a python package for cv2.imshow '
+# LONG_DESCRIPTION = ' a python package for ...  '
+
+# Setting up
+setup(
+    name="showCV",
+    version=VERSION,
+    author="xufengnian",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    license='MIT',
+    keywords=['python','cv2','show'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
