f275db2fe3c957e65fac9c0b076dd1f56ec67247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:30 +0000

    Add package fremu/0.0.7/fremu-0.0.7.tar.gz

---
+++ packages/fremu/fremu-0.0.7.tar.gz/fremu-0.0.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fremu",
+    version="0.0.7",
+    author="Jiachen Bai",
+    author_email="astrobaijc@gmail.com",  
+    description="Emulator for f(R) gravity",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://github.com/astrobai/fremu",
+    py_modules=['fremu'],
+    packages=['fremu'],
+    package_data={'fremu': ['cache/*']},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'torch',
+        'scikit-learn',
+        'camb'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
