3214c6a568752b3d75b5615e0d819e4f865fec59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:04:00 +0000

    Add package docx-asn1/1.0.7/docx_asn1-1.0.7-py3-none-any.whl

---
+++ packages/docx-asn1/docx_asn1-1.0.7-py3-none-any.whl/docx_asn1/utils.py
@@ -0,0 +1,45 @@
+from docx import Document
+from sys import argv, stderr
+from os.path import isfile
+
+
+def extract_text_from_docx(
+    filename, start_text="-- ASN1START", stop_text="-- ASN1STOP"
+):
+    """extract asn1 from a docx file"""
+    if not isfile(filename):
+        print(f"File {filename} not found", file=stderr)
+        return None
+    doc = Document(filename)
+    full_text = []
+    inside_range = False
+    for para in doc.paragraphs:
+        if start_text in para.text:
+            inside_range = True
+            continue
+        elif stop_text in para.text:
+            full_text.append(para.text)
+            inside_range = False
+            continue
+
+        # Add the paragraph text if inside the desired range
+        if inside_range:
+            full_text.append(para.text)
+    return "\n".join(full_text)
+
+
+def main():
+    """main function"""
+    if len(argv) < 2:
+        print("Usage: python decode_asn1.py <filename>", file=stderr)
+        print("Or: python decode_asn1.py <filename> <outputfile>", file=stderr)
+        return
+    extracted_text = extract_text_from_docx(argv[1])
+    if extracted_text is None:
+        return
+    if len(argv) > 2:
+        output_filename = argv[2]
+        with open(output_filename, "w", encoding="utf-8") as file:
+            file.write(extracted_text)
+    else:
+        print(extracted_text)
