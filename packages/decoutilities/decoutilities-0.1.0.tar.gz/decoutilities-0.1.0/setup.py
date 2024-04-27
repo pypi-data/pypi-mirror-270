21f8d6dac5743240dae14f4ef2b4b08358f67f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:24 +0000

    Add package decoutilities/0.1.0/decoutilities-0.1.0.tar.gz

---
+++ packages/decoutilities/decoutilities-0.1.0.tar.gz/decoutilities-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='decoutilities',
+version='0.1.0',
+author='Hugo Torres',
+author_email='contact@redactado.es',
+description='Enhance the readability of your code with decorators.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
