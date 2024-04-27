47f33091e18fa0508e2c754554194f4cb1626360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:51 +0000

    Add package telegram-markdown-text/0.4.1/telegram_markdown_text-0.4.1-py3-none-any.whl

---
+++ packages/telegram-markdown-text/telegram_markdown_text-0.4.1-py3-none-any.whl/telegram_markdown_text/__init__.py
@@ -0,0 +1,18 @@
+from .markdown_text import *
+
+__version__ = '0.4.1'
+__all__ = [
+    "MarkdownText",
+    "Bold",
+    "Italic",
+    "Underline",
+    "Strikethrough",
+    "Spoiler",
+    "InlineUrl",
+    "InlineUser",
+    "Emoji",
+    "InlineCode",
+    "InlineCodeBlock",
+    "QuoteBlock",
+    "PlainText",
+]
