85083a1f0846d1720ca39ede98574546e2bfcdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:38 +0000

    Add package m3-cli/3.112.4/m3_cli-3.112.4-py3-none-any.whl

---
+++ packages/m3-cli/m3_cli-3.112.4-py3-none-any.whl/m3cli/plugins/upload-terraform-template.py
@@ -0,0 +1,35 @@
+"""
+The custom logic for the command m3 upload-terraform-template.
+This logic is created to convert parameters from the Human readable format to
+appropriate for M3 SDK API request.
+"""
+import json
+
+from m3cli.utils.utilities import load_file_contents
+
+
+def create_custom_request(request):
+    approval_params = ['reviewers', 'approvalRule']
+    approval_values = [p for p in approval_params
+                       if request.parameters.get(p) is not None]
+    approval_len = len(approval_values)
+    if approval_len != 0:
+        if approval_len != len(approval_params):
+            raise AssertionError(
+                'Parameters: "--approver", "--approval-policy" '
+                'are required if the template needs review')
+        request.parameters["needReview"] = True
+    file_path = request.parameters.pop('filepath')
+    request.parameters['templateContent'] = load_file_contents(file_path)
+    request.parameters['templateActivity'] = 'UPLOAD'
+    return request
+
+
+def create_custom_response(request, response):
+    response = json.loads(response)
+    if response['success']:
+        template_id = response.get('templateUUID')
+        return f'The template was uploaded with the identifier "{template_id}"'
+    else:
+        error = response.get('errorMessage')
+        return f'The template failed to upload. Reason: {error}'
