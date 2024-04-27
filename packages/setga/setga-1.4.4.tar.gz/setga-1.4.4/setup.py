0aa7351de027dcf46be47ab06cde457224f23ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:03:41 +0000

    Add package setga/1.4.4/setga-1.4.4.tar.gz

---
+++ packages/setga/setga-1.4.4.tar.gz/setga-1.4.4/setup.py
@@ -0,0 +1,25 @@
+
+from distutils.core import setup
+setup(
+  name = 'setga',         
+  packages = ['setga'], 
+  license='MIT',       
+  description = 'library designed to extract a minimal subset from a given set, optimizing a given (set of) objective(s). Based on the DEAP library.',   # Give a short description about your library
+  author = 'Nikola Kalábová',              
+  author_email = 'nikola@kalabova.eu',     
+  url = 'https://github.com/lavakin/setga',  
+  version = "1.4.4",    
+  keywords = ['Genetic algorithms', 'minimal subset', 'multi-objective', "optimization"],   
+  long_description = "library designed to extract a minimal subset from a given set, optimizing a given (set of) objective(s). Based on the DEAP library.",
+  project_urls = {"Documentation" :"https://setga.readthedocs.io/en/latest/genindex.html"},
+  install_requires=[          
+          'numpy',
+          'deap',
+          "matplotlib",
+      ],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',  
+  ],
+)
