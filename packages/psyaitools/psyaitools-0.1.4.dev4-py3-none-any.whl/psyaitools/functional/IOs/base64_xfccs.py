5a4645b2b0a4fdf2eda5466f69d5d3d8fd698a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:04:05 +0000

    Add package psyaitools/0.1.4.dev4/psyaitools-0.1.4.dev4-py3-none-any.whl

---
+++ packages/psyaitools/psyaitools-0.1.4.dev4-py3-none-any.whl/psyaitools/functional/IOs/base64_xfccs.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from .readFiles import *
+from .base64_wav import *
+from ..representation.xfccs import *
+
+def base64_xfccs(path, audioRow, fileName,feature_set="mfcc"):
+    dat1 = getRecDat(path)
+    base64ToWebm(dat1.loc[audioRow, "response"], fileName, method=2)
+    webm2Wav(fileName)
+    # Split the filename into name and extension
+    nam1, _ = fileName.rsplit('.', 1)
+    fs, sig = getWavDat(nam1 + ".wav")
+    out1, _ = xfccs(fs, sig, feature_set=feature_set)
+    return np.mean(out1, axis=1)
+
+def wav_xfccs(path, feature_set="mfcc"):
+    fs, sig = getWavDat(path)
+    out1, _ = xfccs(fs, sig, feature_set=feature_set)
+    return np.mean(out1, axis=1)
