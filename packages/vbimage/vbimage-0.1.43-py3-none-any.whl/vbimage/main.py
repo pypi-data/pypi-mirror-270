fe2347f7dae4400d011f4cf01238ff79a2d7a228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 03:04:18 +0000

    Add package vbimage/0.1.43/vbimage-0.1.43-py3-none-any.whl

---
+++ packages/vbimage/vbimage-0.1.43-py3-none-any.whl/vbimage/main.py
@@ -0,0 +1,37 @@
+import click
+
+
+# from .resize import resize
+# from .info import info
+from .removebg import removebg
+from .pixelate import pixelate
+from .blur import blur
+from .stack import stack
+from .render import render
+from .layer import layer
+from .grid import grid
+from .contrast import contrast
+
+CONTEXT_SETTINGS = dict(
+    help_option_names=[
+        '-h',
+        '--help'
+    ]
+)
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+def main():
+    pass
+
+
+main.add_command(layer)
+main.add_command(render)
+main.add_command(stack)
+main.add_command(blur)
+main.add_command(pixelate)
+main.add_command(removebg)
+main.add_command(grid)
+main.add_command(contrast)
+# main.add_command(resize)
+# main.add_command(info)
