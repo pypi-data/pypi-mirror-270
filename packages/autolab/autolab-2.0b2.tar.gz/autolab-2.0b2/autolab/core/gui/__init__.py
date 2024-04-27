313b986efc5f2231fba8b0de26daf187bce6cdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:51 +0000

    Add package autolab/2.0b2/autolab-2.0b2.tar.gz

---
+++ packages/autolab/autolab-2.0b2.tar.gz/autolab-2.0b2/autolab/core/gui/__init__.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 14:48:14 2017
+
+@author: Quentin Chateiller
+quentin.chateiller@c2n.upsaclay.fr
+
+
+"""
+#from threading import Event, Thread
+
+#started = Event()
+
+#def gui() :
+#
+#    if started.is_set():
+#        print("Gui already running")
+#    else :
+#        t=AppThread()
+#        t.start()
+
+
+def start():
+    """ Open the Autolab GUI """
+
+    import os
+    from ..config import get_GUI_config
+    GUI_config = get_GUI_config()
+    if GUI_config['QT_API'] != 'default':
+        os.environ['QT_API'] = str(GUI_config['QT_API'])
+    try:
+        import pyqtgraph as pg
+        from qtpy import QtWidgets
+    except ModuleNotFoundError as e:
+        print(f"""Can't use GUI, package(s) missing: {e}
+Need to install pyqtgraph, qtpy, and one of PyQt5, PySide2, PyQt6 or PySide6.
+
+Using pip:
+pip install pyqtgraph
+pip install qtpy
+pip install PyQt5
+pip install PySide2
+pip install PyQt6
+pip install PySide6
+
+Using anaconda:
+conda install pyqtgraph
+conda install qtpy
+conda install pyqt
+conda install -c conda-forge pyside2
+no PyQt6 anaconda version available
+conda install -c conda-forge pyside6
+""")
+    else:
+        background = GUI_config['image_background']
+        foreground = GUI_config['image_foreground']
+        pg.setConfigOptions(background=background, foreground=foreground)
+        pg.setConfigOption('imageAxisOrder', 'row-major')
+
+        app = QtWidgets.QApplication.instance()
+        if app is None:
+            app = QtWidgets.QApplication([])
+
+        if GUI_config['font_size'] != 'default':
+            font = app.font()
+            font.setPointSize(int(GUI_config['font_size']))
+            app.setFont(font)
+
+        from .controlcenter.main import ControlCenter
+        gui = ControlCenter()
+        gui.initialize()
+        gui.show()
+        app.exec()
+
+
+#class AppThread(Thread):
+#
+#    def __init__(self):
+#
+#        Thread.__init__(self)
+#
+#    def run(self):
+#
+#        started.set()
+#
+#        _run()
+#
+#        started.clear()
+#
