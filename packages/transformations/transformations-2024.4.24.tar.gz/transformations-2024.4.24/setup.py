e58d5f05d3efc0fbfe259a77920eef99dfa7616d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:29 +0000

    Add package transformations/2024.4.24/transformations-2024.4.24.tar.gz

---
+++ packages/transformations/transformations-2024.4.24.tar.gz/transformations-2024.4.24/setup.py
@@ -0,0 +1,93 @@
+# transformations/setup.py
+
+"""Transformations package Setuptools script."""
+
+import re
+import sys
+
+import numpy
+from setuptools import Extension, setup
+
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
+with open('transformations/transformations.py', encoding='utf-8') as fh:
+    code = fh.read()
+
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev0')
+
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
+
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+license = search(
+    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+
+license = license.replace('# ', '').replace('#', '')
+
+if 'sdist' in sys.argv:
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+    with open('README.rst', 'w', encoding='utf-8') as fh:
+        fh.write(readme)
+
+
+setup(
+    name='transformations',
+    version=version,
+    license='BSD',
+    description=description,
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Christoph Gohlke',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/transformations/issues',
+        'Source Code': 'https://github.com/cgohlke/transformations',
+        # 'Documentation': 'https://',
+    },
+    python_requires='>=3.9',
+    install_requires=['numpy'],
+    packages=['transformations'],
+    ext_modules=[
+        Extension(
+            'transformations._transformations',
+            ['transformations/transformations.c'],
+            include_dirs=[numpy.get_include()],
+        )
+    ],
+    zip_safe=False,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
