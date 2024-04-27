6f545b373f6b6827c9ad21496d24230925e956e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:04:18 +0000

    Add package cnlitellm/0.2.5/cnlitellm-0.2.5.tar.gz

---
+++ packages/cnlitellm/cnlitellm-0.2.5.tar.gz/cnlitellm-0.2.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cnlitellm',
+    version='0.2.5',
+    packages=find_packages(),
+    license='MIT',
+    description='A Python library for unified access to Chinese domestic large language models.',
+    author='everfly',
+    author_email='tagriver@gmail.com',
+    url='https://github.com/EvalsOne/CNLiteLLM',
+    install_requires=[
+        'openai',
+        'pydantic',
+        'zhipuai',
+        'tenacity',
+        'dashscope',
+        'websocket_client',
+        'requests'
+    ],
+)
