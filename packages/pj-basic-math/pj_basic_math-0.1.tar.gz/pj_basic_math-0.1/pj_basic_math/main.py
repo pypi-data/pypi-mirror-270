a61d5a498a842e6523df927cd03fba7f6f3113c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 14:03:30 +0000

    Add package pj-basic-math/0.1/pj_basic_math-0.1.tar.gz

---
+++ packages/pj-basic-math/pj_basic_math-0.1.tar.gz/pj_basic_math-0.1/pj_basic_math/main.py
@@ -0,0 +1,36 @@
+def add_number(a,b):
+    """
+    Args:
+        a (int): Primer numero a sumar
+        b (int): Segundo numero a sumar
+    
+    Ejemplo:
+    >>> add_numbers(2,3)
+    5
+    """
+    return a + b
+
+def substract_number(a,b):
+    """
+    Args:
+        a (int): Primer numero a restar
+        b (int): Segundo numero a restar
+    
+    Ejemplo:
+    >>> substract_number(4,2)
+    2
+    """
+    return a - b
+
+def multiply_number(a,b):
+    """
+    Args:
+        a (int): Primer numero a multiplicar
+        b (int): Segundo numero a multiplicar
+    
+    Ejemplo:
+    >>> multiply_number(4,2)
+    8
+    """
+    return a * b
+
