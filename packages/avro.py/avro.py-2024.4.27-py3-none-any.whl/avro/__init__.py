820d3158b896a04d059145c9285b12e8e5b0a1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:28 +0000

    Add package avro.py/2024.4.27/avro.py-2024.4.27-py3-none-any.whl

---
+++ packages/avro.py/avro.py-2024.4.27-py3-none-any.whl/avro/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: MIT
+
+
+# Import local modules.
+from .main import *
+
+# Set package information.
+__description__ = 'A modern Pythonic implementation of Avro Phonetic.'
+
+__version_info__ = ('2024', '4', '27')  # YYYY / MM / DD
+__version__ = '.'.join(__version_info__)
