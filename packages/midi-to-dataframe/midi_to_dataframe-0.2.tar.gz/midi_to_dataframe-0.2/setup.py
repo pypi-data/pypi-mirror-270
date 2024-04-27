de17ea0c2a537b0f5cc8fe16bfb04dfd40e772e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:03:53 +0000

    Add package midi-to-dataframe/0.2/midi_to_dataframe-0.2.tar.gz

---
+++ packages/midi-to-dataframe/midi_to_dataframe-0.2.tar.gz/midi_to_dataframe-0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+# Define version information
+VERSION = '0.2'
+FULLVERSION = VERSION
+
+requirements = [
+]
+
+setup(name='midi_to_dataframe',
+      version=VERSION,
+      author='Taylor Peer',
+      url='https://github.com/TaylorPeer/midi-to-dataframe',
+      description="Python library for converting between MIDI files and Pandas DataFrame objects.",
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=requirements,
+      package_data={}
+      )
