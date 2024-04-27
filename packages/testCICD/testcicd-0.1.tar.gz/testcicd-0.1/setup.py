137a38ed9f36d0b07f47cdc0137fef9b66e52c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:04:15 +0000

    Add package testCICD/0.1/testcicd-0.1.tar.gz

---
+++ packages/testCICD/testcicd-0.1.tar.gz/testcicd-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+  
+setup( 
+    name='testCICD', 
+    version='0.1', 
+    description='A sample Python package to test CI/CD', 
+    long_description='A sample Python package to test CI/CD',
+    author='Angelo Ferrando', 
+    author_email='angelo.ferrando42@gmail.com', 
+    packages=[''], 
+    install_requires=[], 
+) 
