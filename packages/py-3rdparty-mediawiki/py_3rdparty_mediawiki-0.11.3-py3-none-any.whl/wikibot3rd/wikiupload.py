b70200a3b90cebcab50c76eb568f7af11b37b260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 15:03:54 +0000

    Add package py-3rdparty-mediawiki/0.11.3/py_3rdparty_mediawiki-0.11.3-py3-none-any.whl

---
+++ packages/py-3rdparty-mediawiki/py_3rdparty_mediawiki-0.11.3-py3-none-any.whl/wikibot3rd/wikiupload.py
@@ -0,0 +1,15 @@
+"""
+Created on 2020-11-12
+
+@author: wf
+"""
+import sys
+
+import wikibot3rd.wikipush
+
+DEBUG = False
+
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-d")
+    sys.exit(wikibot3rd.wikipush.mainUpload())
