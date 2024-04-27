3dba4ac1eed13951689aa66706903c24e0790560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 04:03:44 +0000

    Add package DocumentAI-std/0.2.6.dev1/DocumentAI_std-0.2.6.dev1.tar.gz

---
+++ packages/DocumentAI-std/DocumentAI_std-0.2.6.dev1.tar.gz/DocumentAI_std-0.2.6.dev1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="DocumentAI_std",
+    version="0.2.6.dev1",
+    # packages=find_packages(where='DocumentAI_std'),  # Include all packages within this directory
+    packages=find_packages(exclude=["DocumentAI_std.tests"]),
+    # package_dir={'': 'DocumentAI_std'},  # Set root package directory
+    install_requires=required,
+    url="",
+    license="",
+    author="Hamza Gbada",
+    author_email="",
+    description="The main standards for Latis Document AI project",
+)
