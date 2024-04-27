5715fd286c43744f732b4726d959666489f579fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 14:03:17 +0000

    Add package pygpt-net/2.1.80/pygpt_net-2.1.80-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.1.80-py3-none-any.whl/pygpt_net/controller/__init__.py
@@ -0,0 +1,166 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.04.24 01:00:00                  #
+# ================================================== #
+
+from pygpt_net.controller.agent import Agent
+from pygpt_net.controller.assistant import Assistant
+from pygpt_net.controller.attachment import Attachment
+from pygpt_net.controller.audio import Audio
+from pygpt_net.controller.calendar import Calendar
+from pygpt_net.controller.camera import Camera
+from pygpt_net.controller.chat import Chat
+from pygpt_net.controller.command import Command
+from pygpt_net.controller.config import Config
+from pygpt_net.controller.ctx import Ctx
+from pygpt_net.controller.debug import Debug
+from pygpt_net.controller.dialogs import Dialogs
+from pygpt_net.controller.files import Files
+from pygpt_net.controller.finder import Finder
+from pygpt_net.controller.idx import Idx
+from pygpt_net.controller.lang import Lang
+from pygpt_net.controller.launcher import Launcher
+from pygpt_net.controller.layout import Layout
+from pygpt_net.controller.mode import Mode
+from pygpt_net.controller.model import Model
+from pygpt_net.controller.notepad import Notepad
+from pygpt_net.controller.painter import Painter
+from pygpt_net.controller.plugins import Plugins
+from pygpt_net.controller.presets import Presets
+from pygpt_net.controller.settings import Settings
+from pygpt_net.controller.theme import Theme
+from pygpt_net.controller.ui import UI
+
+class Controller:
+    def __init__(self, window=None):
+        """
+        Main controller
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.agent = Agent(window)
+        self.assistant = Assistant(window)
+        self.attachment = Attachment(window)
+        self.audio = Audio(window)
+        self.calendar = Calendar(window)
+        self.camera = Camera(window)
+        self.chat = Chat(window)
+        self.command = Command(window)
+        self.config = Config(window)
+        self.ctx = Ctx(window)
+        self.debug = Debug(window)
+        self.dialogs = Dialogs(window)
+        self.files = Files(window)
+        self.finder = Finder(window)
+        self.idx = Idx(window)
+        self.lang = Lang(window)
+        self.launcher = Launcher(window)
+        self.layout = Layout(window)
+        self.mode = Mode(window)
+        self.model = Model(window)
+        self.notepad = Notepad(window)
+        self.painter = Painter(window)
+        self.plugins = Plugins(window)
+        self.presets = Presets(window)
+        self.settings = Settings(window)
+        self.theme = Theme(window)
+        self.ui = UI(window)
+
+    def setup(self):
+        """Setup controller"""
+        self.debug.setup()  # prepare log level
+        self.chat.render.setup()  # setup render engine
+
+        # setup layout
+        self.layout.setup()
+        self.ui.setup()
+
+        # setup controllers
+        self.lang.setup()
+        self.assistant.setup()
+        self.chat.setup()
+        self.agent.setup()
+        self.ctx.setup()
+        self.presets.setup()
+        self.idx.setup()
+        self.ui.update_tokens()
+        self.dialogs.setup()
+        self.audio.setup()
+        self.attachment.setup()
+        self.notepad.setup()
+        self.camera.setup_ui()
+
+    def post_setup(self):
+        """Post-setup, after plugins are loaded"""
+        self.settings.setup()
+        self.plugins.settings.setup()
+        self.model.editor.setup()
+        self.launcher.post_setup()
+        self.calendar.setup()  # after everything is loaded
+        self.painter.setup()  # load previous image if exists
+        self.debug.post_setup()  # post setup debug after all loaded
+
+        # show license terms dialog
+        if not self.window.core.config.get('license.accepted'):
+            self.dialogs.info.toggle(
+                'license',
+                width=500,
+                height=480,
+            )
+            self.window.ui.dialog['info.license'].setFocus()
+
+    def after_setup(self):
+        """After-setup, after all loaded"""
+        self.plugins.update()
+
+    def on_update(self):
+        """On app main loop update"""
+        pass
+
+    def init(self):
+        """Init base settings"""
+        self.settings.load()
+
+    def reload(self):
+        """Reload components"""
+        self.window.core.db.close()  # close current database
+        self.window.core.db.init(force=True)  # re-init database with new path
+        self.window.core.patch()
+        self.window.core.ctx.reset()
+        self.window.core.notepad.locked = True
+        self.window.core.notepad.reset()
+        self.presets.locked = True
+        self.settings.setup()
+        self.window.core.presets.load()
+        self.window.core.debug.update_logger_path()
+        self.window.ui.dialogs.app_log.update()
+        self.ctx.setup()
+        self.ctx.update()
+        self.ctx.refresh()
+        self.assistant.setup()
+        self.attachment.setup()
+        self.presets.refresh()
+        self.idx.setup()
+        self.calendar.setup()
+        self.plugins.settings.setup()
+        self.plugins.update()
+        self.painter.setup()
+        self.notepad.update_tabs()
+        self.window.core.notepad.locked = False
+        self.files.update_explorer(reload=True)
+        self.lang.reload_config()
+        self.lang.setup()
+        self.theme.setup()
+        self.debug.toggle_menu()
+        self.debug.update()
+        self.chat.common.setup()
+        self.window.tools.on_reload()
+        self.presets.locked = False
+        self.window.controller.ctx.new_if_empty()  # create new empty context if empty
---
+++ packages/pygpt-net/pygpt_net-2.1.80-py3-none-any.whl/pygpt_net/controller/assistant/__init__.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.04.26 23:00:00                  #
+# ================================================== #
+
+import webbrowser
+
+from pygpt_net.item.assistant import AssistantItem
+
+from .editor import Editor
+from .files import Files
+from .threads import Threads
+from .store import VectorStore
+
+from pygpt_net.utils import trans
+
+
+class Assistant:
+    def __init__(self, window=None):
+        """
+        Assistant controller
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.editor = Editor(window)
+        self.files = Files(window)
+        self.threads = Threads(window)
+        self.store = VectorStore(window)
+
+    def setup(self):
+        """Setup assistants"""
+        self.window.core.assistants.load()
+        self.editor.setup()
+        self.update()
+
+    def update(self, update_list: bool = True):
+        """
+        Update assistants list
+
+        :param update_list: update list
+        """
+        if update_list:
+            self.update_list()
+        self.window.controller.assistant.files.update_list()
+        self.select_current()
+
+    def update_list(self):
+        """Update assistants list"""
+        items = self.window.core.assistants.get_all()
+        self.window.ui.toolbox.assistants.update(items)
+
+    def run_stop(self):
+        """Stop assistant run"""
+        ctx = self.window.core.ctx.last_item
+        if ctx is not None:
+            if ctx.run_id is not None and ctx.thread is not None:
+                status = self.window.core.gpt.assistants.run_stop(ctx)
+                if status == "cancelling":
+                    print("Run has been canceled.")
+
+    def prepare(self):
+        """Prepare assistants"""
+        # create or get current thread, it is required before conversation start
+        if self.window.core.config.get('assistant_thread') is None:
+            try:
+                self.window.ui.status(trans('status.sending'))
+                self.window.core.config.set('assistant_thread', self.threads.create_thread())
+            except Exception as e:
+                self.window.core.debug.log(e)
+                self.window.ui.dialogs.alert(e)
+
+    def refresh(self):
+        """Update assistants"""
+        self.select_default()
+
+    def select(self, idx: int):
+        """
+        Select assistant on the list
+
+        :param idx: idx on the list
+        """
+        # check if change is not locked
+        if self.change_locked():
+            return
+
+        # mark assistant as selected
+        id = self.window.core.assistants.get_by_idx(idx)
+        self.select_by_id(id)
+
+    def select_by_id(self, id: str):
+        """
+        Select assistant on the list
+
+        :param id: assistant ID
+        """
+        self.window.core.config.set('assistant', id)
+
+        # update attachments list with list of attachments from assistant
+        mode = self.window.core.config.get('mode')
+        assistant = self.window.core.assistants.get_by_id(id)
+        self.window.controller.attachment.update()
+        self.update(False)
+
+        # update model if exists in assistant
+        if assistant is not None:
+            model = assistant.model
+            if model is not None and model != "":
+                if model in self.window.core.models.items:
+                    self.window.core.config.set('model', model)
+                    self.window.core.config.data['current_model'][mode] = model
+                    self.window.controller.model.set(mode, model)
+                    self.window.controller.model.update_list()
+                    self.window.controller.model.select_current()
+            self.window.ui.nodes['preset.prompt'].setPlainText(assistant.instructions)
+            self.refresh()
+
+        self.window.controller.ctx.update_ctx()  # update current ctx info
+
+    def from_global(self):
+        """Update current preset from global prompt"""
+        id = self.window.core.config.get('assistant')
+        if id is not None and id != "":
+            if self.window.core.assistants.has(id):
+                assistant = self.window.core.assistants.get_by_id(id)
+                assistant.instructions = self.window.core.config.get('prompt')
+
+    def select_current(self):
+        """Select assistant by current"""
+        assistant_id = self.window.core.config.get('assistant')
+        items = self.window.core.assistants.get_all()
+        if assistant_id in items:
+            idx = list(items.keys()).index(assistant_id)
+            current = self.window.ui.models['assistants'].index(idx, 0)
+            self.window.ui.nodes['assistants'].setCurrentIndex(current)
+            self.window.core.config.set('prompt', items[assistant_id].instructions)
+            self.window.ui.nodes['preset.prompt'].setPlainText(items[assistant_id].instructions)
+
+    def select_default(self):
+        """Set default assistant"""
+        assistant = self.window.core.config.get('assistant')
+        if assistant is None or assistant == "":
+            mode = self.window.core.config.get('mode')
+            if mode == 'assistant':
+                self.window.core.config.set(
+                    'assistant',
+                    self.window.core.assistants.get_default_assistant(),
+                )
+                self.update()
+
+    def create(self) -> AssistantItem:
+        """
+        Create assistant in API
+
+        :return: AssistantItem
+        """
+        assistant = self.window.core.assistants.create()
+        self.editor.assign_data(assistant)
+        try:
+            return self.window.core.gpt.assistants.create(assistant)
+        except Exception as e:
+            self.window.core.debug.log(e)
+            self.window.ui.dialogs.alert(e)
+
+    def update_data(self, assistant: AssistantItem):
+        """
+        Update assistant
+
+        :param assistant: AssistantItem
+        """
+        self.editor.assign_data(assistant)
+        try:
+            return self.window.core.gpt.assistants.update(assistant)
+        except Exception as e:
+            self.window.core.debug.log(e)
+            self.window.ui.dialogs.alert(e)
+
+    def import_api(self, force: bool = False):
+        """
+        Import all remote assistants from API
+
+        :param force: if true, imports without confirmation
+        """
+        if not force:
+            self.window.ui.dialogs.confirm(
+                type='assistant_import',
+                id='',
+                msg=trans('confirm.assistant.import'),
+            )
+            return
+
+        # run asynchronous
+        self.window.ui.status("Importing assistants...please wait...")
+        self.window.core.gpt.assistants.importer.import_assistants()
+
+    def handle_imported_assistants(self, num: int):
+        """
+        Handle imported assistants
+
+        :param num: number of imported assistants
+        """
+        self.window.ui.status("OK. Imported assistants: " + str(num) + ".")
+        self.update()
+        self.store.update()
+        self.files.update()
+
+    def handle_imported_assistants_failed(self, error: any):
+        """
+        Handle error on importing assistants
+
+        :param error: error message
+        """
+        self.window.core.debug.log(error)
+        print("Error importing assistants")
+        self.window.ui.dialogs.alert(error)
+        self.update()
+
+    def clear(self, force: bool = False):
+        """
+        Clear assistant data
+
+        :param force: force clear data
+        """
+        id = self.window.core.config.get('assistant')
+
+        if not force:
+            self.window.ui.dialogs.confirm(
+                type='assistant_clear',
+                id='',
+                msg=trans('confirm.assistant.clear'),
+            )
+            return
+
+        if id is not None and id != "":
+            if self.window.core.assistants.has(id):
+                assistant = self.window.core.assistants.get_by_id(id)
+                assistant.reset()
+
+        self.window.ui.status(trans('status.assistant.cleared'))
+        self.update()
+
+    def delete(self, idx: int = None, force: bool = False):
+        """
+        Delete assistant
+
+        :param idx: assistant index (row index)
+        :param force: force delete without confirmation
+        """
+        if idx is not None:
+            id = self.window.core.assistants.get_by_idx(idx)
+            if id is not None and id != "":
+                if self.window.core.assistants.has(id):
+                    # if exists then show confirmation dialog
+                    if not force:
+                        self.window.ui.dialogs.confirm(
+                            type='assistant_delete',
+                            id=idx,
+                            msg=trans('confirm.assistant.delete'),
+                        )
+                        return
+
+                    # clear if this is current assistant
+                    if id == self.window.core.config.get('assistant'):
+                        self.window.core.config.set('assistant', None)
+                        self.window.core.config.set('assistant_thread', None)
+
+                    # delete in API
+                    try:
+                        self.window.core.gpt.assistants.delete(id)
+                    except Exception as e:
+                        self.window.ui.dialogs.alert(e)
+
+                    self.window.core.assistants.delete(id)
+                    self.update()
+                    self.window.ui.status(trans('status.assistant.deleted'))
+
+    def goto_online(self):
+        """Open Assistants page"""
+        webbrowser.open('https://platform.openai.com/assistants')
+
+    def change_locked(self) -> bool:
+        """
+        Check if assistant change is locked
+
+        :return: True if locked
+        """
+        return self.window.controller.chat.input.generating
