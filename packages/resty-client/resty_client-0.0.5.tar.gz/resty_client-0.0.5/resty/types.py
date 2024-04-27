3f899f27d8327448944d50be94064bb356880696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:04:07 +0000

    Add package resty-client/0.0.5/resty_client-0.0.5.tar.gz

---
+++ packages/resty-client/resty_client-0.0.5.tar.gz/resty_client-0.0.5/resty/types.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass, field
+from typing import Mapping, Iterable
+
+from pydantic import BaseModel
+
+from resty.enums import Method, Endpoint
+
+Schema = BaseModel
+
+
+@dataclass
+class Request:
+    url: str
+    method: Method
+    endpoint: Endpoint = None
+    data: Mapping | Iterable = None
+    json: Mapping | Iterable = None
+    timeout: int | None = None
+    params: dict = field(default_factory=dict)
+    headers: dict = field(default_factory=dict)
+    cookies: dict = field(default_factory=dict)
+    redirects: bool = False
+    middleware_options: dict = field(default_factory=dict)
+
+
+@dataclass
+class Response:
+    request: Request
+    status: int
+    content: bytes = None
+    text: str = None
+    json: list | dict = None
+    middleware_options: dict = field(default_factory=dict)
