9d6c9f849cc78602e6b60d7a2aa25ef4ddf030c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:43 +0000

    Add package indipyclient/0.0.7/indipyclient-0.0.7-py3-none-any.whl

---
+++ packages/indipyclient/indipyclient-0.0.7-py3-none-any.whl/indipyclient/__init__.py
@@ -0,0 +1,5 @@
+from .ipyclient import IPyClient
+from .events import (delProperty, defSwitchVector, defTextVector, defNumberVector, defLightVector, defBLOBVector,
+                     setSwitchVector, setTextVector, setNumberVector, setLightVector, setBLOBVector, Message, VectorTimeOut)
+
+version = "0.0.7"
