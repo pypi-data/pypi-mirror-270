98982511e9f904aa3aa87d930c5043b588c22195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:48 +0000

    Add package django-utz/0.2.0/django_utz-0.2.0-py3-none-any.whl

---
+++ packages/django-utz/django_utz-0.2.0-py3-none-any.whl/django_utz/__init__.py
@@ -0,0 +1,24 @@
+"""
+#### django_utz
+
+Decorators for Django models and DRF model serializers, custom serializer fields, 
+template tags and filters that aid easy conversion of timezone aware fields to a user's timezone.
+
+@Author: Daniel T. Afolayan (ti-oluwa.github.io)
+"""
+
+__version__ = "0.2.0"
+__author__ = "Daniel T. Afolayan"
+
+alias = "django-user-timezone"
+
+try:
+    import zoneinfo
+except ImportError:
+    from backports import zoneinfo
+except Exception:
+    raise Exception(
+        "django_utz couldn't import the zoneinfo module.\
+         Perhaps you are on an older version of Python, run `pip install backports.zoneinfo` to continue."
+    )
+
---
+++ packages/django-utz/django_utz-0.2.0-py3-none-any.whl/django_utz/middleware.py
@@ -0,0 +1,38 @@
+"""Middleware that aims to make the current request user available globally in the application."""
+
+from django.utils.deprecation import MiddlewareMixin
+import threading
+from django.contrib.auth.models import AbstractUser, User, AbstractBaseUser
+from typing import TypeVar
+
+ProjectUserModel = TypeVar("ProjectUserModel", AbstractUser, User, AbstractBaseUser)
+
+local_thread_storage = threading.local()
+
+
+class DjangoUTZMiddleware(MiddlewareMixin):
+    
+    def process_request(self, request) -> None:
+        """Stores the current authenticated user in the local thread storage."""
+        if request.user.is_authenticated:
+            setattr(local_thread_storage, "django_utz-request_user", request.user)
+        else:
+            setattr(local_thread_storage, "django_utz-request_user", None)
+        
+
+    def process_response(self, request, response):
+        """
+        Deletes the current authenticated user from the local thread storage.
+        """
+        if hasattr(local_thread_storage, "django_utz-request_user"):
+            delattr(local_thread_storage, "django_utz-request_user")
+        return response
+
+
+def get_request_user() -> ProjectUserModel | None:
+    """Returns the currently authenticated user. If no user is authenticated, returns None."""
+    if hasattr(local_thread_storage, "django_utz-request_user"):
+        request_user = getattr(local_thread_storage, "django_utz-request_user")
+        if request_user is not None and request_user.is_authenticated:
+            return request_user     
+    return None
