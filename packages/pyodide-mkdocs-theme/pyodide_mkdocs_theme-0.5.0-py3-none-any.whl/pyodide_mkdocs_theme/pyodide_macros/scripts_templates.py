7fdaf8e1a2e363afbf6a8ec78672079077456f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:37 +0000

    Add package pyodide-mkdocs-theme/0.5.0/pyodide_mkdocs_theme-0.5.0-py3-none-any.whl

---
+++ packages/pyodide-mkdocs-theme/pyodide_mkdocs_theme-0.5.0-py3-none-any.whl/pyodide_mkdocs_theme/pyodide_macros/scripts_templates.py
@@ -0,0 +1,24 @@
+"""
+pyodide-mkdocs-theme
+Copyleft GNU GPLv3 🄯 2024 Frédéric Zinelli
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+"""
+
+""" Generated file, do not modify """
+
+SCRIPTS_TEMPLATES = {
+    "pyodide": "<link rel=\"stylesheet\" href=\"{{ config.plugins.pyodide_macros.rebase(base_url) }}/pyodide-mkdocs/perPageScripts/start-pyodide.css\">\n<script type=\"application/javascript\" src=\"{{ config.plugins.pyodide_macros.rebase(base_url) }}/pyodide-mkdocs/perPageScripts/start-pyodide.js\"></script>"
+}
