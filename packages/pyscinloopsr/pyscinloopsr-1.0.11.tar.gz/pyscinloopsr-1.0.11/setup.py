6c7fcf709798bf3b6009aeff2f4d4beb16a12a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 14:03:26 +0000

    Add package pyscinloopsr/1.0.11/pyscinloopsr-1.0.11.tar.gz

---
+++ packages/pyscinloopsr/pyscinloopsr-1.0.11.tar.gz/pyscinloopsr-1.0.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyscinloopsr',
+    packages=find_packages(include=['pyscinloopsr']),
+    version='1.0.11',
+    description='Facilitator for scientists in the loop of SR modelling',
+    author='Djalma Pereira',
+    #install_requires='pysr'
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'], #tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
