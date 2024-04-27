3a7fe1deb3650e4dff4e170db0cd18cc14d9055c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-27 05:03:52 +0000

    Add package xmonkey-namonica/0.1.1/xmonkey_namonica-0.1.1.tar.gz

---
+++ packages/xmonkey-namonica/xmonkey_namonica-0.1.1.tar.gz/xmonkey_namonica-0.1.1/README.md
@@ -0,0 +1,5 @@
+# xmonkey-namonica
+
+xmonkey-namonica "pkg:nuget/newtonsoft.json@13.0.1/" --full
+xmonkey-namonica "pkg:pypi/flask@3.0.3/" --full
+xmonkey-namonica "pkg:npm/tslib@2.6.2/" --full
---
+++ packages/xmonkey-namonica/xmonkey_namonica-0.1.1.tar.gz/xmonkey_namonica-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xmonkey_namonica',
+    version='0.1.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'xmonkey-namonica = xmonkey_namonica.cli:main'  # Changed here
+        ]
+    },
+    install_requires=[
+        # Dependencies such as 'requests', etc.
+    ],
+)
