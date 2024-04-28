f65beb15d8d5ad8cf5e962192e470882eb1d752b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:03:45 +0000

    Add package semantic-router/0.0.35/semantic_router-0.0.35.tar.gz

---
+++ packages/semantic-router/semantic_router-0.0.35.tar.gz/semantic_router-0.0.35/semantic_router/encoders/__init__.py
@@ -0,0 +1,29 @@
+from semantic_router.encoders.base import BaseEncoder
+from semantic_router.encoders.bm25 import BM25Encoder
+from semantic_router.encoders.clip import CLIPEncoder
+from semantic_router.encoders.cohere import CohereEncoder
+from semantic_router.encoders.fastembed import FastEmbedEncoder
+from semantic_router.encoders.google import GoogleEncoder
+from semantic_router.encoders.huggingface import HuggingFaceEncoder
+from semantic_router.encoders.huggingface import HFEndpointEncoder
+from semantic_router.encoders.mistral import MistralEncoder
+from semantic_router.encoders.openai import OpenAIEncoder
+from semantic_router.encoders.tfidf import TfidfEncoder
+from semantic_router.encoders.vit import VitEncoder
+from semantic_router.encoders.zure import AzureOpenAIEncoder
+
+__all__ = [
+    "BaseEncoder",
+    "AzureOpenAIEncoder",
+    "CohereEncoder",
+    "OpenAIEncoder",
+    "BM25Encoder",
+    "TfidfEncoder",
+    "FastEmbedEncoder",
+    "HuggingFaceEncoder",
+    "HFEndpointEncoder",
+    "MistralEncoder",
+    "VitEncoder",
+    "CLIPEncoder",
+    "GoogleEncoder",
+]
---
+++ packages/semantic-router/semantic_router-0.0.35.tar.gz/semantic_router-0.0.35/semantic_router/schema.py
@@ -0,0 +1,98 @@
+from enum import Enum
+from typing import List, Optional
+
+from pydantic.v1 import BaseModel
+from pydantic.v1.dataclasses import dataclass
+
+from semantic_router.encoders import (
+    BaseEncoder,
+    CohereEncoder,
+    FastEmbedEncoder,
+    GoogleEncoder,
+    MistralEncoder,
+    OpenAIEncoder,
+)
+
+
+class EncoderType(Enum):
+    HUGGINGFACE = "huggingface"
+    FASTEMBED = "fastembed"
+    OPENAI = "openai"
+    COHERE = "cohere"
+    MISTRAL = "mistral"
+    GOOGLE = "google"
+
+
+class RouteChoice(BaseModel):
+    name: Optional[str] = None
+    function_call: Optional[dict] = None
+    similarity_score: Optional[float] = None
+
+
+@dataclass
+class Encoder:
+    type: EncoderType
+    name: Optional[str]
+    model: BaseEncoder
+
+    def __init__(self, type: str, name: Optional[str]):
+        self.type = EncoderType(type)
+        self.name = name
+        if self.type == EncoderType.HUGGINGFACE:
+            raise NotImplementedError
+        elif self.type == EncoderType.FASTEMBED:
+            self.model = FastEmbedEncoder(name=name)
+        elif self.type == EncoderType.OPENAI:
+            self.model = OpenAIEncoder(name=name)
+        elif self.type == EncoderType.COHERE:
+            self.model = CohereEncoder(name=name)
+        elif self.type == EncoderType.MISTRAL:
+            self.model = MistralEncoder(name=name)
+        elif self.type == EncoderType.GOOGLE:
+            self.model = GoogleEncoder(name=name)
+        else:
+            raise ValueError
+
+    def __call__(self, texts: List[str]) -> List[List[float]]:
+        return self.model(texts)
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+    def to_openai(self):
+        if self.role.lower() not in ["user", "assistant", "system"]:
+            raise ValueError("Role must be either 'user', 'assistant' or 'system'")
+        return {"role": self.role, "content": self.content}
+
+    def to_cohere(self):
+        return {"role": self.role, "message": self.content}
+
+    def to_llamacpp(self):
+        return {"role": self.role, "content": self.content}
+
+    def to_mistral(self):
+        return {"role": self.role, "content": self.content}
+
+    def __str__(self):
+        return f"{self.role}: {self.content}"
+
+
+class DocumentSplit(BaseModel):
+    docs: List[str]
+    is_triggered: bool = False
+    triggered_score: Optional[float] = None
+    token_count: Optional[int] = None
+    metadata: Optional[dict] = None
+
+    @property
+    def content(self) -> str:
+        return " ".join(self.docs)
+
+
+class Metric(Enum):
+    COSINE = "cosine"
+    DOTPRODUCT = "dotproduct"
+    EUCLIDEAN = "euclidean"
+    MANHATTAN = "manhattan"
