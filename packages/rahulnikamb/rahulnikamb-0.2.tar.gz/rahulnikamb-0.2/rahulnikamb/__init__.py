2818bd60166129f8dbd5285e649ef81f7b741ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:09 +0000

    Add package rahulnikamb/0.2/rahulnikamb-0.2.tar.gz

---
+++ packages/rahulnikamb/rahulnikamb-0.2.tar.gz/rahulnikamb-0.2/rahulnikamb/__init__.py
@@ -0,0 +1,6 @@
+from .main import add
+from .main import subtract
+from .main import multiply
+from .main import divide
+from .main import greet
+from .main import square
---
+++ packages/rahulnikamb/rahulnikamb-0.2.tar.gz/rahulnikamb-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rahulnikamb',
+      version='0.2',
+      description='operation & greeting library',
+      packages=['rahulnikamb'],
+      author_email='codewithrahulnikam@gmail.com',
+      zip_safe=False,
+      )
