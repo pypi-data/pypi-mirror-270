fcc63cf7c32cddd5e00bd5de1b63ca7ee6045065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:47 +0000

    Add package tjfu/3.3.0/tjfu-3.3.0-py3-none-any.whl

---
+++ packages/tjfu/tjfu-3.3.0-py3-none-any.whl/tjfu/route.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, render_template
+
+class Route:
+    def __init__(
+        self,
+        name: str,
+        url_prefix: str
+    ):
+        self._name = name
+        self._url_prefix = url_prefix
+
+        self._blueprint = Blueprint(
+            self._name,
+            __name__
+        )
+        
+        self._blueprint.route('/')(self._index)
+    
+    def _index(self, *args, **kwargs):
+        return f"Hello from: {self._name}"
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def url_prefix(self):
+        return self._url_prefix
+    
+    @property
+    def blueprint(self):
+        return self._blueprint
+    
+    def register_route(self, route):
+        self._blueprint.register_blueprint(
+            route.blueprint,
+            url_prefix=route.url_prefix
+        )
+
+class SimpleRenderTemplateRoute(Route):
+    def __init__(
+        self,
+        name: str,
+        url_prefix: str,
+        html_file: str
+    ):
+        super().__init__(name, url_prefix)
+        
+        self._blueprint.route("/")(self._index)
+        
+        self._html_file = html_file
+    
+    def _index(self):
+        return render_template(
+            self._html_file
+        )
+    
+    
