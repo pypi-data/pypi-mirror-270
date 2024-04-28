0827a8d43031ef92caf17c785d6e46014cf390f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:04 +0000

    Add package abess/0.4.8/abess-0.4.8-cp37-cp37m-win_amd64.whl

---
+++ packages/abess/abess-0.4.8-cp37-cp37m-win_amd64.whl/abess/__init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    :
+# @Author  :
+# @Site    :
+# @File    : __init__.py
+
+__version__ = "0.4.8"
+__author__ = ("Jin Zhu, Kangkang Jiang, "
+              "Junhao Huang, Yanhang Zhang, "
+              "Yanhang Zhang, Shiyun Lin, "
+              "Junxian Zhu, Xueqin Wang")
+
+from .linear import (
+    LinearRegression,
+    LogisticRegression,
+    CoxPHSurvivalAnalysis,
+    PoissonRegression,
+    MultiTaskRegression,
+    MultinomialRegression,
+    GammaRegression,
+    OrdinalRegression
+)
+from .decomposition import (SparsePCA, RobustPCA)
+from .datasets import (make_glm_data, make_multivariate_glm_data)
+
+# To be deprecated in version 0.6.0
+from .linear import (  # noqa
+    abessLm, abessLogistic, abessCox, abessPoisson,
+    abessMultigaussian, abessMultinomial, abessGamma
+)
+from .pca import (abessPCA, abessRPCA)  # noqa
+
+__all__ = [
+    # linear
+    "LinearRegression",
+    "LogisticRegression",
+    "CoxPHSurvivalAnalysis",
+    "PoissonRegression",
+    "MultiTaskRegression",
+    "MultinomialRegression",
+    "GammaRegression",
+    "OrdinalRegression",
+    # decomposition
+    "SparsePCA",
+    "RobustPCA",
+    # datasets
+    "make_glm_data",
+    "make_multivariate_glm_data"
+]
