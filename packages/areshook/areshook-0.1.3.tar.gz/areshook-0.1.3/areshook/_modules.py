2bc1b53731318091f9a7778d6f78cf95d080a410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:06:16 +0000

    Add package areshook/0.1.3/areshook-0.1.3.tar.gz

---
+++ packages/areshook/areshook-0.1.3.tar.gz/areshook-0.1.3/areshook/_modules.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class AresMessage(BaseModel):
+    device_serial: str
+    package_name: str
+    pid: int
+    session_id: str
+    script_name: str
+    message_type: str
+    message: dict
+    timestamp: int
