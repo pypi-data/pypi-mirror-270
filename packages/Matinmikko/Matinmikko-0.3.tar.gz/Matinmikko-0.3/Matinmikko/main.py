5ebeacfbba86d9e13f15c029b74d0ca3366052f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:06:16 +0000

    Add package Matinmikko/0.3/Matinmikko-0.3.tar.gz

---
+++ packages/Matinmikko/Matinmikko-0.3.tar.gz/Matinmikko-0.3/Matinmikko/main.py
@@ -0,0 +1,21 @@
+def eof(message=None):
+    """
+    A delightful message to celebrate the user's coding journey, with optional customization.
+
+    Args:
+        message (str, optional): A custom message to replace the default one. Defaults to None.
+    """
+
+    if message:
+        print(message)
+    else:
+        print(
+            " Congratulations! You've reached the end of this amazing code adventure! "
+        )
+        print(" Thank you for using our code – you're officially a coding wizard! ")
+        print(
+            " May your bugs be few, your coffee always warm, and your coding days be joyful! "
+        )
+        print(
+            " Keep coding, stay curious, and remember: You're making a positive impact! "
+        )
