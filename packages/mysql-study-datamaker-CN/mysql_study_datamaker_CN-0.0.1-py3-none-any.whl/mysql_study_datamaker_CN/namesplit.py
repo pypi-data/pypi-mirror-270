270a228a6c44ed9f362299311d48da5349282b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:05:55 +0000

    Add package mysql-study-datamaker-CN/0.0.1/mysql_study_datamaker_CN-0.0.1-py3-none-any.whl

---
+++ packages/mysql-study-datamaker-CN/mysql_study_datamaker_CN-0.0.1-py3-none-any.whl/mysql_study_datamaker_CN/namesplit.py
@@ -0,0 +1,13 @@
+# with open("source/names.txt", encoding="utf-8") as f:
+#     names = f.read().split()
+#
+# with open("source/new_names.txt", "w", encoding="utf-8") as f:
+#     f.write("\n".join(names))
+
+# with open("source/family.txt", encoding="utf-8") as f:
+#     names = f.read().split()
+#     line = [item[-1] if len(item) < 5 else item[-2:] for item in names]
+#     # print(line, len(line))
+#
+# with open("source/new_family.txt", "w", encoding="utf-8") as f:
+#     f.write("\n".join(line))
