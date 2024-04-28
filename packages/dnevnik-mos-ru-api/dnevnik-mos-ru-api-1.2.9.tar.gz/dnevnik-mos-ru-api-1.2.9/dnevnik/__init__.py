1330de57271f56a414f222d9eba2247164fa2c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:04:17 +0000

    Add package dnevnik-mos-ru-api/1.2.9/dnevnik-mos-ru-api-1.2.9.tar.gz

---
+++ packages/dnevnik-mos-ru-api/dnevnik-mos-ru-api-1.2.9.tar.gz/dnevnik-mos-ru-api-1.2.9/dnevnik/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "1.2.9"
+
+from .Authenticator import Authenticator
+from .FileAuthenticator import FileAuthenticator
+from .PredefinedAuthenticator import PredefinedAuthenticator
+from .PlayWrightAuthenticator import PlayWrightAuthenticator
+from .TOTPRequested import TOTPRequested
+from .IncorrectLoginPassword import IncorrectLoginPassword
+from .Utils import Utils
+from .DnevnikClient import DnevnikClient
