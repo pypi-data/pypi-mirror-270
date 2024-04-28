2ba55a5d9a83fda4ceac5e61a76dd6ef7370e125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:03:46 +0000

    Add package YoutubeUser/0.0.2/youtubeuser-0.0.2.tar.gz

---
+++ packages/YoutubeUser/youtubeuser-0.0.2.tar.gz/youtubeuser-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YoutubeUser",
+    version='0.0.2',
+    author='R1TGAMING',
+    author_email='rafisofyangaming1234@gmail.com',
+    description='A Lib For Search Youtube User Or Channels',
+  
+    long_description_content_type='text/markdown',
+    url='https://github.com/R1TGAMING/YoutubeUser',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
