29f5e5d3435dec24bdb804350c8904ccce0e9fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:03:49 +0000

    Add package STAIR-tools/1.1.9/STAIR-tools-1.1.9.tar.gz

---
+++ packages/STAIR-tools/STAIR-tools-1.1.9.tar.gz/STAIR-tools-1.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='STAIR-tools',  
+    packages=find_packages(),
+    package_data={
+        'STAIR-tools': ['ABAanno/ccfv3/*.nrrd', 'ABAanno/*.csv', 'ABAanno/*.pkl'],
+    },
+    version='1.1.9',
+    description='Spatial Transcriptomic Alignment, Integration, and de novo 3D Reconstruction',
+    author='yuanyuanyu',
+    license='MIT',
+    URL='https://github.com/yuyuanyuana/STAIR'
+)
