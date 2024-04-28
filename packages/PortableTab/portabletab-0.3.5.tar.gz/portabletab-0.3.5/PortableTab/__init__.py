b7a7520f5e7533d94a1f8776de92bc7d5de21160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:06:10 +0000

    Add package PortableTab/0.3.5/portabletab-0.3.5.tar.gz

---
+++ packages/PortableTab/portabletab-0.3.5.tar.gz/portabletab-0.3.5/PortableTab/__init__.py
@@ -0,0 +1,11 @@
+from .base_table import BaseTable
+from .capnp_manager import CapnpManager
+from .capnp_table import CapnpTable
+
+__version__ = '0.3.5'
+
+__all__ = [
+    "BaseTable",
+    "CapnpManager",
+    "CapnpTable",
+]
