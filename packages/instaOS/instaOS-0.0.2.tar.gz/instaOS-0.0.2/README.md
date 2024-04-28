b831c388f333bd27aaf559ef411b5aaf2cbf7581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:55 +0000

    Add package instaOS/0.0.2/instaOS-0.0.2.tar.gz

---
+++ packages/instaOS/instaOS-0.0.2.tar.gz/instaOS-0.0.2/README.md
@@ -0,0 +1,15 @@
+# instagram
+
+Under construction! Not ready for use yet! Currently experimenting and planning!
+
+Developed by Umit KOC (c) 2024
+
+## Examples of How To Use (Buggy Alpha Version)
+
+Creating A Server
+
+```python
+
+
+
+```
---
+++ packages/instaOS/instaOS-0.0.2.tar.gz/instaOS-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with codecs.open(path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Instagram followers list and following list'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="instaOS",
+    version=VERSION,
+    author="Umit KOC",
+    author_email="umitkoc.com@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['selenium', 'tqdm', 'webdriver-manager','chromedriver-autoinstaller','python-dotenv','chrome-driver'],
+    keywords=['instagram', 'followers', 'following', 'not following'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
