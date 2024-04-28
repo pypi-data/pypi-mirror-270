fc1d4a83478d0c4300e125dab92fe426f6e9ae3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:04 +0000

    Add package simplenote-local/1.0.3/simplenote_local-1.0.3.tar.gz

---
+++ packages/simplenote-local/simplenote_local-1.0.3.tar.gz/simplenote_local-1.0.3/tests/test_note.py
@@ -0,0 +1,11 @@
+def test_note_filename:
+    assert False
+
+def test_note_filename_excess:
+    assert False
+
+def test_note_unusable_filename:
+    assert False
+
+def test_note_body_with_newlines:
+    assert False
