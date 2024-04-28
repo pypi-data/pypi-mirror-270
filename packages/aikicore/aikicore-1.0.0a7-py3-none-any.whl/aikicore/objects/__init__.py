8ff46942bbcbcff3793ab60a0876882cdf578dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:03:50 +0000

    Add package aikicore/1.0.0a7/aikicore-1.0.0a7-py3-none-any.whl

---
+++ packages/aikicore/aikicore-1.0.0a7-py3-none-any.whl/aikicore/objects/__init__.py
@@ -0,0 +1,4 @@
+from .error import Error, ErrorManager, DomainError, AppError
+from .routing import MessageContext, FeatureHandler
+from .domain import AppDomainModel, AppValueObject, AppEntity
+from .container import ContainerConfiguration, Container, DynamicContainer
