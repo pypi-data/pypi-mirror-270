7d97ab4693c1ce375299d6cb98ddfb45fad51664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:06:16 +0000

    Add package PFlowC/1.2.0/PFlowC-1.2.0.tar.gz

---
+++ packages/PFlowC/PFlowC-1.2.0.tar.gz/PFlowC-1.2.0/pfc/main.py
@@ -0,0 +1,47 @@
+# _*_ codign:utf8 _*_
+"""====================================
+@Author:Sadam·Sadik
+@Email：1903249375@qq.com
+@Date：2024/4/22
+@Software: PyCharm
+@disc:
+======================================="""
+import json
+import logging
+import os
+
+import click
+
+from pfc.utils import logger
+from pfc.utils.proxy_helper.macosx import set_web_proxy, set_cmd_proxy
+
+
+@click.command(help="Command line interface for FlowPilot")
+def main():
+    config_fp = os.path.expanduser("~/.flowPilot/config.json")
+    if not os.path.isfile(config_fp):
+        click.echo("upstream config file: {} does not exist.".format(config_fp))
+        host = click.prompt("Input the proxy upstream host")
+        port = click.prompt("Input the proxy upstream port")
+        config = {"host": host, "port": port, "bypass_domains": [
+            "127.0.0.1",
+            "192.168.0.0/16",
+            "172.16.0.0/16",
+            "10.0.0.0/8"
+        ]}
+        os.makedirs(os.path.dirname(config_fp), exist_ok=True)
+        with open(config_fp, "w", encoding='utf-8') as f:
+            json.dump(config, f, ensure_ascii=False)
+    else:
+        config = json.load(open(config_fp))
+    host = config["host"]
+    port = config["port"]
+    ignores_list = config["bypass_domains"]
+    bypass_domains = list(set(ignores_list))
+    set_web_proxy(host, port, bypass_domains)
+    set_cmd_proxy(host, port, bypass_domains)
+
+
+if __name__ == '__main__':
+    logger.init("FlowPilot", console_level=logging.INFO)
+    main()
