c3c630c7eb98789967bb1a75679c7481ea750c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:04:12 +0000

    Add package amjd/1.0/amjd-1.0.tar.gz

---
+++ packages/amjd/amjd-1.0.tar.gz/amjd-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amjd',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'random',
+    ],
+    author='AMJD',
+    author_email='22445577sjad@gmail.com',
+    description='gen cc',
+    url=' ',
+)
+
