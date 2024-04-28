2c105688c62290976526f20b2706ee024cc5c5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:54 +0000

    Add package arm-text-detection/0.0.1/arm_text_detection-0.0.1.tar.gz

---
+++ packages/arm-text-detection/arm_text_detection-0.0.1.tar.gz/arm_text_detection-0.0.1/setup.py.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='arm_text_detection',
+      version='0.0',
+      description='Detection armenian text ',
+      packages=['arm_text_detection'],
+      author_email='renalshagin777@gmail.com',
+      zip_safe=False)
