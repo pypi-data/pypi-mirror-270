a433ac494dca456a384f77377b3df70fc245af92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:04:11 +0000

    Add package banks/0.3.0/banks-0.3.0.tar.gz

---
+++ packages/banks/banks-0.3.0.tar.gz/banks-0.3.0/src/banks/prompt.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: 2023-present Massimiliano Pippi <mpippi@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from typing import Optional
+
+from banks.env import env, async_enabled
+from banks.errors import AsyncError
+
+
+class Prompt:
+    def __init__(self, text: str) -> None:
+        self._template = env.from_string(text)
+
+    @classmethod
+    def from_template(cls, name: str) -> "Prompt":
+        p = cls("")
+        p._template = env.get_template(name)
+        return p
+
+    def text(self, data: Optional[dict] = None) -> str:
+        data = data or {}
+        return self._template.render(data)
+
+
+class AsyncPrompt(Prompt):
+    def __init__(self, text: str) -> None:
+        super().__init__(text)
+
+        if not async_enabled:
+            msg = "Async is not enabled. Please set the environment variable 'BANKS_ASYNC_ENABLED=on' and try again."
+            raise AsyncError(msg)
+
+    async def text(self, data: Optional[dict] = None) -> str:
+        data = data or {}
+        return await self._template.render_async(data)
