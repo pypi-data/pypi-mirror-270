26d92c011e29b2c9750f35563c5d94c4d59f1bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:04:00 +0000

    Add package QSugar/0.0.3/QSugar-0.0.3.tar.gz

---
+++ packages/QSugar/QSugar-0.0.3.tar.gz/QSugar-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QSugar',
+    version='0.0.3',
+    description='Reactive PySide framework,simple and friendly',
+    author='Niwik',
+    author_email='niwik.dev@gmail.com',
+    url='https://github.com/Niwik-Dev/QSugar',
+    license='LGPL Licence',
+    packages=find_packages(),
+    scripts=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
