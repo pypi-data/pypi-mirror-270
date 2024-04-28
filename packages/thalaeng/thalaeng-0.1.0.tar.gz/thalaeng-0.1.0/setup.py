7eeb474aa998ce29169b2e1f4b743fbaad626087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:03:57 +0000

    Add package thalaeng/0.1.0/thalaeng-0.1.0.tar.gz

---
+++ packages/thalaeng/thalaeng-0.1.0.tar.gz/thalaeng-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thalaeng',
+    version='0.1.0',
+    description='python package for showing a log on python console',
+    long_description='python package for showing a log on python console',
+    author='Sirakis Ng',
+    author_email='sirakis.ng@gmail.com',
+    url='https://github.com/chokun-sirakis/Thalaeng',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+)
