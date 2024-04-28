734ce35789759770e43b5627c05e3c966bb26397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:04:07 +0000

    Add package prisoners-dilemma-sdk/0.0.4/prisoners_dilemma_sdk-0.0.4.tar.gz

---
+++ packages/prisoners-dilemma-sdk/prisoners_dilemma_sdk-0.0.4.tar.gz/prisoners_dilemma_sdk-0.0.4/prisoners_dilemma_sdk/core.py
@@ -0,0 +1,21 @@
+import json
+from abc import ABC, abstractmethod
+
+
+class GameState:
+    def __init__(self, game_state_json):
+        decoded_json = json.loads(game_state_json)
+        self.round = decoded_json['round']
+        self.player1_history = decoded_json['player1_history']
+        self.player2_history = decoded_json['player2_history']
+
+
+class PrisonersDilemmaStrategy(ABC):
+    @abstractmethod
+    def make_decision(self, game_state: GameState) -> bool:
+        """
+        Make a decision based on the current game state.
+        :param game_state: The current game state.
+        :return: True to cooperate, False to defect.
+        """
+        pass
