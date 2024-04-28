0b5bbbcf1eda0f1534b724719813d5ae0f91d6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:04:13 +0000

    Add package praanscribe/1.0.1/praanscribe-1.0.1.tar.gz

---
+++ packages/praanscribe/praanscribe-1.0.1.tar.gz/praanscribe-1.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='praanscribe',
+    version='1.0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'praanscribe=praanscribe.script:main',
+        ],
+    },
+    install_requires=[
+        'SpeechRecognition',
+        # Add other dependencies if needed
+    ],
+    author='Ali Çağan Kaya',
+    author_email='alicagank@icloud.com',
+    description='A small application for automatically transcribing audio and creating TextGrid files to be used in Praat.',
+    url='https://github.com/alicagank/praanscribe',
+)
