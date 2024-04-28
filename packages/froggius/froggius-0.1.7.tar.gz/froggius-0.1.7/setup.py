64ae3298b308f1b99f5daaef630d0a5a21efb926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:04:19 +0000

    Add package froggius/0.1.7/froggius-0.1.7.tar.gz

---
+++ packages/froggius/froggius-0.1.7.tar.gz/froggius-0.1.7/setup.py
@@ -0,0 +1,29 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="froggius",
+    packages=find_packages(include=["froggius"]),
+    description="Froggius is a lightweight and dumb easy logging libary for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MPL-2.0",
+    version="0.1.7",
+    author="zlElo",
+    author_email="mail@zlelo.de",
+    url="https://github.com/zlElo/Froggius",
+    keywords=["logging", "logger", "easy-to-use", "log"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
