236406bade4969fcd640dd11cc432781ef87db37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:09 +0000

    Add package CVE-2021-42063/0.0.2/CVE-2021-42063-0.0.2.tar.gz

---
+++ packages/CVE-2021-42063/CVE-2021-42063-0.0.2.tar.gz/CVE-2021-42063-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A Tool to find an Easy Bounty - CVE-2021-42063'
+LONG_DESCRIPTION = 'This is a tool used by several security researchers to find CVE-2021-42063.'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="CVE-2021-42063",
+    version=VERSION,
+    author="@karthithehacker",
+    author_email="<contact@karthithehacker.com>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': [
+            'CVE-2021-42063 = cve.main:main',
+        ],
+    },
+    install_requires=['urllib3', 'requests', 'click'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
