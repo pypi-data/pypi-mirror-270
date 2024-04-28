ae391674adf7a04c78d88211d0035e38f02ed526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:04 +0000

    Add package CVE-2018-8033/0.0.1/CVE-2018-8033-0.0.1.tar.gz

---
+++ packages/CVE-2018-8033/CVE-2018-8033-0.0.1.tar.gz/CVE-2018-8033-0.0.1/cve/utils/const.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+ * CVE-2018-8033
+ * CVE-2018-8033 Bug scanner for WebPentesters and Bugbounty Hunters
+ *
+ * @Developed By Cappricio Securities <https://cappriciosec.com>
+ */
+
+
+"""
+
+
+class Data:
+    blog = 'https://blogs.cappriciosec.com/cve/141/CVE-2018-8033%20Dissecting%20an%20XXE%20Vulnerability%20in%20Apache%20OFBiz'
+    api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
+    config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
+    payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/CVE-2018-8033.xml'
+    bugname = 'XMLRPC XXE ===> LFI'
+
+
+class Colors:
+    RED = '\x1b[31;1m'
+    BLUE = '\x1b[34;1m'
+    GREEN = '\x1b[32;1m'
+    RESET = '\x1b[0m'
+    MAGENTA = '\x1b[35;1m'
