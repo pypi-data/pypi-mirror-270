434a77ef994a057d8b9eb8193ccdb9d18d51419a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:46 +0000

    Add package smp/1.0.0/smp-1.0.0-py3-none-any.whl

---
+++ packages/smp/smp-1.0.0-py3-none-any.whl/smp/user/intercreate.py
@@ -0,0 +1,53 @@
+"""The Simple Management Protocol (SMP) Intercreate Management group."""
+
+from enum import IntEnum, auto, unique
+from typing import ClassVar
+
+from smp import error, header, message
+
+
+class _IntercreateManagementGroup:
+    _GROUP_ID: ClassVar = header.UserGroupId.INTERCREATE
+
+
+class ImageUploadWriteRequest(_IntercreateManagementGroup, message.WriteRequest):
+    _COMMAND_ID = header.CommandId.Intercreate.UPLOAD
+
+    off: int
+    """The offset in the image to write to."""
+
+    data: bytes
+    """The data to write to the image."""
+
+    image: int | None = None
+    """The image to write to; required when off == 0."""
+
+    len: int | None = None
+    """The length of the data to write; required when off == 0."""
+
+    sha: bytes | None = None
+    """The SHA-256 hash of the image; optional when off == 0, else ignored."""
+
+
+class ImageUploadWriteResponse(_IntercreateManagementGroup, message.WriteResponse):
+    _COMMAND_ID = header.CommandId.Intercreate.UPLOAD
+
+    off: int
+    """The offset in the image after the request was written."""
+
+
+@unique
+class IC_MGMT_ERR(IntEnum):
+    OK = 0
+    """No error."""
+
+    INVALID_IMAGE = auto()
+    """No image matched the image provided."""
+
+
+class ErrorV0(error.ErrorV0[IC_MGMT_ERR]):
+    _GROUP_ID = header.UserGroupId.INTERCREATE
+
+
+class ErrorV1(error.ErrorV1[IC_MGMT_ERR]):
+    _GROUP_ID = header.UserGroupId.INTERCREATE
