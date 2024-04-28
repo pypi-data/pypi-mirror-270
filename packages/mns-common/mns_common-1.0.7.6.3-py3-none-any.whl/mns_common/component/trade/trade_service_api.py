8436ec8e874056acce31bf06a41c83702613c8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:05:57 +0000

    Add package mns-common/1.0.7.6.3/mns_common-1.0.7.6.3-py3-none-any.whl

---
+++ packages/mns-common/mns_common-1.0.7.6.3-py3-none-any.whl/mns_common/component/trade/trade_service_api.py
@@ -0,0 +1,61 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 14
+project_path = file_path[0:end]
+sys.path.append(project_path)
+from loguru import logger
+import easytrader
+from easytrader import grid_strategies
+
+user = easytrader.use('ths')
+user.connect(r'D:\Program Files\ths\xiadan.exe')
+user.grid_strategy = grid_strategies.Xls
+user.grid_strategy_instance.tmp_folder = 'C:\\custom_folder'
+
+
+# 下单
+def order_buy(symbol, buy_price, buy_volume):
+    logger.warning("买入代码:{},买入价格:{},买入数量:{}", symbol, buy_price, buy_volume)
+    user.enable_type_keys_for_editor()
+    buy_result = user.buy(symbol, buy_price, buy_volume)
+
+    return buy_result
+
+
+# 自动一键打新
+def auto_ipo_buy():
+    user.auto_ipo()
+
+
+# 获取持仓
+def get_position():
+    return user.position
+
+
+# 卖出
+def order_sell(symbol, sell_price, sell_volume):
+    user.enable_type_keys_for_editor()
+    sell_result = user.sell(symbol, sell_price, sell_volume)
+    return sell_result
+
+
+# 取消
+def order_cancel(entrust_no):
+    user.enable_type_keys_for_editor()
+    cancel_result = user.cancel_entrust(entrust_no)
+    return cancel_result
+
+
+if __name__ == '__main__':
+    # while True:
+    position_df = get_position()
+    print(position_df)
+    # buy_result_df = order_buy('300085', '10.28', 1000)
+    # print(buy_result_df)
+    # auto_ipo_buy()
+    # cancel_result_df = order_cancel('0111735004')
+    # print(cancel_result_df)
+    # sell_result_df = order_sell('300917', '24.88', 200)
+    # print(sell_result_df)
