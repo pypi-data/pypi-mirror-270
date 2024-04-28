5dbf56e8925a43b4abb6c76f76c9b116423b92a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:03:46 +0000

    Add package faster-whisper-api-proxy/0.0.1/faster_whisper_api_proxy-0.0.1.tar.gz

---
+++ packages/faster-whisper-api-proxy/faster_whisper_api_proxy-0.0.1.tar.gz/faster_whisper_api_proxy-0.0.1/README.md
@@ -0,0 +1,2 @@
+# faster_whisper_api_proxy
+a python module that is a drop in replacement for faster_whisper but calls a remote faster_whisper implementation via API
---
+++ packages/faster-whisper-api-proxy/faster_whisper_api_proxy-0.0.1.tar.gz/faster_whisper_api_proxy-0.0.1/setup.py
@@ -0,0 +1,39 @@
+# setup.py
+import pathlib
+from setuptools import setup, find_packages
+import pkg_resources
+import os
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+def read_version(fname="faster_whisper_api_proxy/version.py"):
+    exec(compile(open(fname, encoding="utf-8").read(), fname, "exec"))
+    return locals()["__version__"]
+
+
+setup(
+    name='faster_whisper_api_proxy',  # Name of your module
+    description='a python module that is a drop in replacement for faster_whisper but calls a remote faster_whisper implementation via API',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    version=read_version(),
+    url='https://github.com/rkilchmn/faster_whisper_api_proxy',
+    author='Roger Kilchenmann',
+    author_email='roger@kilchenmann.net',
+    keywords=['AI','faster_whisper','transcribe','natural language','speech recognition','api','remote'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
+)
