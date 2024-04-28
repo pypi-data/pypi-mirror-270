4677ade715563f936089247355f7519229f74d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:06:09 +0000

    Add package clovers-apscheduler/0.1.4/clovers_apscheduler-0.1.4.tar.gz

---
+++ packages/clovers-apscheduler/clovers_apscheduler-0.1.4.tar.gz/clovers_apscheduler-0.1.4/clovers_apscheduler/__init__.py
@@ -0,0 +1,28 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from clovers.core.config import config as clovers_config
+from clovers.core.plugin import Plugin
+from .config import Config
+
+
+config_key = __package__
+config_data = Config.model_validate(clovers_config.get(config_key, {}))
+clovers_config[config_key] = config_data.model_dump()
+
+plugin = Plugin()
+scheduler = AsyncIOScheduler()
+scheduler.configure(**config_data.scheduler_config)
+
+
+@plugin.startup
+async def _():
+    if not scheduler.running:
+        scheduler.start()
+
+
+@plugin.shutdown
+async def _():
+    if scheduler.running:
+        scheduler.shutdown()
+
+
+__plugin__ = plugin
