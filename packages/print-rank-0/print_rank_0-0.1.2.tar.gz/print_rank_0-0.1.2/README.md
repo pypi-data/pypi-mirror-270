9a6fe55f2a9a86c4ee6cbfc73f938b4b513e3048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:56 +0000

    Add package print-rank-0/0.1.2/print_rank_0-0.1.2.tar.gz

---
+++ packages/print-rank-0/print_rank_0-0.1.2.tar.gz/print_rank_0-0.1.2/README.md
@@ -0,0 +1,5 @@
+# print_rank_0
+
+`print_rank_0`: Print only on rank 0.
+
+`print_rank_last`: Print only on last rank.
---
+++ packages/print-rank-0/print_rank_0-0.1.2.tar.gz/print_rank_0-0.1.2/print_rank_0/__init__.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def print_rank_0(*args, **kwargs):
+    """Print only on rank 0."""
+    import torch
+    if torch.distributed.get_rank() == 0:
+        print(*args, **kwargs)
+
+
+def print_rank_last(*args, **kwargs):
+    """Print only on last rank."""
+    import torch
+    last_rank = torch.distributed.get_world_size() - 1
+    if torch.distributed.get_rank() == last_rank:
+        print(*args, **kwargs)
+
+
+class CallableModule(sys.modules[__name__].__class__):
+    def __call__(self, *args, **kwargs):
+        return self.print_rank_0(*args, **kwargs)
+
+
+sys.modules[__name__].__class__ = CallableModule
