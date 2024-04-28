d3ff3b1973a5e485f5b13c0cfce94379a4b47111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:56 +0000

    Add package gpti/2.0/gpti-2.0.tar.gz

---
+++ packages/gpti/gpti-2.0.tar.gz/gpti-2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+_long_description = ''
+try:
+    with open('README.md', 'r', encoding='utf-8') as f:
+        _long_description = f.read()
+except Exception as e:
+    _long_description = ''
+
+setup(
+    name='gpti',
+    version='2.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='yandricr',
+    author_email='yandribret@gmail.com',
+    description='This package simplifies your interaction with various GPT models, removing the need for tokens or other methods to access GPT',
+    long_description=_long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/yandricr/gpti-py/',
+    project_urls={
+        'Documentation': 'https://nexra.aryahcr.cc/',
+        'Funding': 'https://ko-fi.com/yandricr',
+        'Source': 'https://github.com/yandricr/gpti-py/'
+    },
+    keywords='gpt gpt-3 gpt-3.5 gpt-4 gpti gpt-free ai generate-image prodia bing chat stream dalle dalle-2 llama-2 stable-diffusion pixart EMI render3d pixel-art playground animagine-xl',
+    license='MIT',
+    package_data={'': ['LICENSE']},
+    include_package_data=True
+)
