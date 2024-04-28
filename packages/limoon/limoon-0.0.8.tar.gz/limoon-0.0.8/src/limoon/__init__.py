bb492427aa6cf210627602092fb14b8c7900ccbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 15:05:52 +0000

    Add package limoon/0.0.8/limoon-0.0.8.tar.gz

---
+++ packages/limoon/limoon-0.0.8.tar.gz/limoon-0.0.8/src/limoon/__init__.py
@@ -0,0 +1,21 @@
+from limoon.constant import BASE_URL
+from limoon.core import *
+from limoon.model import *
+
+__all__ = (
+    get_topic,
+    get_entry,
+    get_author,
+    get_author_topic,
+    get_author_rank,
+    get_agenda,
+    get_debe,
+    search_topic,
+    Author,
+    Entry,
+    Rank,
+    Topic,
+    Badge,
+    BASE_URL,
+)
+__version__ = "0.0.8"
