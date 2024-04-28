87e3b7172dc82be912bf02fb7b647be4a132d087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:03:24 +0000

    Add package backpipe/0.5.0/backpipe-0.5.0.tar.gz

---
+++ packages/backpipe/backpipe-0.5.0.tar.gz/backpipe-0.5.0/backpipe/__init__.py
@@ -0,0 +1,26 @@
+"""
+--- Backpipe ---
+
+Backends simplified.
+Backpipe is an API creation framework.
+
+Backpipe by Simoso68 is licensed under the GNU GPL v3.
+
+Check it out under:
+
+https://github.com/Simoso68/backpipe
+https://pypi.org/project/backpipe
+"""
+
+# Request.params will be renamed to Request.query at a later point in time.
+
+from .app import BackPipe
+from .rq import Request
+from .config import config
+from colorama import init as __init
+
+import backpipe.tools as tools
+
+__init()
+
+__version__ = "0.5.0"
