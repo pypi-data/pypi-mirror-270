39ac482cafd6747d4d26a4f3ad516eb82e448c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:03 +0000

    Add package secretscraper/1.3.3/secretscraper-1.3.3.tar.gz

---
+++ packages/secretscraper/secretscraper-1.3.3.tar.gz/secretscraper-1.3.3/src/secretscraper/util.py
@@ -0,0 +1,50 @@
+"""Common utility functions."""
+
+from collections import namedtuple
+
+from dynaconf import LazySettings
+
+from .exception import SecretScraperException
+
+Range = namedtuple("Range", ["start", "end"])
+
+
+def read_rules_from_setting(settings: LazySettings) -> dict[str, str]:
+    """Read rules from settings
+
+    :param settings: Dynaconf settings
+    :return dict[str, str]: key for rule name and value for regex literal
+    """
+    rules_dict = dict()
+    try:
+        rules = settings.RULES
+        for rule in rules:
+            name = rule.get("name")
+            regex = rule.get("regex")
+            loaded = rule.get("loaded")
+            if loaded is True:
+                rules_dict[name] = regex
+    except Exception as e:
+        raise SecretScraperException(
+            f"Exception occur when reading rules from setting: {e}"
+        ) from e
+    return rules_dict
+
+
+def is_static_resource(path: str) -> bool:
+    """Check whether a path is a static resource"""
+    exts = ['.png', '.jpg', '.jpeg', '.gif', '.css', '.ico']
+    for ext in exts:
+        if path.endswith(ext):
+            return True
+    return False
+
+
+def to_host_port(netloc: str) -> tuple[str, str]:
+    """Convert netloc to host and port"""
+    r = netloc.split(":")
+    if len(r) == 1:
+        return r[0], ""
+    if len(r) == 2:
+        return r[0].strip(), r[1].strip()
+    return '', ''
