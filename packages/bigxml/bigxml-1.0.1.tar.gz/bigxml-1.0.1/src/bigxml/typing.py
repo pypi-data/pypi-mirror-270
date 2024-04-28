f021032cca98c9f15dc6973cb4a2f0855665c813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:58 +0000

    Add package bigxml/1.0.1/bigxml-1.0.1.tar.gz

---
+++ packages/bigxml/bigxml-1.0.1.tar.gz/bigxml-1.0.1/src/bigxml/typing.py
@@ -0,0 +1,43 @@
+import sys
+from typing import (
+    Any,
+    Callable,
+    Iterable,
+    Iterator,
+    Optional,
+    Protocol,
+    Type,
+    TypeVar,
+    Union,
+)
+
+if sys.version_info < (3, 10):  # pragma: no cover
+    from typing_extensions import ParamSpec
+else:  # pragma: no cover
+    from typing import ParamSpec
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
+U = TypeVar("U")
+F = TypeVar("F", bound=Callable[..., Any])
+K = TypeVar("K", bound=Type[Any])
+
+T_co = TypeVar("T_co", covariant=True)
+
+
+class SupportsRead(Protocol[T_co]):
+    def read(self, size: Optional[int] = None) -> T_co: ...  # pragma: no cover
+
+
+Streamable = Union[SupportsRead[bytes], bytes, Iterable["Streamable"]]
+
+
+class ClassHandlerWithCustomWrapper0(Protocol[T_co]):
+    def xml_handler(self) -> Optional[Iterable[T_co]]: ...  # pragma: no cover
+
+
+class ClassHandlerWithCustomWrapper1(Protocol[T_co]):
+    def xml_handler(
+        self, items: Iterator[Any]
+    ) -> Optional[Iterable[T_co]]: ...  # pragma: no cover
