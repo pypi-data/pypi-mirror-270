43f853dc1e14c051810743203fc3ddaff67c38b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:04:15 +0000

    Add package avalips/0.1/avalips-0.1.tar.gz

---
+++ packages/avalips/avalips-0.1.tar.gz/avalips-0.1/README.md
@@ -0,0 +1,3 @@
+# avalips 
+<h2>A smarter migration accelerator lib</h2>
+<h2>Author: jose.iram@avanade.com</h2>
---
+++ packages/avalips/avalips-0.1.tar.gz/avalips-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'avalips',
+  packages = ['avalips'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A smarter migration accelerator lib',
+  author = 'Liran BH',
+  author_email = 'jose.iram@avanade.com',
+  url = 'https://github.com/joseiram-avd/workspace.git',
+  download_url = 'https://github.com/joseiram-avd/workspace/tarball/0.1',
+  keywords = ['lips', 'hana', 'databricks', 'lakehouse', 'avanade'], 
+  classifiers = [],
+)
