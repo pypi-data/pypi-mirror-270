44fac2eb926749a94dd31f0f558cdccae22fcf44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 04:03:57 +0000

    Add package nonebot-plugin-simulator-xiuxian/1.5.32/nonebot_plugin_simulator_xiuxian-1.5.32-py3-none-any.whl

---
+++ packages/nonebot-plugin-simulator-xiuxian/nonebot_plugin_simulator_xiuxian-1.5.32-py3-none-any.whl/nonebot_plugin_simulator_xiuxian/__init__.py
@@ -0,0 +1,16 @@
+from nonebot import require
+from nonebot.adapters.onebot.v11 import (
+    Bot,
+    GroupMessageEvent
+)
+require("xiuxian")
+require("xiuxian_boss")
+require("xiuxian_bank")
+require("xiuxian_sect")
+require("xiuxian_info")
+require("xiuxian_buff")
+require("xiuxian_back")
+require("xiuxian_rift")
+require("xiuxian_mixelixir")
+require("xiuxian_work")
+require("xiuxian_base")
