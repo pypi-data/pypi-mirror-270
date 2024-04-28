2c11b3d49a1f2dd99d2c929e7e2d2b2565082659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 14:04:01 +0000

    Add package rahulnikamalpha/0.11/rahulnikamalpha-0.11.tar.gz

---
+++ packages/rahulnikamalpha/rahulnikamalpha-0.11.tar.gz/rahulnikamalpha-0.11/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='rahulnikamalpha',
+      version='0.11',
+      description='operation & greeting library',
+      packages=['rahulnikamalpha'],
+      author_email='codewithrahulnikam@gmail.com',
+      zip_safe=False)
