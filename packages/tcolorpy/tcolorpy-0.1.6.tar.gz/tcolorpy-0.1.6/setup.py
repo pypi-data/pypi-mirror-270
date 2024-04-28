e3b2efb2b6caf0ac49e0aa6c7db4e12e071f1e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-28 03:03:54 +0000

    Add package tcolorpy/0.1.6/tcolorpy-0.1.6.tar.gz

---
+++ packages/tcolorpy/tcolorpy-0.1.6.tar.gz/tcolorpy-0.1.6/setup.py
@@ -0,0 +1,75 @@
+import os.path
+from typing import Dict, Type
+
+import setuptools
+
+
+MODULE_NAME = "tcolorpy"
+REPOSITORY_URL = f"https://github.com/thombashi/{MODULE_NAME:s}"
+REQUIREMENT_DIR = "requirements"
+
+pkg_info: Dict[str, str] = {}
+
+
+def get_release_command_class() -> Dict[str, Type[setuptools.Command]]:
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME, "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with open("README.rst", encoding="utf8") as f:
+    LONG_DESCRIPTION = f.read()
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    TESTS_REQUIRES = [line.strip() for line in f if line.strip()]
+
+setuptools.setup(
+    name=MODULE_NAME,
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description="tcolopy is a Python library to apply true color for terminal text.",
+    include_package_data=True,
+    keywords=["ANSI escape", "terminal color", "truecolor"],
+    license=pkg_info["__license__"],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(exclude=["test*"]),
+    package_data={MODULE_NAME: ["py.typed"]},
+    project_urls={
+        "Changlog": f"{REPOSITORY_URL:s}/blob/master/CHANGELOG.md",
+        "Source": REPOSITORY_URL,
+        "Tracker": f"{REPOSITORY_URL:s}/issues",
+    },
+    python_requires=">=3.7",
+    extras_require={"test": TESTS_REQUIRES},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Terminals",
+        "Topic :: Text Processing",
+        "Typing :: Typed",
+    ],
+    zip_safe=False,
+    cmdclass=get_release_command_class(),
+)
