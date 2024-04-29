31927a5d43bb5486a905c67f8e2ae62f937bd771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:58 +0000

    Add package OZI/1.2.1/OZI-1.2.1.tar.gz

---
+++ packages/OZI/OZI-1.2.1.tar.gz/OZI-1.2.1/ozi/__init__.py
@@ -0,0 +1,10 @@
+# ozi/__init__.py
+# Part of the OZI Project, under the Apache License v2.0 with LLVM Exceptions.
+# See LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""Core OZI packaging management plane module."""
+from .spec import current_version
+
+__version__ = current_version()
+__author__ = 'Eden Rose Duff MSc'
+__all__ = ('__version__', '__author__', '__doc__')
