f28b4d951e2e64987a0b551f4fce03badeb0ab60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:45 +0000

    Add package pydra-mrtrix3/3.0.3a0/pydra_mrtrix3-3.0.3a0-py3-none-any.whl

---
+++ packages/pydra-mrtrix3/pydra_mrtrix3-3.0.3a0-py3-none-any.whl/pydra/tasks/mrtrix3/v3_0/tests/test_meshconvert.py
@@ -0,0 +1,20 @@
+# Auto-generated test for meshconvert
+
+from fileformats.generic import File, Directory, FsObject  # noqa
+from fileformats.medimage import Nifti1  # noqa
+from fileformats.medimage_mrtrix3 import ImageFormat, ImageIn, Tracks  # noqa
+from pydra.tasks.mrtrix3.v3_0 import meshconvert
+
+
+def test_meshconvert(tmp_path, cli_parse_only):
+
+    task = meshconvert(
+        in_file=File.sample(),
+        out_file=File.sample(),
+        binary=True,
+        transform=tuple(["a-string", "a-string"]),
+        debug=True,
+        force=True,
+    )
+    result = task(plugin="serial")
+    assert not result.errored
---
+++ packages/pydra-mrtrix3/pydra_mrtrix3-3.0.3a0-py3-none-any.whl/pydra/tasks/mrtrix3/v3_0/tests/test_meshfilter.py
@@ -0,0 +1,21 @@
+# Auto-generated test for meshfilter
+
+from fileformats.generic import File, Directory, FsObject  # noqa
+from fileformats.medimage import Nifti1  # noqa
+from fileformats.medimage_mrtrix3 import ImageFormat, ImageIn, Tracks  # noqa
+from pydra.tasks.mrtrix3.v3_0 import meshfilter
+
+
+def test_meshfilter(tmp_path, cli_parse_only):
+
+    task = meshfilter(
+        in_file=File.sample(),
+        filter="smooth",
+        out_file=File.sample(),
+        smooth_spatial=1.0,
+        smooth_influence=1.0,
+        debug=True,
+        force=True,
+    )
+    result = task(plugin="serial")
+    assert not result.errored
