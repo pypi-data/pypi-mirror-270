1b51d59183e293bf7c3aceae693bd241e795e8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:03 +0000

    Add package paibox/1.0.0/paibox-1.0.0-py3-none-any.whl

---
+++ packages/paibox/paibox-1.0.0-py3-none-any.whl/paibox/network.py
@@ -0,0 +1,321 @@
+import warnings
+from typing import Callable, List, Optional, Tuple, Type, Union
+
+import numpy as np
+from typing_extensions import TypeAlias
+
+from .base import DynamicSys, NeuDyn
+from .collector import Collector
+from .exceptions import PAIBoxWarning, RegisterError
+from .mixin import Container
+from .neuron import Neuron
+from .node import NodeDict
+from .projection import InputProj, Projection
+from .synapses.base import RIGISTER_MASTER_KEY_FORMAT, SynSys
+
+__all__ = ["DynSysGroup", "Network"]
+
+ComponentsType: TypeAlias = Union[InputProj, Neuron, SynSys]
+
+
+class DynSysGroup(DynamicSys, Container):
+
+    def __init__(
+        self,
+        *components_as_tuple,
+        component_type: Type = DynamicSys,
+        name: Optional[str] = None,
+        **components_as_dict,
+    ) -> None:
+        super().__init__(name)
+        self.children = NodeDict(
+            self.elem_format(component_type, *components_as_tuple, **components_as_dict)
+        )
+
+    def update(self, **kwargs) -> None:
+        """Find nodes of the network recursively."""
+        nodes = (
+            self.nodes(include_self=False, find_recursive=True)
+            .subset(DynamicSys)
+            .unique()
+        )
+
+        for node in nodes.subset(Projection).values():
+            node(**kwargs)
+
+        for node in nodes.subset(SynSys).values():
+            node()
+
+        for node in nodes.subset(NeuDyn).values():
+            node()
+
+    def reset_state(self) -> None:
+        nodes = (
+            self.nodes(include_self=False, find_recursive=True)
+            .subset(DynamicSys)
+            .unique()
+        )
+
+        for node in nodes.subset(Projection).values():
+            node.reset_state()
+
+        for node in nodes.subset(SynSys).values():
+            node.reset_state()
+
+        for node in nodes.subset(NeuDyn).values():
+            node.reset_state()
+
+    def __call__(self, **kwargs) -> None:
+        return self.update(**kwargs)
+
+    def add_components(self, *implicit: DynamicSys, **explicit: DynamicSys) -> None:
+        """Add new components. When a component is passed in explicitly, its tag name can   \
+            be specified. Otherwise `.name` will be used.
+
+        NOTE: After instantiated the components outside the `DynSysGroup`, you should call  \
+            `add_components()` to actually add the new components to itself.
+        """
+        for comp in implicit:
+            setattr(self, comp.name, comp)
+
+        for tag, comp in explicit.items():
+            setattr(self, tag, comp)
+
+    def disconnect_syn(
+        self, target_syn: SynSys, exclude_source: bool = False
+    ) -> SynSys:
+        """Disconnect a synapse in the nwtwork.
+
+        Args:
+            - target_syn: target synapse.
+            - exclude_source: whether to disconnect the source. If so, remove the synapse   \
+                from the network
+
+        Returns: the disconnected synapse.
+        """
+        ret = target_syn.dest.unregister_master(
+            RIGISTER_MASTER_KEY_FORMAT.format(target_syn.name)
+        )
+        if ret is not target_syn:
+            raise RegisterError("unregister failed.")
+
+        if not exclude_source:
+            self._remove_component(target_syn)
+
+        return target_syn
+
+    def disconnect_neuron_from(
+        self, neuron_a: Neuron, neuron_b: Neuron
+    ) -> List[SynSys]:
+        """Disconnect synapses between `Neuron` A & B and remove the synapses from the network.
+
+        Args:
+            - neuron_a: target neuron A.
+            - neuron_b: target neuron B.
+
+        Returns: the disconnected synapses in list.
+        """
+        return self._disconn_neuron(
+            neuron_a,
+            lambda syn: syn.source is neuron_a and syn.dest is neuron_b,
+            neuron_b,
+            remove_syn=True,
+        )
+
+    # Not sure about specific needs
+    # def diconnect_neuron_succ(self, neuron: Neuron) -> List[SynSys]:
+    #     """Disconnect successor synapses of `neuron`.
+
+    #     Args:
+    #         - neuron: target neuron.
+    #         - remove: whether to remove the original synapses from the network.
+    #         - new_source: only valid when `remove` is false.
+
+    #     Returns: the disconnected synapses.
+    #     """
+    #     return self._disconn_neuron(
+    #         neuron, lambda syn: syn.source is neuron, remove_syn=True
+    #     )
+
+    # def replace_neuron_succ(self, neuron: Neuron, new_source: Neuron) -> List[SynSys]:
+    #     """Replace the source of successor synapses of `neuron` with new one."""
+    #     disconn_syns = self._disconn_neuron(
+    #         neuron, lambda syn: syn.source is neuron, remove_syn=False
+    #     )
+
+    #     for syn in disconn_syns:
+    #         syn.source = new_source
+
+    #     return disconn_syns
+
+    # def replace_neuron_pred(self, neuron: Neuron, new_source: Neuron) -> List[SynSys]:
+    #     """Replace the destination of predecessor synapses of `neuron` with new one.
+
+    #     Args:
+    #         - neuron: target neuron.
+    #         - remove: whether to remove the original synapses from the network.
+
+    #     Returns: the disconnected synapses.
+    #     """
+    #     disconn_syns = self._disconn_neuron(
+    #         neuron, lambda syn: syn.dest is neuron, remove_syn=False
+    #     )
+
+    #     for syn in disconn_syns:
+    #         syn.dest = new_source
+
+    #     return disconn_syns
+
+    def insert_between_neuron(
+        self,
+        neuron_a: Neuron,
+        neuron_b: Neuron,
+        cpn_to_insert: Tuple[ComponentsType, ...],
+        replace: bool = True,
+    ) -> List[SynSys]:
+        """Insert new components between `Neuron` A & B.
+
+        Args:
+            - neuron_a: target neuron A.
+            - neuron_b: target neuron B.
+            - cpn_to_insert: components to insert between `neuron_a` & `neuron_b`.
+            - replace: whether to disconnect the original synapses. Default is `True`.
+
+        Returns: the disconnected synapses in list.
+        """
+        if replace:
+            removed_syn = self.disconnect_neuron_from(neuron_a, neuron_b)
+        else:
+            removed_syn = []
+
+        self.add_components(*cpn_to_insert)
+
+        return removed_syn
+
+    def _remove_component(self, remove: DynamicSys) -> None:
+        """Remove a component in the network."""
+        for tag, obj in self.__dict__.items():
+            if obj is remove:
+                delattr(self, tag)
+                break
+
+        return None
+
+    def _disconn_neuron(
+        self,
+        neuron_a: Neuron,
+        condition: Callable[[SynSys], bool],
+        neuron_b: Optional[Neuron] = None,
+        remove_syn: bool = True,
+    ) -> List[SynSys]:
+        nodes = (
+            self.nodes(include_self=False, find_recursive=True)
+            .subset(DynamicSys)
+            .unique()
+        )
+
+        if neuron_b is None:
+            self._assert_neuron(nodes, neuron_a)
+        else:
+            self._assert_neuron(nodes, neuron_a, neuron_b)
+
+        target_syns = self._find_syn_to_disconn(nodes, condition)
+
+        if target_syns:
+            for syn in target_syns:
+                self._disconn_syn(syn)
+
+                # The disconnected synapses will not effect the simulation, but will
+                # effect the placement in the backend.
+                # If the disconnected synapses aren't removed from the network, do cleaning
+                # before the compilation in the backend.
+                # TODO Add a pre-processing step before the compilation.
+                if remove_syn:
+                    self._remove_component(syn)
+
+            return target_syns
+        else:
+            warnings.warn("there is no synapses to disconnect.", PAIBoxWarning)
+            return []
+
+    @staticmethod
+    def _find_syn_to_disconn(
+        nodes: Collector, condition: Callable[[SynSys], bool]
+    ) -> List[SynSys]:
+        syns = []
+
+        for syn in nodes.subset(SynSys).values():
+            if condition(syn):
+                syns.append(syn)
+
+        return syns
+
+    @staticmethod
+    def _disconn_syn(target_syn: SynSys) -> None:
+        ret = target_syn.dest.unregister_master(
+            RIGISTER_MASTER_KEY_FORMAT.format(target_syn.name)
+        )
+        if ret is not target_syn:
+            raise RegisterError("unregister failed.")
+
+    @staticmethod
+    def _disconn_succ_syn(target_syn: SynSys) -> None:
+        ret = target_syn.dest.unregister_master(
+            RIGISTER_MASTER_KEY_FORMAT.format(target_syn.name)
+        )
+        if ret is not target_syn:
+            raise RegisterError("unregister failed.")
+
+    @staticmethod
+    def _assert_neuron(nodes: Collector, *neurons: Neuron) -> None:
+        neu_dyns = nodes.subset(Neuron)
+
+        if any(neuron not in neu_dyns.values() for neuron in neurons):
+            raise ValueError("not all neurons found in the network.")
+
+
+Network: TypeAlias = DynSysGroup
+
+
+class Sequential(DynamicSys, Container):
+    def __init__(
+        self,
+        *components,
+        name: Optional[str] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(name)
+        self.children = NodeDict(self.elem_format(DynamicSys, *components))
+
+    def update(self, x: np.ndarray) -> np.ndarray:
+        for child in self.children.values():
+            x = child(x)
+
+        return x
+
+    def reset_state(self) -> None:
+        for child in self.children.values():
+            child.reset_state()
+
+    def __getitem__(self, item: Union[str, int, slice]):
+        if isinstance(item, str):
+            if item in self.children:
+                return self.children[item]
+            else:
+                raise KeyError(f"key '{item}' not found.")
+
+        if isinstance(item, int):
+            if item > len(self):
+                raise IndexError(f"index out of range {item}.")
+
+            return tuple(self.children.values())[item]
+
+        if isinstance(item, slice):
+            return Sequential(**dict(tuple(self.children.items())[item]))
+
+        raise TypeError(
+            f"expected type str, int or slice, but got {item}, type {type(item)}."
+        )
+
+    def __len__(self) -> int:
+        return len(self.children)
---
+++ packages/paibox/paibox-1.0.0-py3-none-any.whl/paibox/synapses/synapses.py
@@ -0,0 +1,226 @@
+import warnings
+from typing import Optional, Union
+
+import numpy as np
+
+from paibox.base import NeuDyn
+from paibox.neuron import Neuron
+from paibox.projection import InputProj
+from paibox.types import DataArrayType
+
+from .base import (
+    Conv1dSyn,
+    Conv2dSyn,
+    ConvTranspose1dSyn,
+    ConvTranspose2dSyn,
+    FullConnSyn,
+)
+from .conv_utils import _KOrder3d, _KOrder4d, _pair, _single, _Size1Type, _Size2Type
+from .transforms import GeneralConnType as GConnType
+
+__all__ = ["FullConn", "Conv1d", "Conv2d", "ConvTranspose1d", "ConvTranspose2d"]
+
+
+class FullConn(FullConnSyn):
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: NeuDyn,
+        weights: DataArrayType = 1,
+        *,
+        conn_type: GConnType = GConnType.MatConn,
+        name: Optional[str] = None,
+    ) -> None:
+        """Full-connected synapses.
+
+        Args:
+            - source: source neuron.
+            - dest: destination neuron.
+            - weights: weights of the synapses. It can be a scalar or `np.ndarray`.
+            - conn_type: the type of connection.
+            - name: name of the full-connected synapses. Optional.
+        """
+        super().__init__(source, dest, weights, conn_type, name)
+
+
+class NoDecay(FullConn):
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: NeuDyn,
+        weights: DataArrayType = 1,
+        *,
+        conn_type: GConnType = GConnType.MatConn,
+        name: Optional[str] = None,
+    ) -> None:
+        warnings.warn(
+            "'NoDecay' class will be deprecated in future versions. Use 'FullConn' instead.",
+            DeprecationWarning,
+        )
+
+        super().__init__(source, dest, weights, conn_type=conn_type, name=name)
+
+
+class Conv1d(Conv1dSyn):
+    def __init__(
+        self,
+        source: Union[Neuron, InputProj],
+        dest: Neuron,
+        kernel: np.ndarray,
+        *,
+        stride: _Size1Type = 1,
+        padding: _Size1Type = 0,
+        kernel_order: _KOrder3d = "OIL",
+        name: Optional[str] = None,
+    ) -> None:
+        """1d convolution synapses in fully-unrolled format.
+
+        Args:
+            - source: source neuron. The dimensions need to be expressed explicitly as (C,L).
+            - dest: destination neuron.
+            - kernel: convolution kernel. Its dimension order is either (O,I,L) or (I,O,L), depending on the    \
+                argument `kernel_order`.
+            - stride: the step size of the kernel sliding. It can be a scalar or an integer.
+            - padding: the amount of zero-padding applied to the input. It can be a scalar or an integer.
+            - kernel_order: dimension order of kernel, (O,I,L) or (I,O,L). (O,I,L) stands for (output channels, \
+                input channels, length).
+            - name: name of the 1d convolution. Optional.
+
+        NOTE: See https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html#torch.nn.Conv1d for details.
+        """
+        if kernel_order not in ("OIL", "IOL"):
+            raise ValueError(f"kernel order must be 'OIL' or 'IOL'.")
+
+        super().__init__(
+            source, dest, kernel, _single(stride), _single(padding), kernel_order, name
+        )
+
+
+class Conv2d(Conv2dSyn):
+    def __init__(
+        self,
+        source: Union[Neuron, InputProj],
+        dest: Neuron,
+        kernel: np.ndarray,
+        *,
+        stride: _Size2Type = 1,
+        padding: _Size2Type = 0,
+        kernel_order: _KOrder4d = "OIHW",
+        name: Optional[str] = None,
+    ) -> None:
+        """2d convolution synapses in fully-unrolled format.
+
+        Args:
+            - source: source neuron. The dimensions need to be expressed explicitly as (C,H,W).
+            - dest: destination neuron.
+            - kernel: convolution kernel. Its dimension order is either (O,I,H,W) or (I,O,H,W), depending on the\
+                argument `kernel_order`.
+            - stride: the step size of the kernel sliding. It can be a scalar or a tuple of 2 integers.
+            - padding: the amount of zero-padding applied to the input. It can be a scalar or a tuple of 2 integers.
+            - kernel_order: dimension order of kernel, (O,I,H,W) or (I,O,H,W). (O,I,H,W) stands for (output     \
+                channels, input channels, height, width).
+            - name: name of the 2d convolution. Optional.
+
+        NOTE: See https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html#torch.nn.Conv2d for details.
+        """
+        if kernel_order not in ("OIHW", "IOHW"):
+            raise ValueError(f"kernel order must be 'OIHW' or 'IOHW'.")
+
+        super().__init__(
+            source, dest, kernel, _pair(stride), _pair(padding), kernel_order, name
+        )
+
+
+class ConvTranspose1d(ConvTranspose1dSyn):
+    def __init__(
+        self,
+        source: Union[Neuron, InputProj],
+        dest: Neuron,
+        kernel: np.ndarray,
+        *,
+        stride: _Size1Type = 1,
+        padding: _Size1Type = 0,
+        output_padding: _Size1Type = 0,
+        kernel_order: _KOrder3d = "OIL",
+        name: Optional[str] = None,
+    ) -> None:
+        """1d transposed convolution synapses in fully-unrolled format.
+
+        Args:
+            - source: source neuron. The dimensions need to be expressed explicitly as (C,L).
+            - dest: destination neuron.
+            - kernel: convolution kernel. Its dimension order is either (O,I,L) or (I,O,L), depending on the    \
+                argument `kernel_order`.
+            - stride: stride of the convolution. It can be a scalar or an integer.
+            - padding: the amount of zero-padding applied to the input. It can be a scalar or an integer.
+            - output_padding: the additional size added to one side of the output shape. It can be a scalar or  \
+                an integer.
+            - kernel_order: dimension order of kernel, (O,I,L) or (I,O,L). (O,I,L) stands for (output channels, \
+                input channels, length).
+            - name: name of the 1d transposed convolution. Optional.
+
+        NOTE: See https://pytorch.org/docs/stable/generated/torch.nn.ConvTranspose1d.html#torch.nn.ConvTranspose1d  \
+            for details.
+        """
+        if kernel_order not in ("OIL", "IOL"):
+            raise ValueError(f"kernel order must be 'OIL' or 'IOL'.")
+
+        super().__init__(
+            source,
+            dest,
+            kernel,
+            _single(stride),
+            _single(padding),
+            _single(output_padding),
+            kernel_order,
+            name,
+        )
+
+
+class ConvTranspose2d(ConvTranspose2dSyn):
+    def __init__(
+        self,
+        source: Union[Neuron, InputProj],
+        dest: Neuron,
+        kernel: np.ndarray,
+        *,
+        stride: _Size2Type = 1,
+        padding: _Size2Type = 0,
+        output_padding: _Size2Type = 0,
+        kernel_order: _KOrder4d = "OIHW",
+        name: Optional[str] = None,
+    ) -> None:
+        """2d transposed convolution synapses in fully-unrolled format.
+
+        Args:
+            - source: source neuron. The dimensions need to be expressed explicitly as (C,H,W) or (H,W,C). The  \
+                feature map dimension order is specified by `fm_order`.
+            - dest: destination neuron.
+            - kernel: convolution kernel. Its dimension order must be (O,I,H,W) or (I,O,H,W), depending on the  \
+                argument `kernel_order`.
+            - stride: stride of the convolution. It can be a scalar or a tuple of 2 integers.
+            - padding: the amount of zero-padding applied to the input. It can be a scalar or a tuple of 2 integers.
+            - output_padding: the additional size added to one side of the output shape. It can be a scalar or  \
+                a tuple of 2 integers.
+            - fm_order: dimension order of feature map. The order of input & output feature maps must be        \
+                consistent, (C,H,W) or (H,W,C).
+            - kernel_order: dimension order of kernel, (O,I,H,W) or (I,O,H,W). (O,I,H,W) stands for (output     \
+                channels, input channels, height, width).
+            - name: name of the 2d transposed convolution. Optional.
+
+        NOTE: See https://pytorch.org/docs/stable/generated/torch.nn.ConvTranspose2d.html#torch.nn.ConvTranspose2d  \
+            for details.
+        """
+        if kernel_order not in ("OIHW", "IOHW"):
+            raise ValueError(f"kernel order must be 'OIHW' or 'IOHW'.")
+
+        super().__init__(
+            source,
+            dest,
+            kernel,
+            _pair(stride),
+            _pair(padding),
+            _pair(output_padding),
+            kernel_order,
+            name,
+        )
