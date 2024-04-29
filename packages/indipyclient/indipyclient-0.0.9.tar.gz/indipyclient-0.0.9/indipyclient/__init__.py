9f0e0ee68573a22e4a7b8d3dda8be68401601236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:26 +0000

    Add package indipyclient/0.0.9/indipyclient-0.0.9.tar.gz

---
+++ packages/indipyclient/indipyclient-0.0.9.tar.gz/indipyclient-0.0.9/indipyclient/__init__.py
@@ -0,0 +1,7 @@
+from .ipyclient import IPyClient
+from .events import (delProperty, defSwitchVector, defTextVector, defNumberVector, defLightVector, defBLOBVector,
+                     setSwitchVector, setTextVector, setNumberVector, setLightVector, setBLOBVector, Message, VectorTimeOut)
+
+from .propertymembers import getfloat
+
+version = "0.0.9"
