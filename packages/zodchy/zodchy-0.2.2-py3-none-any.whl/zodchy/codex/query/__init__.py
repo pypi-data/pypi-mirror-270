420bf5610564ebed90a74efb56ef99e5ca21f0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:05:55 +0000

    Add package zodchy/0.2.2/zodchy-0.2.2-py3-none-any.whl

---
+++ packages/zodchy/zodchy-0.2.2-py3-none-any.whl/zodchy/codex/query/__init__.py
@@ -0,0 +1,24 @@
+from .bits import (
+    ClauseBit,
+    FilterBit,
+    EQ,
+    NE,
+    LE,
+    GE,
+    LT,
+    GT,
+    LIKE,
+    IS,
+    NOT,
+    RANGE,
+    SET,
+    OrderBit,
+    ASC,
+    DESC,
+    SliceBit,
+    Limit,
+    Offset
+)
+from .parsing import (
+    NotationParser
+)
