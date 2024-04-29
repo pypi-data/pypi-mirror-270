9957d37eb64e5572f222996abad74bb2e128f235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:05:43 +0000

    Add package xi-mzidentml-converter/0.1.27/xi_mzidentml_converter-0.1.27.tar.gz

---
+++ packages/xi-mzidentml-converter/xi_mzidentml_converter-0.1.27.tar.gz/xi_mzidentml_converter-0.1.27/models/__init__.py
@@ -0,0 +1,20 @@
+"""
+Import all the models here
+"""
+
+# Please keep __all__ alphabetized.
+__all__ = [
+    "analysiscollection",
+    "base",
+    "dbsequence",
+    "enzyme",
+    "modifiedpeptide",
+    "peptideevidence",
+    "projectdetail",
+    "projectsubdetail",
+    "searchmodification",
+    "spectrum",
+    "spectrumidentification",
+    "spectrumidentificationprotocol",
+    "upload"
+]
