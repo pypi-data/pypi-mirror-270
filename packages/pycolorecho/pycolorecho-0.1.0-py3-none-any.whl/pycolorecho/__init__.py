3667390ae4641c6085c93d065a890ef2696d04c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:52 +0000

    Add package pycolorecho/0.1.0/pycolorecho-0.1.0-py3-none-any.whl

---
+++ packages/pycolorecho/pycolorecho-0.1.0-py3-none-any.whl/pycolorecho/__init__.py
@@ -0,0 +1,25 @@
+__all__ = [
+    "RESET",
+    "Layer",
+    "Color",
+    "TextBackgroundColor",
+    "TextColor",
+    "TextEffect",
+    "TextCase",
+    "ColorMapper",
+    "echo",
+    "HEXCodes",
+    "__author__",
+    "__description__",
+    "__name__",
+    "__version__"
+]
+__author__ = "coldsofttech"
+__description__ = """
+Simple Python package for colorized terminal output
+"""
+__name__ = "pycolorecho"
+__version__ = "0.1.0"
+
+from pycolorecho.__main__ import RESET, Layer, Color, TextBackgroundColor, TextColor, TextEffect, TextCase, \
+    ColorMapper, echo, HEXCodes
