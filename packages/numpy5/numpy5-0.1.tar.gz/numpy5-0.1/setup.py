955008b4893457527faea70fd6ad79c057440eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:25 +0000

    Add package numpy5/0.1/numpy5-0.1.tar.gz

---
+++ packages/numpy5/numpy5-0.1.tar.gz/numpy5-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='numpy5',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your dependencies here
+    ],
+    # other metadata like author, description, license, etc.
+)
