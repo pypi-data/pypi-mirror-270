4da3207021dc60df079fdd42fde8e11d14a33216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 16:04:21 +0000

    Add package solutions-builder/1.18.1/solutions_builder-1.18.1.tar.gz

---
+++ packages/solutions-builder/solutions_builder-1.18.1.tar.gz/solutions_builder-1.18.1/README.md
@@ -0,0 +1,176 @@
+# Google Cloud Solutions Builder
+
+**A solution framework to generate a new project with built-in structure and modules
+to accelerate your project setup.**
+
+## TL;DR
+
+Solutions Builder is a boilerplate template for building repeatable
+solutions with the best practices in architecture on Google Cloud, including GKE
+clusters, Cloud Run, Test Automation, CI/CD, as well as development process.
+
+This template provides built-in and ready-to-ship modules including:
+
+- Easy-to-deploy [Terraform](https://www.terraform.io/) boilerplate modules
+- Container-based microservices, can be deployed to a Kubernetes cluster or Cloud Run.
+- Unified deployment using Skaffold.
+- Automatically generated API documentation with Swagger UI.
+- CI/CD deployment (with GitHub Actions).
+- Cloud Run templates.
+
+## Roadmap
+
+Please see [Feature Requests in the GitHub issue list](https://github.com/GoogleCloudPlatform/solutions-builder/issues?q=is%3Aopen+is%3Aissue+label%3A%22feature+request%22).
+
+## Prerequisite
+
+| Tool       | Required Version | Installation                                        |
+| ---------- | ---------------- | --------------------------------------------------- |
+| Python     | &gt;= 3.11       |                                                     |
+| gcloud CLI | Latest           | https://cloud.google.com/sdk/docs/install           |
+| Terraform  | &gt;= v1.3.7     | https://developer.hashicorp.com/terraform/downloads |
+| Skaffold   | &gt;= v2.4.0     | https://skaffold.dev/docs/install/                  |
+
+[Optional] If you plan to deploy services on a GKE cluster, please install the following:
+
+| Tool      | Required Version | Installation                                               |
+| --------- | ---------------- | ---------------------------------------------------------- |
+| Kustomize | &gt;= v5.0.0     | https://kubectl.docs.kubernetes.io/installation/kustomize/ |
+
+## Installing Solutions Builder CLI
+
+With `pip`:
+
+```
+pip install solutions-builder
+```
+
+With `pipx`:
+
+```
+pip install --user pipx
+pipx install solutions-builder
+```
+
+## Quick Start
+
+This quick start steps will do the following:
+
+- Create a new GCP project and initialize Cloud foundation.
+- Add a RESTful API service for managing Todo list.
+- Deploy the service to Cloud Run.
+
+Set up GCP project
+
+```
+export PROJECT_ID=my-solution-gcp-id
+
+# (Optional) Create a new GCP project. You can also use an existing GCP project.
+gcloud projects create $PROJECT_ID
+
+# Set gcloud CLI to the GCP project.
+gcloud config set project $PROJECT_ID
+```
+
+Generate a new solution folder.
+
+```
+sb new my-solution
+```
+
+This will prompt options and variables:
+
+```
+🎤 What is your project name? (Spaces are allowed.)
+   my-solution
+🎤 What is your Google Cloud project ID?
+   my-solution-gcp-id
+🎤 What is your Google Cloud project number?
+   12345678
+🎤 Which Google Cloud region?
+   us-central1
+🎤 Use GCS Bucket for Terraform backend?
+   Yes
+```
+
+Go to the newly created project folder
+
+```
+cd my-solution
+sb infra apply 1-bootstrap
+```
+
+Initialize Cloud infrastructure
+
+- Option 1: (Recommended) Log in to the jump host and run the following command(s) in the solution folder.
+- Option 2: Run the following commands in your local machine.
+
+```
+sb infra apply 2-foundation
+```
+
+Add a RESTful API service with **Todo** data model to this solution.
+
+```
+sb components add -t restful_service todo_service
+```
+
+Fill details in the prompt:
+
+- Component name: **todo_service**
+- Resource name: **todo-service**
+- Relative path: **todo-service**
+- GCP region: **us-central1**
+- Data model name: **todo**
+- Add Cloud Run to deployment methods: **yes**
+- Create network endpoint group (NEG) for serverless ingress: **yes**
+- Default deploy method? (cloudrun or gke): **Cloud Run**
+
+Add an HTTP Load Balancer for Cloud Run service(s)
+
+```
+sb components add -t terraform_httplb_cloudrun terraform_httplb_cloudrun
+sb infra apply 3-httplb-cloudrun
+```
+
+Build and deploy
+
+```
+sb deploy
+```
+
+- See other deployment options in [solutions_builder/modules](solutions_builder/modules).
+
+## CLI Usage
+
+For more information on how to use the CLI, please refer to the [CLI_USAGE.md](docs/CLI_USAGE.md).
+
+## Additional Documentations
+
+You can find more documentations in [docs](docs) folder. In a nutshell, it covers the following:
+
+- [INSTALLATION.md](docs/INSTALLATION.md) - The overall installation guide.
+- [DEVELOPMENT.md](docs/DEVELOPMENT.md) - Development guide and code submission process.
+- [TROUBLESHOOTING.md](docs/TROUBLESHOOTING.md) - Development guide and code submission process.
+
+In the [docs/components](docs/components) folder, it contains a few more guidance based on each component/feature available in this template.
+
+- [gke.md](docs/components/gke.md) covers the overall development guidance on Google Kubernetes Engine.
+- [cloudrun.md](docs/components/cloudrun.md) covers the guidance if you want to deploy microservice to Cloud Run.
+
+## FAQ
+
+- Who are the target audience/users for this Solutions Builder?
+
+  - A: Any engineering team to start a new solution development project.
+
+- Can I choose to deploy microservice just to Cloud Run?
+
+  - A: Yes, please refer to [INSTALLATION Guide](docs/INSTALLATION.md) for more details.
+
+- Can I use this template for non-Google or multi-Cloud environments?
+  - A: We design this Solutions Builder to work 100% out of the box with Google Cloud products. However, you could customize the solution to meet your needs on multi-Cloud environment. See [Why Google Cloud](https://cloud.google.com/why-google-cloud) for details.
+
+## Troubleshoot
+
+See [TROUBLESHOOTING Guide](docs/TROUBLESHOOTING.md) for details.
---
+++ packages/solutions-builder/solutions_builder-1.18.1.tar.gz/solutions_builder-1.18.1/solutions_builder/cli/component.py
@@ -0,0 +1,208 @@
+"""
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import typer
+from typing import Optional
+from typing_extensions import Annotated
+from copier import run_copy
+from .cli_utils import *
+
+component_app = typer.Typer()
+
+
+@component_app.command()
+def add(component_name,
+        component_template: Annotated[str,
+                                      typer.Option("--template", "-t")] = None,
+        solution_path: Annotated[Optional[str],
+                                 typer.Argument()] = ".",
+        yes: Optional[bool] = False,
+        answers=None):
+  validate_solution_folder(solution_path)
+
+  # Check if component_template is empty.
+  if not component_template:
+    print("Missing component_template. Please set --template or -t with one "
+          " of the component templates:")
+    list_component_templates()
+    return
+
+  confirm(
+      f"This will add component '{component_name}' to "
+      f"{solution_path}/components folder. Continue?",
+      skip=yes)
+
+  answers_dict = get_answers_dict(answers)
+  process_component("add",
+                    component_name, component_template,
+                    solution_path, data=answers_dict)
+  print_success(
+      f"Complete. Component {component_name} added to solution at {solution_path}\n"
+  )
+
+
+@component_app.command()
+def update(component_name,
+           solution_path: Annotated[Optional[str],
+                                    typer.Argument()] = ".",
+           yes: Optional[bool] = False,
+           answers=None):
+  validate_solution_folder(solution_path)
+  confirm(
+      f"This will update '{component_name}' in "
+      f"'{solution_path}/components'. Continue?",
+      skip=yes)
+
+  sb_yaml = read_yaml(f"{solution_path}/sb.yaml")
+  components = sb_yaml.get("components", {})
+  component_dict = components.get(component_name, {})
+  component_template = component_dict.get("component_template")
+
+  answers_dict = get_answers_dict(answers)
+  process_component("update",
+                    component_name,
+                    component_template,
+                    solution_path,
+                    data=answers_dict,
+                    use_existing_answers=yes)
+  print_success(
+      f"Complete. Component {component_name} updated to solution at {solution_path}\n"
+  )
+
+
+def update_component_to_root_yaml(component_name, answers, solution_path):
+  # Update Solution root YAML with new component name.
+  solution_yaml_dict = read_yaml(f"{solution_path}/sb.yaml") or {}
+  components = solution_yaml_dict["components"] or {}
+  components[component_name] = answers
+  solution_yaml_dict["components"] = components
+  write_yaml(f"{solution_path}/sb.yaml", solution_yaml_dict)
+
+
+def process_component(method,
+                      component_name,
+                      component_template,
+                      solution_path,
+                      data={},
+                      use_existing_answers=False):
+
+  assert component_template, f"component_template is not empty."
+
+  destination_path = "."
+  current_dir = os.path.dirname(__file__)
+  answers_file = None
+
+  # Get basic info from root sb.yaml.
+  sb_yaml = read_yaml(f"{solution_path}/sb.yaml")
+  global_variables = sb_yaml.get("global_variables", {})
+  component_answers = {}
+
+  # If the component name is a Git URL, use the URL as-is in copier.
+  if check_git_url(component_template):
+    print(f"Loading component from remote Git URL: {component_template}")
+    template_path = component_template
+
+  # Otherwise, try to locate the component in local modules/ folder.
+  else:
+    if method == "update":
+      data["component_name"] = component_name
+      if component_name not in sb_yaml["components"]:
+        raise NameError(
+            f"Component {component_name} is not defined in the root yaml 'sb.yaml' file."
+        )
+      component_answers = sb_yaml["components"][component_name]
+      component_template = component_answers["component_template"]
+      template_path = f"{current_dir}/../modules/{component_template}"
+      answers_file = f".st/module_answers/{component_name}.yaml"
+
+      # Use existing answer values in data, skipping the prompt.
+      if use_existing_answers:
+        answers_yaml = read_yaml(answers_file)
+        for key, value in answers_yaml.items():
+          data[key] = value
+
+    else:
+      template_path = f"{current_dir}/../modules/{component_template}"
+      if not os.path.exists(template_path):
+        raise FileNotFoundError(
+            f"Component {component_template} does not exist in modules folder.")
+
+    # Get destination_path defined in copier.yaml
+    copier_dict = get_copier_yaml(template_path)
+    destination_path = solution_path + "/" + copier_dict["_metadata"].get(
+        "destination_path")
+    destination_path = destination_path.replace("//", "/")
+
+  data["component_name"] = component_name
+  data["project_id"] = global_variables["project_id"]
+  data["project_number"] = global_variables["project_number"]
+  data["solution_path"] = solution_path
+  data["template_path"] = template_path
+
+  # Run copier with data.
+  worker = run_copy(template_path,
+                    destination_path,
+                    data=data,
+                    answers_file=answers_file,
+                    unsafe=True)
+
+  # Get answer values inputed by user.
+  answers = worker.answers.user
+
+  for key, value in worker.answers.user_defaults.items():
+    if key not in answers:
+      answers[key] = component_answers.get(key) or value
+  answers["component_template"] = component_template
+  answers["destination_path"] = copier_dict["_metadata"].get(
+      "destination_path")
+
+  # Update component's answer back to sb.yaml.
+  update_component_to_root_yaml(answers["component_name"], answers,
+                                solution_path)
+
+  # Patch skaffold.yaml
+  for patch_file in copier_dict.get("_patch", []):
+    print(f"Patching {patch_file}...")
+    new_yaml = patch_yaml(f"{solution_path}/{patch_file}",
+                          f"{solution_path}/{patch_file}.patch")
+    new_yaml["requires"] = dedupe(new_yaml["requires"])
+    write_yaml(f"{solution_path}/{patch_file}", new_yaml)
+    os.remove(f"{solution_path}/{patch_file}.patch")
+
+  print()
+
+# List installed components.
+
+
+@component_app.command()
+def list(solution_path: Annotated[Optional[str], typer.Argument()] = "."):
+  sb_yaml = read_yaml(f"{solution_path}/sb.yaml")
+  components = sb_yaml.get("components", [])
+  print("Installed components:")
+  for component_name, properties in components.items():
+    typer.echo(
+        typer.style(f"- {component_name} ", fg=typer.colors.WHITE, bold=True) +
+        typer.style(f"(from: {properties['component_template']})",
+                    fg=typer.colors.BLACK,
+                    bold=True))
+  print()
+
+
+# List available components to add.
+@component_app.command()
+def available():
+  print("Available components to add:\n")
+  list_component_templates()
