3623c35d87c111dd792eb6ca0d63f2bd5b91afa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:07 +0000

    Add package mlforge/0.1.3a0/mlforge-0.1.3a0.tar.gz

---
+++ packages/mlforge/mlforge-0.1.3a0.tar.gz/mlforge-0.1.3a0/tests/test_pbar.py
@@ -0,0 +1,45 @@
+"""
+
+Tests for the progress bar functionality in the Pipeline class.
+
+"""
+# pylint: disable=E1101:no-member, W0201:attribute-defined-outside-init, W0511:fixme
+# pylint: disable=C0103:invalid-name, W0212:protected-access
+# pylint: disable=C0116:missing-function-docstring, C0115:missing-class-docstring
+# pylint: disable=R0913:too-many-arguments, R0903:too-few-public-methods
+# pylint: disable=R0914:too-many-locals, R0915:too-many-statements
+# pylint: disable=W0106:expression-not-assigned, R1702:too-many-branches
+# pylint: disable=missing-function-docstring
+# pylint: disable=W0212:protected-access, C0413:import-misplaced
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../mlforge')))
+
+from mlforge import Pipeline
+
+
+class Test_PbarUpdate:
+
+    # Creates a progress bar with the total number of steps in the pipeline.
+    def test_progress_bar_creation(self):
+        pipeline = Pipeline()
+        pipeline.pipeline = [1, 2, 3, 4, 5]
+        pipeline._pbar_create()
+        assert pipeline.pbar.progress._tasks[0].total == len(pipeline.pipeline)
+
+    # The pipeline has no steps, so the progress bar is not created.
+    def test_no_progress_bar_creation(self):
+        pipeline = Pipeline()
+        pipeline._pbar_create()
+        assert pipeline.pbar is None
+
+    # Can update the progress bar by a step of 1
+    def test_update_progress_bar_by_step_of_1(self):
+        pipeline = Pipeline()
+        pipeline.pipeline = [1, 2, 3, 4, 5]
+        pipeline._pbar_create()
+        pipeline._pbar_update(1)
+        assert pipeline.pbar.progress._tasks[0].completed == 1
