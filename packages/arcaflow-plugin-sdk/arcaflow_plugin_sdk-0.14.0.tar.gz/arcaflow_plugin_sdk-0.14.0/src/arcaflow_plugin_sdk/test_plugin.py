cde406ea236018421176b56e765aa11f5047b039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:06:28 +0000

    Add package arcaflow-plugin-sdk/0.14.0/arcaflow_plugin_sdk-0.14.0.tar.gz

---
+++ packages/arcaflow-plugin-sdk/arcaflow_plugin_sdk-0.14.0.tar.gz/arcaflow_plugin_sdk-0.14.0/src/arcaflow_plugin_sdk/test_plugin.py
@@ -0,0 +1,56 @@
+import dataclasses
+import io
+import tempfile
+import typing
+import unittest
+
+from arcaflow_plugin_sdk import plugin
+
+
+@dataclasses.dataclass
+class StdoutTestInput:
+    pass
+
+
+@dataclasses.dataclass
+class StdoutTestOutput:
+    pass
+
+
+@plugin.step(
+    "stdout-test",
+    "Stdout test",
+    "A test for writing to stdout.",
+    {"success": StdoutTestOutput},
+)
+def stdout_test_step(
+    input: StdoutTestInput,
+) -> typing.Tuple[str, StdoutTestOutput]:
+    print("Hello world!")
+    return "success", StdoutTestOutput()
+
+
+class StdoutTest(unittest.TestCase):
+    def test_capture_stdout(self):
+        s = plugin.build_schema(stdout_test_step)
+        tmp = tempfile.NamedTemporaryFile(suffix=".json")
+
+        def cleanup():
+            tmp.close()
+
+        self.addCleanup(cleanup)
+        tmp.write(bytes("{}", "utf-8"))
+        tmp.flush()
+
+        i = io.StringIO()
+        o = io.StringIO()
+        e = io.StringIO()
+        exit_code = plugin.run(
+            s, ["test.py", "-f", tmp.name, "--debug"], i, o, e
+        )
+        self.assertEqual(0, exit_code)
+        self.assertEqual("Hello world!\n", e.getvalue())
+
+
+if __name__ == "__main__":
+    unittest.main()
