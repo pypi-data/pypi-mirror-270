87d9dc98d7be1ce7c0e04388bebdf8e56851a762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:26 +0000

    Add package gppt/4.1.0/gppt-4.1.0-py3-none-any.whl

---
+++ packages/gppt/gppt-4.1.0-py3-none-any.whl/gppt/__init__.py
@@ -0,0 +1,31 @@
+from .auth import PixivAuth
+from .gppt import GetPixivToken
+from .types import (
+    LoginCred,
+    LoginInfo,
+    LoginUserInfo,
+    OAuthAPIResponse,
+    PixivLoginFailed,
+    ProfileURIs,
+)
+
+__version__ = "4.1.0"
+
+__all__ = [
+    "PixivAuth",
+    "PixivAuth",
+    "LoginCred",
+    "LoginInfo",
+    "LoginUserInfo",
+    "OAuthAPIResponse",
+    "PixivLoginFailed",
+    "ProfileURIs",
+    "CLIENT_ID",
+    "AUTH_TOKEN_URL",
+    "CALLBACK_URI",
+    "CLIENT_SECRET",
+    "LOGIN_URL",
+    "REDIRECT_URI",
+    "USER_AGENT",
+    "GetPixivToken",
+]
