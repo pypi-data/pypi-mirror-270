438092bbdbe95676129b4ceb8010d4702262f159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:08 +0000

    Add package Verbex/2.0.2/verbex-2.0.2.tar.gz

---
+++ packages/Verbex/verbex-2.0.2.tar.gz/verbex-2.0.2/src/verbex/__init__.py
@@ -0,0 +1,12 @@
+"""Verbal regular expression library."""
+
+# try:
+#    from importlib.metadata import version
+# except ImportError:
+#    from importlib_metadata import version
+
+from .verbex import CharClass as CharClass
+from .verbex import SpecialChar as SpecialChar
+from .verbex import Verbex as Verbex
+
+# __version__ = version("verbex")
