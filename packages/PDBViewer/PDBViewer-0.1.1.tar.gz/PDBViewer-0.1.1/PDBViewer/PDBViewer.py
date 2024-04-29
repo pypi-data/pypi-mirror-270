8abdb12de7932af2c6d7c5aee352e7e19db4b3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:46 +0000

    Add package PDBViewer/0.1.1/PDBViewer-0.1.1.tar.gz

---
+++ packages/PDBViewer/PDBViewer-0.1.1.tar.gz/PDBViewer-0.1.1/PDBViewer/PDBViewer.py
@@ -0,0 +1,41 @@
+import urllib.request
+import gzip
+import py3Dmol
+import os
+
+class PDBViewer:
+    def __init__(self):
+        pass
+
+    def fetch_pdb(self, pdb_id):
+        pdb_id = str(pdb_id).lower()
+        url = f'ftp://ftp.rcsb.org/pub/pdb/data/structures/all/pdb/pdb{pdb_id}.ent.gz'
+        download_dir = os.path.dirname(os.path.abspath(__file__))
+        pdb_file = os.path.join(download_dir, f'{pdb_id}.pdb')
+        if not os.path.exists(pdb_file):
+            urllib.request.urlretrieve(url, f'{pdb_file}.gz')
+            with gzip.open(f'{pdb_file}.gz', 'rb') as f_in:
+                with open(pdb_file, 'wb') as f_out:
+                    f_out.write(f_in.read())
+
+    def view(self, pdb_id):
+        self.fetch_pdb(pdb_id)
+        pdb_id = str(pdb_id).lower()
+        pdb_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'{pdb_id}.pdb')
+        with open(pdb_file, 'r') as f:
+            pdb_data = f.read()
+        view = py3Dmol.view(width=600, height=400)
+        view.addModel(pdb_data, 'pdb')
+        view.setStyle({'cartoon': {'color': 'spectrum'}})
+        view.zoomTo()
+        view.show()
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python script.py pdb_id")
+        sys.exit(1)
+    pdb_id = sys.argv[1]
+    viewer = PDBViewer()
+    viewer.view(pdb_id)
+
