b7e68449484852f1a4d360ddbdf83463a37eed27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:05:29 +0000

    Add package agent_path/0.1.1/agent_path-0.1.1-py3-none-any.whl

---
+++ packages/agent_path/agent_path-0.1.1-py3-none-any.whl/agent_path/utils.py
@@ -0,0 +1,21 @@
+import re
+from fancy_print import FancyPrinter
+from typing import Dict
+
+
+fancy_print = FancyPrinter()
+
+
+_re_json_state = re.compile(r"{.+[:,].+}|\[.+[,:].+\]", re.DOTALL | re.MULTILINE)
+
+
+def get_json_object(dirty_str: str) -> str:
+    return _re_json_state.findall(dirty_str)[0]
+
+
+_re_xml_state = re.compile(r"<(\w+)>(.*?)<\/\1>", re.DOTALL | re.MULTILINE)
+
+
+def get_xml_tags(dirty_str: str) -> Dict[str, str]:
+    matches = _re_xml_state.findall(dirty_str)
+    return {match[0]: match[1].strip() for match in matches}
