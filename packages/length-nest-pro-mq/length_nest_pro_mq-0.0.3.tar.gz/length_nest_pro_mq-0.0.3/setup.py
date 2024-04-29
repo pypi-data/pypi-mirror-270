fa0c339202516bb3f81dfbdd7cf05d77430b7799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:28 +0000

    Add package length-nest-pro-mq/0.0.3/length_nest_pro_mq-0.0.3.tar.gz

---
+++ packages/length-nest-pro-mq/length_nest_pro_mq-0.0.3.tar.gz/length_nest_pro_mq-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="length-nest-pro-MQ",
+    version='0.0.3',
+    author="Niels Bos",
+    author_email="nielsb00@gmail.com",
+    description="length_nest_pro_MQ",
+    long_description="Adapted version of length_nest_pro, enhanced with message queue",
+    packages=find_packages(),
+    install_requires=['numpy', 'wheel', 'setuptools', 'pika'],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=["rabbit mq", "length nest pro"]
+
+)
