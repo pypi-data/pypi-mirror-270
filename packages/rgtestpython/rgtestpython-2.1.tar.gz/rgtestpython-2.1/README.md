f5fa98b1162ace4fddfab17c2e9dcc9fcbc87c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 14:04:59 +0000

    Add package rgtestpython/2.1/rgtestpython-2.1.tar.gz

---
+++ packages/rgtestpython/rgtestpython-2.1.tar.gz/rgtestpython-2.1/README.md
@@ -0,0 +1,7 @@
+# pip install rgpython 
+
+## usage:from rgpython import *
+
+## rgpython.rg.rg()
+
+## rgpython.rg1.Cmsg.view('hello')
---
+++ packages/rgtestpython/rgtestpython-2.1.tar.gz/rgtestpython-2.1/setup.py
@@ -0,0 +1,15 @@
+﻿import setuptools
+
+with open("README.md","r") as fs:
+    long_description=fs.read()
+
+setuptools.setup(name="rgtestpython",version="2.1",author="rongge",\
+                 author_email="78380834@qq.com",description="测试python包",\
+                 long_description=long_description,\
+                 long_description_content_type="text/markdown",\
+                 packages=setuptools.find_packages(),\
+                 classifiers=["Programming Language :: Python :: 3"],\
+                              # "License::OSI Approved::MIT License",\
+                              # "Operating System::OS Independent"],\
+                 install_requires=['pillow',],\
+                 python_requires='>=3',)
