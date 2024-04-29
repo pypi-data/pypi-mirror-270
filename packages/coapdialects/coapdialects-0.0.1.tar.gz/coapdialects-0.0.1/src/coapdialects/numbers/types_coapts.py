3026f22f9020037b61b9ce4e83e95e140b830ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 05:05:37 +0000

    Add package coapdialects/0.0.1/coapdialects-0.0.1.tar.gz

---
+++ packages/coapdialects/coapdialects-0.0.1.tar.gz/coapdialects-0.0.1/src/coapdialects/numbers/types_coapts.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Christian Amsüss and the aiocoap contributors
+#
+# SPDX-License-Identifier: MIT
+
+"""List of known values for the CoAP "Type" field.
+
+As this field is only 2 bits, its valid values are comprehensively enumerated
+in the `Type` object.
+"""
+
+from enum import IntEnum
+
+class Type(IntEnum):
+    CON = 1 # Confirmable
+    NON = 3 # Non-confirmable
+    ACK = 0 # Acknowledgement
+    RST = 2 # Reset
+
+    def __str__(self):
+        return self.name
+
+CON, NON, ACK, RST = Type.CON, Type.NON, Type.ACK, Type.RST
+
+__all__ = ['Type', 'CON', 'NON', 'ACK', 'RST']
