2760b2707e3df95af6c23662cb20ee0e53ef5f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:18 +0000

    Add package helloworld32352/0.2/helloworld32352-0.2.tar.gz

---
+++ packages/helloworld32352/helloworld32352-0.2.tar.gz/helloworld32352-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='helloworld32352',
+    version='0.2',
+    packages=find_packages(),
+    description='A simple hello world package',
+    author='Satpal Singh',
+    author_email='satpal.code@gmail.com',
+    url='https://github.com/satpalsr/helloworld32352',
+    install_requires=[
+        # Add dependencies here
+    ],
+)
