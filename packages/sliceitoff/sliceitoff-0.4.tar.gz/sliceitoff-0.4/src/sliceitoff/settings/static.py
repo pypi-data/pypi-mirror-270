87c9ba3b3d1e836b7e8af86e6e678617742d3405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:13 +0000

    Add package sliceitoff/0.4/sliceitoff-0.4.tar.gz

---
+++ packages/sliceitoff/sliceitoff-0.4.tar.gz/sliceitoff-0.4/src/sliceitoff/settings/static.py
@@ -0,0 +1,26 @@
+""" settings.static - default settings etc static data """
+
+DEFAULT_SETTINGS = [
+    ("hiscore", "281238!HIL"),
+    ("hiscore", "190355!VIL"),
+    ("hiscore", "164710!KSU"),
+    ("hiscore", "100000!---"),
+    ("hiscore", "90000!---"),
+    ("hiscore", "80000!-S-"),
+    ("hiscore", "70000!-L-"),
+    ("hiscore", "60000!-I-"),
+    ("hiscore", "50000!-C-"),
+    ("hiscore", "40000!-E-"),
+    ("hiscore", "30000!---"),
+    ("hiscore", "25000!-I-"),
+    ("hiscore", "20000!-T-"),
+    ("hiscore", "15000!---"),
+    ("hiscore", "10000!-O-"),
+    ("hiscore", "8000!-F-"),
+    ("hiscore", "6000!-F-"),
+    ("hiscore", "4000!---"),
+    ("hiscore", "2000!---"),
+    ("hiscore", "0!---"),
+    ("sfx_volume", "10"),
+    ("music_volume", "10")
+]
