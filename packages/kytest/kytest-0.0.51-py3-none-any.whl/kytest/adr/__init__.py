24e84afe893acd0ea574459e71309f0a29156155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:07 +0000

    Add package kytest/0.0.51/kytest-0.0.51-py3-none-any.whl

---
+++ packages/kytest/kytest-0.0.51-py3-none-any.whl/kytest/adr/__init__.py
@@ -0,0 +1,11 @@
+from .element import AdrElem as Elem
+from .driver import AdrDriver
+from .case import TestCase
+from .page import Page
+
+__all__ = [
+    "Elem",
+    "AdrDriver",
+    "TestCase",
+    "Page"
+]
---
+++ packages/kytest/kytest-0.0.51-py3-none-any.whl/kytest/ios/__init__.py
@@ -0,0 +1,12 @@
+from .element import IosElem as Elem
+from .driver import IosDriver
+from .case import TestCase
+from .page import Page
+
+__all__ = [
+    "Elem",
+    "IosDriver",
+    "TestCase",
+    "Page"
+]
+
