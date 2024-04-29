7e87faea8ef3a5e2465742b0f9334412a5b1b4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:28 +0000

    Add package pydantic-argparse/0.9.0/pydantic_argparse-0.9.0-py3-none-any.whl

---
+++ packages/pydantic-argparse/pydantic_argparse-0.9.0-py3-none-any.whl/pydantic_argparse/utils/types.py
@@ -0,0 +1,54 @@
+"""Types Utility Functions for Declarative Typed Argument Parsing.
+
+The `types` module contains a utility function used for determining and
+comparing the types of `pydantic fields.
+"""
+
+
+# Local
+from pydantic_argparse.compatibility import pydantic
+
+# Typing
+from typing import Any, Tuple, Union, get_origin
+
+
+def is_field_a(
+    field: pydantic.fields.ModelField,
+    types: Union[Any, Tuple[Any, ...]],
+) -> bool:
+    """Checks whether the subject *is* any of the supplied types.
+
+    The checks are performed as follows:
+
+    1. `field` *is* one of the `types`
+    2. `field` *is an instance* of one of the `types`
+    3. `field` *is a subclass* of one of the `types`
+
+    If any of these conditions are `True`, then the function returns `True`,
+    else `False`.
+
+    Args:
+        field (pydantic.fields.ModelField): Subject field to check type of.
+        types (Union[Any, Tuple[Any, ...]]): Type(s) to compare field against.
+
+    Returns:
+        bool: Whether the field *is* considered one of the types.
+    """
+    # Create tuple if only one type was provided
+    if not isinstance(types, tuple):
+        types = (types, )
+
+    # Get field type, or origin if applicable
+    field_type = get_origin(field.outer_type_) or field.outer_type_
+
+    # Check `isinstance` and `issubclass` validity
+    # In order for `isinstance` and `issubclass` to be valid, all arguments
+    # should be instances of `type`, otherwise `TypeError` *may* be raised.
+    is_valid = all(isinstance(t, type) for t in (*types, field_type))
+
+    # Perform checks and return
+    return (
+        field_type in types
+        or (is_valid and isinstance(field_type, types))
+        or (is_valid and issubclass(field_type, types))
+    )
