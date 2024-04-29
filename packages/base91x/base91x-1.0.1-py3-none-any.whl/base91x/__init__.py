3b2d72fd883f1863193c546dcdea4bff0dd6eda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:05:54 +0000

    Add package base91x/1.0.1/base91x-1.0.1-py3-none-any.whl

---
+++ packages/base91x/base91x-1.0.1-py3-none-any.whl/base91x/__init__.py
@@ -0,0 +1,8 @@
+from .base91x import BASE91X_ALPHABET
+from .base91x import BASE91X_LEN
+from .base91x import decode
+from .base91x import encode
+
+__all__ = ["__version__"]
+
+__version__ = "1.0.1"
