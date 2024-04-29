24a109d84c9b55f971acb4d44a415acdeacc39fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:00 +0000

    Add package bert4keras3/1.3.3/bert4keras3-1.3.3-py2.py3-none-any.whl

---
+++ packages/bert4keras3/bert4keras3-1.3.3-py2.py3-none-any.whl/bert4keras3/__init__.py
@@ -0,0 +1,8 @@
+#! -*- coding: utf-8 -*-
+
+__version__ = '1.3'
+
+from bert4keras3 import backend,layers,models,snippets,tokenizers
+from bert4keras3 import Models
+from bert4keras3 import Layers_add
+from bert4keras3.backend import ops
