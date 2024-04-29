73b67f90aead2dce1ca34709a39d775f951299bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:52 +0000

    Add package pycheckers/0.0.1/pycheckers-0.0.1.tar.gz

---
+++ packages/pycheckers/pycheckers-0.0.1.tar.gz/pycheckers-0.0.1/README.md
@@ -0,0 +1,2 @@
+# pycheckers
+A simple 2D checkers game made using a graphics library by John Zelle
---
+++ packages/pycheckers/pycheckers-0.0.1.tar.gz/pycheckers-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="pycheckers",
+    version="0.0.1",
+    install_requires=[
+        "socket",
+        "pickle",
+        "tkinter",
+        "random",
+        "time",
+        "graphics.py",
+    ],
+)
