1a2df43c614e1cfc2007abfb8fa6f5828253e223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:03:58 +0000

    Add package conf-root/0.2.0/conf_root-0.2.0-py3-none-any.whl

---
+++ packages/conf-root/conf_root-0.2.0-py3-none-any.whl/conf_root/agents/JsonAgent.py
@@ -0,0 +1,29 @@
+from conf_root.agents.BasicAgent import BasicAgent
+import json
+
+
+class JsonAgent(BasicAgent):
+    default_extension = 'json'
+
+    def create(self, cls):
+        # 将dataclass默认值转换为dict，便于序列化
+        default_dict = self.dataclass_default_dict(cls)
+
+        # 将dict转换为YAML并写入文件
+        if len(default_dict.keys()) > 0:
+            with open(self.location, "w") as file:
+                json.dump(default_dict, file)
+
+    def load(self, cls, obj):
+        with open(self.location, encoding='utf-8') as file:
+            data = json.load(file)
+
+        # 将dict展开为对象。
+        self.dict_to_dataclass(data, cls, obj)
+
+    def save(self, obj):
+        data_dict = self.dataclass_to_dict(obj)
+
+        # 将dict转换为YAML并写入文件
+        with open(self.location, "w") as file:
+            json.dump(data_dict, file)
