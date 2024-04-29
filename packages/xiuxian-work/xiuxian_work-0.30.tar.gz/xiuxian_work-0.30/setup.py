49570d935b63b16723987e1e0afadd20a3d46140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:54 +0000

    Add package xiuxian-work/0.30/xiuxian_work-0.30.tar.gz

---
+++ packages/xiuxian-work/xiuxian_work-0.30.tar.gz/xiuxian_work-0.30/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_namespace_packages,find_packages
+
+setup(
+name='xiuxian_work',
+version='0.30',
+description='修仙数据包',
+#long_description=open('README.md','r').read(),
+author='甘城菜月',
+author_email='2859385794@qq.com',
+license='MIT license',
+include_package_data=True,
+packages=find_namespace_packages(include=["xiuxian_work"]),
+platforms='all',
+install_requires=[""],
+url='https://github.com/luoyefufeng/nonebot_plugin_simulator_xiuxian',
+)
+
+
