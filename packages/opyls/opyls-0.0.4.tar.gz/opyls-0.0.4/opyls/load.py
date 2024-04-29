7d0b4a21d39776ca7d4fef739cbdc6412b8a376f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:05:23 +0000

    Add package opyls/0.0.4/opyls-0.0.4.tar.gz

---
+++ packages/opyls/opyls-0.0.4.tar.gz/opyls-0.0.4/opyls/load.py
@@ -0,0 +1,33 @@
+from configparser import ConfigParser
+
+
+# https://www.postgresqltutorial.com/postgresql-python/connect/
+def load_ini(filename: str, section: str) -> dict[str, str]:
+    """
+        Load configuration from an INI file.
+
+        Args:
+            filename (str): The path to the INI file.
+            section (str): The name of the section to load.
+
+        Returns:
+            Dict[str, str]: A dictionary containing the configuration parameters.
+
+        Raises:
+            Exception: If the specified section is not found in the INI file.
+
+        Example:
+            load_ini("config.ini", "section_name")
+        """
+    parser = ConfigParser()
+    parser.read(filename)
+
+    config = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            config[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return config
