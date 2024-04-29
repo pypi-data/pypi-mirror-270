5f4d3ce8da8d4cb4061f02abc34d3f522ed2bd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:05:06 +0000

    Add package jetline/0.1.23/jetline-0.1.23.tar.gz

---
+++ packages/jetline/jetline-0.1.23.tar.gz/jetline-0.1.23/jetline/templates/data.py
@@ -0,0 +1,15 @@
+from jetline.data.data import Data
+from jetline.data.helper import choose_file
+
+
+class YourDataClass(Data):
+    def __init__(self):
+        super().__init__(
+            name="Name",
+            data=choose_file(),
+
+        )
+
+    class Meta:
+        y = 0
+        x = 0
