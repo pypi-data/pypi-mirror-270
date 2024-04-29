2e5c85dd68dacdf0a1268a4642a529c28703d76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:24 +0000

    Add package dictaclass/0.6.0/dictaclass-0.6.0.tar.gz

---
+++ packages/dictaclass/dictaclass-0.6.0.tar.gz/dictaclass-0.6.0/dictaclass/dictaclass.py
@@ -0,0 +1,372 @@
+from typing import (
+    Any,
+    Dict,
+    Type,
+    TypeVar,
+    List,
+    Set,
+    Callable,
+    Union,
+    Optional,
+    get_type_hints,
+)
+from dataclasses import is_dataclass, asdict
+from datetime import datetime, timezone
+
+import sys
+
+T = TypeVar("T")
+
+
+def _transformer_noop(source: str) -> str:
+    return source
+
+
+def _on_extra_field_noop(*args):
+    pass
+
+
+def _to_dataclass_38(
+    dataclass_type: Type[T],
+    data: Any,
+    key_transformer: Callable[[str], str],
+    on_extra_field: Callable[[Type, str, Any], None],
+    implicit_optional: bool,
+) -> T:
+    from typing import get_args, get_origin
+
+    dict_value: Any
+
+    if not is_dataclass(dataclass_type) or data is None:
+        return data
+
+    assert isinstance(data, dict)
+
+    dataclass_dict: Dict[str, Any] = dict()
+    used_keys: Set[str] = set()
+
+    for annotation_name, annotation_type in get_type_hints(dataclass_type).items():
+        try:
+            key = key_transformer(annotation_name)
+            dict_value = data[key]
+            used_keys.add(key)
+        except KeyError:
+            # TODO(braynstorm):
+            #   Check if the field has default value. Only "continue" if it does.
+            #   The current code is safe but will raise less readable exceptions.
+            continue
+
+        optional = implicit_optional
+        origin = get_origin(annotation_type)
+        if origin is Union:
+            args = list(get_args(annotation_type))
+            len_args = len(args)
+            type_none = type(None)
+            optional = type_none in args
+
+            assert len_args == 2 and optional, "dictaclass doesn't support real Unions."
+            args.remove(type_none)
+
+            annotation_type = args[0]
+            origin = get_origin(annotation_type)
+
+        if dict_value is None:
+            assert optional, f"dictaclass key '{key}' is not optional."
+
+            dataclass_dict[annotation_name] = None
+            continue
+
+        if origin is set:
+            value_type = get_args(annotation_type)[0]
+            dataclass_dict[annotation_name] = set(
+                _to_dataclass_38(
+                    value_type, v, key_transformer, on_extra_field, implicit_optional
+                )
+                for v in dict_value
+            )
+        elif origin is list:
+            value_type = get_args(annotation_type)[0]
+            dataclass_dict[annotation_name] = [
+                _to_dataclass_38(
+                    value_type, v, key_transformer, on_extra_field, implicit_optional
+                )
+                for v in dict_value
+            ]
+        elif origin is dict:
+            key_type, value_type = get_args(annotation_type)
+            assert key_type == str
+            dataclass_dict[annotation_name] = {
+                key: _to_dataclass_38(
+                    value_type,
+                    value,
+                    key_transformer,
+                    on_extra_field,
+                    implicit_optional,
+                )
+                for key, value in dict_value.items()
+            }
+        elif annotation_type is datetime:
+            if isinstance(dict_value, (int, float)):
+                dataclass_dict[annotation_name] = datetime.fromtimestamp(
+                    dict_value,
+                    tz=timezone.utc,
+                )
+            elif isinstance(dict_value, str):
+                dataclass_dict[annotation_name] = datetime.fromisoformat(dict_value)
+            elif isinstance(dict_value, datetime):
+                dataclass_dict[annotation_name] = dict_value
+            else:
+                raise ValueError(
+                    f"Unsupported type for datetime: {type(dict_value)}: {dict_value}"
+                )
+        else:
+            dataclass_dict[annotation_name] = _to_dataclass_38(
+                annotation_type,
+                dict_value,
+                key_transformer,
+                on_extra_field,
+                implicit_optional,
+            )
+
+    extra_keys = set(data.keys())
+    extra_keys.difference_update(used_keys)
+
+    for field in extra_keys:
+        value = data[field]
+        on_extra_field(dataclass_type, field, value)
+
+    return dataclass_type(**dataclass_dict)
+
+
+def _to_dataclass_37(
+    dataclass_type: Type[T],
+    data: Any,
+    key_transformer: Callable[[str], str],
+    on_extra_field: Callable[[Type, str, Any], None],
+    implicit_optional: bool,
+) -> T:
+    dict_value: Any
+
+    if not is_dataclass(dataclass_type) or data is None:
+        return data
+
+    assert isinstance(data, dict)
+
+    dataclass_dict: Dict[str, Any] = dict()
+    used_keys: Set[str] = set()
+
+    annotations = dict()
+    for c in dataclass_type.mro():
+        try:
+            annotations.update(get_type_hints(c))
+        except AttributeError:
+            pass
+
+    for annotation_name, annotation_type in annotations.items():
+        try:
+            key = key_transformer(annotation_name)
+            dict_value = data[key]
+            used_keys.add(key)
+        except KeyError:
+            # TODO(bozho2):
+            #   Check if the field has default value. Only "continue" if it does.
+            #   The current code is safe but will raise less readable exceptions.
+            continue
+
+        optional = implicit_optional
+
+        # NOTE(braynstorm):
+        #   typing._GenericAlias is inaccessible, except:
+        #   - Dict.__class__ is typing._GenericAlias
+        #   - List.__class__ is typing._GenericAlias
+        #   - Set.__class__  is typing._GenericAlias
+        is_generic_alias = isinstance(annotation_type, List.__class__)
+
+        # NOTE(braynstorm):
+        #   Remap Optional[X](== Union[X, NoneType]) to X, and set `optional`.
+        if is_generic_alias:
+            if annotation_type.__origin__ is Union:
+                args = list(annotation_type.__args__)
+                type_none = type(None)
+                len_args = len(args)
+                optional = type_none in args
+
+                assert (
+                    len_args == 2 and optional
+                ), "dictaclass doesn't support real Unions."
+                args.remove(type_none)
+
+                annotation_type = args[0]
+                is_generic_alias = isinstance(annotation_type, List.__class__)
+
+        if dict_value is None:
+            assert optional, f"dictaclass key '{key}' is not optional."
+
+            dataclass_dict[annotation_name] = None
+            continue
+
+        if is_generic_alias:
+            origin = annotation_type.__origin__
+            args = annotation_type.__args__
+            if origin == set:
+                value_type = args[0]
+                dataclass_dict[annotation_name] = set(
+                    _to_dataclass_37(
+                        value_type,
+                        v,
+                        key_transformer,
+                        on_extra_field,
+                        implicit_optional,
+                    )
+                    for v in dict_value
+                )
+            elif origin == list:
+                value_type = args[0]
+                dataclass_dict[annotation_name] = [
+                    _to_dataclass_37(
+                        value_type,
+                        v,
+                        key_transformer,
+                        on_extra_field,
+                        implicit_optional,
+                    )
+                    for v in dict_value
+                ]
+            elif origin == dict:
+                key_type = args[0]
+                value_type = args[1]
+                assert key_type == str
+                dataclass_dict[annotation_name] = {
+                    k: _to_dataclass_37(
+                        value_type,
+                        v,
+                        key_transformer,
+                        on_extra_field,
+                        implicit_optional,
+                    )
+                    for k, v in dict_value.items()
+                }
+            else:
+                raise Exception(f"Unsupported typehint __origin__ = {origin}.")
+        elif annotation_type is datetime:
+            if isinstance(dict_value, (int, float)):
+                dataclass_dict[annotation_name] = datetime.fromtimestamp(
+                    dict_value,
+                    tz=timezone.utc,
+                )
+            elif isinstance(dict_value, str):
+                dataclass_dict[annotation_name] = datetime.fromisoformat(dict_value)
+            elif isinstance(dict_value, datetime):
+                dataclass_dict[annotation_name] = dict_value
+            else:
+                raise ValueError(
+                    f"Unsupported type for datetime: {type(dict_value)}: {dict_value}"
+                )
+        else:
+            dataclass_dict[annotation_name] = _to_dataclass_37(
+                annotation_type,
+                dict_value,
+                key_transformer,
+                on_extra_field,
+                implicit_optional,
+            )
+
+    extra_keys = set(data.keys())
+    extra_keys.difference_update(used_keys)
+
+    for field in extra_keys:
+        value = data[field]
+        on_extra_field(dataclass_type, field, value)
+
+    return dataclass_type(**dataclass_dict)
+
+
+def to_dataclass(
+    dataclass_type: Type[T],
+    data: Any,
+    key_transformer: Optional[Callable[[str], str]] = None,
+    on_extra_field: Optional[Callable[[Type, str, Any], None]] = None,
+    implicit_optional: bool = False,
+) -> T:
+    """
+    Convert nested dicts/lists to a dataclass structure.
+
+    Supported generics subtypes:
+        - `List`
+        - `Set`
+        - `Dict`
+
+    >>> from dataclasses import dataclass
+    >>> from to_dataclass import to_dataclass
+    >>> #
+    >>> @dataclass
+    >>> class Point
+    >>>     x: int
+    >>>     y: int
+    >>> @dataclass
+    >>> class Shape
+    >>>     points: List[Point]
+    >>> #
+    >>> assert to_dataclass(
+    >>>     Point,
+    >>>     dict(
+    >>>         points=[
+    >>>             dict(x=0, y=0),
+    >>>             dict(x=10, y=40),
+    >>>         ]
+    >>>     )
+    >>> ) == Line(Point(0, 0), Point(10, 40))
+
+    Args:
+        dataclass_type (Type[T]): The type of the root dataclass.
+        data (Any): json.loads()'d data.
+        key_transformer (Callable[[str], str] | None, optional):
+            Name transformer function, to convert dataclass field names to
+            names of the input data. Defaults to None - no transformation.
+        on_extra_field (Callable[[Type, str, Any], None] | None, optional):
+            Called when to_dataclass finds a field in a dictionary that is
+            not requested by the dataclass it is filling.
+            Args: the dataclass type, name of the field, value of the field.
+        implicit_optional (bool, optional:
+            When set to True, every field is interpreted as Optional[T].
+    Returns:
+        T: _description_
+
+
+    """
+    if key_transformer is None:
+        key_transformer = _transformer_noop
+
+    if on_extra_field is None:
+        on_extra_field = _on_extra_field_noop
+
+    if sys.version_info.minor >= 8:
+        return _to_dataclass_38(
+            dataclass_type,
+            data,
+            key_transformer,
+            on_extra_field,
+            implicit_optional,
+        )
+    else:
+        return _to_dataclass_37(
+            dataclass_type,
+            data,
+            key_transformer,
+            on_extra_field,
+            implicit_optional,
+        )
+
+
+def dataclass_to_dict(dataclassObject: Any) -> Dict[str, Any]:
+    """
+    Convert a dataclass hierarchy to a simple dict/list hierarchy.
+    """
+    return asdict(dataclassObject)
+
+
+def json_dumps_default(obj: Any) -> Any:
+    if isinstance(obj, datetime):
+        return obj.astimezone(timezone.utc).timestamp()
+    else:
+        return obj
---
+++ packages/dictaclass/dictaclass-0.6.0.tar.gz/dictaclass-0.6.0/tests/test_dictaclass.py
@@ -0,0 +1,452 @@
+from dictaclass.dictaclass import to_dataclass, dataclass_to_dict, json_dumps_default
+
+from dataclasses import dataclass
+
+from typing import Any, Dict, List, Optional, Set, Tuple, Type, Union
+
+import pytest
+
+
+class Test_Dictaclass:
+    def test_flat(self) -> None:
+        @dataclass(frozen=True)
+        class ExampleDC:
+            a: str
+            b: int
+            c: float
+
+        v = to_dataclass(ExampleDC, dict(a="asdf", b=10, c=3.1415))
+
+        assert isinstance(v, ExampleDC)
+        assert v.a == "asdf"
+        assert v.b == 10
+        assert v.c == 3.1415
+
+    def test_inheritance(self) -> None:
+        @dataclass(frozen=True)
+        class Base:
+            a: str
+
+        @dataclass(frozen=True)
+        class Example(Base):
+            b: int
+            c: float
+
+        v = to_dataclass(Example, dict(a="asdf", b=10, c=3.1415))
+
+        assert isinstance(v, Example)
+        assert v.a == "asdf"
+        assert v.b == 10
+        assert v.c == 3.1415
+
+    def test_optional_values(self) -> None:
+        @dataclass(frozen=True)
+        class Example:
+            b: int
+            c: float = 0.0
+
+        v = to_dataclass(Example, dict(b=10, c=3.1415))
+        assert isinstance(v, Example)
+        assert v.b == 10
+        assert v.c == 3.1415
+
+        v = to_dataclass(Example, dict(b=10))
+        assert isinstance(v, Example)
+        assert v.b == 10
+        assert v.c == 0
+
+    def test_with_primitive_list(self) -> None:
+        @dataclass(frozen=True)
+        class ExampleDC:
+            a: str
+            b: List[int]
+
+        v = to_dataclass(ExampleDC, dict(a="asdf", b=[1, 2, 3]))
+
+        assert isinstance(v, ExampleDC)
+        assert v.a == "asdf"
+        assert isinstance(v.b, list)
+        assert v.b == [1, 2, 3]
+
+    def test_with_primitive_set(self) -> None:
+        @dataclass(frozen=True)
+        class ExampleDC:
+            a: str
+            b: Set[int]
+
+        v = to_dataclass(ExampleDC, dict(a="asdf", b=[1, 2, 3]))
+
+        assert isinstance(v, ExampleDC)
+        assert v.a == "asdf"
+        assert isinstance(v.b, set)
+        assert v.b == {1, 2, 3}
+
+    def test_with_primitive_dict(self) -> None:
+        @dataclass(frozen=True)
+        class ExampleDC:
+            a: str
+            b: Dict[str, int]
+
+        v = to_dataclass(ExampleDC, dict(a="asdf", b=dict(a=1, b=2)))
+
+        assert isinstance(v, ExampleDC)
+        assert v.a == "asdf"
+        assert isinstance(v.b, dict)
+        assert v.b == dict(a=1, b=2)
+
+    def test_with_nested_list(self) -> None:
+        @dataclass(frozen=True)
+        class Pair:
+            first: str
+            last: str
+
+        @dataclass(frozen=True)
+        class Object:
+            pairs: List[Pair]
+
+        v = to_dataclass(
+            Object,
+            {
+                "pairs": [
+                    {"first": "f0", "last": "l0"},
+                    {"first": "f1", "last": "l1"},
+                ]
+            },
+        )
+        assert isinstance(v, Object)
+        assert isinstance(v.pairs, list)
+        assert len(v.pairs) == 2
+
+        assert isinstance(v.pairs[0], Pair)
+        assert isinstance(v.pairs[1], Pair)
+
+        assert isinstance(v.pairs[0].first, str)
+        assert isinstance(v.pairs[0].last, str)
+
+        assert isinstance(v.pairs[1].first, str)
+        assert isinstance(v.pairs[1].last, str)
+
+        assert v.pairs[0].first == "f0"
+        assert v.pairs[0].last == "l0"
+        assert v.pairs[1].first == "f1"
+        assert v.pairs[1].last == "l1"
+
+    def test_with_nested_primitive_list(self) -> None:
+        @dataclass(frozen=True)
+        class Object:
+            pairs: List[List[str]]
+
+        v = to_dataclass(
+            Object,
+            {
+                "pairs": [
+                    ["a", "b", "c"],
+                    ["d", "e", "f"],
+                ]
+            },
+        )
+        assert isinstance(v, Object)
+        assert isinstance(v.pairs, list)
+        assert len(v.pairs) == 2
+        assert isinstance(v.pairs[0], list)
+        assert isinstance(v.pairs[1], list)
+        assert v.pairs == [
+            ["a", "b", "c"],
+            ["d", "e", "f"],
+        ]
+
+    def test_with_nested_set(self) -> None:
+        @dataclass(frozen=True)
+        class Pair:
+            first: str
+            last: str
+
+        @dataclass(frozen=True)
+        class Object:
+            pairs: Set[Pair]
+
+        v = to_dataclass(
+            Object,
+            {
+                "pairs": [
+                    {"first": "f0", "last": "l0"},
+                    {"first": "f1", "last": "l1"},
+                ]
+            },
+        )
+        assert isinstance(v, Object)
+        assert isinstance(v.pairs, set)
+        assert v.pairs == {
+            Pair("f0", "l0"),
+            Pair("f1", "l1"),
+        }
+
+    def test_with_nested_dict(self) -> None:
+        @dataclass(frozen=True)
+        class Pair:
+            first: str
+            last: str
+
+        @dataclass(frozen=True)
+        class Object:
+            pairs: Dict[str, Pair]
+
+        v = to_dataclass(
+            Object,
+            {
+                "pairs": {
+                    "p0": {"first": "f0", "last": "l0"},
+                    "p1": {"first": "f1", "last": "l1"},
+                }
+            },
+        )
+        assert isinstance(v, Object)
+        assert isinstance(v.pairs, dict)
+        assert set(v.pairs.keys()) == {"p0", "p1"}
+
+        assert isinstance(v.pairs["p0"], Pair)
+        assert isinstance(v.pairs["p1"], Pair)
+
+        assert isinstance(v.pairs["p0"].first, str)
+        assert isinstance(v.pairs["p0"].last, str)
+
+        assert isinstance(v.pairs["p1"].first, str)
+        assert isinstance(v.pairs["p1"].last, str)
+
+        assert v.pairs["p0"].first == "f0"
+        assert v.pairs["p0"].last == "l0"
+        assert v.pairs["p1"].first == "f1"
+        assert v.pairs["p1"].last == "l1"
+
+    def test_with_nested_set_2(self) -> None:
+        import json
+
+        @dataclass(frozen=True)
+        class Pair:
+            first: str
+            last: str
+
+        @dataclass(frozen=True)
+        class PairPair:
+            a: Pair
+
+        @dataclass(frozen=True)
+        class Object:
+            pairs: Set[PairPair]
+
+        v = to_dataclass(
+            Object,
+            json.loads(
+                """
+                {
+                    "pairs": [
+                        {"a": {"first": "f0", "last": "l0"}},
+                        {"a": {"first": "f1", "last": "l1"}}
+                    ]
+                }
+                """
+            ),
+        )
+
+        assert isinstance(v, Object)
+        assert isinstance(v.pairs, set)
+        assert len(v.pairs) == 2
+        assert v.pairs == {PairPair(Pair("f0", "l0")), PairPair(Pair("f1", "l1"))}
+
+    def test_flat_with_extra_fields(self) -> None:
+        @dataclass(frozen=True)
+        class Pair:
+            first: str
+            last: str
+
+        extras = []
+        v = to_dataclass(
+            Pair,
+            {"first": "a", "last": "b", "middle": "lol"},
+            on_extra_field=lambda klass, field_name, field_value: extras.append(
+                (klass, field_name, field_value)
+            ),
+        )
+        assert isinstance(v, Pair)
+        assert v.first == "a"
+        assert v.last == "b"
+        assert not hasattr(v, "middle")
+        assert extras == [
+            (Pair, "middle", "lol"),
+        ]
+
+    def test_nested_with_extra_fields(self) -> None:
+        @dataclass(frozen=True)
+        class C:
+            num: int
+
+        @dataclass(frozen=True)
+        class B:
+            c: C
+
+        @dataclass(frozen=True)
+        class A:
+            b: B
+
+        extras: List[Tuple[Type[Any], str, Any]] = []
+
+        def on_extra_field(data_type: Type[Any], key: str, value: Any):
+            extras.append((data_type, key, value))
+
+        a = to_dataclass(
+            A,
+            dict(b=dict(c=dict(num=10, extra_num=11), extra_num=12), extra_num=13),
+            on_extra_field=on_extra_field,
+        )
+        assert isinstance(a, A)
+        assert isinstance(a.b, B)
+        assert isinstance(a.b.c, C)
+        assert not hasattr(a, "extra_num")
+        assert not hasattr(a.b, "extra_num")
+        assert not hasattr(a.b.c, "extra_num")
+        assert len(extras) == 3
+        assert extras == [
+            (C, "extra_num", 11),
+            (B, "extra_num", 12),
+            (A, "extra_num", 13),
+        ]
+
+
+class Test_Dictaclass_OrderedDict:
+    def test_with_nested_class(self) -> None:
+        from collections import OrderedDict
+
+        @dataclass(frozen=True)
+        class Child:
+            name: str
+            level: int
+
+        @dataclass(frozen=True)
+        class Parent:
+            child: Child
+
+        v = to_dataclass(
+            Parent,
+            OrderedDict(
+                {
+                    "child": OrderedDict(
+                        {
+                            "name": "asdf",
+                            "level": 10,
+                        }
+                    )
+                }
+            ),
+        )
+
+        assert isinstance(v, Parent)
+        assert v
+        assert v.child
+        assert v.child.name == "asdf"
+        assert v.child.level == 10
+
+
+class Test_Dataclass_Optional:
+    def test_flat_with_implicit_optional(self) -> None:
+        @dataclass
+        class Example:
+            a: int
+            b: Dict[str, str]
+            c: List[str]
+            d: Set[str]
+
+        r = to_dataclass(
+            Example, dict(a=None, b=None, c=None, d=None), implicit_optional=True
+        )
+        assert isinstance(r, Example)
+        assert r.a is None
+        assert r.b is None
+        assert r.c is None
+        assert r.d is None
+
+        with pytest.raises(AssertionError):
+            r = to_dataclass(
+                Example, dict(a=None, b=None, c=None, d=None), implicit_optional=False
+            )
+
+    def test_flat_with_explicit_optional(self) -> None:
+        @dataclass
+        class Example:
+            a: Optional[int]
+            b: Optional[Dict[str, str]]
+            c: Optional[List[str]]
+            d: Optional[Set[str]]
+
+        r = to_dataclass(
+            Example, dict(a=None, b=None, c=None, d=None), implicit_optional=False
+        )
+        assert isinstance(r, Example)
+        assert r.a is None
+        assert r.b is None
+        assert r.c is None
+        assert r.d is None
+
+        r = to_dataclass(Example, dict(a=1, b={}, c=[], d=[]), implicit_optional=False)
+        assert isinstance(r, Example)
+        assert r.a == 1
+        assert r.b == {}
+        assert r.c == []
+        assert r.d == set()
+
+    def test_nested_with_explicit_optional(self) -> None:
+        @dataclass
+        class ExampleG:
+            a: int
+
+        @dataclass
+        class Example:
+            g: Optional[ExampleG]
+
+        r = to_dataclass(Example, dict(g=None))
+        assert isinstance(r, Example)
+        assert r.g is None
+
+        r = to_dataclass(Example, dict(g=dict(a=10)))
+        assert isinstance(r, Example)
+        assert r.g is not None
+        assert r.g.a == 10
+
+
+class Test_Dictaclass_NameTransform:
+    def test_to_snake_case_flat(self) -> None:
+        import inflection
+
+        @dataclass(frozen=True)
+        class ExampleDC:
+            url_encoded: str
+
+        v = to_dataclass(ExampleDC, dict(UrlEncoded="asdf"), inflection.camelize)
+
+        assert isinstance(v, ExampleDC)
+        assert v.url_encoded == "asdf"
+
+
+class Test_Dictaclass_Datetime:
+    def test_with_datetime(self) -> None:
+        import json
+        import datetime
+
+        utc = datetime.timezone.utc
+
+        @dataclass(frozen=True)
+        class ExampleDC:
+            timestamp: datetime.datetime
+
+        dc = ExampleDC(timestamp=datetime.datetime.now())
+        dc_dict = dataclass_to_dict(dc)
+        dc_json = json.dumps(dc_dict, default=json_dumps_default)
+
+        jdc_dict = json.loads(dc_json)
+        assert isinstance(jdc_dict["timestamp"], (int, float))
+
+        jdc = to_dataclass(ExampleDC, jdc_dict)
+        assert jdc.timestamp.day == dc.timestamp.day
+        assert isinstance(jdc.timestamp, datetime.datetime)
+
+        # NOTE: We can't compare the timezone directly because the timezone is set to UTC when deserializing.
+        assert jdc.timestamp.astimezone(utc) == dc.timestamp.astimezone(utc)
