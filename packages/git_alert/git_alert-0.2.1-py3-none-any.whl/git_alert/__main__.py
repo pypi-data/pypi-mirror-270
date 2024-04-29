9761a0983b871426ee6c250c21a1a486d709aba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 05:05:37 +0000

    Add package git_alert/0.2.1/git_alert-0.2.1-py3-none-any.whl

---
+++ packages/git_alert/git_alert-0.2.1-py3-none-any.whl/git_alert/__main__.py
@@ -0,0 +1,16 @@
+import sys
+
+from git_alert.argument_parser import argument_parser
+from git_alert.repositories import Repositories
+from git_alert.traverse import GitAlert
+
+repos = Repositories()
+
+args = argument_parser(sys.argv[1:])
+
+alert = GitAlert(pth=args.path, ignore=args.ignore, repos=repos)
+
+alert.traverse(args.path)
+alert.check()
+alert.repos.display(only_dirty=args.only_dirty)
+alert.repos.summary()
