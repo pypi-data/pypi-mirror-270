654563e860e8a944e728c9ae54054aa76e03d3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:58 +0000

    Add package pbix-refresh/0.4.30/pbix_refresh-0.4.30.tar.gz

---
+++ packages/pbix-refresh/pbix_refresh-0.4.30.tar.gz/pbix_refresh-0.4.30/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+# import setuptools
+
+packages = ['pbix_refresh']
+setup(name='pbix_refresh',
+      version='0.4.30',
+      author='xigua, 刷新Power bi desktop',
+      packages=packages,
+      package_dir={'requests': 'requests'}, )
