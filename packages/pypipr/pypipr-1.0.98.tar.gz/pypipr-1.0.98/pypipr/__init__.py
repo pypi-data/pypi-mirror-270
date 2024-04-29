593c9eda372b28918bf8bf26a74746ef3e24f604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:55 +0000

    Add package pypipr/1.0.98/pypipr-1.0.98.tar.gz

---
+++ packages/pypipr/pypipr-1.0.98.tar.gz/pypipr-1.0.98/pypipr/__init__.py
@@ -0,0 +1,98 @@
+from .ibuiltins.ComparePerformance import ComparePerformance
+from .ibuiltins.LINUX import LINUX
+from .ibuiltins.RunParallel import RunParallel
+from .ibuiltins.WINDOWS import WINDOWS
+from .ibuiltins.avg import avg
+from .ibuiltins.basename import basename
+from .ibuiltins.chr_to_int import chr_to_int
+from .ibuiltins.chunk_array import chunk_array
+from .ibuiltins.create_folder import create_folder
+from .ibuiltins.datetime_from_string import datetime_from_string
+from .ibuiltins.datetime_now import datetime_now
+from .ibuiltins.dict_first import dict_first
+from .ibuiltins.dirname import dirname
+from .ibuiltins.exit_if_empty import exit_if_empty
+from .ibuiltins.filter_empty import filter_empty
+from .ibuiltins.get_by_index import get_by_index
+from .ibuiltins.get_class_method import get_class_method
+from .ibuiltins.get_filemtime import get_filemtime
+from .ibuiltins.get_filesize import get_filesize
+from .ibuiltins.int_to_chr import int_to_chr
+from .ibuiltins.is_empty import is_empty
+from .ibuiltins.is_iterable import is_iterable
+from .ibuiltins.is_valid_url import is_valid_url
+from .ibuiltins.ivars import ivars
+from .ibuiltins.password_generator import password_generator
+from .ibuiltins.random_bool import random_bool
+from .ibuiltins.restart import restart
+from .ibuiltins.set_timeout import set_timeout
+from .ibuiltins.sets_ordered import sets_ordered
+from .ibuiltins.str_cmp import str_cmp
+from .ibuiltins.to_str import to_str
+from .iconsole.choices import choices
+from .iconsole.console_run import console_run
+from .iconsole.input_char import input_char
+from .iconsole.log import log
+from .iconsole.print_colorize import print_colorize
+from .iconsole.print_dir import print_dir
+from .iconsole.print_log import print_log
+from .iconsole.print_to_last_line import print_to_last_line
+from .iconsole.text_colorize import text_colorize
+from .idjango.APIMixinView import APIMixinView
+from .iengineering.PintUreg import PintUreg
+from .iengineering.PintUregQuantity import PintUregQuantity
+from .iengineering.batch_calculate import batch_calculate
+from .iengineering.batchmaker import batchmaker
+from .iengineering.calculate import calculate
+from .iflow.auto_reload import auto_reload
+from .iflow.github_pull import github_pull
+from .iflow.github_push import github_push
+from .iflow.github_user import github_user
+from .iflow.pip_freeze_without_version import pip_freeze_without_version
+from .iflow.poetry_publish import poetry_publish
+from .iflow.poetry_update_version import poetry_update_version
+from .ifunctions.iargv import iargv
+from .ifunctions.idumps import idumps
+from .ifunctions.idumps_html import idumps_html
+from .ifunctions.ienv import ienv
+from .ifunctions.iexec import iexec
+from .ifunctions.ijoin import ijoin
+from .ifunctions.iloads import iloads
+from .ifunctions.iloads_html import iloads_html
+from .ifunctions.iopen import iopen
+from .ifunctions.iprint import iprint
+from .ifunctions.irange import irange
+from .ifunctions.ireplace import ireplace
+from .ifunctions.iscandir import iscandir
+from .ifunctions.isplit import isplit
+import asyncio
+import colorama
+import csv
+import datetime
+import functools
+import inspect
+import io
+import json
+import lxml
+import math
+import multiprocessing
+import operator
+import os
+import pathlib
+import pint
+import pprint
+import queue
+import random
+import re
+import requests
+import string
+import subprocess
+import sys
+import textwrap
+import threading
+import time
+import tzdata
+import uuid
+import webbrowser
+import yaml
+import zoneinfo
