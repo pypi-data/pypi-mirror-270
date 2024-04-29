e5a327f39319994d158697c7c60183337ab59a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:28 +0000

    Add package paloalto-panorama-sdk/0.0.1/paloalto_panorama_sdk-0.0.1.tar.gz

---
+++ packages/paloalto-panorama-sdk/paloalto_panorama_sdk-0.0.1.tar.gz/paloalto_panorama_sdk-0.0.1/paloalto_panorama_sdk/panorama_sdk.py
@@ -0,0 +1,28 @@
+import os
+from paloalto_panorama_sdk.key_manager import KeyManager
+from paloalto_panorama_sdk.service_manager import ServiceManager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class PanoramaSDK:
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.apikey = KeyManager.get_api_key(
+            panorama_url=self.url,
+            user=self.username,
+            password=self.password
+        )
+        self.service = ServiceManager(self.url, self.username, self.password)
+
+
+if __name__ == '__main__':
+    panSDK = PanoramaSDK(
+        url=os.getenv("url", ""),
+        username=os.getenv("user", "test"),
+        password=os.getenv("password", "testpass")
+    )
+    print(panSDK.apikey)
