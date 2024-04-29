a3bc29758ce84a7361ffaa2982b0a2862793238e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:34 +0000

    Add package subsurface/2024.1.0/subsurface-2024.1.0-py3-none-any.whl

---
+++ packages/subsurface/subsurface-2024.1.0-py3-none-any.whl/subsurface/__init__.py
@@ -0,0 +1,29 @@
+import subsurface.reader
+import subsurface.interfaces
+import subsurface.writer
+import subsurface.visualization
+from subsurface.structs import *
+from datetime import datetime
+import dotenv
+
+dotenv.load_dotenv()
+
+try:
+    from subsurface import visualization
+except ImportError:
+    pass
+
+# Version.
+try:
+    # - Released versions just tags:       0.8.0
+    # - GitHub commits add .dev#+hash:     0.8.1.dev4+g2785721
+    # - Uncommitted changes add timestamp: 0.8.1.dev4+g2785721.d20191022
+    from ._version import version as __version__
+except ImportError:
+    # If it was not installed, then we don't know the version. We could throw a
+    # warning here, but this case *should* be rare. subsurface should be
+    # installed properly!
+    __version__ = 'unknown-'+datetime.today().strftime('%Y%m%d')
+
+if __name__ == '__main__':
+    pass
