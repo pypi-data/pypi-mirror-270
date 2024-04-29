50d03cc2b0b71340e3fd52e2573c988233b873d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 14:05:02 +0000

    Add package IslandSheds/0.0.16/islandsheds-0.0.16.tar.gz

---
+++ packages/IslandSheds/islandsheds-0.0.16.tar.gz/islandsheds-0.0.16/setup.py
@@ -0,0 +1,42 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+VERSION = '0.0.16'
+DESCRIPTION = 'A Caribbean watershed shapefile downloader package.'
+LONG_DESCRIPTION = 'Python package for downloading country-specific Caribbean watershed data.'
+
+setup(
+    name='IslandSheds',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        'geopandas',
+        'requests',
+    ],
+    author='Kent Thomas',
+    author_email='kthomas.wsrn@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kgdthomas/IslandSheds',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords='watershed geospatial data download Caribbean',
+    license='MIT',
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'islandsheds=islandsheds.downloader:main',
+        ],
+    },
+    project_urls={
+        'Documentation': 'https://islandsheds.readthedocs.io/',
+        'Source': 'https://github.com/kgdthomas/IslandSheds',
+        'Tracker': 'https://github.com/kgdthomas/IslandSheds/issues',
+    },
+)
