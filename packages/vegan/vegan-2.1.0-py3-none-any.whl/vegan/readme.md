4dbb7bb46df0673f6e959467882f3dd7516ef96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:05:14 +0000

    Add package vegan/2.1.0/vegan-2.1.0-py3-none-any.whl

---
+++ packages/vegan/vegan-2.1.0-py3-none-any.whl/vegan/readme.md
@@ -0,0 +1,48 @@
+
+
+
+
+******
+
+Bravo!  You have received a Mercantilism Diploma in "vegan" from   
+the Orbital Convergence University International Air and Water 
+Embassy of the Rust Planet (the planet that is one ellipse close to
+the Sun than Earth's ellipse).
+
+You are now officially certified to include "vegan" in your practice!
+
+Encore! Encore! Encore! Encore!
+
+******
+
+
+# vegan
+
+---
+
+## description
+
+
+---		
+		
+## obtainment
+`[USD] pip install vegan`
+
+
+## on
+
+
+---
+
+## tutorial
+`[USD] vegan help`
+
+---
+
+## contacts
+Bryan@Status600.com
+
+
+
+
+
---
+++ packages/vegan/vegan-2.1.0-py3-none-any.whl/vegan/shows_v2/treasure/nature/land/_ops/measures_sums/__init__.py
@@ -0,0 +1,100 @@
+
+
+
+
+'''
+	summary:
+		This should be for calculating the land measures sum of 1 treasure,
+		however the treasure can have multiple packages.
+'''
+
+
+'''
+	from vegan.shows_v2.treasure.nature.land._ops.measures_sums import calc_measures_sums
+	calc_measures_sums (
+		land = land
+	)
+'''
+
+#----
+#
+import vegan.measures.number.sci_note_2 as sci_note_2
+#
+#
+from fractions import Fraction
+#
+#----
+
+def calc_measures_sums (
+	land = {},
+	records = 0
+):
+	grove = land ['grove']
+	land_measures = land ['measures']
+	
+	'''
+		
+	'''
+	for ingredient in grove:
+		if (len (ingredient ["natures"]) == 0):
+			continue;
+		if (len (ingredient ["natures"]) >= 2):
+			raise Exception (f"This def is for calculating the sum of a grove with only 1 treasure (food or supp) added")
+		
+		number_of_packages = Fraction (
+			ingredient ["natures"] [ 0 ] ["amount"]
+		)
+		#assert (number_of_packages == 1), number_of_packages
+	
+		ingredient_measures = ingredient ["measures"]
+		for measure_name in ingredient_measures:
+			ingredient_measure = ingredient_measures [ measure_name ]
+	
+			if (measure_name in [ "biological activity" ]):
+				continue;
+			if (measure_name in [ "mass + mass equivalents", "energy" ]):
+				if (measure_name == "mass + mass equivalents"):
+					unit = "grams"
+				elif (measure_name == "biological activity"):
+					unit = "IU"
+				elif (measure_name == "energy"):
+					unit = "food calories"
+				else:
+					raise Exception ("?")
+			else:
+				raise Exception (f"Measure: '{ measure_name }' was not accounted for.") 
+			
+			addend = Fraction (
+				ingredient_measure [ "per recipe" ] [ unit ] [ "fraction string" ]
+			) * number_of_packages
+			
+			print ("addend:", float (addend))
+			
+			'''
+				This adds the ingredient amount to the land amount.
+			'''
+			if (measure_name in land_measures):
+				current_land_amount = Fraction (
+					land_measures [ measure_name ]["per recipe"][unit]["fraction string"]
+				)
+				if (records >= 1):
+					print ('"recipe" sums:')
+					print ('	current:', current_land_amount)
+					print ('	addend:', addend)
+					print ('	addition:', current_land_amount + addend)
+					print ()
+				
+				the_fraction_string = str (
+					current_land_amount +
+					addend
+				)
+				land_measures [ measure_name ] ["per recipe"] [ unit ] ["fraction string"] = the_fraction_string
+				land_measures [ measure_name ] ["per recipe"] [ unit ] ["scinote string"] = (
+					sci_note_2.produce (Fraction (the_fraction_string))
+				)
+				
+				
+			else:
+				raise Exception (f"The measure '{ measure_name }' was not accounted for.")
+			
+	return;
