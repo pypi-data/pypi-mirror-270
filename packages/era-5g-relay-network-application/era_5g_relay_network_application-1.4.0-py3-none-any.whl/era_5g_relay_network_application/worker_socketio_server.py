5037aafa28ced3c50e5fc2d3c5e9dae2e4f5333e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:05:43 +0000

    Add package era-5g-relay-network-application/1.4.0/era_5g_relay_network_application-1.4.0-py3-none-any.whl

---
+++ packages/era-5g-relay-network-application/era_5g_relay_network_application-1.4.0-py3-none-any.whl/era_5g_relay_network_application/worker_socketio_server.py
@@ -0,0 +1,28 @@
+import logging
+from multiprocessing.queues import Queue
+from typing import Any, Optional
+
+from socketio.exceptions import BadNamespaceError
+
+from era_5g_interface.exceptions import BackPressureException
+from era_5g_relay_network_application import SendFunctionProtocol
+from era_5g_relay_network_application.worker_socketio import WorkerSocketIO
+
+
+class WorkerSocketIOServer(WorkerSocketIO):
+    """Worker object for sending data to multiple subscribed clients over socket io."""
+
+    def __init__(self, queue: Queue, subscribers, send_function: Optional[SendFunctionProtocol], **kw):
+        super().__init__(queue, send_function, **kw)
+        self.subscribers = subscribers
+
+    def send_data(self, data: Any):
+        assert self.send_function
+
+        for s in self.subscribers:
+            try:
+                self.send_function(data, sid=s)
+            except BackPressureException:
+                logging.warning("Backpressure applied.")
+            except BadNamespaceError:
+                logging.warning("Trying to send data while not connected to the network application")
