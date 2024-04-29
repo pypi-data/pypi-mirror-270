8d229031b206619afe1162d4013a6c364d512f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:05:14 +0000

    Add package mkcyarray/1.0.0/mkcyarray-1.0.0.tar.gz

---
+++ packages/mkcyarray/mkcyarray-1.0.0.tar.gz/mkcyarray-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension, find_packages
+
+
+try:
+    from Cython.Build import cythonize
+    cython_directives = {
+        'embedsignature': True,
+    }
+    extensions = cythonize(
+        [Extension("mkcyarray.cyarray", ["src/mkcyarray/cyarray.pyx"])]
+    )
+except ImportError:
+    extensions = [Extension("mkcyarray.cyarray", ["src/mkcyarray/cyarray.c"])]
+
+setup(
+    name="mkcyarray",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    ext_modules=extensions,
+)
