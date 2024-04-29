f8ae515eb0cb0255d9550b726ef39905c03ce917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 17:07:10 +0000

    Add package unleashed-chat/0.1.4/unleashed_chat-0.1.4.tar.gz

---
+++ packages/unleashed-chat/unleashed_chat-0.1.4.tar.gz/unleashed_chat-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unleashed-chat',
+    version='0.1.4',
+    author="Reece Hunter",
+    description="A CLI wrapper for Unleashed.chat's chatbot service.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/reecehunter/unleashed-chat",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.7.1',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'openai'
+    ],
+    entry_points={
+        'console_scripts': [
+            'unleashed-chat=unleashed_chat.main:main',
+        ],
+    },
+)
