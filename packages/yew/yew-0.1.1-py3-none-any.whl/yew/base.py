6b9efd129324a32d49d603bfa943505a5a25fe4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:52 +0000

    Add package yew/0.1.1/yew-0.1.1-py3-none-any.whl

---
+++ packages/yew/yew-0.1.1-py3-none-any.whl/yew/base.py
@@ -0,0 +1,5 @@
+class ApiBase:
+    def __init__(self):
+        self.base_url = "https://prices.runescape.wiki/api/v1/osrs/"
+        self.rs_url = "https://secure.runescape.com/m=itemdb_oldschool/api"
+        self.user_agent = "GE APY"
