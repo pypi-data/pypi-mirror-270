6712245848fb9e7a7a8ca3d034a8f51577ab0dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:06:30 +0000

    Add package pytest-embedded-arduino/1.10.0/pytest_embedded_arduino-1.10.0.tar.gz

---
+++ packages/pytest-embedded-arduino/pytest_embedded_arduino-1.10.0.tar.gz/pytest_embedded_arduino-1.10.0/pytest_embedded_arduino/__init__.py
@@ -0,0 +1,11 @@
+"""Make pytest-embedded plugin work with Arduino."""
+
+from .app import ArduinoApp
+from .serial import ArduinoSerial
+
+__all__ = [
+    'ArduinoApp',
+    'ArduinoSerial',
+]
+
+__version__ = '1.10.0'
