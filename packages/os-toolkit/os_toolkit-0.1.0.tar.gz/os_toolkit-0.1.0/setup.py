546dbd6caaf106c2d07df718062e279076d4b9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 17:07:15 +0000

    Add package os-toolkit/0.1.0/os_toolkit-0.1.0.tar.gz

---
+++ packages/os-toolkit/os_toolkit-0.1.0.tar.gz/os_toolkit-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="package build on top of os and add more convient functionality",
+    name="os_toolkit",
+    version="0.1.0",
+    packages=find_packages(),
+    license="MIT",
+    requires=[]
+
+)
