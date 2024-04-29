2d94ac476241e2dc7bac3d2c7cb91b3aa5996449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:09 +0000

    Add package loggingpython/1.4.17/loggingpython-1.4.17-py3-none-any.whl

---
+++ packages/loggingpython/loggingpython-1.4.17-py3-none-any.whl/loggingpython/handler/csvhandler.py
@@ -0,0 +1,171 @@
+# MIT License
+#
+# Copyright (c) 2024 Mr-Major-K
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+This module defines the `CSVHandler` class, a component of the `loggingpython`
+package designed to handle logging messages by writing them to a CSV file. The
+`CSVHandler` class is a concrete implementation of the abstract `Handler`
+class, providing the functionality to output log messages in a structured and
+easily accessible format, making it suitable for further analysis or review.
+
+The `CSVHandler` class supports the creation of new log files based on the
+current date and allows customization of the log format string. It ensures
+that log messages are stored in a structured and easily accessible format,
+making it suitable for further analysis or review.
+
+Example usage:
+
+    from loggingpython.handler import CSVHandler
+
+    # Set up a CSV handler
+    csv_handler = CSVHandler('logfile', path='logs')
+
+    # Add the CSV handler to the logger
+    logger.addHandler(csv_handler)
+
+    # Log a message
+    logger.info('This is an informational message.')
+
+This module is part of the `loggingpython` package, which aims to provide a
+comprehensive logging solution for Python applications, including error
+handling and logging mechanisms for both client and server-side operations.
+"""
+
+import os
+from datetime import datetime
+import pandas as pd
+
+from .handler import Handler
+
+
+class CSVHandler(Handler):
+    """
+    `loggingpython`
+
+    A class for handling log messages in CSV format.
+
+    This class inherits from the Handler class and implements specific
+    methods for formatting and outputting log messages in CSV files. It
+    supports the creation of new log files based on the current date and
+    allows customization of the log format string. The CSVHandler ensures
+    that log messages are stored in a structured and easily accessible format,
+    making it suitable for further analysis or review.
+    """
+
+    def __init__(self,
+                 name: str,
+                 path: str = "logs",
+                 logformat_string: str = "%(asctime)s: [%(loggername)s]: \
+[%(loglevel)s]: %(message)s") -> None:
+        """
+        Initializes the CSVHandler with the given name, log path, and log
+            format string.
+
+        Args:
+            name (str): The name of the log file.
+            path (str, optional): The path where the log files will be
+                stored. Defaults to "logs".
+            logformat_string (str, optional): The format string for the log
+                messages. Defaults to "%(asctime)s: [%(loggername)s]:
+                [%(loglevel)s]: %(message)s".
+        """
+        self._mk_logdir(path)
+        self.name: str = name
+        self.path: str = path
+        self._current_date: str = datetime.now().strftime("%Y-%m-%d")
+        self.file: str = f"{self.path}/{self.name}_{self._current_date}.csv"
+        self._mk_logfile(self.file)
+        self.file = open(self.file, "a")
+
+        self.logformat_string: str = logformat_string
+
+    def emit(self, record: dict) -> None:
+        """
+        Writes a log record to the file.
+
+        Args:
+            record (dict): A dictionary containing the log record details.
+        """
+        formatted_message_values = self._format_message(record)
+
+        df = pd.DataFrame([formatted_message_values])
+
+        formatted_message = df.to_csv(index=False, header=False, sep=";",
+                                      lineterminator="\n")
+
+        self._update_file()
+        self.file.write(formatted_message)
+        self.file.flush()
+
+    def _update_file(self):
+        """
+        Updates the log file if the current date has changed.
+        """
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        if current_date != self._current_date:
+            self.current_date = current_date
+            self._close_file()
+            filename = f"{self.logpath}/{self.name}_{self._current_date}.csv"
+            self.file = open(filename, "a")
+
+    def _close_file(self):
+        """
+        Closes the current log file.
+        """
+        if self.file:
+            self.file.close()
+
+    def _mk_logdir(self, logpath: str) -> None:
+        """
+        Creates the log directory if it does not exist.
+
+        Args:
+            logpath (str): The path of the log directory.
+        """
+        if not os.path.exists(logpath):
+            os.makedirs(logpath)
+
+    def _mk_logfile(self, file: str) -> None:
+        """
+        Creates the log file if it does not exist.
+
+        Args:
+            file (str): The path of the log file.
+        """
+        if not os.path.exists(file):
+            os.open(file, os.O_CREAT)
+
+    def _get_datestemp(self) -> str:
+        """
+        Returns the current date in the format "YYYY-MM-DD".
+
+        Returns:
+            str: The current date.
+        """
+        return datetime.now().strftime("%Y-%m-%d")
+
+    def __repr__(self) -> str:
+        return f"CSVHandler({self.name}, {self.path}, {self.logformat_string})"
+
+    def __str__(self) -> str:
+        return f"CSVHandler with: {self.name}, {self.path} and \
+{self.logformat_string}"
---
+++ packages/loggingpython/loggingpython-1.4.17-py3-none-any.whl/loggingpython/jsonhandler.py
@@ -0,0 +1,148 @@
+import os
+import json
+from datetime import datetime
+
+from .handler import Handler
+
+
+class JSONHandler(Handler):
+    def __init__(self, name: str, path: str = "logs") -> None:
+        """
+        Initializes the FileHandler with the given name, log path, and log
+            format string.
+
+        Args:
+            name (str): The name of the log file.
+            path (str, optional): The path where the log files will be
+                stored. Defaults to "logs".
+            logformat_string (str, optional): The format string for the log
+                messages. Defaults to "%(asctime)s: [%(loggername)s]:
+                [%(loglevel)s]: %(message)s".
+        """
+        self._mk_logdir(path)
+        self.name: str = name
+        self.path: str = path
+        self._current_date: str = datetime.now().strftime("%Y-%m-%d")
+        self.file: str = f"{self.path}/{self.name}_{self._current_date}.json"
+        self._mk_logfile(self.file)
+        # Initialisieren Sie das JSON-Objekt hier
+        self.log_data: dict[str, str] = {}
+
+    def emit(self, record: dict) -> None:
+        """
+        Fügt eine Log-Nachricht zum JSON-Objekt hinzu und überprüft, ob das
+            Datum geändert wurde.
+
+        Args:
+            record (dict): Ein Wörterbuch, das die Details des Log-Eintrags
+                enthält.
+        """
+        # Generieren Sie einen Hash für die Log-Nachricht
+        message_hash = hash(str(record))
+        # Fügen Sie die Log-Nachricht zum JSON-Objekt hinzu
+        self.log_data[str(message_hash)] = record
+        # Überprüfen Sie, ob das Datum geändert wurde und aktualisieren Sie
+        # die Datei entsprechend
+        self._update_file()
+        # Schreiben Sie das JSON-Objekt in die Datei
+        self._write_log_data_to_file()
+
+    def _write_log_data_to_file(self) -> None:
+        """
+        Writes the JSON object to the file.
+        """
+        with open(self.file, 'w') as file:
+            file.write(json.dumps(self.log_data, indent=4))
+
+    def _update_file(self):
+        """
+        Updates the log file if the current date has changed.
+        """
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        if current_date != self._current_date:
+            self._current_date = current_date
+            self._close_file()
+            file: str = f"{self.path}/{self.name}_{self._current_date}.json"
+            self.file = open(file, "a")
+            self.log_data: dict[str, str] = {}
+
+    def _close_file(self):
+        """
+        Closes the current log file.
+        """
+        if self.file:
+            self.file.close()
+
+    def _mk_logdir(self, logpath: str) -> None:
+        """
+        Creates the log directory if it does not exist.
+
+        Args:
+            logpath (str): The path of the log directory.
+        """
+        if not os.path.exists(logpath):
+            os.makedirs(logpath)
+
+    def _mk_logfile(self, file: str) -> None:
+        """
+        Creates the log file if it does not exist.
+
+        Args:
+            file (str): The path of the log file.
+        """
+        if not os.path.exists(file):
+            os.open(file, os.O_CREAT)
+
+    def _get_datestemp(self) -> str:
+        """
+        Returns the current date in the format "YYYY-MM-DD".
+
+        Returns:
+            str: The current date.
+        """
+        return datetime.now().strftime("%Y-%m-%d")
+
+    def _format_message(self, record: dict) -> str:
+        """
+        Formats a log message based on the provided log data.
+
+        Args:
+            record (dict): A dictionary containing the log message details.
+
+        Returns:
+            str: The formatted log message.
+        """
+        values = {
+            "loggername": record.get("loggername", ""),
+            "iso_8601_time": record.get("iso_8601_time", ""),
+            "asctime": record.get("asctime", ""),
+            "loglevel": record.get("loglevel", ""),
+            "message": record.get("message", ""),
+        }
+
+        return self._format_in_json(values)
+
+    def _format_in_json(self, record: dict) -> dict:
+        """
+        Formats a log message based on the provided log data into a JSON
+            object with hashed keys.
+
+        Args:
+            record (dict): A dictionary containing the log message details.
+
+        Returns:
+            dict: The formatted log message as a JSON object with hashed keys.
+        """
+        sorted_keys = sorted(record.keys())
+        string_representation = str({key: record[key] for key in sorted_keys})
+        message_hash = hash(string_representation)
+        values = {
+            message_hash: record,
+        }
+        return values
+
+    def __repr__(self) -> str:
+        return f"JSONHandler:{self.name}, {self.path}, {self.logformat_string}"
+
+    def __str__(self) -> str:
+        return f"JSONHandler:{self.name}, {self.path}, {self.logformat_string}"
