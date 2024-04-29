3886502f368af38b08f6bff06ae51d77e6636ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:06:30 +0000

    Add package pytest-embedded-qemu/1.10.0/pytest_embedded_qemu-1.10.0-py3-none-any.whl

---
+++ packages/pytest-embedded-qemu/pytest_embedded_qemu-1.10.0-py3-none-any.whl/pytest_embedded_qemu/__init__.py
@@ -0,0 +1,18 @@
+"""Make pytest-embedded plugin work with QEMU."""
+
+DEFAULT_IMAGE_FN = 'flash_image.bin'
+ENCRYPTED_IMAGE_FN = f'encrypted_{DEFAULT_IMAGE_FN}'
+
+from .app import QemuApp  # noqa
+from .dut import QemuDut  # noqa
+from .qemu import Qemu  # noqa
+
+__all__ = [
+    'DEFAULT_IMAGE_FN',
+    'ENCRYPTED_IMAGE_FN',
+    'Qemu',
+    'QemuApp',
+    'QemuDut',
+]
+
+__version__ = '1.10.0'
