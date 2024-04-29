c4d3c3a14a83b06eff0b38b80a3ffc8aeca3e4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 05:05:47 +0000

    Add package reflex-native/0.0.0/reflex-native-0.0.0.tar.gz

---
+++ packages/reflex-native/reflex-native-0.0.0.tar.gz/reflex-native-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reflex-native',
+    version='0.0.0',
+    packages=find_packages(),
+    description='',
+    author='Cory',
+    author_email='',
+    url='',
+    license=''
+)
