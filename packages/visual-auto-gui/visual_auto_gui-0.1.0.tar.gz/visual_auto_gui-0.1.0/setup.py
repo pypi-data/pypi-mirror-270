764be03e03e71ed305ad104911452ca0010900d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:32 +0000

    Add package visual-auto-gui/0.1.0/visual_auto_gui-0.1.0.tar.gz

---
+++ packages/visual-auto-gui/visual_auto_gui-0.1.0.tar.gz/visual_auto_gui-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="automate your pc tasks such as clicking scrolling using python with Visual capability",
+    name="visual_auto_gui",
+    version="0.1.0",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[""],
+
+    # example
+    # install_requires=['pandas>=1.0',
+    # 'scipy==1.1',
+    # 'matplotlib>=2.2.1,<3'],
+    
+
+)
