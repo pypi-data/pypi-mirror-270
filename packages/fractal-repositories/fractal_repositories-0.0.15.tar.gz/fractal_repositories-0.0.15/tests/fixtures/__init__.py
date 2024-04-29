5851c42e89849e1f0a60a0123a2399c99b9a1337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:38 +0000

    Add package fractal-repositories/0.0.15/fractal_repositories-0.0.15.tar.gz

---
+++ packages/fractal-repositories/fractal_repositories-0.0.15.tar.gz/fractal_repositories-0.0.15/tests/fixtures/__init__.py
@@ -0,0 +1,6 @@
+from tests.fixtures.cloudstorage import *  # NOQA
+from tests.fixtures.django import *  # NOQA
+from tests.fixtures.firestore import *  # NOQA
+from tests.fixtures.mongo import *  # NOQA
+from tests.fixtures.repositories import *  # NOQA
+from tests.fixtures.sqlalchemy import *  # NOQA
