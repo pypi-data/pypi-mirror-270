044e7fd0aaface7e05cb1fc41237bb20b4bb7688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 04:04:59 +0000

    Add package authexchange/0.7.3/authexchange-0.7.3-py3-none-any.whl

---
+++ packages/authexchange/authexchange-0.7.3-py3-none-any.whl/authexchange/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A server to handle the OAuth token exchange with FusionAuth.
+"""
+__version__ = "0.7.3"
+__app_name__ = __name__  # for use in __main__ to avoid override
