ddfc4bc05e288a79135f577c62d4af51fc35971f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:50 +0000

    Add package py-string-tool/0.1.1/py_string_tool-0.1.1.tar.gz

---
+++ packages/py-string-tool/py_string_tool-0.1.1.tar.gz/py_string_tool-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    author= "Dear Norathee",
+    description="useful additional string functions",
+    name="py_string_tool",
+    version="0.1.1",
+    packages=find_packages(),
+    license="MIT",
+    requires=["thefuzz","difflib","langdetect"],
+    
+
+)
