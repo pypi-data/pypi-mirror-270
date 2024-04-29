f373523b804afe7c91b0104fafe7318cb87ce01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 14:04:56 +0000

    Add package WSI-PeruDB/0.1/WSI_PeruDB-0.1-py3-none-any.whl

---
+++ packages/WSI-PeruDB/WSI_PeruDB-0.1-py3-none-any.whl/libname/__init__.py
@@ -0,0 +1,7 @@
+from map import generate_map
+from FilterDepartment import deparment_information
+from lmwl_filtering import plot_lmwl
+from temporalseries import analyze_temporal_series
+from departments_comparison import compare_deparments
+from dataset_downloading import download_dataset
+from siteinformation_downloading import download_site_information
