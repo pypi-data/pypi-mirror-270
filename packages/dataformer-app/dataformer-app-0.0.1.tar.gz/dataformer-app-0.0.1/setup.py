83d42558535db7f1be6b35142d31ca61a8e78b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:28 +0000

    Add package dataformer-app/0.0.1/dataformer-app-0.0.1.tar.gz

---
+++ packages/dataformer-app/dataformer-app-0.0.1.tar.gz/dataformer-app-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dataformer-app',
+    version='0.0.1',
+    packages=find_packages(),
+    description='App to Create, Curate & Clean Datasets for Large Language Models.',
+    author='Bhabha AI',
+    author_email='contact@dataformer.ai',
+    url='https://github.com/BhabhaAI/dataformer-app',
+    install_requires=[
+        # Add dependencies here
+    ],
+)
