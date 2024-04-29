479ee72f163d6e1df58d41f799627b2d0d9779fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 03:04:04 +0000

    Add package shellinspector/0.13.0/shellinspector-0.13.0.tar.gz

---
+++ packages/shellinspector/shellinspector-0.13.0.tar.gz/shellinspector-0.13.0/docs/syntax.md
@@ -0,0 +1,282 @@
+# Syntax
+
+First, prepare a spec file, e.g. `ssh_connection.ispec`:
+
+```
+% whoami
+root
+% pwd
+/root
+```
+
+Then, run it:
+
+```
+$ python -m test tests/ssh_connection.ispec
+PASS % whoami
+PASS % pwd
+```
+
+## Syntax
+
+All lines (except expected output) start with a prefix character (`P`), followed
+by arguments (`command`). The following lines specify an expected output
+(`expected out put ...`).
+
+```
+P command
+expected
+output
+...
+```
+
+### Run commands
+
+To plainly interact with the shell as the root user or from within an uberspace,
+start the line with `$` (user) or `%` (root):
+
+```
+% whoami
+root
+$ whoami
+someuser
+```
+
+#### Matching
+
+The lines following the command line are compared to the stdout & stderr the
+command prints out. There are multiple
+
+##### Literal Match
+
+Ensure the output of the command matches the specified exactly:
+
+```
+% head -n2 /etc/passwd
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/usr/bin/nologin
+```
+
+Note that there is always a final new line after the expected output, so this
+will currently always fail:
+
+```
+$ echo -n a
+a
+```
+
+##### Regex Match
+
+Interpret the specified output as a regex with `re.MULTILINE` and try to match
+it:
+
+```
+%~ ls /home
+.*testuser.*
+%~ cat /etc/passwd
+^testuser
+$~ whoami
+\Atestuser\Z
+```
+
+Keep in mind that `\A` and `\Z` (not `^` and `$`) are needed to match start and
+end of the regex.
+
+New lines are only preserved between lines, but not after the final one.
+Otherwise this would never match, since `r"aaaa\n"` can't match `aaaa`.
+
+```
+$~ echo aaaab
+aaaa
+```
+
+##### Ignore
+
+To just discard and ignore the output, use `_`:
+
+```
+%_ ls /home
+%
+```
+
+### Includes
+
+If a set of command repeates over and over in different spec files, you can
+extract it into a dedicated file to be used in different files. For example,
+creating a user happens over and over. Put it into `create_user.ispec`.
+
+```
+% uberspace user create -u testuser
+```
+
+Then, include it into other files by using `<` and the path. Note that the path
+is always relative to the file that contains the include.
+
+```
+< create_user.ispec
+% ls /home
+testuser
+```
+
+### Comments
+
+Any lines starting with `#` are ignored:
+
+```
+# the next line will run the command `echo`:
+$ echo a
+a
+# this line does nothing
+# EOF
+```
+
+## Target user & machine
+
+By default commands run inside the uberspace8 VM as the specified user (`%` for
+root, `$` for the user last specified). To override this, expand the lines to
+look like a shell prompt:
+
+```
+# run on the local dev/ci machine
+[@local]$ hostname
+luto-portable
+# run as a different user on uberspace8 VM
+[usr1@remote]$ whoami
+usr1
+# run as the user last specified, in this case usr1
+$ whoam
+usr1
+```
+
+Using `$` without a specified user as the first `$`-command will cause an error.
+
+## Sessions
+
+You can connect to the test host more than once by adding `:session_name` to the
+username. Here we connect twice, change into two different directories and then
+check that the sessions are still separate:
+
+```
+[vagrant:session1@remote]$ cd photos
+[vagrant:session2@remote]$ cd videos
+[vagrant:session1@remote]$ pwd
+/home/vagrant/photos
+[vagrant:session2@remote]$ pwd
+/home/vagrant/videos
+```
+
+## Logout
+
+Use the `logout` command to terminate a session. If you use the same
+user/session-name/host again, a new one will start automatically.
+
+```
+[@local]$ echo a
+a
+[@local]$ logout
+[@local]$ echo b
+b
+```
+
+## Return Codes
+
+Exit codes of all commands are checked automatically. If the code is >0, the
+command is considered a failure:
+
+```
+$ python -m test false.ispec
+running false.ispec
+FAIL $ test -d /etc/nonexistant
+command failed (RC=1)
+```
+
+To ignore the check, hide the return code like so:
+
+```
+$ test -d /etc/nonexistant || true
+```
+
+## Environment
+
+To prepare the test envrionment, create a file called `test.ispec.env` next to
+`test.ispec`:
+
+`test.ispec`:
+
+```
+$ echo $DNS_SERVER
+1.1.1.1
+```
+
+`test.ispec.env`:
+
+```
+# which DNS server to test
+DNS_SERVER=1.1.1.1
+```
+
+## Parametrized tests
+
+To run many similar tests, create a file called `test.ispec.examples` next to
+`test.ispec`:
+
+`test.ispec`:
+
+```
+$~ {PY_EXE} --version
+{PY_VERSION}
+```
+
+`test.ispec.examples`
+
+```
+PY_EXE        PY_VERSION
+python3.10    3.10
+python3.11    3.11
+```
+
+## Python code
+
+Sometimes shellinspector checks are not enough, so you can also run python
+snippets like so:
+
+`test.ispec`:
+
+```
+! check_postgres_connection("testy")
+[@local]! set_env("did_python_run", "it did")
+[@local]$ echo $did_python_run
+it did
+```
+
+`test.ispec.py`
+
+```python
+def set_env(context, key, value):
+    context.env[key] = value
+    return True
+```
+
+The extra argument `ctx` is of type `ShellinspectorContext`, you can use the
+following attributes:
+
+- `.applied_example`, `dict`, (readonly): current config, see "Parametrized tests".
+- `.env`, `dict`, (read/write): shell environment variables.
+
+Return `True` to let this command pass, return error message as non-empty str
+to fail. Other values will error.
+
+Note that even though `[@local]!` specifies a host and user, the python code is
+always executed on the control machine. The host/user spec only determines which
+session is used for `ctx.env`.
+
+## Examples
+
+```
+# Create a user and check the default python version
+< create_user.ispec
+$~ python --version
+^Python 3.10
+$~ pip --version
+^pip.*python 3.10
+```
---
+++ packages/shellinspector/shellinspector-0.13.0.tar.gz/shellinspector-0.13.0/tests/test_parser.py
@@ -0,0 +1,398 @@
+from pathlib import Path
+
+import pytest
+
+from shellinspector.parser import AssertMode
+from shellinspector.parser import Command
+from shellinspector.parser import ExecutionMode
+from shellinspector.parser import parse
+
+
+def test_parse():
+    specfile = parse(
+        "/dev/null",
+        [
+            "[usr@]$ echo a",
+            "# ignored",
+            "a",
+            "$ echo b",
+            "b",
+            "% ls",
+            "file",
+            "dir",
+            "otherfile",
+            "%~ ls dir",
+            "file",
+            "! func()",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 0, errors
+    assert len(commands) == 5
+
+    assert commands[0].execution_mode == ExecutionMode.USER
+    assert commands[0].user == "usr"
+    assert commands[0].command == "echo a"
+    assert commands[0].assert_mode == AssertMode.LITERAL
+    assert commands[0].expected == "a\n"
+    assert commands[0].source_file == Path("/dev/null")
+    assert commands[0].source_line_no == 1
+    assert commands[1].execution_mode == ExecutionMode.USER
+    assert commands[1].user == "usr"
+    assert commands[1].command == "echo b"
+    assert commands[1].assert_mode == AssertMode.LITERAL
+    assert commands[1].expected == "b\n"
+    assert commands[1].source_file == Path("/dev/null")
+    assert commands[1].source_line_no == 4
+    assert commands[2].execution_mode == ExecutionMode.ROOT
+    assert commands[2].user == "root"
+    assert commands[2].command == "ls"
+    assert commands[2].assert_mode == AssertMode.LITERAL
+    assert commands[2].expected == "file\ndir\notherfile\n"
+    assert commands[2].source_file == Path("/dev/null")
+    assert commands[2].source_line_no == 6
+    assert commands[3].execution_mode == ExecutionMode.ROOT
+    assert commands[3].user == "root"
+    assert commands[3].command == "ls dir"
+    assert commands[3].assert_mode == AssertMode.REGEX
+    assert commands[3].source_file == Path("/dev/null")
+    assert commands[3].source_line_no == 10
+    assert commands[4].execution_mode == ExecutionMode.PYTHON
+    assert commands[4].command == "func()"
+    assert commands[4].assert_mode == AssertMode.LITERAL
+    assert commands[4].source_file == Path("/dev/null")
+    assert commands[4].source_line_no == 12
+
+
+def test_parse_whitespace_literal():
+    specfile = parse(
+        "/dev/null",
+        [
+            "% echo ab",
+            "a",
+            "b",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 0
+    assert len(commands) == 1
+
+    assert commands[0].expected == "a\nb\n"
+
+
+def test_parse_whitespace_regex():
+    specfile = parse(
+        "/dev/null",
+        [
+            "%~ /usr/bin/which --help",
+            "Usage: /usr/bin/which [options] [--] COMMAND [...]",
+            "Write the full path of COMMAND",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 0
+    assert len(commands) == 1
+
+    # no trailing newline at the end because this confuses the regex
+    assert (
+        commands[0].expected
+        == "Usage: /usr/bin/which [options] [--] COMMAND [...]\nWrite the full path of COMMAND"
+    )
+
+
+def test_parse_error_no_user():
+    specfile = parse(
+        "/dev/null",
+        [
+            "$ echo a",
+            "a",
+        ],
+    )
+    _, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 1
+    assert "not have a user specified" in errors[0].message
+
+
+def test_parse_error():
+    path = Path(__file__).parent / "virtual.ispec"
+    specfile = parse(
+        path,
+        [
+            "random text1",
+            "random text2",
+            "% ls1",
+            "file",
+            "% ls2",
+            "file",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 2
+    assert errors[0].source_file == path
+    assert errors[0].source_line_no == 1
+    assert errors[0].source_line == "random text1"
+    assert "output before first command," in errors[0].message
+    assert errors[1].source_file == path
+    assert errors[1].source_line_no == 2
+    assert errors[1].source_line == "random text2"
+    assert "output before first command," in errors[1].message
+
+    assert len(commands) == 2
+    assert commands[0].command == "ls1"
+    assert commands[0].expected
+    assert commands[0].source_line_no == 3
+    assert commands[1].command == "ls2"
+    assert commands[1].expected
+    assert commands[1].source_line_no == 5
+
+
+def test_parse_error_include():
+    path = Path(__file__).parent / "virtual.ispec"
+    specfile = parse(
+        path,
+        [
+            "% ls1",
+            "file",
+            "<data/test_error.ispec",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 1
+    assert errors[0].source_file == path.parent / "data/test_error.ispec"
+    assert errors[0].source_line_no == 1
+    assert errors[0].source_line == "a"
+
+    assert len(commands) == 2
+    assert commands[0].command == "ls1"
+    assert commands[0].expected
+    assert commands[0].source_file == path
+    assert commands[0].source_line_no == 1
+    assert commands[1].command == "ls2"
+    assert commands[1].expected
+    assert commands[1].source_file == path.parent / "data/test_error.ispec"
+    assert commands[1].source_line_no == 2
+
+
+def test_user_reuse():
+    specfile = parse(
+        "/dev/null",
+        [
+            "[someuser@]$ ls",
+            "$ ls",
+            "% ls",
+            "$ ls",
+            "[someuser@somehost]$ ls",
+            "$ ls",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 0
+
+    assert commands[0].user == "someuser"
+    assert commands[0].host == "remote"
+    assert commands[1].user == "someuser"
+    assert commands[1].host == "remote"
+    assert commands[2].user == "root"
+    assert commands[2].host == "remote"
+    assert commands[3].user == "someuser"
+    assert commands[3].host == "remote"
+    assert commands[4].user == "someuser"
+    assert commands[4].host == "somehost"
+    assert commands[5].user == "someuser"
+    assert commands[5].host == "somehost"
+
+
+def test_empty():
+    specfile = parse("/dev/null", [])
+    commands, errors = (specfile.commands, specfile.errors)
+    assert len(errors) == 0
+    assert len(commands) == 0
+
+
+@pytest.mark.parametrize(
+    "line,result",
+    [
+        (
+            "% ls",
+            {"execution_mode": ExecutionMode.ROOT, "assert_mode": AssertMode.LITERAL},
+        ),
+        (
+            "%~ ls",
+            {"execution_mode": ExecutionMode.ROOT, "assert_mode": AssertMode.REGEX},
+        ),
+        (
+            "%_ ls",
+            {"execution_mode": ExecutionMode.ROOT, "assert_mode": AssertMode.IGNORE},
+        ),
+        (
+            "[someuser@somehost]$ ls",
+            {
+                "execution_mode": ExecutionMode.USER,
+                "user": "someuser",
+                "host": "somehost",
+            },
+        ),
+        (
+            "[someuser@]$ ls",
+            {
+                "execution_mode": ExecutionMode.USER,
+                "user": "someuser",
+                "host": "remote",
+            },
+        ),
+        (
+            "[someuser:sess1@]$ ls",
+            {
+                "execution_mode": ExecutionMode.USER,
+                "user": "someuser",
+                "host": "remote",
+                "session_name": "sess1",
+            },
+        ),
+        (
+            "[someuser@local]% ls",
+            {"execution_mode": ExecutionMode.ROOT, "user": "root", "host": "local"},
+        ),
+        (
+            "[someuser@local]! ls",
+            {
+                "execution_mode": ExecutionMode.PYTHON,
+                "user": "someuser",
+                "host": "local",
+            },
+        ),
+    ],
+)
+def test_variants(line, result):
+    specfile = parse("/dev/null", [line])
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 0
+    assert commands[0].command == "ls"
+
+    for k, v in result.items():
+        assert getattr(commands[0], k) == v, k
+
+
+def test_include():
+    path = Path(__file__).parent / "virtual.ispec"
+    specfile = parse(
+        path,
+        [
+            "% ls",
+            "file",
+            "<data/test.ispec",
+            "% ls",
+            "file",
+        ],
+    )
+    commands, errors = (specfile.commands, specfile.errors)
+
+    assert len(errors) == 0
+    assert len(commands) == 3
+
+    assert commands[0].execution_mode == ExecutionMode.ROOT
+    assert commands[0].command == "ls"
+    assert commands[0].expected == "file\n"
+    assert commands[0].source_file == path
+    assert commands[0].source_line_no == 1
+    assert commands[1].execution_mode == ExecutionMode.ROOT
+    assert commands[1].command == "whoami"
+    assert commands[1].expected == "root\n"
+    assert commands[1].source_file == Path(__file__).parent / "data/test.ispec"
+    assert commands[1].source_line_no == 1
+    assert commands[2].execution_mode == ExecutionMode.ROOT
+    assert commands[2].command == "ls"
+    assert commands[2].expected == "file\n"
+    assert commands[2].source_file == path
+    assert commands[2].source_line_no == 4
+
+
+def test_environment():
+    path = Path(__file__).parent / "e2e/500_envtest.ispec"
+    specfile = parse(path, [])
+
+    assert len(specfile.errors) == 0
+
+    assert specfile.environment == {
+        "something": "else",
+        "withspace": "with space",
+        "valuewithequals": "e=quals",
+        # provided twice, last value counts
+        "number": "2",
+        "quotetest": "'",
+    }
+
+
+def test_examples():
+    path = Path(__file__).parent / "e2e/600_examplestest.ispec"
+    specfile = parse(path, [])
+
+    assert len(specfile.errors) == 0
+
+    assert specfile.examples == [
+        {
+            "PY_COMMAND": "python3.10",
+            "PY_VERSION": "3.10",
+        },
+        {
+            "PY_COMMAND": "python3.11",
+            "PY_VERSION": "3.11",
+        },
+    ]
+
+
+def test_include_missing_file():
+    path = Path(__file__).parent / "virtual.ispec"
+    specfile = parse(
+        path,
+        [
+            "% ls",
+            "file",
+            "<data/test_not_existent.ispec",
+            "% ls",
+            "file",
+        ],
+    )
+
+    assert len(specfile.errors) == 1
+    assert "test_not_existent.ispec does not exist" in specfile.errors[0].message
+
+
+def test_command_short_literal():
+    cmd = Command(
+        ExecutionMode.USER,
+        "echo a",
+        None,
+        None,
+        "local",
+        AssertMode.LITERAL,
+        "a\nb\n",
+        "/some.ispec",
+        1,
+        "$ echo a\nb",
+    )
+    assert cmd.short == "USER(None@local) `echo a` (expect 2 lines, LITERAL)"
+
+
+def test_command_short_regex():
+    cmd = Command(
+        ExecutionMode.USER,
+        "echo a",
+        None,
+        None,
+        "local",
+        AssertMode.REGEX,
+        "a\nb",
+        "/some.ispec",
+        1,
+        "$ echo a\nb",
+    )
+    assert cmd.short == "USER(None@local) `echo a` (expect 2 lines, REGEX)"
