dc345d7bac9cb9b4819fd215517fd807526dda71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 19:05:50 +0000

    Add package django-environment-config/0.0.2/django_environment_config-0.0.2.tar.gz

---
+++ packages/django-environment-config/django_environment_config-0.0.2.tar.gz/django_environment_config-0.0.2/env_config/constants.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+__all__ = [
+    "Undefined",
+]
+
+
+class Undefined:  # pragma: no cover
+    """Value to represent an undefined value."""
+
+    def __repr__(self) -> str:
+        return self.__class__.__name__
+
+    def __bool__(self) -> bool:
+        return False
+
+
+Undefined = Undefined()
