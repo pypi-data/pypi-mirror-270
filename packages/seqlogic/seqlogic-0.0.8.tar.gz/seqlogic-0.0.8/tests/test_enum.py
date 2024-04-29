27e2654b5ee6804258f80bdeb9dcd645a6da6e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-29 18:04:30 +0000

    Add package seqlogic/0.0.8/seqlogic-0.0.8.tar.gz

---
+++ packages/seqlogic/seqlogic-0.0.8.tar.gz/seqlogic-0.0.8/tests/test_enum.py
@@ -0,0 +1,89 @@
+"""Test seqlogic.lbool.VecEnum."""
+
+# pyright: reportArgumentType=false
+# pyright: reportAttributeAccessIssue=false
+# pyright: reportCallIssue=false
+
+import pytest
+
+from seqlogic.lbool import VecEnum, ones, zeros
+
+
+class Color(VecEnum):
+    """Boilerplate bit array enum."""
+
+    RED = "2b00"
+    GREEN = "2b01"
+    BLUE = "2b10"
+
+
+def test_empty():
+    class Empty(VecEnum):
+        pass
+
+    e = Empty()
+    assert len(e) == 0
+    assert e.name == ""
+    assert e.data == 0
+    assert str(e) == ""
+    assert Empty() is e
+
+
+def test_basic():
+    assert len(Color.RED) == 2
+    assert Color.RED.name == "RED"
+    assert Color.RED.data == 0b0101
+    assert str(Color.RED) == "2b00"
+    assert Color("2b00") is Color.RED
+
+    assert len(Color.GREEN) == 2
+    assert Color.GREEN.name == "GREEN"
+    assert Color.GREEN.data == 0b0110
+    assert str(Color.GREEN) == "2b01"
+    assert Color("2b01") is Color.GREEN
+
+    assert len(Color.BLUE) == 2
+    assert Color.BLUE.name == "BLUE"
+    assert Color.BLUE.data == 0b1001
+    assert str(Color.BLUE) == "2b10"
+    assert Color("2b10") is Color.BLUE
+
+    assert len(Color.X) == 2
+    assert Color.X.name == "X"
+    assert Color.X.data == 0
+    assert str(Color.X) == "2bXX"
+    assert Color("2bXX") is Color.X
+
+    with pytest.raises(ValueError):
+        _ = Color("2b11")
+
+
+def test_slicing():
+    assert Color.GREEN[0] == ones(1)
+    assert Color.GREEN[1] == zeros(1)
+
+
+def test_enum_error():
+    """Test enum spec errors."""
+    with pytest.raises(ValueError):
+
+        class InvalidName(VecEnum):
+            X = "4bXXXX"
+
+        _ = InvalidName()
+
+    # The literal must be a str
+    with pytest.raises(TypeError):
+
+        class InvalidType(VecEnum):
+            FOO = 42
+
+        _ = InvalidType()
+
+    with pytest.raises(ValueError):
+
+        class InvalidMembers(VecEnum):
+            A = "2b00"
+            B = "3b000"
+
+        _ = InvalidMembers()
---
+++ packages/seqlogic/seqlogic-0.0.8.tar.gz/seqlogic-0.0.8/tests/test_struct.py
@@ -0,0 +1,58 @@
+"""Test seqlogic.lbool.VecStruct."""
+
+# PyRight is confused by MetaClass behavior
+# pyright: reportArgumentType=false
+# pyright: reportAttributeAccessIssue=false
+# pyright: reportCallIssue=false
+
+from seqlogic.lbool import Vec, VecStruct, vec
+
+
+class Pixel(VecStruct):
+    r: Vec[8]
+    g: Vec[8]
+    b: Vec[8]
+
+
+def test_empty():
+    class EmptyStruct(VecStruct):
+        pass
+
+    s = EmptyStruct()
+    assert len(s) == 0
+    assert s.data == 0
+    assert str(s) == "EmptyStruct()"
+
+
+def test_basic():
+    p = Pixel(r=vec("8b0001_0001"), g=vec("8b0010_0010"), b=vec("8b0100_0100"))
+
+    assert str(p.r) == "8b0001_0001"
+    assert str(p.g) == "8b0010_0010"
+    assert str(p.b) == "8b0100_0100"
+
+    assert str(p) == "Pixel(r=8b0001_0001, g=8b0010_0010, b=8b0100_0100)"
+    assert repr(p) == (
+        "Pixel("
+        "r=Vec[8](0b0101011001010110)"
+        ", g=Vec[8](0b0101100101011001)"
+        ", b=Vec[8](0b0110010101100101))"
+    )
+
+    assert len(p) == 24
+
+    # Partial assignment
+    p = Pixel()
+    assert str(p) == "Pixel(r=8bXXXX_XXXX, g=8bXXXX_XXXX, b=8bXXXX_XXXX)"
+    p = Pixel(r=vec("8b0001_0001"))
+    assert str(p) == "Pixel(r=8b0001_0001, g=8bXXXX_XXXX, b=8bXXXX_XXXX)"
+    p = Pixel(g=vec("8b0010_0010"))
+    assert str(p) == "Pixel(r=8bXXXX_XXXX, g=8b0010_0010, b=8bXXXX_XXXX)"
+    p = Pixel(b=vec("8b0100_0100"))
+    assert str(p) == "Pixel(r=8bXXXX_XXXX, g=8bXXXX_XXXX, b=8b0100_0100)"
+    p = Pixel(r=vec("8b0001_0001"), g=vec("8b0010_0010"))
+    assert str(p) == "Pixel(r=8b0001_0001, g=8b0010_0010, b=8bXXXX_XXXX)"
+    p = Pixel(r=vec("8b0001_0001"), b=vec("8b0100_0100"))
+    assert str(p) == "Pixel(r=8b0001_0001, g=8bXXXX_XXXX, b=8b0100_0100)"
+    p = Pixel(g=vec("8b0010_0010"), b=vec("8b0100_0100"))
+    assert str(p) == "Pixel(r=8bXXXX_XXXX, g=8b0010_0010, b=8b0100_0100)"
