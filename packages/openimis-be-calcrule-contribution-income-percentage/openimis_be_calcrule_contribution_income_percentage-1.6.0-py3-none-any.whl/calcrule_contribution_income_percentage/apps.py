55e68a1c8ad9ceadabce5e4b4350645b16d58926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:31 +0000

    Add package openimis-be-calcrule-contribution-income-percentage/1.6.0/openimis_be_calcrule_contribution_income_percentage-1.6.0-py3-none-any.whl

---
+++ packages/openimis-be-calcrule-contribution-income-percentage/openimis_be_calcrule_contribution_income_percentage-1.6.0-py3-none-any.whl/calcrule_contribution_income_percentage/apps.py
@@ -0,0 +1,27 @@
+import importlib
+import inspect
+from django.apps import AppConfig
+from calculation.apps import CALCULATION_RULES
+
+from core.abs_calculation_rule import AbsCalculationRule
+
+
+MODULE_NAME = "calcrule_contribution_income_percentage"
+DEFAULT_CFG = {}
+
+
+def read_all_calculation_rules():
+    """function to read all calculation rules from that module"""
+    for name, cls in inspect.getmembers(importlib.import_module(".calculation_rule", "calcrule_contribution_income_percentage"), inspect.isclass):
+        if 'calcrule' in cls.__module__.split('.')[0]:
+            CALCULATION_RULES.append(cls)
+            cls.ready()
+
+
+class CalculationRuleFSIncomePercentageConfig(AppConfig):
+    name = MODULE_NAME
+
+    def ready(self):
+        from core.models import ModuleConfiguration
+        cfg = ModuleConfiguration.get_or_default(MODULE_NAME, DEFAULT_CFG)
+        read_all_calculation_rules()
