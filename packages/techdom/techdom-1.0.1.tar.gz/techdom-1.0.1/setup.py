96c58fec83ec6cd9b95aea33298511ada7159939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:05 +0000

    Add package techdom/1.0.1/techdom-1.0.1.tar.gz

---
+++ packages/techdom/techdom-1.0.1.tar.gz/techdom-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='techdom',
+    version='1.0.1',
+    packages=find_packages(),
+    author='abc',
+    author_email='dragonslayer461313@gmail.com',
+    description='Simple library for techdoms',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/FranklyShovelTrap/TechdomLibrary',
+    install_requires=[
+        'screeninfo',
+        'pillow',
+        'pyttsx3',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    package_data={
+        '': ['*.py', 'scripts/*']
+    }
+)
