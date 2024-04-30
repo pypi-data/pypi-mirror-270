0a640d474f4bf23d1f1b71a27d02ef627b1e42af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 03:04:32 +0000

    Add package prediction-market-agent-tooling/0.18.0/prediction_market_agent_tooling-0.18.0-py3-none-any.whl

---
+++ packages/prediction-market-agent-tooling/prediction_market_agent_tooling-0.18.0-py3-none-any.whl/prediction_market_agent_tooling/markets/markets.py
@@ -0,0 +1,54 @@
+import typing as t
+from datetime import datetime
+from enum import Enum
+
+from prediction_market_agent_tooling.markets.agent_market import (
+    AgentMarket,
+    FilterBy,
+    SortBy,
+)
+from prediction_market_agent_tooling.markets.manifold.manifold import (
+    ManifoldAgentMarket,
+)
+from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
+from prediction_market_agent_tooling.markets.polymarket.polymarket import (
+    PolymarketAgentMarket,
+)
+
+
+class MarketType(str, Enum):
+    MANIFOLD = "manifold"
+    OMEN = "omen"
+    POLYMARKET = "polymarket"
+
+    @property
+    def market_class(self) -> type[AgentMarket]:
+        if self not in MARKET_TYPE_TO_AGENT_MARKET:
+            raise ValueError(f"Unknown market type: {self}")
+        return MARKET_TYPE_TO_AGENT_MARKET[self]
+
+
+MARKET_TYPE_TO_AGENT_MARKET: dict[MarketType, type[AgentMarket]] = {
+    MarketType.MANIFOLD: ManifoldAgentMarket,
+    MarketType.OMEN: OmenAgentMarket,
+    MarketType.POLYMARKET: PolymarketAgentMarket,
+}
+
+
+def get_binary_markets(
+    limit: int,
+    market_type: MarketType,
+    filter_by: FilterBy = FilterBy.OPEN,
+    sort_by: SortBy = SortBy.NONE,
+    excluded_questions: set[str] | None = None,
+    created_after: datetime | None = None,
+) -> t.Sequence[AgentMarket]:
+    agent_market_class = MARKET_TYPE_TO_AGENT_MARKET[market_type]
+    markets = agent_market_class.get_binary_markets(
+        limit=limit,
+        sort_by=sort_by,
+        filter_by=filter_by,
+        created_after=created_after,
+        excluded_questions=excluded_questions,
+    )
+    return markets
