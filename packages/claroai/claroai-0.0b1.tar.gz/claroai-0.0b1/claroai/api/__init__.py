6d6ba0d2fb9734eab5c4f27a65dd7da4183d9f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 16:05:55 +0000

    Add package claroai/0.0b1/claroai-0.0b1.tar.gz

---
+++ packages/claroai/claroai-0.0b1.tar.gz/claroai-0.0b1/claroai/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from claroai.api.conversation_api import ConversationApi
+from claroai.api.interaction_api import InteractionApi
+from claroai.api.prediction_api import PredictionApi
+
