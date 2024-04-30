361468f0fdad5b6a14b76465b16ff615ebebc93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:21 +0000

    Add package eprime_data/0.2.dev0/eprime_data-0.2.dev0.tar.gz

---
+++ packages/eprime_data/eprime_data-0.2.dev0.tar.gz/eprime_data-0.2.dev0/README.md
@@ -0,0 +1,5 @@
+# E-Prime-Data
+
+Python library &amp; command line tool to handle and convert txt-data file from E-prime
+
+(c) Oliver Lindemann
---
+++ packages/eprime_data/eprime_data-0.2.dev0.tar.gz/eprime_data-0.2.dev0/eprime_data/__init__.py
@@ -0,0 +1,16 @@
+"""handling e-prime txt data files"""
+
+__version__ = "0.2.dev0"
+__author__ = "Oliver Lindemann"
+
+import sys as _sys
+from .lib import EPrimeData
+
+
+if _sys.version_info[0] != 3 or _sys.version_info[1] < 10:
+
+    raise RuntimeError("{} {} ".format("EPrime-data", __version__) +
+                        "is not compatible with Python {0}.{1}.".format(
+        _sys.version_info[0],
+        _sys.version_info[1]) +
+        "\n\nPlease use Python 3.10 or higher.")
