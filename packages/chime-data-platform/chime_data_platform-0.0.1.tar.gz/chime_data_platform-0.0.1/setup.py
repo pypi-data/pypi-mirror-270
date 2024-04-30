50f9835fe35f687df0c3fe84a08774c9db0735d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:03 +0000

    Add package chime-data-platform/0.0.1/chime_data_platform-0.0.1.tar.gz

---
+++ packages/chime-data-platform/chime_data_platform-0.0.1.tar.gz/chime_data_platform-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='chime-data-platform',
+    packages=find_packages(include=['chime-data-platform']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
