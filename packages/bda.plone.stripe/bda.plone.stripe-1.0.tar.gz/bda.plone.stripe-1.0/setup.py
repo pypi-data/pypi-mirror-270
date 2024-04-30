af0650a7bc253d90773aa4286cb4a93f28177969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 16:06:08 +0000

    Add package bda.plone.stripe/1.0/bda.plone.stripe-1.0.tar.gz

---
+++ packages/bda.plone.stripe/bda.plone.stripe-1.0.tar.gz/bda.plone.stripe-1.0/setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+
+def read_file(name):
+    with open(os.path.join(os.path.dirname(__file__), name)) as f:
+        return f.read()
+
+
+version = "1.0"
+shortdesc = "Stripe payment for bda.plone.shop"
+longdesc = '\n\n'.join([read_file(name) for name in [
+    'README.rst',
+    'CHANGES.rst',
+    'LICENSE.rst'
+]])
+
+
+setup(
+    name='bda.plone.stripe',
+    version=version,
+    description=shortdesc,
+    long_description=longdesc,
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Addon",
+        "Framework :: Zope",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    author='BlueDynamics Alliance',
+    author_email='dev@bluedynamics.com',
+    license='GNU General Public Licence',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages=['bda', 'bda.plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'bda.plone.shop',
+        'stripe',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
