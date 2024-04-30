7236237a69709592bf1f4c700fa20e0a4bc69b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:31 +0000

    Add package crudlfap/0.7.43/crudlfap-0.7.43.tar.gz

---
+++ packages/crudlfap/crudlfap-0.7.43.tar.gz/crudlfap-0.7.43/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import sys
+
+
+setup(
+    name='crudlfap',
+    setup_requires='setupmeta',
+    version='0.7.43',
+    description='Rich frontend for generic views with Django',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/crudlfap',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    keywords='django crud',
+    install_requires=[
+        'django>=4.2,<5',
+        'jinja2',
+        'django-jinja',
+        'django-bootstrap3',
+        'django-material-1.3-dj32==1.3.3',
+        'django-tables2',
+        'django-filter',
+        'timeago',
+        'lookupy-unmanaged',
+        'sql-metadata',
+    ],
+    tests_require=['tox'],
+    extras_require=dict(
+        dev=[
+          'django-collectdir',
+          'django-reversion',
+          'django-debug-toolbar',
+          'django-extensions',
+          'devpy',
+          'dj-static',
+        ],
+    ),
+    entry_points={
+        'console_scripts': [
+            'crudlfap = crudlfap_example.manage:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3',
+)
