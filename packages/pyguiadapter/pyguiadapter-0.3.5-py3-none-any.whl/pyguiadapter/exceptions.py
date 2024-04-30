35baf3fcb138c22edf01853567ccf35eb76856b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 03:05:14 +0000

    Add package pyguiadapter/0.3.5/pyguiadapter-0.3.5-py3-none-any.whl

---
+++ packages/pyguiadapter/pyguiadapter-0.3.5-py3-none-any.whl/pyguiadapter/exceptions.py
@@ -0,0 +1,14 @@
+class AlreadyExistsError(Exception):
+    pass
+
+
+class NotExistError(Exception):
+    pass
+
+
+class AppNotStartedError(Exception):
+    pass
+
+
+class ClassInitCancelled(Exception):
+    pass
