6b65215977ea5d0e66149466f399738e14cbbf6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:11 +0000

    Add package adilsm/0.0.12/adilsm-0.0.12.tar.gz

---
+++ packages/adilsm/adilsm-0.0.12.tar.gz/adilsm-0.0.12/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+requirements = """
+adnmtf==0.1.164
+scipy==1.9.1 
+"""
+
+description = "Integrated Latent Multi Source Model"
+
+setup(
+    name="adilsm",
+    version='0.0.12',
+    author="Mazars",
+    author_email="victor.chemla@mazars.fr",
+    description=f"{description}",
+    packages=find_packages(),
+    install_requires=requirements,
+)
