3d34a32fb69d34b22fb2507f567f68075d841555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:08 +0000

    Add package csulb-dataset/0.1.4/csulb_dataset-0.1.4.tar.gz

---
+++ packages/csulb-dataset/csulb_dataset-0.1.4.tar.gz/csulb_dataset-0.1.4/csulb_mock_dataset/data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+
+def get_data():
+    data_path = os.path.join(os.path.dirname(__file__), '../data/cmp1.csv')
+    data = pd.read_csv(data_path)
+    print(data.columns.tolist())  # This prints the list of headers
+    return pd.read_csv(data_path)
+
+get_data()
