f938cd3516b3450de97fa6e521c31174bfbe4096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:15 +0000

    Add package csulb-mock-dataset/0.1.10/csulb_mock_dataset-0.1.10-py3-none-any.whl

---
+++ packages/csulb-mock-dataset/csulb_mock_dataset-0.1.10-py3-none-any.whl/csulb_mock_dataset/data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+def get_data():
+    data_path = os.path.join(os.path.dirname(__file__), 'data', 'cmp1.csv')
+    print(f"Attempting to read CSV from: {data_path}")
+    data = pd.read_csv(data_path)
+    print(data.columns.tolist())  # This prints the list of headers
+    return pd.read_csv(data_path)
+
+get_data()
