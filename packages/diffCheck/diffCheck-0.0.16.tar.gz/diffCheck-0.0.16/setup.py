c87fe98a5d68f9652b64506d871d86fc448857af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:47 +0000

    Add package diffCheck/0.0.16/diffCheck-0.0.16.tar.gz

---
+++ packages/diffCheck/diffCheck-0.0.16.tar.gz/diffCheck-0.0.16/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+
+
+
+setup(
+    name="diffCheck",
+    version="0.0.16",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pybind11>=2.5.0"
+        # other dependencies...
+    ],
+    description="DiffCheck is a package to check the differences between two timber structures",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Andrea Settimi, Damien Gilliard, Eleni Skevaki, Marirena Kladeftira, Julien Gamerro, Stefana Parascho, and Yves Weinand",
+    author_email="andrea.settimi@epfl.ch",
+    url="https://github.com/diffCheckOrg/diffCheck",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    # include_package_data=True,
+    package_data={
+        "diffCheck": ["*.dll", "*.pyd"]
+        },
+
+    # ext_modules=ext_modules,
+    # cmdclass={"build_ext": build_ext},
+    # zip_safe=False,
+)
