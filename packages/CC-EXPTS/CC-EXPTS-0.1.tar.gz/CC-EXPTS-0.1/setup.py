574adcd51c7ea9d4e15ccdcd7a36c77153cdaff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:07 +0000

    Add package CC-EXPTS/0.1/CC-EXPTS-0.1.tar.gz

---
+++ packages/CC-EXPTS/CC-EXPTS-0.1.tar.gz/CC-EXPTS-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CC-EXPTS",
+    version="0.1",
+    description="CC EXPERIMETNS",
+    author="Ishan",
+    author_email="ishannaik7@gmail.com",
+    packages=find_packages(),
+    package_data={"CC-EXPTS": ["*"]},
+)
