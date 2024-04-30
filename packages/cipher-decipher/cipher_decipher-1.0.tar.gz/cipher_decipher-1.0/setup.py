af9aa40b833f411c642b842e6fda4baea480c52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 16:06:01 +0000

    Add package cipher-decipher/1.0/cipher_decipher-1.0.tar.gz

---
+++ packages/cipher-decipher/cipher_decipher-1.0.tar.gz/cipher_decipher-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+    name="cipher_decipher",
+    version="1.0",
+    packages=find_packages(),
+    author="koushik katakam",
+    description="A Spy's Whisper: Encryption & Decryption Of Messages Using Python",
+    long_description=open('D:\Demooo\cipherdecipher_module\cipher-and-decipher\cipherdecipher.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown'
+)
