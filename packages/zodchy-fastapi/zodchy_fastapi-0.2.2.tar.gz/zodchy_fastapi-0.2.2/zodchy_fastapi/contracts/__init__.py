3d853673375e0b06430a49400f97797b51c2abba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:09:38 +0000

    Add package zodchy-fastapi/0.2.2/zodchy_fastapi-0.2.2.tar.gz

---
+++ packages/zodchy-fastapi/zodchy_fastapi-0.2.2.tar.gz/zodchy_fastapi-0.2.2/zodchy_fastapi/contracts/__init__.py
@@ -0,0 +1,18 @@
+from .request import (
+    Request,
+    RequestModel
+)
+
+from .response import (
+    Response,
+    ResponseModel,
+    DataModel,
+    QueryResultResponseModel,
+    PaginationModel,
+    PaginatedResponseModel,
+    ResponseEvent,
+    ResponseError
+)
+from .tools import (
+    RequestModelAdapter
+)
