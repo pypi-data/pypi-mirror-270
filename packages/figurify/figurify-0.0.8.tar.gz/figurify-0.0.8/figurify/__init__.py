23b69cf01b82308379cb51535ec27d81858a734b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:34 +0000

    Add package figurify/0.0.8/figurify-0.0.8.tar.gz

---
+++ packages/figurify/figurify-0.0.8.tar.gz/figurify-0.0.8/figurify/__init__.py
@@ -0,0 +1,36 @@
+from figurify.plane_metrics.circle import *
+from figurify.plane_metrics.ellipse import *
+from figurify.plane_metrics.parallelogram import *
+from figurify.plane_metrics.polygon import *
+from figurify.plane_metrics.quadrilateral import *
+from figurify.plane_metrics.rectangle import *
+from figurify.plane_metrics.trapezoid import *
+from figurify.plane_metrics.triangle import *
+
+from figurify.geometric_solids.cone import *
+from figurify.geometric_solids.cube import *
+from figurify.geometric_solids.cuboid import *
+from figurify.geometric_solids.cylinder import *
+from figurify.geometric_solids.prism import *
+from figurify.geometric_solids.pyramid import *
+from figurify.geometric_solids.sphere import *
+from figurify.geometric_solids.torus import *
+
+from figurify.figure_physics.dynamics import *
+from figurify.figure_physics.energy import *
+from figurify.figure_physics.forces import *
+from figurify.figure_physics.gravity import *
+from figurify.figure_physics.kinematics import *
+from figurify.figure_physics.momentum import *
+
+from figurify.units import *
+from figurify.material_properties import *
+from figurify import *
+
+from figurify import plane_metrics
+from figurify import geometric_solids
+from figurify import figure_physics
+
+from figurify.plane_metrics import *
+from figurify.geometric_solids import *
+from figurify.figure_physics import *
