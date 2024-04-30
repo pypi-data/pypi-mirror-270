ed9686e05e51a6a88cbfbcda2523672fc4e8e177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:24 +0000

    Add package trellisplot/0.0.4/trellisplot-0.0.4.tar.gz

---
+++ packages/trellisplot/trellisplot-0.0.4.tar.gz/trellisplot-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4' 
+DESCRIPTION = 'Trellis plot tool'
+LONG_DESCRIPTION = 'Package to plot trellis functionality. Updated with width normalization, new default colormap, and reduced boxplot'
+
+# Setting up
+setup(
+        name="trellisplot", 
+        version=VERSION,
+        author="Ethan S. Lee",
+        author_email="<sukraelee@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['matplotlib', 'numpy', 'seaborn', 'pandas', 'scipy'],
+        
+        keywords=['python', 'trellisplot'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
