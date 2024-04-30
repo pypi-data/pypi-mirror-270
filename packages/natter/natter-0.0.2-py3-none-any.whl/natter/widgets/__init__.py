ffd6025b2ff7a30180dd37d694c3f8d7a1d326b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:09:40 +0000

    Add package natter/0.0.2/natter-0.0.2-py3-none-any.whl

---
+++ packages/natter/natter-0.0.2-py3-none-any.whl/natter/widgets/__init__.py
@@ -0,0 +1,12 @@
+"""The widgets for the application."""
+
+##############################################################################
+# Local imports.
+from .output import Agent, Error, Output, User
+from .user_input import UserInput
+
+##############################################################################
+# Exports.
+__all__ = ["Agent", "Error", "User", "Output", "UserInput"]
+
+### __init__.py ends here
