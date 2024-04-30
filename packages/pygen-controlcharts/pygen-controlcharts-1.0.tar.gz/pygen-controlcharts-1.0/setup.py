aa3267e66cbd567001b694360088e4421f52d327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 15:04:36 +0000

    Add package pygen-controlcharts/1.0/pygen-controlcharts-1.0.tar.gz

---
+++ packages/pygen-controlcharts/pygen-controlcharts-1.0.tar.gz/pygen-controlcharts-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+	name='pygen-controlcharts',
+	version='1.0',
+	author='andrey140',
+	author_email='andrexa33455@gmail.com',
+	description='Will add',
+	url='https://github.com/SpbskvA/math-stat.git',
+	packages=['pygen-controlcharts'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
