7cad0f429f3d70fb876b2a2b1fee072102763ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:09:44 +0000

    Add package sympytensor/1.2.1/sympytensor-1.2.1-py3-none-any.whl

---
+++ packages/sympytensor/sympytensor-1.2.1-py3-none-any.whl/sympytensor/__init__.py
@@ -0,0 +1,5 @@
+from sympytensor.pymc import SympyDeterministic
+from sympytensor.pytensor import as_tensor, pytensor_function
+
+__version__ = "1.2.1`"
+__all__ = ["as_tensor", "pytensor_function", "SympyDeterministic"]
