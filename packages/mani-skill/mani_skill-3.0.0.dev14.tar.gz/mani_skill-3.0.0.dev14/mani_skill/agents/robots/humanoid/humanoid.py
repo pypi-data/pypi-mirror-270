51a115557fa4d9d46dc069f3f030977675f47f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 15:04:31 +0000

    Add package mani-skill/3.0.0.dev14/mani_skill-3.0.0.dev14.tar.gz

---
+++ packages/mani-skill/mani_skill-3.0.0.dev14.tar.gz/mani_skill-3.0.0.dev14/mani_skill/agents/robots/humanoid/humanoid.py
@@ -0,0 +1,66 @@
+import numpy as np
+import sapien
+
+from mani_skill import PACKAGE_ASSET_DIR
+from mani_skill.agents.base_agent import BaseAgent, Keyframe
+from mani_skill.agents.controllers import *
+from mani_skill.agents.registration import register_agent
+from mani_skill.sensors.camera import CameraConfig
+
+
+@register_agent()
+class Humanoid(BaseAgent):
+    uid = "humanoid"
+    mjcf_path = f"{PACKAGE_ASSET_DIR}/robots/humanoid/humanoid.xml"
+    urdf_config = dict()
+    fix_root_link = False  # False as there is a freejoint on the root body
+
+    keyframes = dict(
+        squat=Keyframe(
+            qpos=np.array(
+                [
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.12,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    0.0,
+                    -0.25,
+                    -0.25,
+                    -0.25,
+                    -0.25,
+                    -0.5,
+                    -0.5,
+                    -1.3,
+                    -1.3,
+                    -0.8,
+                    -0.8,
+                    0.0,
+                    0.0,
+                ]
+            ),
+            pose=sapien.Pose(p=[0, 0, -0.375]),
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def _controller_configs(self):
+        pd_joint_delta_pos = PDJointPosControllerConfig(
+            [j.name for j in self.robot.active_joints],
+            -1,
+            1,
+            damping=5,
+            stiffness=20,
+            force_limit=100,
+            use_delta=True,
+        )
+        return dict(pd_joint_delta_pos=pd_joint_delta_pos)
+
+    def _after_init(self):
+        pass
---
+++ packages/mani-skill/mani_skill-3.0.0.dev14.tar.gz/mani_skill-3.0.0.dev14/mani_skill/envs/tasks/__init__.py
@@ -0,0 +1,9 @@
+from .control import *
+from .dexterity import RotateSingleObjectInHand, RotateValveEnv
+from .empty_env import EmptyEnv
+from .fmb import FMBAssembly1Env
+from .humanoid import *
+from .open_cabinet_drawer import OpenCabinetDoorEnv, OpenCabinetDrawerEnv
+from .quadruped import *
+from .rotate_cube import RotateCubeEnv
+from .tabletop import *
