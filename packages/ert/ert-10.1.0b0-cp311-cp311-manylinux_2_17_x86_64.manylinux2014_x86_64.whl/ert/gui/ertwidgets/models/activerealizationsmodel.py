af2ce8b14fa8b06ae276e107bbf6c131e5dd4f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:11:25 +0000

    Add package ert/10.1.0b0/ert-10.1.0b0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-10.1.0b0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/gui/ertwidgets/models/activerealizationsmodel.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from ert.gui.ertwidgets.models.valuemodel import ValueModel
+from ert.validation import ActiveRange, mask_to_rangestring
+
+
+class ActiveRealizationsModel(ValueModel):
+    def __init__(self, ensemble_size: int):
+        self.ensemble_size = ensemble_size
+        ValueModel.__init__(self, self.getDefaultValue())
+        self._custom = False
+
+    def setValue(self, value: str):
+        if not value or not value.strip() or value == self.getDefaultValue():
+            self._custom = False
+            ValueModel.setValue(self, self.getDefaultValue())
+        else:
+            self._custom = True
+            ValueModel.setValue(self, value)
+
+    def setValueFromMask(self, mask):
+        self.setValue(mask_to_rangestring(mask))
+
+    def getDefaultValue(self):
+        size = self.ensemble_size
+        return f"0-{size-1:d}"
+
+    def getActiveRealizationsMask(self) -> List[bool]:
+        return ActiveRange(rangestring=self.getValue(), length=self.ensemble_size).mask
