8f3dc6241664feea70ad01a7f3c4ea2e8b75184e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:17 +0000

    Add package sapit/1.4/sapit-1.4.tar.gz

---
+++ packages/sapit/sapit-1.4.tar.gz/sapit-1.4/sapit/__init__.py
@@ -0,0 +1,4 @@
+from .main import say
+from .main import save
+from .main import say_female
+from .main import say_male
---
+++ packages/sapit/sapit-1.4.tar.gz/sapit-1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sapit',
+    version = '1.4',
+    description = 'A Text-to-speech module',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    author_email = 'alilodhibusiness@gmail.com',
+    author = 'Ali Lodhi',
+    packages = ['sapit'],
+    install_requires = [
+        'pyttsx3 == 2.90',
+        'pydub == 0.25.1',
+        'ffmpeg == 1.4'
+    ],
+
+    entry_points = {
+        "console_scripts": [
+            "sapit = sapit:say",
+        ],
+    },
+)
