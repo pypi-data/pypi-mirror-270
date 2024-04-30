f27dc96eac31b2f488dd4d9c074ac1b460d52cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:36 +0000

    Add package flink-sdk/0.0.1/flink_sdk-0.0.1.tar.gz

---
+++ packages/flink-sdk/flink_sdk-0.0.1.tar.gz/flink_sdk-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='flink-sdk',
+    packages=find_packages(include=['flink-sdk']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
