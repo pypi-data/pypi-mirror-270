aebcd6630860edf7a2a3dbbac3ed16884e211bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:09:43 +0000

    Add package PyAeroAcoustics/0.0.2/PyAeroAcoustics-0.0.2.tar.gz

---
+++ packages/PyAeroAcoustics/PyAeroAcoustics-0.0.2.tar.gz/PyAeroAcoustics-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'PyAeroAcoustics'
+LONG_DESCRIPTION = 'PyAeroAcoustics'
+
+setup(
+        name="PyAeroAcoustics", 
+        version=VERSION,
+        author="Thomas Brunner",
+        author_email="<thomas.brunner@tugraz.at>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'PyAeroAcoustics'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
