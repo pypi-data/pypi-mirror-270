69062414bc4294cb239ef313c676a0513387ff8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:35 +0000

    Add package datarecipe/1.2.12/datarecipe-1.2.12.tar.gz

---
+++ packages/datarecipe/datarecipe-1.2.12.tar.gz/datarecipe-1.2.12/datarecipe/__init__.py
@@ -0,0 +1,20 @@
+name = "datarecipe"
+
+from .common_tools import(
+    send_email,
+    local_to_df,
+    df_to_xlsx,
+    df_to_csv
+)
+
+from .mysql import(
+    update_data,
+    clear_db,
+    fetch_table_data,
+    sql_query,
+    load_db_config
+)
+
+from .examine import(
+    check_empty
+)
