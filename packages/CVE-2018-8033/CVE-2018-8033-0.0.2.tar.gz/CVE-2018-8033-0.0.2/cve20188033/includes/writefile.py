fd3f0587c2a47c3d8df483039ff93d1dab2b162a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:24 +0000

    Add package CVE-2018-8033/0.0.2/CVE-2018-8033-0.0.2.tar.gz

---
+++ packages/CVE-2018-8033/CVE-2018-8033-0.0.2.tar.gz/CVE-2018-8033-0.0.2/cve20188033/includes/writefile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+ * CVE-2018-8033
+ * CVE-2018-8033 Bug scanner for WebPentesters and Bugbounty Hunters
+ *
+ * @Developed By Cappricio Securities <https://cappriciosec.com>
+ */
+
+
+"""
+
+
+def writedata(output, data):
+    with open(output, 'a') as file:
+        file.write(data)
