7016749d29e24993121d290607c5492ba50f9d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:10:45 +0000

    Add package bleuscore/0.1.2/bleuscore-0.1.2.tar.gz

---
+++ packages/bleuscore/bleuscore-0.1.2.tar.gz/bleuscore-0.1.2/src/ngram.rs
@@ -0,0 +1,116 @@
+use counter::Counter;
+use std::collections::HashMap;
+
+/// Here the tokens' type is `&[String]` rather than `&Vec<String>`
+/// to fix `clippy::not_unsafe_ptr_arg_deref` error.
+pub fn get_token_ngram_counter(tokens: &[String], max_order: usize) -> HashMap<&[String], usize> {
+    let mut count_map: HashMap<&[String], usize> = HashMap::new();
+    for order in 1..=max_order {
+        for start_index in 0..(tokens.len().saturating_sub(order - 1)) {
+            let ngram = &tokens[start_index..(start_index + order)];
+            count_map
+                .entry(ngram)
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+        }
+    }
+    count_map
+}
+
+/// TODO: change to use Counter to count ngram
+#[allow(dead_code)]
+fn get_ngram_counter(line: &str, max_order: usize) -> Counter<&str> {
+    let mut counts: Counter<&str> = Counter::new();
+    for order in 1..=max_order {
+        for start_index in 0..(line.len().saturating_sub(order - 1)) {
+            // println!("line: {}, start_index: {}, order: {}", line, start_index, order);
+            let ngram = &line[start_index..(start_index + order)];
+            // println!("ngram: {}", ngram);
+            counts[&ngram] += 1;
+        }
+    }
+    counts
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ngram::{get_ngram_counter, get_token_ngram_counter};
+
+    #[test]
+    fn test_get_token_ngram_short() {
+        let tokens = vec!["a".to_string(), "b".to_string()];
+        let counter = get_token_ngram_counter(&tokens, 4);
+        assert_eq!(counter[&tokens[0..=0]], 1);
+        assert_eq!(counter[&tokens[1..=1]], 1);
+        assert_eq!(counter[&tokens[0..=1]], 1);
+    }
+
+    #[test]
+    fn test_get_token_ngram_long() {
+        // aabc
+        let tokens: Vec<String> = vec![
+            "a".to_string(),
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+        ];
+        let counter = get_token_ngram_counter(&tokens, 4);
+        assert_eq!(counter[&tokens[0..=0]], 2); // 'a': 2
+        assert_eq!(counter[&tokens[2..=2]], 1); // 'b': 1
+        assert_eq!(counter[&tokens[3..=3]], 1); // 'c': 1
+
+        assert_eq!(counter[&tokens[0..=1]], 1); // 'aa': 1
+        assert_eq!(counter[&tokens[1..=2]], 1); // 'ab': 1
+        assert_eq!(counter[&tokens[2..=3]], 1); // 'bc': 1
+
+        assert_eq!(counter[&tokens[0..=2]], 1); // 'aab': 1
+        assert_eq!(counter[&tokens[1..=3]], 1); // 'abc': 1
+        assert_eq!(counter[&tokens[0..=3]], 1); // 'abcd': 1
+
+        assert_eq!(counter.len(), 9);
+    }
+
+    #[test]
+    fn test_get_ngram_short() {
+        let counter = get_ngram_counter("ab", 4);
+        assert_eq!(counter[&"a"], 1);
+        assert_eq!(counter[&"b"], 1);
+        assert_eq!(counter[&"ab"], 1);
+    }
+
+    #[test]
+    fn test_get_ngram_long() {
+        let counter = get_ngram_counter("aabc", 4);
+        assert_eq!(counter[&"a"], 2);
+        assert_eq!(counter[&"b"], 1);
+        assert_eq!(counter[&"c"], 1);
+        assert_eq!(counter[&"d"], 0);
+
+        assert_eq!(counter[&"aa"], 1);
+        assert_eq!(counter[&"ab"], 1);
+        assert_eq!(counter[&"bc"], 1);
+        assert_eq!(counter[&"ac"], 0);
+
+        assert_eq!(counter[&"aab"], 1);
+        assert_eq!(counter[&"aabc"], 1);
+    }
+}
+
+#[cfg(test)]
+mod benchmark {
+    use crate::ngram::get_ngram_counter;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_ngram(b: &mut Bencher) {
+        let line = "aabc";
+        let max_order = 4;
+
+        let iter_num: usize = 100;
+        b.iter(|| {
+            std::hint::black_box(for _ in 1..=iter_num {
+                get_ngram_counter(line, max_order);
+            });
+        });
+    }
+}
