d8ee3e9739f6401e5981004fe2cf9d619a6a362f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:36 +0000

    Add package tkfontawesome-mizraith/0.3/tkfontawesome_mizraith-0.3.tar.gz

---
+++ packages/tkfontawesome-mizraith/tkfontawesome_mizraith-0.3.tar.gz/tkfontawesome_mizraith-0.3/development/test.py
@@ -0,0 +1,15 @@
+from ctypes import windll
+
+windll.user32.SetProcessDPIAware()
+
+import tkinter as tk
+from tkfontawesome_mizraith import icon_to_image
+
+root = tk.Tk()
+fb = icon_to_image("facebook", fill="#4267B2", scale_to_width=64)
+send = icon_to_image("paper-plane", fill="#1D9F75", scale_to_width=64)
+
+tk.Label(root, image=fb).pack(padx=10, pady=10)
+tk.Button(root, image=send).pack(padx=10, pady=10)p
+
+root.mainloop()
