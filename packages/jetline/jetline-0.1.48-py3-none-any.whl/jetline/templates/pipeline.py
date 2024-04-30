f6a4680aef10c19ad328fed6136a27b27cc64585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:32 +0000

    Add package jetline/0.1.48/jetline-0.1.48-py3-none-any.whl

---
+++ packages/jetline/jetline-0.1.48-py3-none-any.whl/jetline/templates/pipeline.py
@@ -0,0 +1,16 @@
+# from jetline.pipeline.pipeline import Pipeline
+# from jetline.pipeline.node import Node
+# from first_pipe import nodes as func
+#
+#
+# def register(data_manager) -> Pipeline:
+#     return Pipeline(
+#         nodes=[
+#             Node(
+#                 function=func.node_function_1,
+#                 inputs=["Name"],
+#                 outputs=["Name"],
+#                 viz={"y": 150, "x": 0, "sources": ['Name']}
+#             ),
+#         ],
+#         data_manager=data_manager)
