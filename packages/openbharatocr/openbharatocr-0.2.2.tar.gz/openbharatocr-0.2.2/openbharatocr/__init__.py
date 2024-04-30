f85eb36987c95cee6c13465b5320ea49aefd3124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:36 +0000

    Add package openbharatocr/0.2.2/openbharatocr-0.2.2.tar.gz

---
+++ packages/openbharatocr/openbharatocr-0.2.2.tar.gz/openbharatocr-0.2.2/openbharatocr/__init__.py
@@ -0,0 +1,24 @@
+# author:    Kunal Kumar Kushwaha
+# website:   http://www.essentia.dev
+
+# import the required packages
+from openbharatocr.ocr.api import (
+    pan,
+    front_aadhaar,
+    back_aadhaar,
+    driving_licence,
+    passport,
+    voter_id_front,
+    voter_id_back,
+)
+
+
+__all__ = [
+    "pan",
+    "front_aadhaar",
+    "back_aadhaar",
+    "driving_licence",
+    "passport",
+    "voter_id_front",
+    "voter_id_back",
+]
