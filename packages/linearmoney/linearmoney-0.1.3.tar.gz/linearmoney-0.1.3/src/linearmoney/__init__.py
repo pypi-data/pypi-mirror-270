db58d5ee4a86481ced87cc6736f308087b9540f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 15:04:24 +0000

    Add package linearmoney/0.1.3/linearmoney-0.1.3.tar.gz

---
+++ packages/linearmoney/linearmoney-0.1.3.tar.gz/linearmoney-0.1.3/src/linearmoney/__init__.py
@@ -0,0 +1,36 @@
+"""A library for working with money algebraically."""
+
+from __future__ import annotations
+
+__all__ = [
+    "vector",
+    "scalar",
+    "data",
+    "exceptions",
+    "resources",
+    "mixins",
+    "cache",
+]
+
+
+import logging
+
+# Setup logging
+logger = logging.getLogger(__name__)
+
+logger.info("linearmoney root logger initialized.")
+
+
+from . import (  # noqa: E402
+    vector,
+    cache,
+    data,
+    exceptions,
+    scalar,
+    mixins,
+    resources,
+)
+
+__version__ = "0.1.3"
+
+CLDR_VERSION = resources.get_package_resource("cldr_version")
