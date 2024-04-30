2eff52dadb15f91e6f6c29de43261ae39968a11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:23 +0000

    Add package eprime_data/0.2.dev1/eprime_data-0.2.dev1.tar.gz

---
+++ packages/eprime_data/eprime_data-0.2.dev1.tar.gz/eprime_data-0.2.dev1/eprime_data/dataframe.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from .lib import EPrimeData
+
+try:
+    import pandas as pd
+except ModuleNotFoundError as err:
+    raise RuntimeError("To using dataframe function please install Pandas!") from err
+
+
+def dataframe(eprime_data:EPrimeData,
+              level: int, add_subject_id=False):
+    """returns Pandas dataframe"""
+    return pd.DataFrame(eprime_data.data(level=level, add_subject_id=add_subject_id))
+
+
+def save_to_feather(eprime_data:EPrimeData,
+                    arrow_file: Path | str,
+                    level: int,
+                    add_subject_id:bool=False,
+                    compression: str = "zstd"):
+
+    arrow_file = Path(arrow_file)
+    rtn = dataframe(eprime_data, level, add_subject_id=add_subject_id)
+    rtn.to_feather(arrow_file, compression=compression)
+    return rtn
+
+
