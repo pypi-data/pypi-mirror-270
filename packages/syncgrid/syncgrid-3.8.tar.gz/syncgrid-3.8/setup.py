a0674c6dc56fa35238303e0b3e497336e39f3b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:03 +0000

    Add package syncgrid/3.8/syncgrid-3.8.tar.gz

---
+++ packages/syncgrid/syncgrid-3.8.tar.gz/syncgrid-3.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='syncgrid',
+      version='3.8',
+      description='syncgrid, library for distributed systems',
+      packages=['syncgrid'],
+      author_email='syncgrid@gmail.com',
+      zip_safe=False,
+      )
---
+++ packages/syncgrid/syncgrid-3.8.tar.gz/syncgrid-3.8/syncgrid/__init__.py
@@ -0,0 +1,12 @@
+from .main import calculateSecondsForNodeTime
+
+# 1.Implement multithreaded application. 
+# 2.Implement Inter-process communication for addition of two numbers.
+# 3.Implement Inter-process communication for subtraction of two numbers.
+# 4.Implement Inter-process communication for multiplication of two numbers.
+# 5.Implement Inter-process communication for division of two numbers.
+# 6.Implement Bully Election Algorithm.
+# 7.Implement Load Balancing algorithm
+# 8.Implement Berkley Clock Synchronization Algorithm
+# 9.Implement Deadlock management
+# 10.Implement Ring Mutual Exclusion algorithm
