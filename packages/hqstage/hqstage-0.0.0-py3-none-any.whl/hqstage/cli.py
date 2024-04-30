d6973aa60f6d7e5fa4114d43ad23a41ff854a0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 03:04:25 +0000

    Add package hqstage/0.0.0/hqstage-0.0.0-py3-none-any.whl

---
+++ packages/hqstage/hqstage-0.0.0-py3-none-any.whl/hqstage/cli.py
@@ -0,0 +1,28 @@
+###
+# Created Date: 2024-04-29
+# Author: Sebastian Lehmann
+# -----
+# Copyright © 2024 HQS Quantum Simulations GmbH. All Rights Reserved.
+###
+
+"""Stub of the HQStage cli."""
+
+
+def main() -> None:
+    """Main."""
+    print(
+        """
+
+--------------------------------------------------------------
+  This package is a stub. Currently, hqstage can NOT be
+  installed via pypi.org.
+
+  For instructions on how to install and get started
+  using the real hqstage package, please visit
+  https://docs.cloud.quantumsimulations.de/.
+
+  Thank you very much!
+--------------------------------------------------------------
+
+    """
+    )
