e646957fd82da7a6776742ad7e0dd8928bc5414f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:08:45 +0000

    Add package ecv-python-development/0.0.58/ecv_python_development-0.0.58-py3-none-any.whl

---
+++ packages/ecv-python-development/ecv_python_development-0.0.58-py3-none-any.whl/ecv_python_development/database/_pynamodb/base.py
@@ -0,0 +1,30 @@
+import os
+
+from pynamodb.attributes import (
+    BooleanAttribute,
+    MapAttribute,
+    NumberAttribute,
+    UnicodeAttribute,
+)
+from pynamodb.indexes import AllProjection, GlobalSecondaryIndex
+from pynamodb.models import Model as PynamoDBModel
+
+PYDANTIC_PYNAMO_MAPPING = {
+    str: UnicodeAttribute,
+    int: NumberAttribute,
+    float: NumberAttribute,
+    bool: BooleanAttribute,
+    dict: MapAttribute,
+}
+
+
+class PynamoDBBaseModel(PynamoDBModel):
+    class Meta:
+        table_name = ""
+        region = os.environ.get("AWS_REGION", "us-east-1")
+
+
+class PynamoDBIndexModel(GlobalSecondaryIndex):
+    class Meta:
+        index_name = ""
+        projection = AllProjection()
