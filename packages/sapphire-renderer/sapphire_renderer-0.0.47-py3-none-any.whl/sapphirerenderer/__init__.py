b8874f4ce7358ffe252321793c4aba0b9c4c848a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:09:37 +0000

    Add package sapphire-renderer/0.0.47/sapphire_renderer-0.0.47-py3-none-any.whl

---
+++ packages/sapphire-renderer/sapphire_renderer-0.0.47-py3-none-any.whl/sapphirerenderer/__init__.py
@@ -0,0 +1,6 @@
+from .src.sapphirerenderer.utility_objects.particle_manager import ParticleManager
+
+from .src.sapphirerenderer.object_classes.base_object import Object
+from .src.sapphirerenderer.object_classes.flat_faces_object import FlatFacesObject
+from .src.sapphirerenderer.object_classes.text_object import TextObject
+from .src.sapphirerenderer.object_classes.wireframe_object import WireframeObject
