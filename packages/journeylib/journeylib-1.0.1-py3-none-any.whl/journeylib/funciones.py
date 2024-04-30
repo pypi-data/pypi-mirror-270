135fc33ac274545901f1ed47ca43918f6269f4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:33 +0000

    Add package journeylib/1.0.1/journeylib-1.0.1-py3-none-any.whl

---
+++ packages/journeylib/journeylib-1.0.1-py3-none-any.whl/journeylib/funciones.py
@@ -0,0 +1,25 @@
+#Imports necesarios para las funciones de la libreria
+import requests
+from fastapi.responses import JSONResponse
+
+#Metodo de prueba para el despliege de la libreria
+def hola_journey():
+    print('Holitaa, esta es una librería diseñada y publicada por JourneyGen.')
+
+
+
+#Implementacion de las funciones necesarias para la libreria
+"""
+METODO INSERTAR HISTORICO
+Descripcion: Inserta un nuevo par de mensajes a la bd de CASH
+"""
+def ins_historico(usr, num_chat, lista_msg):
+    
+    datos_post = { # Formato con el que se consume la api
+        'id_usuario': usr,
+        'num_chat': num_chat,
+        'msg': lista_msg
+    }
+    response = requests.post('http://ismi.fi.upm.es:8080/insertar/chat', json=datos_post)
+
+    return JSONResponse(content=response.json(), status_code=response.status_code)
