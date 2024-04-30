33259fa0f34998a4628146b632542b4bda86fa8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:36 +0000

    Add package cltrier_nlp/0.1.0/cltrier_nlp-0.1.0.tar.gz

---
+++ packages/cltrier_nlp/cltrier_nlp-0.1.0.tar.gz/cltrier_nlp-0.1.0/cltrier_nlp/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print("Nothing Here Just Yet!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/cltrier_nlp/cltrier_nlp-0.1.0.tar.gz/cltrier_nlp-0.1.0/cltrier_nlp/corpus/__init__.py
@@ -0,0 +1,72 @@
+import collections
+import typing
+import string
+
+import nltk
+import pydantic
+
+from . import util
+from .sentence import Sentence
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+
+class Corpus(pydantic.BaseModel):
+    raw: str
+    sentences: typing.List[Sentence] = []
+
+    def model_post_init(self, __context) -> None:
+        if not self.sentences:
+            self.sentences = [
+                Sentence(content=sent)
+                for sent in
+                nltk.tokenize.sent_tokenize(
+                    self.raw,
+                    language=util.detect_language(self.raw)
+                )
+            ]
+
+    @pydantic.computed_field
+    @property
+    def tokens(self) -> typing.List[str]:
+        return [
+            tok
+            for sent in self.sentences
+            for tok in sent.tokens
+        ]
+
+    def count_languages(self) -> collections.Counter:
+        return collections.Counter([sent.language for sent in self.sentences])
+
+    def count_tokens(self) -> collections.Counter:
+        __stopwords = list(set().union(*[
+            nltk.corpus.stopwords.words(lang)
+            for lang in self.count_languages().keys()
+            if lang in nltk.corpus.stopwords.fileids()
+        ]))
+
+        return collections.Counter([
+            tok for tok in self.tokens
+            if tok not in [*__stopwords, *string.punctuation]
+        ])
+
+    def create_subset_by_language(self, language: str) -> 'Corpus':
+        return Corpus(
+            sentences=(
+                sentences := [
+                    sent for sent in self.sentences
+                    if sent.language == language
+                ]
+            ),
+            raw=' '.join([
+                sent.content for sent in sentences
+            ])
+        )
+
+    @classmethod
+    def from_txt(cls, path: str) -> 'Corpus':
+        return cls(raw=open(path).read())
+
+    def __len__(self) -> int:
+        return len(self.sentences)
