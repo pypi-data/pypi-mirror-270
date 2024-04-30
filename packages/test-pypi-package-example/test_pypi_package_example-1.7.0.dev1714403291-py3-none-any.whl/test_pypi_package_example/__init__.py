5ab5234a156d6638dd6dd27e601aa09ddbbc8a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:36 +0000

    Add package test-pypi-package-example/1.7.0.dev1714403291/test_pypi_package_example-1.7.0.dev1714403291-py3-none-any.whl

---
+++ packages/test-pypi-package-example/test_pypi_package_example-1.7.0.dev1714403291-py3-none-any.whl/test_pypi_package_example/__init__.py
@@ -0,0 +1,14 @@
+###
+# #%L
+# aiSSEMBLE::Foundation::Test Python
+# %%
+# Copyright (C) 2021 Booz Allen
+# %%
+# This software package is licensed under the Booz Allen Public License. All Rights Reserved.
+# #L%
+###
+__version__ = "0.1.0"
+
+from main import *
+
+hello = HelloWorld()
