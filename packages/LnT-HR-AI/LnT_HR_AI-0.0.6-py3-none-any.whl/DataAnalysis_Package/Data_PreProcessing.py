6667c4d549b6e2135a13bf26cf3cdb72bf3809bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 16:05:32 +0000

    Add package LnT-HR-AI/0.0.6/LnT_HR_AI-0.0.6-py3-none-any.whl

---
+++ packages/LnT-HR-AI/LnT_HR_AI-0.0.6-py3-none-any.whl/DataAnalysis_Package/Data_PreProcessing.py
@@ -0,0 +1,217 @@
+
+def DataPreProcessing():
+    # Library for Data Manipulation
+    import numpy as np
+    import pandas as pd
+    # Library for Statistical Modelling
+    from sklearn.preprocessing import LabelEncoder
+    import tkinter as tk
+    from tkinter import filedialog
+    root = tk.Tk()
+    root.withdraw() # Hide the root window
+    print("Select the file IBM-HR-Analytics-Employee-Attrition-and-Performance.csv")
+
+    # Open the file dialog and get the selected file path
+    file_path = filedialog.askopenfilename()
+    
+    # Print the selected file path
+    address=""
+    for iterator in file_path:
+        if iterator=="/" :
+            address+="\\\\"
+        else:
+            address+=iterator
+
+        
+    # Close the application
+    root.destroy()
+    
+    # Library for Ignore the warnings
+    import warnings
+    warnings.filterwarnings('always')
+    warnings.filterwarnings('ignore')
+
+    
+    employee_data = pd.read_csv(address)
+
+    # Print top 5 rows in the dataframe.
+
+    employee_data["PerformanceRating"].value_counts()
+    #employee_data.head().style.set_properties(**{'background-color': '#E9F6E2','color': 'black','border-color': '#8b8c8c'})
+
+    # Print bottom 5 rows in the dataframe.
+    employee_data.tail().style.set_properties(**{'background-color': '#E9F6E2','color': 'black','border-color': '#8b8c8c'})
+    ## <span style='color:blue'> 1] COMPUTING SIZE OF DATASET </span>
+
+    # Print the shape of the DataFrame
+    print("The shape of data frame:", employee_data.shape)
+    # Print the length (number of rows) of the DataFrame
+    print("Number of Rows in the dataframe:", len(employee_data))
+    # Print the number of columns in the DataFrame
+    print("Number of Columns in the dataframe:", len(employee_data.columns))
+
+    ## <span style='color:blue'> 2] ENLIST COLUMNS OF DATASET </span>
+
+    print("Column labels in the dataset in column order:")
+    for column in employee_data.columns:
+        print(column)
+
+    ## <span style='color:blue'> 3] GENERATING BASIC INFORMATION OF ATTRIBUTES </span>
+
+    # Print the Long summary of the dataframe by setting verbose = True
+    # Check for Non-Null or Nan Nalues in the dataset.
+    print(employee_data.info(verbose = True))
+
+    ### <font color=red>Inference:</font>
+
+    ## <span style='color:blue'> 4] ENLISTING NUMERICAL FEATURES </span>
+
+    employee_data.select_dtypes(np.number).sample(5).style.set_properties(**{'background-color': '#E9F6E2',
+                                                                'color': 'black','border-color': '#8b8c8c'})
+
+    ### <font color=red>Inference:</font>
+    ### 4.1] Labelling Categories in Numerical Feature
+
+    employee_data["Education"] = employee_data["Education"].replace({1:"Below College",2:"College",3:"Bachelor",4:"Master",5:"Doctor"})
+
+    employee_data["EnvironmentSatisfaction"] = employee_data["EnvironmentSatisfaction"].replace({1:"Low",2:"Medium",3:"High",4:"Very High"})
+
+    employee_data["JobInvolvement"] = employee_data["JobInvolvement"].replace({1:"Low",2:"Medium",3:"High",4:"Very High"})
+
+    employee_data["JobLevel"] = employee_data["JobLevel"].replace({1:"Entry Level",2:"Junior Level",3:"Mid Level",4:"Senior Level",
+                                            5:"Executive Level"})
+
+    employee_data["JobSatisfaction"] = employee_data["JobSatisfaction"].replace({1:"Low",2:"Medium",3:"High",4:"Very High"})
+
+    employee_data["PerformanceRating"] = employee_data["PerformanceRating"].replace({1:"Low",2:"Good",3:"Excellent",4:"Outstanding"})
+
+    employee_data["RelationshipSatisfaction"] = employee_data["RelationshipSatisfaction"].replace({1:"Low",2:"Medium",3:"High",4:"Very High"})
+
+    employee_data["WorkLifeBalance"] = employee_data["WorkLifeBalance"].replace({1:"Bad",2:"Good",3:"Better",4:"Best"})
+
+    ## <span style='color:blue'> 5] ENLISTING CATEGORICAL FEATURES </span>
+
+    employee_data.select_dtypes(include="O").sample(5).style.set_properties(**{'background-color': '#E9F6E2',
+                                                                    'color': 'black','border-color': '#8b8c8c'})
+
+    ## <span style='color:blue'> 6] CHECK FOR MISSING VALUES </span>
+
+    x=0
+    Infield=[]
+    for j, i in zip(employee_data["JobRole"], employee_data["EducationField"]):
+        
+        if (j =="Sales Executive" or j=="Human Resources" or j=="Sales Representative") and (i =="Medical"or  i=="Technical Degree"):
+            Infield.append(1)      
+        else: 
+            Infield.append(0)
+            
+
+    employee_data["Infield"]=Infield
+
+
+
+    # Calculate the number of missing values in each column
+        
+    missing_df = employee_data.isnull().sum().to_frame().rename(columns={0:"Total No. of Missing Values"})
+    missing_df["% of Missing Values"] = round((missing_df["Total No. of Missing Values"]/len(employee_data))*100,2)
+    missing_df
+
+    ### <font color=red>Inference:</font>
+
+    ## <span style='color:blue'> 7] DESCRIPTIVE ANALYSIS ON NUMERICAL ATTRIBUTES </span>
+
+    employee_data.describe().T
+
+    ### <font color=red>Inference:</font>
+    ## <span style='color:blue'> 8] DROP UNNECESSARY COLUMNS </span>
+
+    employee_data.drop(['EmployeeCount', 'EmployeeNumber', 'Over18', 'StandardHours'], axis="columns", inplace=True)
+
+    # Print top 5 rows in the dataframe.
+
+    # Print the shape of the DataFrame
+    print("The shape of data frame:", employee_data.shape)
+    # Print the length (number of rows) of the DataFrame
+    print("Number of Rows in the dataframe:", len(employee_data))
+    # Print the number of columns in the DataFrame
+    print("Number of Columns in the dataframe:", len(employee_data.columns))
+
+    print("Column labels in the dataset in column order:")
+    for column in employee_data.columns:
+        print(column)
+
+    ## <span style='color:blue'> 9] DESCRIPTIVE ANALYSIS ON CATEGORICAL ATTRIBUTES </span>
+
+    employee_data.describe(include="O").T
+
+
+    # Calculate the number of unique values in each column
+    for column in employee_data.columns:
+        print(f"{column} - Number of unique values : {employee_data[column].nunique()}")
+        print("=============================================================")
+
+    categorical_features = []
+    for column in employee_data.columns:
+        if employee_data[column].dtype == object and len(employee_data[column].unique()) <= 30:
+            categorical_features.append(column)
+            print(f"{column} : {employee_data[column].unique()}")
+            print(employee_data[column].value_counts())
+            print("====================================================================================")
+    categorical_features.remove('Attrition')
+
+
+    # Save DataFrame to CSV file
+    employee_data.to_csv('IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv', index=False)
+    import os
+
+    # Assuming the file is saved in the current directory
+    if 'IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv' in os.listdir('.'):
+        print("File created successfully.")
+    else:
+        print("File not found.")
+        import os
+
+# Assuming 'file_path' is the path to your file
+    file_path = 'IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv'
+
+    # Get the absolute path of the file
+    full_path = os.path.abspath(file_path)
+
+    # Print the full path
+    print(full_path)
+
+
+
+DataPreProcessing()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/LnT-HR-AI/LnT_HR_AI-0.0.6-py3-none-any.whl/DataAnalysis_Package/Statistical_Analysis.py
@@ -0,0 +1,134 @@
+def data_analysis():
+    # Library for Data Manipulation
+    import numpy as np
+    import pandas as pd
+
+    # Library for Data Visualization
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    sns.set(style="white",font_scale=1.5)
+    sns.set(rc={"axes.facecolor":"#FFFAF0","figure.facecolor":"#FFFAF0"})
+    sns.set_context("poster",font_scale = .7)
+
+    # Library to perform Statistical Analysis.
+    from scipy import stats
+    from scipy.stats import chi2
+    from scipy.stats import chi2_contingency
+    import tkinter as tk
+    from tkinter import filedialog
+    
+    # Library for Ignore the warnings
+    import warnings
+    warnings.filterwarnings('always')
+    warnings.filterwarnings('ignore')
+    print("Select the file IBM-HR-Analytics-Employee-Attrition-and-Performance-Revised.csv")
+    root = tk.Tk()
+    root.withdraw() # Hide the root window
+    
+    # Open the file dialog and get the selected file path
+    file_path = filedialog.askopenfilename()
+    
+    # Print the selected file path
+    address=""
+    for iterator in file_path:
+                if iterator=="/" :
+                      address+="\\\\"
+                else:
+                        address+=iterator
+
+        
+    # Close the application
+    root.destroy()
+
+    employee_data = pd.read_csv(address)
+
+    # Print top 5 rows in the dataframe.
+    employee_data.head().style.set_properties(**{'background-color': '#E9F6E2','color': 'black','border-color': '#8b8c8c'})
+
+    # Print bottom 5 rows in the dataframe.
+    employee_data.tail().style.set_properties(**{'background-color': '#E9F6E2','color': 'black','border-color': '#8b8c8c'})
+
+    # Print the shape of the DataFrame
+    print("The shape of data frame:", employee_data.shape)
+    # Print the length (number of rows) of the DataFrame
+    print("Number of Rows in the dataframe:", len(employee_data))
+    # Print the number of columns in the DataFrame
+    print("Number of Columns in the dataframe:", len(employee_data.columns))
+
+
+
+    num_cols = employee_data.select_dtypes(np.number).columns
+
+    new_df = employee_data.copy()
+
+    new_df["Attrition"] = new_df["Attrition"].replace({"No":0,"Yes":1})
+
+    f_scores = {}
+    p_values = {}
+
+    for column in num_cols:
+        f_score, p_value = stats.f_oneway(new_df[column],new_df["Attrition"])
+        
+        f_scores[column] = f_score
+        p_values[column] = p_value
+
+
+    plt.figure(figsize=(15,6))
+    keys = list(f_scores.keys())
+    values = list(f_scores.values())
+
+    sns.barplot(x=keys, y=values)
+    plt.title("Anova-Test F_scores Comparison",fontweight="black",size=20,pad=15)
+    plt.xticks(rotation=90)
+
+    for index,value in enumerate(values):
+        plt.text(index,value,int(value), ha="center", va="bottom",fontweight="black",size=15)
+    plt.show()
+
+    ## <span style='color:blue'> 3] COMPARING F_SCORE AND P_VALUE OF ANOVA TEST. </span>
+
+    test_df = pd.DataFrame({"Features":keys,"F_Score":values})
+    test_df["P_value"] = [format(p, '.20f') for p in list(p_values.values())]
+
+    test_df
+
+    ## <font color=red>Inference:</font>
+
+
+    cat_cols = employee_data.select_dtypes(include="object").columns.tolist()
+    cat_cols.remove("Attrition")
+
+    chi2_statistic = {}
+    p_values = {}
+
+    # Perform chi-square test for each column
+    for col in cat_cols:
+        contingency_table = pd.crosstab(employee_data[col], employee_data['Attrition'])
+        chi2, p_value, _, _ = chi2_contingency(contingency_table)
+        chi2_statistic[col] = chi2
+        p_values[col] = p_value
+
+    ## <span style='color:blue'> 5] VISUALIZE THE CHI-SQUARE STATISTICS VALUES </span>
+
+    columns = list(chi2_statistic.keys())
+    values = list(chi2_statistic.values())
+
+    plt.figure(figsize=(16,6))
+    sns.barplot(x=columns, y=values)
+    plt.xticks(rotation=90)
+    plt.title("Chi2 Statistic Value of each Categorical Columns",fontweight="black",size=20,pad=15)
+    for index,value in enumerate(values):
+        plt.text(index,value,round(value,2),ha="center",va="bottom",fontweight="black",size=15)
+
+    plt.show()
+
+    ## <span style='color:blue'> 6] COMPARING CHI^2_STATISTICS AND P_VALUE OF CHI^2 TEST. </span>
+
+    test_df = pd.DataFrame({"Features":columns,"Chi_2 Statistic":values})
+    test_df["P_value"] =  [format(p, '.20f') for p in list(p_values.values())]
+
+    
+    ## <span style='color:blue'> 7] DESCRIPTIVE ANALYSIS ON CATEGORICAL ATTRIBUTES </span>
+
+    ## <font color=red>Inference:</font>
+
