071f5854c6af818530ac85013880634fcb4c99ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 14:04:53 +0000

    Add package santex/1.2/santex-1.2.tar.gz

---
+++ packages/santex/santex-1.2.tar.gz/santex-1.2/santex/__init__.py
@@ -0,0 +1,6 @@
+from .material import Material
+from .ebsd import EBSD
+from .tensor import Tensor
+from .anisotropy import Anisotropy
+from .anisotropy.plot_seismic_velocity import plot_vp_2d
+from .isotropy import Isotropy
