16e0905221a18ec7e363f748541da08679b452fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:09:47 +0000

    Add package parea-ai/0.2.144/parea_ai-0.2.144.tar.gz

---
+++ packages/parea-ai/parea_ai-0.2.144.tar.gz/parea_ai-0.2.144/parea/utils/trace_integrations/langchain.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from uuid import UUID
+
+from langchain_core.tracers import BaseTracer
+from langchain_core.tracers.schemas import ChainRun, LLMRun, Run, ToolRun
+
+from parea.constants import TURN_OFF_PAREA_LOGGING
+from parea.parea_logger import parea_logger
+from parea.schemas.log import TraceIntegrations
+
+
+class PareaAILangchainTracer(BaseTracer):
+    parent_trace_id: UUID
+
+    def _persist_run(self, run: Union[Run, LLMRun, ChainRun, ToolRun]) -> None:
+        if TURN_OFF_PAREA_LOGGING:
+            return
+        self.parent_trace_id = run.id
+        # using .dict() since langchain Run class currently set to Pydantic v1
+        data = run.dict()
+        parea_logger.record_vendor_log(data, TraceIntegrations.LANGCHAIN)
+
+    def get_parent_trace_id(self) -> UUID:
+        return self.parent_trace_id
+
+    def _on_llm_end(self, run: Run) -> None:
+        self._persist_run(run)
+
+    def _on_chain_end(self, run: Run) -> None:
+        self._persist_run(run)
