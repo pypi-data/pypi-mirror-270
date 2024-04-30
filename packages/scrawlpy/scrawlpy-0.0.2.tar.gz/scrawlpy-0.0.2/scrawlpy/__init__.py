14914a365978a5c8bd75e0984e009a15177459d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 15:04:51 +0000

    Add package scrawlpy/0.0.2/scrawlpy-0.0.2.tar.gz

---
+++ packages/scrawlpy/scrawlpy-0.0.2.tar.gz/scrawlpy-0.0.2/scrawlpy/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2024/4/28 16:19
+from scrawlpy.core.spiders import Spider
+from scrawlpy.http.request import Request
---
+++ packages/scrawlpy/scrawlpy-0.0.2.tar.gz/scrawlpy-0.0.2/scrawlpy/core/spiders/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2024/4/28 16:28
+
+__all__ = ["Spider", ]
+
+from scrawlpy.core.spiders.spider import Spider
