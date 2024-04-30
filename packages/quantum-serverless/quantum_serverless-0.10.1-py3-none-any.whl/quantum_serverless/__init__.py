9c3f3eb73c8871d90cc250abba3cdc8e827c489c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 06:04:05 +0000

    Add package quantum-serverless/0.10.1/quantum_serverless-0.10.1-py3-none-any.whl

---
+++ packages/quantum-serverless/quantum_serverless-0.10.1-py3-none-any.whl/quantum_serverless/__init__.py
@@ -0,0 +1,53 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+.. currentmodule:: quantum_serverless
+
+.. autosummary::
+    :toctree: ../stubs/
+
+    QuantumServerless
+    QuantumServerlessException
+    get_auto_discovered_provider
+"""
+# pylint: disable=W0404
+from importlib_metadata import version as metadata_version, PackageNotFoundError
+
+from .core import (
+    BaseProvider,
+    distribute_task,
+    distribute_qiskit_pattern,
+    get,
+    put,
+    get_refs_by_status,
+    Provider,
+    ServerlessProvider,
+    IBMServerlessProvider,
+    RayProvider,
+    LocalProvider,
+    save_result,
+    Configuration,
+)
+from .quantum_serverless import (
+    QuantumServerless,
+    get_auto_discovered_provider,
+    QuantumServerlessException,
+)
+from .core.pattern import QiskitPattern
+from .serializers import get_arguments
+
+try:
+    __version__ = metadata_version("quantum_serverless")
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    pass
---
+++ packages/quantum-serverless/quantum_serverless-0.10.1-py3-none-any.whl/quantum_serverless/core/__init__.py
@@ -0,0 +1,85 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+============================================
+Core module (:mod:`quantum_serverless.core`)
+============================================
+
+.. currentmodule:: quantum_serverless.core
+
+Quantum serverless core module classes and functions
+====================================================
+
+Core abstractions
+-----------------
+
+.. autosummary::
+    :toctree: ../stubs/
+
+    Provider
+    ServerlessProvider
+    IBMServerlessProvider
+    BaseProvider
+    RayProvider
+    LocalProvider
+    ComputeResource
+    Job
+    GatewayJobClient
+    BaseJobClient
+    RayJobClient
+    save_result
+    QiskitPattern
+    Target
+    CircuitMeta
+    fetch_execution_meta
+    distribute_task
+    distribute_program
+    distribute_qiskit_pattern
+    get
+    put
+    get_refs_by_status
+
+"""
+
+from .provider import (
+    BaseProvider,
+    ComputeResource,
+    Provider,
+    ServerlessProvider,
+    IBMServerlessProvider,
+    LocalProvider,
+    RayProvider,
+)
+
+from .job import (
+    BaseJobClient,
+    RayJobClient,
+    GatewayJobClient,
+    LocalJobClient,
+    Job,
+    save_result,
+    Configuration,
+)
+from .pattern import QiskitPattern
+from .decorators import (
+    remote,
+    get,
+    put,
+    get_refs_by_status,
+    fetch_execution_meta,
+    distribute_task,
+    distribute_qiskit_pattern,
+    distribute_program,
+    Target,
+    CircuitMeta,
+)
