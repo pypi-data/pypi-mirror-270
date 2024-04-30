5e5071541373be059d7d2d5f7714e8bf7882fc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 17:04:48 +0000

    Add package kr8s/0.14.4/kr8s-0.14.4.tar.gz

---
+++ packages/kr8s/kr8s-0.14.4.tar.gz/kr8s-0.14.4/examples/kubectl-ng/kubectl_ng/cli.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2024, Kr8s Developers (See LICENSE for list)
+# SPDX-License-Identifier: BSD 3-Clause License
+import asyncio
+from functools import wraps
+
+import typer
+
+from ._api_resources import api_resources
+from ._api_versions import api_versions
+from ._create import create
+from ._delete import delete
+from ._exec import kexec
+from ._get import get
+from ._version import version
+from ._wait import wait
+
+
+def _typer_async(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+
+    return wrapper
+
+
+def register(app, func, alias=None):
+    if asyncio.iscoroutinefunction(func):
+        func = _typer_async(func)
+    if alias is not None:
+        app.command(alias)(func)
+    else:
+        app.command()(func)
+
+
+app = typer.Typer(no_args_is_help=True)
+register(app, api_resources)
+register(app, api_versions)
+register(app, create)
+register(app, delete)
+register(app, get)
+register(app, version)
+register(app, wait)
+register(app, kexec, "exec")
+
+
+def go():
+    app()
+
+
+if __name__ == "__main__":
+    go()
