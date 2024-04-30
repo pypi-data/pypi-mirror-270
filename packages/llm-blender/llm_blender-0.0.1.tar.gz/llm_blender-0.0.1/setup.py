80a88c8cefdc5dcbe78b0c0f468edc6d21f01e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:10:17 +0000

    Add package llm-blender/0.0.1/llm_blender-0.0.1.tar.gz

---
+++ packages/llm-blender/llm_blender-0.0.1.tar.gz/llm_blender-0.0.1/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llm_blender',
+    version='0.0.1',
+    description='LLM-Blender, an innovative ensembling framework to attain consistently superior performance by leveraging the diverse strengths and weaknesses of multiple open-source large language models (LLMs). LLM-Blender cut the weaknesses through ranking and integrate the strengths through fusing generation to enhance the capability of LLMs.',
+    author='Dongfu Jiang',
+    author_email='dongfu.jdf@gmail.com',
+    packages=find_packages(),
+    url='https://yuchenlin.xyz/LLM-Blender/',
+    install_requires=[
+        'transformers',
+        'torch',
+        'numpy',
+        'accelerate',
+        'safetensors',
+        'dataclasses-json',
+        'sentencepiece',
+        'protobuf',
+    ],
+    extras_require={
+        "example": [
+            'datasets',
+            'scipy',
+            'jupyter'
+        ],
+        "train": [
+            'datasets',
+            'bitsandbytes',
+            'deepspeed',
+            'wandb',
+        ],
+        "data": [
+            'datasets',
+            'openai',
+            'peft',
+            'fschat',
+        ],
+        "eval": [
+            'datasets',
+            'pycocoevalcap',
+            'spacy',
+            'prettytable',
+            'evaluate',
+            'bert_score',
+            'tabulate',
+            'scipy',
+            'nltk',
+            'scikit-learn',
+            'sacrebleu',
+            'rouge_score',
+        ],
+    },
+)
