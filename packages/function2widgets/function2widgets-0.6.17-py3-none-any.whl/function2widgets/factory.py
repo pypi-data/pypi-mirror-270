afc37231515b6ff75547fa1b8049fac8b56cf374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 04:10:38 +0000

    Add package function2widgets/0.6.17/function2widgets-0.6.17-py3-none-any.whl

---
+++ packages/function2widgets/function2widgets-0.6.17-py3-none-any.whl/function2widgets/factory.py
@@ -0,0 +1,81 @@
+from typing import Type, Dict
+
+from PyQt6.QtWidgets import QApplication
+
+from function2widgets.common import (
+    AlreadyRegisteredError,
+    NotRegisteredError,
+)
+from function2widgets.info import ParameterInfo, FunctionInfo
+from function2widgets.parser.function_parser import FunctionInfoParser
+from function2widgets.widget import BaseParameterWidget, BaseWidgetArgs
+from function2widgets.widgets.allwidgets import BASIC_PARAMETER_WIDGETS
+
+
+class ParameterWidgetFactory(object):
+    def __init__(self, register_basic_parameter_widgets: bool = True):
+        self._widget_classes = {}
+
+        if register_basic_parameter_widgets:
+            self.register_all(BASIC_PARAMETER_WIDGETS)
+
+    def register(self, widget_class_name: str, widget_class: Type[BaseParameterWidget]):
+        if widget_class_name in self._widget_classes:
+            raise AlreadyRegisteredError(
+                QApplication.tr(f"widget type {widget_class_name} already registered")
+            )
+        self._widget_classes[widget_class_name] = widget_class
+
+    def register_all(self, widgets: Dict[str, Type[BaseParameterWidget]]):
+        for widget_class_name, widget_class in widgets.items():
+            self.register(widget_class_name, widget_class)
+
+    def unregister(self, widget_class_name: str):
+        if widget_class_name not in self._widget_classes:
+            raise NotRegisteredError(
+                QApplication.tr(f"widget type {widget_class_name} not registered")
+            )
+        del self._widget_classes[widget_class_name]
+
+    def is_registered(self, widget_class_name: str) -> bool:
+        return widget_class_name in self._widget_classes
+
+    def get_widget_class(self, widget_class_name: str) -> Type[BaseParameterWidget]:
+        if not self.is_registered(widget_class_name):
+            raise NotRegisteredError(
+                QApplication.tr(f"widget type {widget_class_name} not registered")
+            )
+        return self._widget_classes[widget_class_name]
+
+    def clear(self):
+        self._widget_classes.clear()
+
+    def create_widget_for_parameter(
+        self, param_info: ParameterInfo
+    ) -> BaseParameterWidget:
+        param_widget_info = param_info.widget
+        if not param_widget_info:
+            param_widget_info = FunctionInfoParser.make_default_param_widget_info(
+                param_info
+            )
+
+        widget = self._create_widget(
+            widget_class_name=param_widget_info.widget_class,
+            **param_widget_info.widget_args,
+        )
+        return widget
+
+    def create_widgets_for_function(
+        self, func_info: FunctionInfo
+    ) -> Dict[str, BaseParameterWidget]:
+        widgets = {}
+        for param_info in func_info.parameters:
+            widget = self.create_widget_for_parameter(param_info)
+            widgets[param_info.name] = widget
+        return widgets
+
+    def _create_widget(self, widget_class_name: str, **kwargs) -> BaseParameterWidget:
+        widget_class = self.get_widget_class(widget_class_name)
+        widget_args_class: Type[BaseWidgetArgs] = widget_class.widget_args_class()
+        widget_args = widget_args_class.new(kwargs=kwargs)
+        return widget_class(args=widget_args, parent=None)
---
+++ packages/function2widgets/function2widgets-0.6.17-py3-none-any.whl/function2widgets/widgets/misc/dateedit.py
@@ -0,0 +1,97 @@
+import dataclasses
+from datetime import datetime, date
+from typing import Optional, cast, Union
+
+from PyQt6.QtCore import Qt, QDate
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QDateEdit
+
+from function2widgets.common import to_date
+from function2widgets.widget import InvalidValueError
+from function2widgets.widgets.base import (
+    CommonParameterWidget,
+    CommonParameterWidgetArgs,
+)
+from function2widgets.widgets.misc.datetimeedit import DEFAULT_TIME_SPEC, TIME_SPECS
+
+DEFAULT_DISPLAY_FORMAT = "yyyy/M/d"
+
+
+@dataclasses.dataclass(frozen=True)
+class DateEditArgs(CommonParameterWidgetArgs):
+    parameter_name: str
+    default: Union[date, QDate, str, None] = datetime.now().date()
+    display_format: str = DEFAULT_DISPLAY_FORMAT
+    min_date: Union[date, QDate, str, None] = None
+    max_date: Union[date, QDate, str, None] = None
+    calendar_popup: bool = False
+    time_spec: str = DEFAULT_TIME_SPEC
+
+
+class DateEdit(CommonParameterWidget):
+    HIDE_DEFAULT_VALUE_WIDGET = True
+    SET_DEFAULT_ON_INIT = True
+
+    _WidgetArgsClass = DateEditArgs
+
+    def __init__(self, args: DateEditArgs, parent: Optional[QWidget] = None):
+        self._value_widget: Optional[QDateEdit] = None
+
+        super().__init__(args=args, parent=parent)
+
+        if self._args.set_default_on_init:
+            self.set_value(self._args.default)
+
+    @property
+    def _args(self) -> DateEditArgs:
+        return cast(DateEditArgs, super()._args)
+
+    def setup_center_widget(self, center_widget: QWidget):
+        self._value_widget = QDateEdit(center_widget)
+
+        display_format = self._args.display_format or DEFAULT_DISPLAY_FORMAT
+        self._value_widget.setDisplayFormat(display_format)
+
+        time_spec = self._args.time_spec
+        if not time_spec:
+            time_spec = DEFAULT_TIME_SPEC
+        time_spec = TIME_SPECS.get(time_spec, Qt.TimeSpec.LocalTime)
+        self._value_widget.setTimeSpec(time_spec)
+
+        min_date = self._args.min_date
+        if isinstance(min_date, str) and min_date:
+            min_date = to_date(min_date, display_format)
+        if min_date:
+            self._value_widget.setMinimumDate(min_date)
+
+        max_date = self._args.max_date
+        if isinstance(max_date, str) and max_date:
+            max_date = to_date(max_date, display_format)
+        if max_date:
+            self._value_widget.setMaximumDate(max_date)
+
+        self._value_widget.setCalendarPopup(self._args.calendar_popup is True)
+
+        center_widget_layout = QVBoxLayout(center_widget)
+        center_widget_layout.setContentsMargins(0, 0, 0, 0)
+        center_widget.setLayout(center_widget_layout)
+
+        center_widget_layout.addWidget(self._value_widget)
+
+    def set_value(self, value: Union[date, QDate, None]):
+        if not isinstance(value, (date, QDate, str)) and value is not None:
+            raise InvalidValueError(
+                f"value must be date or QDate or a date string, got {type(value)}"
+            )
+        display_format = self._args.display_format or DEFAULT_DISPLAY_FORMAT
+        if isinstance(value, str):
+            value = to_date(value, display_format)
+        super().set_value(value)
+
+    def get_value(self) -> Optional[date]:
+        return super().get_value()
+
+    def set_value_to_widget(self, value: Union[date, QDate]):
+        self._value_widget.setDate(value)
+
+    def get_value_from_widget(self) -> date:
+        return self._value_widget.date().toPyDate()
