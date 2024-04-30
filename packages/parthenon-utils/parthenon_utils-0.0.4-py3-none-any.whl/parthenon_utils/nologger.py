8df39726f5ea442c18b252f512bf33387e23bb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 05:09:12 +0000

    Add package parthenon-utils/0.0.4/parthenon_utils-0.0.4-py3-none-any.whl

---
+++ packages/parthenon-utils/parthenon_utils-0.0.4-py3-none-any.whl/parthenon_utils/nologger.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Miguel Freire Couy'
+__credits__ = ['Miguel Freire Couy']
+__maintainer__ = 'Miguel Freire Couy'
+__email__ = 'miguel.couy@outlook.com'
+__status__ = 'Production'
+
+class NoLogger:
+    def debug(*args, **kwargs): pass
+    def info(*args, **kwargs): pass
+    def warn(*args, **kwargs): pass
+    def error(*args, **kwargs): pass
+    def critical(*args, **kwargs): pass
