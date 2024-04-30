f40d13b6912fe776c99373aca3871ffbd24aff1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-30 16:04:34 +0000

    Add package sdkm/1.0/sdkm-1.0.tar.gz

---
+++ packages/sdkm/sdkm-1.0.tar.gz/sdkm-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+setup(
+    name="sdkm",
+    version="1.0",
+    packages=find_packages(),
+    author="Bhargav Naik",
+    description='sdkm',
+    long_description=open(r'C:\Users\koush\Downloads\Demooo\Python-Snake_Game-main\Snake-game\ReadMe.md', encoding='utf-8').read()
+
+
+)
