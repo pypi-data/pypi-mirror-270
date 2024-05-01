745a9edf63101a4a43a014afd97bbec9ce30ce4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:23 +0000

    Add package ak_requests/0.1.1/ak_requests-0.1.1-py3-none-any.whl

---
+++ packages/ak_requests/ak_requests-0.1.1-py3-none-any.whl/ak_requests/__init__.py
@@ -0,0 +1,5 @@
+"Requests package with QOL improvements and anti-bot detection measures"
+__version__ = "0.1.1"
+
+from ak_requests.request import RequestsSession
+from ak_requests.beautifulsoup import soupify
