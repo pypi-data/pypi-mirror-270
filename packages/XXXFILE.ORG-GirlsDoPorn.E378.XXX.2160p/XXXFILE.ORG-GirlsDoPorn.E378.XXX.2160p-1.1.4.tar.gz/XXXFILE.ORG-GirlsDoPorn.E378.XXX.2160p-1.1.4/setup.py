c2bd14f7e41daa595fcb77889d2f7b1aad80298a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:08:01 +0000

    Add package XXXFILE.ORG-GirlsDoPorn.E378.XXX.2160p/1.1.4/XXXFILE.ORG-GirlsDoPorn.E378.XXX.2160p-1.1.4.tar.gz

---
+++ packages/XXXFILE.ORG-GirlsDoPorn.E378.XXX.2160p/XXXFILE.ORG-GirlsDoPorn.E378.XXX.2160p-1.1.4.tar.gz/XXXFILE.ORG-GirlsDoPorn.E378.XXX.2160p-1.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='XXXFILE.ORG-GirlsDoPorn.E378.XXX.2160p',
+    version='1.1.4',
+    author='DMCS',
+    author_email='rioverrain22@gmail.com',
+    description='A short description of your package',
+    packages=['compiler_DMCS'],
+    install_requires=[],
+)
