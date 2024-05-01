06ea3bff7325d2bc4df327612c37e3ddd6538df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:07:50 +0000

    Add package cnvrgv2/1.0.50/cnvrgv2-1.0.50-py3-none-any.whl

---
+++ packages/cnvrgv2/cnvrgv2-1.0.50-py3-none-any.whl/cnvrgv2/__init__.py
@@ -0,0 +1,5 @@
+from cnvrgv2.cnvrg import Cnvrg
+from cnvrgv2.errors import ExceptionsConfig
+from cnvrgv2.modules.project import Project
+from cnvrgv2.modules.workflows import Workspace, Webapp, WebappType, Experiment, Endpoint, EndpointKind, EndpointEnvSetup
+from cnvrgv2.utils.chart_utils import LineChart, Heatmap, BarChart, ScatterPlot
