4e3ea2a5a9a0e36fb8dd86125da0423daba8f07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:05:07 +0000

    Add package simple-add/0.1.5/simple_add-0.1.5.tar.gz

---
+++ packages/simple-add/simple_add-0.1.5.tar.gz/simple_add-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-add',
+    version='0.1.5',
+    packages=find_packages(),
+    author='AkhilkumarGolla',
+    author_email='akhilkumar.golla@outlook.com',
+    description='A simple program with an add function',
+    long_description='A simple Python program with a function to add two numbers and three numbers',
+    url='https://github.com/Akhilkumargolla/test-ci-cd',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
