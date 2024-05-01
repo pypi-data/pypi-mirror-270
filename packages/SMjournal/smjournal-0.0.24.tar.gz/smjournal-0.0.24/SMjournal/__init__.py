62a4d602d55d624b28cadeab5327af3726fd0043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:05:27 +0000

    Add package SMjournal/0.0.24/smjournal-0.0.24.tar.gz

---
+++ packages/SMjournal/smjournal-0.0.24.tar.gz/smjournal-0.0.24/SMjournal/__init__.py
@@ -0,0 +1,9 @@
+from .collinearity import *
+from .data import *
+from .low_level import *
+from .potentiality import *
+from .meta_reg import *
+
+
+# python3 -m build
+# python3 -m twine upload --repository pypi dist/*
