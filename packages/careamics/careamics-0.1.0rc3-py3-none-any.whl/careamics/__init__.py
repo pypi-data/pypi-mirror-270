e200fd0ce09e770d10254575370b422305a23456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:06:14 +0000

    Add package careamics/0.1.0rc3/careamics-0.1.0rc3-py3-none-any.whl

---
+++ packages/careamics/careamics-0.1.0rc3-py3-none-any.whl/careamics/__init__.py
@@ -0,0 +1,24 @@
+"""Main CAREamics module."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("careamics")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__all__ = [
+    "CAREamist",
+    "CAREamicsModule",
+    "Configuration",
+    "load_configuration",
+    "save_configuration",
+    "CAREamicsTrainDataModule",
+    "CAREamicsPredictDataModule",
+]
+
+from .careamist import CAREamist
+from .config import Configuration, load_configuration, save_configuration
+from .lightning_datamodule import CAREamicsTrainDataModule
+from .lightning_module import CAREamicsModule
+from .lightning_prediction_datamodule import CAREamicsPredictDataModule
