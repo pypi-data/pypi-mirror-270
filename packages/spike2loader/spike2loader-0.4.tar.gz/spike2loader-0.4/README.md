c8d48fa85767b623a6e7bea02b5efcf2176be749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:05:38 +0000

    Add package spike2loader/0.4/spike2loader-0.4.tar.gz

---
+++ packages/spike2loader/spike2loader-0.4.tar.gz/spike2loader-0.4/README.md
@@ -0,0 +1,2 @@
+# Spike2Loader
+A simple loader for Spike2 files (.smrx). Note that this package uses the sonpy package provided by spike2 which requires python 3.9.
---
+++ packages/spike2loader/spike2loader-0.4.tar.gz/spike2loader-0.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name='spike2loader',
+    packages=['spike2loader'],
+    entry_points={
+        'console_scripts': [
+            'smrx2tsv = spike2loader:smrx2tsv',
+        ]
+    },
+    version='0.4',
+    license='MIT',
+    description = 'Reads smrx (spike2) files as pandas dataframes',
+    description_file = "README.md",
+    author="Julien Braine",
+    author_email='julienbraine@yahoo.fr',
+    url='https://github.com/JulienBrn/Spike2Loader',
+    download_url = 'https://github.com/JulienBrn/Spike2Loader.git',
+    package_dir={'': 'src'},
+    keywords=['python',  'logging'],
+    install_requires=['pandas', 'sonpy', "beautifullogger", "tqdm"],
+    long_description="test",
+)
