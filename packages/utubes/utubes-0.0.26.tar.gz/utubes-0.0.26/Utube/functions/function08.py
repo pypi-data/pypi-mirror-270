c4b29b2f3b6050e1b6781478c21df129665db82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:04:41 +0000

    Add package utubes/0.0.26/utubes-0.0.26.tar.gz

---
+++ packages/utubes/utubes-0.0.26.tar.gz/utubes-0.0.26/Utube/functions/function08.py
@@ -0,0 +1,44 @@
+import re
+from ..scripts import FHttps
+from instaloader import Post, Profile
+from instaloader import Instaloader, NodeIterator
+#=====================================================================
+
+class SMessages:
+    def __init__(self, **kwargs):
+        self.result = kwargs.get("result", None)
+        self.errors = kwargs.get("errors", None)
+        self.status = kwargs.get("status", True)
+
+#=====================================================================
+
+class Instagram:
+
+    async def get01(incoming):
+        matchs = re.search(FHttps.DATA01, incoming)
+        moonus = True if matchs else False
+        return SMessages(result=matchs, status=moonus)
+
+    async def get02(incoming):
+        mainse = {"p": "POST", "tv": "IGTV", "reel": "REELS"}
+        conmom = mainse.get(incoming.group(2))
+        moonus = True if conmom else False
+        return SMessages(result=conmom, status=moonus)
+
+    async def get03(bot, shortcode):
+        moonus = Post.from_shortcode(bot.context, shortcode)
+        return moonus
+
+    async def get04(bot, username):
+        moonus = Profile.from_username(bot.context, username)
+        return moonus
+
+    async def get05(bot: Profile) -> NodeIterator[Post]:
+        moonus = bot.get_posts()
+        return moonus
+
+    async def download(bot: Instaloader, update: Post):
+        moonus = bot.download_post(update, update.owner_username)
+        return moonus
+
+#=====================================================================
