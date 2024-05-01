9016b870f939fc280157c516f3291e2f0629d385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:14 +0000

    Add package paradict/0.0.7/paradict-0.0.7-py3-none-any.whl

---
+++ packages/paradict/paradict-0.0.7-py3-none-any.whl/paradict/__init__.py
@@ -0,0 +1,22 @@
+from paradict.serializer import encode, pack, dump, write
+from paradict.serializer.encoder import Encoder
+from paradict.serializer.packer import Packer
+from paradict.deserializer import decode, unpack, load, read
+from paradict.deserializer.decoder import Decoder
+from paradict.deserializer.unpacker import Unpacker
+from paradict.typeref import TypeRef
+from paradict.validator import validate, Validator
+from paradict.kv import split as split_kv
+from paradict.misc import forge_bin, stringify_bin
+from paradict.const import CONFIG_MODE, DATA_MODE
+
+
+__all__ = ["encode", "decode",
+           "write", "read",
+           "pack", "unpack",
+           "load", "dump",
+           "validate", "split_kv",
+           "forge_bin", "stringify_bin",
+           "Encoder", "Decoder",
+           "Packer", "Unpacker",
+           "TypeRef", "Validator", "CONFIG_MODE", "DATA_MODE"]
