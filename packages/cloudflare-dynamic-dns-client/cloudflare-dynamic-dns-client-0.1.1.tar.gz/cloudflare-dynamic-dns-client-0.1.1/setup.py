a7f988044d13c63e01e24291ea9305a99181d27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:05:21 +0000

    Add package cloudflare-dynamic-dns-client/0.1.1/cloudflare-dynamic-dns-client-0.1.1.tar.gz

---
+++ packages/cloudflare-dynamic-dns-client/cloudflare-dynamic-dns-client-0.1.1.tar.gz/cloudflare-dynamic-dns-client-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='cloudflare-dynamic-dns-client',
+    version='0.1.1',
+    packages=['cloudflare_dynamic_dns'],
+		author="Littlewhinging",
+    install_requires=[
+      "cloudflare==2.11.1",
+			"Requests==2.31.0"
+    ],
+		entry_points={
+			'console_scripts': [
+				'cloudflare-dynamic-dns = cloudflare_dynamic_dns.__main__:main'
+			],
+    }
+)
