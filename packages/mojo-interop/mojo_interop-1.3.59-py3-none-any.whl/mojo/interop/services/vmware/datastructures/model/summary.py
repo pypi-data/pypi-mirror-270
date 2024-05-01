f02a8ab6f698bc1b4ecb52c24c10c18314f2eced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:06:50 +0000

    Add package mojo-interop/1.3.59/mojo_interop-1.3.59-py3-none-any.whl

---
+++ packages/mojo-interop/mojo_interop-1.3.59-py3-none-any.whl/mojo/interop/services/vmware/datastructures/model/summary.py
@@ -0,0 +1,24 @@
+
+from dataclasses import dataclass
+
+from mojo.interop.services.vmware.metasphere.vcenter import FolderType
+from mojo.interop.services.vmware.metasphere.vcenter import VmPowerState
+
+@dataclass
+class DatacenterSummary:
+    name: str
+    datacenter: str
+
+@dataclass
+class FolderSummary:
+    name: str
+    folder: str
+    type: FolderType
+
+@dataclass
+class VmSummary:
+    name: str
+    power_state: VmPowerState
+    vm: str
+    cpu_count: int
+    memory_size_MiB: int
