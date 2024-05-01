f1058241cd72d349ffd67d35ece423bd8a8ce7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:22 +0000

    Add package django-paypal/2.1/django-paypal-2.1.tar.gz

---
+++ packages/django-paypal/django-paypal-2.1.tar.gz/django-paypal-2.1/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os.path
+
+from setuptools import find_packages, setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+DESCRIPTION = "A pluggable Django application for integrating PayPal Payments Standard or Payments Pro"
+URL = "https://github.com/spookylukey/django-paypal"
+DOCS_URL = "https://django-paypal.readthedocs.org"
+
+setup(
+    name="django-paypal",
+    version="2.1",
+    author="John Boxall",
+    author_email="john@handimobility.ca",
+    maintainer="Luke Plant",
+    maintainer_email="L.Plant.98@cantab.net",
+    url=URL,
+    python_requires=">=3.7",
+    install_requires=[
+        "Django>=2.2",
+        "requests>=2.5.3",
+        "pytz>=2015.4",
+    ],
+    description=DESCRIPTION,
+    long_description=f"{DESCRIPTION}\n\nHome page: {URL}\n\nDocs: {DOCS_URL}\n",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
