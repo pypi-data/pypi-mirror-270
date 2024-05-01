effb8fada1946d5f8c9d728ba42bc16ea3867840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:07:51 +0000

    Add package scrawlpy/0.0.10/scrawlpy-0.0.10.tar.gz

---
+++ packages/scrawlpy/scrawlpy-0.0.10.tar.gz/scrawlpy-0.0.10/scrawlpy/core/base_spider.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2024/4/28 16:29
+import copy
+import json
+import time
+import traceback
+import uuid
+from abc import ABCMeta, abstractmethod
+from concurrent.futures.thread import ThreadPoolExecutor
+from datetime import datetime
+from typing import Iterable, Union
+
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+from scrawlpy.http.request import Request
+from scrawlpy.utils.logger_util import get_bind_logger
+from scrawlpy.setting import Settings
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+class AbstractSpider(metaclass=ABCMeta):
+    project_name = "abstract_project"  # 项目
+    spider_name = "abstract_spider"  # spider 接口
+    site = "abstract"  # 站点
+    Request = Request
+    __custom_setting__ = dict()
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        self.spider_config = None
+        self.logger = get_bind_logger(self.spider_name)
+        self.requests = None
+        self.is_daemon = False
+        self.requests: Request = Request()
+
+        if self.__custom_setting__:
+            for key, value in self.__custom_setting__.items():
+                setattr(Settings, key, value)
+        self.logger.debug(Settings())
+
+    # def start_requests(self):
+    #     pass
+
+    def parse(self, response, **kwargs):
+        pass
