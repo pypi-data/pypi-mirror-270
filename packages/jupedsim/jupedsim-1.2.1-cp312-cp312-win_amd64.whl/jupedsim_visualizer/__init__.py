051608b45567d91db5d3897657d463417c80e3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:05:59 +0000

    Add package jupedsim/1.2.1/jupedsim-1.2.1-cp312-cp312-win_amd64.whl

---
+++ packages/jupedsim/jupedsim-1.2.1-cp312-cp312-win_amd64.whl/jupedsim_visualizer/__init__.py
@@ -0,0 +1,9 @@
+# Copyright © 2012-2024 Forschungszentrum Jülich GmbH
+# SPDX-License-Identifier: LGPL-3.0-or-later
+import jupedsim_visualizer.config
+import jupedsim_visualizer.geometry
+import jupedsim_visualizer.geometry_widget
+import jupedsim_visualizer.grid
+import jupedsim_visualizer.main_window
+import jupedsim_visualizer.move_controller
+import jupedsim_visualizer.view_geometry_widget  # noqa: F401
