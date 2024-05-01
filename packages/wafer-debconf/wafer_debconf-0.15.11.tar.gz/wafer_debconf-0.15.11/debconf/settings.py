c5ecdc939f5573e531eae83ba2bca9960be4873e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:06:05 +0000

    Add package wafer-debconf/0.15.11/wafer_debconf-0.15.11.tar.gz

---
+++ packages/wafer-debconf/wafer_debconf-0.15.11.tar.gz/wafer_debconf-0.15.11/debconf/settings.py
@@ -0,0 +1,11 @@
+from debconf.common_settings import *
+
+INSTALLED_APPS = (
+    'badges',
+    'debconf',
+    'exports',
+    'bursary',
+    'front_desk',
+    'invoices',
+    'register',
+) + INSTALLED_APPS
