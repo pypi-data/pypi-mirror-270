4c7701bff5360569275bd42ebe06b5ba7dc21c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:24 +0000

    Add package behavysis_pipeline/0.1.1/behavysis_pipeline-0.1.1-py3-none-any.whl

---
+++ packages/behavysis_pipeline/behavysis_pipeline-0.1.1-py3-none-any.whl/behavysis_pipeline/processes/__init__.py
@@ -0,0 +1,15 @@
+"""
+_summary_
+"""
+
+from behavysis_core.data_models import experiment_configs
+
+from .analyse import Analyse
+from .calculate_params import CalculateParams
+from .classify_behaviours import ClassifyBehaviours
+from .evaluate import Evaluate
+from .extract_features import ExtractFeatures
+from .format_vid import FormatVid
+from .preprocess import Preprocess
+from .run_dlc import RunDLC
+from .update_configs import UpdateConfigs
