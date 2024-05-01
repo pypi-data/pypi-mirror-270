0d5877112e941804cd5cac235a99c0abb4ec8826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:04:57 +0000

    Add package lianpwn/0.2.0/lianpwn-0.2.0-py3-none-any.whl

---
+++ packages/lianpwn/lianpwn-0.2.0-py3-none-any.whl/lianpwn/template_gen.py
@@ -0,0 +1,36 @@
+# template_gen.py
+import os
+from pwncli import *
+from lianpwn import lg_err, lg_suc
+import subprocess
+
+
+def generate_template():
+    if os.path.exists("exp.py"):
+        lg_err("File 'exp.py' already exists.")
+        subprocess.run(["mv", "exp.py", "exp.py.bak"])
+    content = """#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#   expBy : @eastXueLian
+#   Debug : ./exp.py debug  ./pwn -t -b b+0xabcd
+#   Remote: ./exp.py remote ./pwn ip:port
+
+from lianpwn import *
+from pwncli import *
+
+cli_script()
+set_remote_libc("libc.so.6")
+
+io: tube = gift.io
+elf: ELF = gift.elf
+libc: ELF = gift.libc
+
+ia()
+"""
+
+    with open("exp.py", "w") as f:
+        f.write(content)
+
+    os.chmod("exp.py", 0o755)
+
+    lg_suc("Template 'exp.py' created successfully!")
