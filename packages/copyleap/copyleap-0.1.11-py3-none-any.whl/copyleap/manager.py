e085f1dfc59b7f7a7141713a14e539183c43e642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:05:23 +0000

    Add package copyleap/0.1.11/copyleap-0.1.11-py3-none-any.whl

---
+++ packages/copyleap/copyleap-0.1.11-py3-none-any.whl/copyleap/manager.py
@@ -0,0 +1,6 @@
+class Manager: 
+    def __init__(self) -> None:
+        self.instructions = ""
+
+    def set_instructions(self, instructions: str) -> None:
+        self.instructions = instructions
