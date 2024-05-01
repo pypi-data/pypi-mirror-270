48122cd1d6cf9406f9260cc2b91f232b380708ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:07:02 +0000

    Add package mojo-interop/1.3.60/mojo_interop-1.3.60-py3-none-any.whl

---
+++ packages/mojo-interop/mojo_interop-1.3.60-py3-none-any.whl/mojo/interop/services/vmware/datastructures/specs/vmplacement.py
@@ -0,0 +1,19 @@
+"""
+.. module:: vmplacementspec
+    :platform: Darwin, Linux, Unix, Windows
+    :synopsis: Contains the VmPlacementSpec object used to indicate the placement of a newly created VM.
+
+.. moduleauthor:: Myron Walker <myron.walker@gmail.com>
+"""
+
+from typing import Optional
+
+from dataclasses import dataclass
+
+@dataclass
+class VmPlacementSpec:
+    datastore: str
+    folder: str
+    cluster: Optional[str] = None
+    host: Optional[str] = None
+    resource_pool: Optional[str] = None
