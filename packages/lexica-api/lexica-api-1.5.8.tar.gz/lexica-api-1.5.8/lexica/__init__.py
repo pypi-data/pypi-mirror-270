d25bab4ffaac2a633cea0137e5799588ab5661fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:43 +0000

    Add package lexica-api/1.5.8/lexica-api-1.5.8.tar.gz

---
+++ packages/lexica-api/lexica-api-1.5.8.tar.gz/lexica-api-1.5.8/lexica/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Qewertyy, MIT License
+
+from lexica.core import Client
+from lexica.core_async import AsyncClient
+from lexica.constants import languageModels
+
+__all__ = [
+    "Client",
+    "AsyncClient",
+    "languageModels"
+]
+
+__version__ = "1.5.8"
+__author__ = "Qewertyy <qewertyy.irl@gmail.com>"
