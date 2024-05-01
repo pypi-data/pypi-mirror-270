1fad99cd8c66f2dbb2812511f9eab62ffedbfac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:40 +0000

    Add package lightrag/0.1.0/lightrag-0.1.0.tar.gz

---
+++ packages/lightrag/lightrag-0.1.0.tar.gz/lightrag-0.1.0/extend/agent/react_agent.py
@@ -0,0 +1,330 @@
+"""
+https://arxiv.org/abs/2210.03629, published in Mar, 2023
+
+Agent is not a model or LLM model.
+Agent is better defined as a system that uses LLM models to plan and replan steps that each involves the usage of various tools,
+such as function calls, another LLM model based on the context and history (memory) to complete a task autonomously.
+
+The future: the agent can write your prompts too. Check out dspy: https://github.com/stanfordnlp/dspy
+
+REact agent can be useful for
+- Multi-hop reasoning [Q&A], including dividing the query into subqueries and answering them one by one.
+- Plan the usage of the given tools: highly flexible. Retriever, Generator modules or any other functions can all be wrapped as tools.
+
+The initial ReAct paper does not support different types of tools. We have greatly extended the flexibility of tool adaption, even including an llm tool
+to answer questions that cant be answered or better be answered by llm using its world knowledge.
+- Every react agent can be given a different tasks, different tools, and different LLM models to complete the task.
+- 'finish' tool is defined to finish the task by joining all subqueries answers.
+"""
+
+from jinja2 import Template
+from lightrag.tool_helper import FunctionTool, AsyncCallable
+from lightrag.string_parser import JsonParser, parse_function_call
+from typing import List, Union, Callable, Optional, Any, Dict
+from dataclasses import dataclass
+from lightrag.light_rag import Generator, GeneratorRunner
+
+
+DEFAULT_REACT_AGENT_PROMPT = r"""
+<<SYS>>
+{# role/task description #}
+You task is to answer user's query with minimum steps and maximum accuracy using the tools provided.
+{# REACT instructions #}
+Each step you will read the previous Thought, Action, and Observation(execution result of the action)steps and then provide the next Thought and Action.
+
+You only have access to the following tools:
+{# tools #}
+{% for tool in tools %}
+{{ loop.index }}. ToolName: {{ tool.metadata.name }}
+    Tool Description: {{ tool.metadata.description }}
+    Tool Parameters: {{ tool.metadata.fn_schema_str }} {#tool args can be misleading, especially if we already have type hints and docstring in the function#}
+{% endfor %}
+{# output is always more robust to use json than string #}
+---
+Your output must be in valid JSON format(raw Python string format) with two keys:
+{
+    "thought": "<Why you are taking this action>",
+    "action": "ToolName(<args>, <kwargs>)"
+}
+- Must double quote the JSON str.
+- Inside of the JSON str, Must use escape double quote and escape backslash for string.
+For example:
+"action": "finish(\"John's.\")"
+---
+{# Specifications TODO: preference between the usage of llm tool vs the other tool #}
+Process:
+- Step 1: Read the user query and potentially divide it into subqueries. And get started with the first subquery.
+- Call one available tool at a time to solve each subquery/subquestion. \
+- At step 'finish', join all subqueries answers and finish the task.
+Remember:
+- Action must call one of the above tools with Took Name. It can not be empty.
+- Read the Tool Description and ensure your args and kwarg follow what each tool expects in types. e.g. (a=1, b=2) if it is keyword argument or (1, 2) if it is positional.
+- You will always end with 'finish' action to finish the task. The answer can be the final answer or failure message.
+- When the initial query is simple, use minimum steps to answer the query.
+{#Examples can be here#}
+{# Check if there are any examples #}
+{% if examples %}
+Learn from the examples:
+{% for example in examples %}
+{{ example }}
+{% endfor %}
+{% endif %}
+<</SYS>>
+-----------------
+User query: {{user_query}}
+{# History #}
+{% for history in step_history %}
+Step {{history.step}}:
+{
+ "thought": "{{history.thought}}",
+ "action": "{{history.action}}",
+}
+"observation": "{{history.observation}}"
+{% endfor %}
+You:
+"""
+
+# NOTE: if the positional and keyword arguments are not working well,
+# you can let it be a json string and use only keyword arguments and use json parser to parse the arguments instead of parse_function_call
+
+
+@dataclass
+class StepOutput:
+    step: int
+    thought: str
+    action: str
+    fun_name: Optional[str] = None  # parsed from action
+    fun_args: Optional[List[Any]] = None  # parsed from action
+    fun_kwargs: Optional[Dict[str, Any]] = None  # parsed from action
+    observation: Optional[str] = (
+        None  # when step is created, observation is not available, the funtion result
+    )
+
+    def __str__(self):
+        return f"Thought {self.step}: {self.thought}\nAction {self.step}: {self.action}\nObservation {self.step}: {self.observation}"
+
+
+# TODO: can create an agent base class
+# TODO: make input a class
+class ReActAgent:
+    def __init__(
+        self,
+        generator: Generator = None,
+        prompt: str = DEFAULT_REACT_AGENT_PROMPT,
+        examples: List[str] = [],
+        tools: List[Union[Callable, AsyncCallable, FunctionTool]] = [],
+        max_steps: int = 10,
+    ):
+        self.prompt = prompt
+        self.examples = examples
+        self.tools = tools
+
+        self.tool_helper_attr: Dict[str, Any] = {}
+        # add a geneartor as a helper
+        self.tool_helper_attr["BaseGeneratorRunner"] = GeneratorRunner(
+            generator=generator
+        )
+
+        def llm_tool(input: str) -> str:
+            """
+            I answer any input query with llm's world knowledge. Use me as a fallback tool or when the query is simple.
+            """
+            # use the generator to answer the query
+            try:
+                return self.tool_helper_attr["BaseGeneratorRunner"](input=input)
+            except Exception as e:
+                print(f"Error using the generator: {e}")
+
+            return None
+
+        def finish(answer: str) -> str:
+            """
+            Finish the task by joinging all subqueries answers.
+            """
+            return answer
+
+        self.tools.extend([llm_tool, finish])
+        # convert all functions to FunctionTool, and track how to call each function, either call or acall
+        self.tools = [
+            (
+                tool
+                if isinstance(tool, FunctionTool)
+                else FunctionTool.from_defaults(fn=tool)
+            )
+            for tool in self.tools
+        ]
+
+        self.tools_map = {tool.metadata.name: tool for tool in self.tools}
+        print(f"tools_map: {self.tools_map}")
+
+        self.generator = generator
+        self.max_steps = max_steps
+        self.step_history: List[StepOutput] = []
+        self.text_output_parser = JsonParser()
+
+    def reset(self):
+        self.step_history = []
+
+    def _parse_text_response(self, response: str, step: int) -> Optional[StepOutput]:
+        """
+        Parse the json output
+        """
+        try:
+            json_obj_response = self.text_output_parser(response)
+            thought_key = "thought"
+            action_key = "action"
+            thought = json_obj_response.get(thought_key, "")
+            action = json_obj_response.get(action_key, "")
+            return StepOutput(step=step, thought=thought, action=action)
+        except Exception as e:
+            print(f"Error parsing response: {e}")
+            return None
+
+    def _execute_action(self, action_step: StepOutput) -> Optional[StepOutput]:
+        """
+        Parse the action string to a function call and execute it. Update the action_step with the result.
+        """
+        action = action_step.action
+        try:
+            fun_name, args, kwargs = parse_function_call(action, self.tools_map)
+            print(f"fun_name: {fun_name}, args: {args}, kwargs: {kwargs}")
+            fun: Union[Callable, AsyncCallable] = self.tools_map[fun_name].fn
+            result = fun(*args, **kwargs)
+            action_step.fun_name = fun_name
+            action_step.fun_args = args
+            action_step.fun_kwargs = kwargs
+
+            action_step.observation = result
+            return action_step
+        except Exception as e:
+            print(f"Error executing {action}: {e}")
+            # pass the error as observation so that the agent can continue and correct the error in the next step
+            action_step.observation = f"Error executing {action}: {e}"
+            return action_step
+
+    def _run_one_step(self, input: str, step: int) -> str:
+        """
+        Run one step of the agent.
+        """
+        template = Template(self.prompt)
+        # TODO: use base class to provide a method to formulate the data
+        prompt = template.render(
+            user_query=input,
+            tools=self.tools,
+            step_history=self.step_history,
+            examples=self.examples,
+        )
+        messages = [
+            {
+                "role": "system",
+                "content": prompt,
+            }
+        ]
+        print(f"messages: {messages}")
+        response = self.generator(messages)
+        print(f"raw generator output: {response}")
+        parsed_response = self._parse_text_response(response, step)
+        # execute the action
+        if parsed_response and parsed_response.action:
+            parsed_response = self._execute_action(parsed_response)
+        else:
+            print(f"Failed to parse response for step {step}")
+        self.step_history.append(parsed_response)
+
+        return response
+
+    def run(self, input: str) -> str:
+        """
+        Run the agent on the given input.
+        """
+        for i in range(self.max_steps):
+            step = i + 1
+            try:
+                self._run_one_step(input, step)
+                if (
+                    self.step_history[-1].fun_name
+                    and self.step_history[-1].fun_name == "finish"
+                ):
+                    break
+            except Exception as e:
+                error_message = f"Error running step {step}: {e}"
+                print(error_message)
+
+        answer = self.step_history[-1].observation
+        print(f"step_history: {self.step_history}")
+        self.reset()
+        return answer
+
+
+if __name__ == "__main__":
+    from extend.generator.groq_generator import GroqGenerator
+
+    def multiply(a: int, b: int) -> int:
+        """
+        Multiply two numbers.
+        """
+        return a * b
+
+    def add(a: int, b: int) -> int:
+        """
+        Add two numbers.
+        """
+        return a + b
+
+    def search(query: str) -> str:
+        """
+        Search the web for the given query.
+        """
+        return "python programming is a great way to learn programming"
+
+    tools = [
+        FunctionTool.from_defaults(fn=multiply),
+        FunctionTool.from_defaults(fn=add),
+        # FunctionTool.from_defaults(fn=search),
+    ]
+    settings = {
+        "provider": "groq",
+        "model": "llama3-70b-8192",  # llama3 is not good with string formatting, llama3 8b is also bad at following instruction, 70b is better but still not as good as gpt-3.5-turbo
+        # mistral also not good: mixtral-8x7b-32768, but with better prompt, it can still work
+        "temperature": 0.0,
+    }
+
+    planner = GroqGenerator(**settings)
+    # settings = {
+    #     "provider": "openai",
+    #     "model": "gpt-3.5-turbo",
+    #     "temperature": 0.0,
+    # }
+    # planner = OpenAIGenerator(**settings)
+    examples = [
+        # r"""
+        # User: What is 9 - 3?
+        # You: {
+        #     "thought": "I need to subtract 3 from 9, but there is no subtraction tool, so I ask llm_tool to answer the query.",
+        #     "action": "llm_tool('What is 9 - 3?')"
+        # }
+        # """
+    ]
+    agent = ReActAgent(generator=planner, tools=tools, max_steps=10, examples=examples)
+    queries = [
+        # "What is 2 times 3?",
+        # "What is 3 plus 4?",
+        # "What is the capital of France? and what is 4 times 5 then add 3?",  # this is actually two queries, or a multi-hop query
+        "Li adapted her pet Apple in 2017 when Apple was only 2 months old, now we are at year 2024, how old is Li's pet Apple?",
+    ]
+    """
+    Results: mixtral-8x7b-32768, 0.9s per query
+    llama3-70b-8192, 1.8s per query
+    gpt-3.5-turbo, 2.2s per query
+    """
+    import time
+
+    average_time = 0
+    for query in queries:
+        t0 = time.time()
+        answer = agent.run(query)
+        average_time += time.time() - t0
+        print(f"Answer: {answer}")
+    print(f"Average time: {average_time / len(queries)}")
+
+    # test multiply(2024 - 2017, 12)
---
+++ packages/lightrag/lightrag-0.1.0.tar.gz/lightrag-0.1.0/lightrag/string_parser.py
@@ -0,0 +1,318 @@
+"""
+LLM applications requires lots of string processing. Such as the text output needed to be parsed into:
+(1) JSON format or other formats
+(2) SQL/Python valid format
+(3) Tool(function) call format
+
+We design this these string_parser modules to be generic to any input text without differentiating them as input text or output text.
+"""
+
+import re
+
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Tuple
+import json
+import ast
+from lightrag.tool_helper import ToolOutput
+
+
+############################################################################################################
+# String as other output format such as JSON, List, etc.
+############################################################################################################
+def extract_json_str(text: str, add_missing_right_brace: bool = True) -> str:
+    """
+    Extract JSON string from text.
+    NOTE: Only handles the first JSON object found in the text. And it expects at least one JSON object in the text.
+    If right brace is not found, we add one to the end of the string.
+    """
+    # NOTE: this regex parsing is taken from langchain.output_parsers.pydantic
+    text = text.strip().replace("{{", "{").replace("}}", "}")
+    start = text.find("{")
+    if start == -1:
+        raise ValueError(f"No JSON object found in the text: {text}")
+
+    # Attempt to find the matching closing brace
+    brace_count = 0
+    end = -1
+    for i in range(start, len(text)):
+        if text[i] == "{":
+            brace_count += 1
+        elif text[i] == "}":
+            brace_count -= 1
+
+        if brace_count == 0:
+            end = i
+            break
+
+    if end == -1 and add_missing_right_brace:
+        # If no closing brace is found, but we are allowed to add one
+        text += "}"
+        end = len(text) - 1
+    elif end == -1:
+        raise ValueError(
+            "Incomplete JSON object found and add_missing_right_brace is False."
+        )
+
+    return text[start : end + 1]
+
+
+def extract_list_str(text: str, add_missing_right_bracket: bool = True) -> str:
+    """
+    Extract the first complete list string from the provided text. If the list string is incomplete
+    (missing the closing bracket), an option allows adding a closing bracket at the end.
+
+    Args:
+        text (str): The text containing potential list data.
+        add_missing_right_bracket (bool): Whether to add a closing bracket if it is missing.
+
+    Returns:
+        str: The extracted list string.
+
+    Raises:
+        ValueError: If no list is found or if the list extraction is incomplete
+                    without the option to add a missing bracket.
+    """
+    text = text.strip()
+    start = text.find("[")
+    if start == -1:
+        raise ValueError("No list found in the text.")
+
+    # Attempt to find the matching closing bracket
+    bracket_count = 0
+    end = -1
+    for i in range(start, len(text)):
+        if text[i] == "[":
+            bracket_count += 1
+        elif text[i] == "]":
+            bracket_count -= 1
+
+        if bracket_count == 0:
+            end = i
+            break
+
+    if end == -1 and add_missing_right_bracket:
+        # If no closing bracket is found, but we are allowed to add one
+        text += "]"
+        end = len(text) - 1
+    elif end == -1:
+        raise ValueError(
+            "Incomplete list found and add_missing_right_bracket is False."
+        )
+
+    return text[start : end + 1]
+
+
+def fix_json_missing_commas(json_str: str) -> str:
+    # Example: adding missing commas, only after double quotes
+    # Regular expression to find missing commas
+    regex = r'(?<=[}\]"\'\d])(\s+)(?=[\{"\[])'
+
+    # Add commas where missing
+    fixed_json_str = re.sub(regex, r",\1", json_str)
+
+    return fixed_json_str
+
+
+def fix_json_escaped_single_quotes(json_str: str) -> str:
+    # First, replace improperly escaped single quotes inside strings
+    # json_str = re.sub(r"(?<!\\)\'", '"', json_str)
+    # Fix escaped single quotes
+    json_str = json_str.replace(r"\'", "'")
+    return json_str
+
+
+############################################################################################################
+# Often used as the output parser for LLM text output
+############################################################################################################
+class BaseTextParser(ABC):
+    """
+    A base class for text parsers. Good for type hinting and inheritance.
+    """
+
+    @abstractmethod
+    def __call__(self, text: str) -> Any:
+        """
+        Parse the provided text and return the parsed data.
+        """
+        pass
+
+
+class JsonParser(BaseTextParser):
+    """
+    A text parser for extracting JSON strings from text to json object.
+    NOTE: ensure only pass one json string in the text.
+    You can use `extract_json_str` to extract the first json string from the text.
+    """
+
+    def __init__(self, add_missing_right_brace: bool = True):
+        self.add_missing_right_brace = add_missing_right_brace
+
+    def __call__(self, text: str) -> Dict[str, Any]:
+        text = text.strip()
+        try:
+            json_str = extract_json_str(text, self.add_missing_right_brace)
+            # 1st attempt to load the json string
+            json_obj = json.loads(json_str)
+            return json_obj
+        except json.JSONDecodeError:
+            # 2nd attemp after fixing the json string
+            try:
+                print("Trying to fix potential missing commas...")
+                json_str = fix_json_missing_commas(json_str)
+                print("Trying to fix scaped single quotes...")
+                json_str = fix_json_escaped_single_quotes(json_str)
+                print(f"Fixed JSON string: {json_str}")
+                json_obj = json.loads(json_str)
+                return json_obj
+            except json.JSONDecodeError as e:
+                # 3rd attemp using yaml
+                try:
+                    import yaml
+
+                    # NOTE: parsing again with pyyaml
+                    #       pyyaml is less strict, and allows for trailing commas
+                    #       right now we rely on this since guidance program generates
+                    #       trailing commas
+                    print("Parsing JSON string with PyYAML...")
+                    json_obj = yaml.safe_load(json_str)
+                    return json_obj
+                except yaml.YAMLError as e_yaml:
+                    raise ValueError(
+                        f"Got invalid JSON object. Error: {e}. Got JSON string: {json_str}"
+                    )
+                except NameError as exc:
+                    raise ImportError("Please pip install PyYAML.") from exc
+
+
+############################################################################################################
+# String as function call
+############################################################################################################
+def evaluate_ast_node(node: ast.AST, context_map: Dict[str, Any] = None):
+    """
+    Recursively evaluates an AST node and returns the corresponding Python object.
+
+    Args:
+        node (ast.AST): The AST node to evaluate. This node can represent various parts of Python expressions,
+                        such as literals, identifiers, lists, dictionaries, and function calls.
+        context_map (Dict[str, Any]): A dictionary that maps variable names to their respective values and functions.
+                                      This context is used to resolve names and execute functions.
+
+    Returns:
+        Any: The result of evaluating the node. The type of the returned object depends on the nature of the node:
+             - Constants return their literal value.
+             - Names are looked up in the context_map.
+             - Lists and tuples return their contained values as a list or tuple.
+             - Dictionaries return a dictionary with keys and values evaluated.
+             - Function calls invoke the function with evaluated arguments and return its result.
+
+    Raises:
+        ValueError: If the node type is unsupported, a ValueError is raised indicating the inability to evaluate the node.
+    """
+    if isinstance(node, ast.Constant):
+        return node.value
+    elif isinstance(node, ast.Dict):
+        return {
+            evaluate_ast_node(k): evaluate_ast_node(v)
+            for k, v in zip(node.keys, node.values)
+        }
+    elif isinstance(node, ast.List):
+        return [evaluate_ast_node(elem) for elem in node.elts]
+    elif isinstance(node, ast.Tuple):
+        return tuple(evaluate_ast_node(elem) for elem in node.elts)
+    elif isinstance(node, ast.UnaryOp) and isinstance(node.op, ast.USub):
+        return -evaluate_ast_node(node.operand, context_map)  # unary minus
+    elif isinstance(
+        node, ast.BinOp
+    ):  # support "multiply(2024-2017, 12)", the "2024-2017" is a "BinOp" node
+        left = evaluate_ast_node(node.left, context_map)
+        right = evaluate_ast_node(node.right, context_map)
+        if isinstance(node.op, ast.Add):
+            return left + right
+        elif isinstance(node.op, ast.Sub):
+            return left - right
+        elif isinstance(node.op, ast.Mult):
+            return left * right
+        elif isinstance(node.op, ast.Div):
+            return left / right
+        elif isinstance(node.op, ast.Mod):
+            return left % right
+        elif isinstance(node.op, ast.Pow):
+            return left**right
+        else:
+            raise ValueError(f"Unsupported binary operator: {type(node.op)}")
+    elif isinstance(node, ast.Name):  # variable name
+        try:
+            output_fun = context_map[node.id]
+            return output_fun
+        # TODO: raise the error back to the caller so that the llm can get the error message
+        except KeyError as e:
+            raise ValueError(
+                f"Error: {e}, {node.id} does not exist in the context_map."
+            )
+
+    elif isinstance(
+        node, ast.Call
+    ):  # another fun or class as argument and value, e.g. add( multiply(4,5), 3)
+        func = evaluate_ast_node(node.func, context_map)
+        args = [evaluate_ast_node(arg, context_map) for arg in node.args]
+        kwargs = {
+            kw.arg: evaluate_ast_node(kw.value, context_map) for kw in node.keywords
+        }
+        print(f"another fun or class as argument and value: {func}, {args}, {kwargs}")
+        output = func(*args, **kwargs)
+        if isinstance(output, ToolOutput):
+            return output.raw_output
+        print(f"output: {output}")
+        return output
+    else:
+        raise ValueError(f"Unsupported AST node type: {type(node)}")
+
+
+def parse_function_call(
+    call_string: str, context_map: Dict[str, Any] = None
+) -> Tuple[str, List[Any], Dict[str, Any]]:
+    """
+    Parse a string representing a function call into its components and ensure safe execution by only allowing function calls from a predefined context map.
+    Args:
+        call_string (str): The string representing the function call.
+        context_map (Dict[str, Any]): A dictionary that maps variable names to their respective values and functions.
+                                      This context is used to resolve names and execute functions.
+    """
+    call_string = call_string.strip()
+    # Parse the string into an AST
+    tree = ast.parse(call_string, mode="eval")
+
+    if isinstance(tree.body, ast.Call):
+        # Extract the function name
+        func_name = tree.body.func.id if isinstance(tree.body.func, ast.Name) else None
+
+        # Prepare the list of arguments and keyword arguments
+        args = [evaluate_ast_node(arg, context_map) for arg in tree.body.args]
+        keywords = {
+            kw.arg: evaluate_ast_node(kw.value, context_map)
+            for kw in tree.body.keywords
+        }
+
+        return func_name, args, keywords
+    else:
+        raise ValueError("Provided string is not a function call.")
+
+
+if __name__ == "__main__":
+    test_input = (
+        '{"name": "John", "age": 30, "attributes": {"height": 180, "weight": 70}'
+    )
+    print(
+        extract_json_str(test_input, add_missing_right_brace=True)
+    )  # Expected to complete the JSON properly
+
+    test_input_2 = 'Some random text before {"key1": "value1"} and more after'
+    print(extract_json_str(test_input_2))  # Expected to extract {"key1": "value1"}
+
+    test_input_3 = "No JSON here"
+    try:
+        print(extract_json_str(test_input_3))
+    except ValueError as e:
+        print(e)  # Expected to raise an error about no JSON object found
+
+    # test
