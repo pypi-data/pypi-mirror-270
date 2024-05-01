05db35314c569e1cc1029c174f8190e9e6586ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:06:44 +0000

    Add package arbmaster/0.1.0/arbmaster-0.1.0-py3-none-any.whl

---
+++ packages/arbmaster/arbmaster-0.1.0-py3-none-any.whl/arbmaster/calc.py
@@ -0,0 +1,40 @@
+from typing import Dict, List
+
+class ArbitrageCalc:
+    
+    def __init__(self, odds: Dict[str, list], stake: float = 100) -> None:
+        self.odds = [v[0]["price"] for v in odds.values() if len(v) >= 1]
+        self.probability: float = self._calc_prob(self.odds)
+        
+        self.stakes: List[float] = self._calc_stakes(stake)
+        self.payout: float = self._calc_payout()
+        self.profit: float = self._calc_profit()
+        self.roi: str = self._calc_roi()
+    
+    def is_arbitrage(self) -> bool:
+        return self.probability < 1
+    
+    def _calc_prob(self, odds) -> float:
+        p = 0
+        for odd in odds:
+            p += 1 / odd
+        return p
+    
+    def _calc_stakes(self, stake: float) -> List[float]:
+        stakes: List[float] = []
+        for odd in self.odds:
+            bet = (stake * (1 / odd)) / self.probability
+            stakes.append(bet)
+        return stakes
+    
+    def _calc_payout(self) -> float:
+        payout: float = 0
+        for odd, stake in zip(self.odds, self.stakes):
+            payout = stake * odd
+        return round(payout, 2)
+    
+    def _calc_profit(self) -> float:
+        return round(self.payout - sum(self.stakes), 2)
+    
+    def _calc_roi(self) -> str:
+        return "{:.2f}%".format((self.profit / sum(self.stakes)) * 100)
