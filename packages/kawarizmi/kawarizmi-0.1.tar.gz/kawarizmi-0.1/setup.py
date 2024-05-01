675533c5c36a9961fd031eeee41c3d4b47429220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:07:08 +0000

    Add package kawarizmi/0.1/kawarizmi-0.1.tar.gz

---
+++ packages/kawarizmi/kawarizmi-0.1.tar.gz/kawarizmi-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kawarizmi',
+    version='0.1',
+
+    packages=find_packages(),
+
+    install_requires=[
+
+        'ultralytics >= 8.1.42',
+        'numpy >= 1.26.4',
+        'opencv-python >= 4.9.0.80',
+        'shapely >= 2.0.3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'kawarizmi = kawarizmi:parking_space_counter',
+        ],
+    },
+)
