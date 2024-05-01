9a75111e9c841185f1002f275483e3fde06d5c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:05:44 +0000

    Add package sparv-sbx-ocr-correction-viklofg-sweocr/0.2.3/sparv_sbx_ocr_correction_viklofg_sweocr-0.2.3.tar.gz

---
+++ packages/sparv-sbx-ocr-correction-viklofg-sweocr/sparv_sbx_ocr_correction_viklofg_sweocr-0.2.3.tar.gz/sparv_sbx_ocr_correction_viklofg_sweocr-0.2.3/tests/test_ocr_suggestor.py
@@ -0,0 +1,109 @@
+from sbx_ocr_correction_viklofg_sweocr.ocr_corrector import OcrCorrector
+
+
+def test_short_text(ocr_corrector: OcrCorrector):
+    text = [
+        "Den",
+        "i",
+        "HandelstidniDgens",
+        "g&rdagsnnmmer",
+        "omtalade",
+        "hvalfisken",
+        ",",
+        "sorn",
+        "fångats",
+        "i",
+        "Frölnndaviken",
+        ".",
+    ]
+    actual = ocr_corrector.calculate_corrections(text)
+
+    expected = [
+        None,
+        None,
+        "Handelstidningens",
+        "gårdagsnummer",
+        None,
+        None,
+        None,
+        "som",
+        None,
+        None,
+        "Frölandsviken",
+        None,
+    ]
+    assert actual == expected
+
+
+def test_long_text(ocr_corrector: OcrCorrector):
+    text1 = [
+        "Förvaltningen",
+        "af",
+        "Biksgäldskontoret",
+        ",",
+        "dess",
+        "medel",
+        "och",
+        "tillhörigheter",
+        "är",
+        "utaf",
+        "Riksdagen",
+        "uppdragen",
+        "åt",
+        "sju",
+        "Fullmäktige",
+        ",",
+        "hvilka",
+        "vid",
+        "lagtima",
+        "riksdag",
+        "utses",
+        "i",
+        "den",
+        "ordning",
+        "71",
+        "§",
+        "Riksdagsordningcn",
+        "stadgar",
+        ".",
+    ]
+    # text2 = """Vid Fullmäktiges sammankomster föres ordet af den, som af Riksdagen
+    # blifvit dertill utsedd; tillkommande Fullmäktige att sjelfva bland sig välja en
+    # vice Ordförande att föra ordet, när hinder för Ordföranden inträffar."""
+    # print(f"{len(text2)=}, {len(text2.encode())=}")
+    actual = ocr_corrector.calculate_corrections(text1)
+    # actual = ocr_corrector.calculate_corrections(text2)
+
+    expected = [
+        None,
+        None,
+        "Riksgäldskontoret",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        "Riksdagsordningen",
+        None,
+        None,
+    ]
+
+    assert actual == expected
