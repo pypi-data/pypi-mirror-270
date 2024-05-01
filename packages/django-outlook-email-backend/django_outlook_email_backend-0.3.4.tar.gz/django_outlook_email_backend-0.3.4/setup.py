a2c4078c3161469ecc519c353037bc3a7e588ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:07:34 +0000

    Add package django-outlook-email-backend/0.3.4/django_outlook_email_backend-0.3.4.tar.gz

---
+++ packages/django-outlook-email-backend/django_outlook_email_backend-0.3.4.tar.gz/django_outlook_email_backend-0.3.4/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='django-outlook-email-backend',
+    version='0.3.4',
+    description='Ouauth2 outlook email backend for Django',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Marc Claramunt',
+    author_email='mclaramunt@enley.com',
+    license='MIT',
+    zip_safe=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    url='https://github.com/enley-es/django-outlook-email-backend',
+)
