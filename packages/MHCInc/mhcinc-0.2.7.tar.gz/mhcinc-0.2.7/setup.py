bd26c071c8593ee48b54cdec12ee5390805c01e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:05:04 +0000

    Add package MHCInc/0.2.7/mhcinc-0.2.7.tar.gz

---
+++ packages/MHCInc/mhcinc-0.2.7.tar.gz/mhcinc-0.2.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r",encoding = "UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MHCInc",
+    version="0.2.7",
+    author="Mhc-inc",
+    author_email="Wf6350177@163.com",
+    description="改进漏洞：将使用模块功能时找不到文件优化",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/MHCInc/",
+    project_urls={
+        "Bug Tracker": "https://github.com/Mhc-inc/MHCInc/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['lxml',
+                  'requests','bs4','pygame'],
+    package_dir={"": "MHCInc"},
+    packages=setuptools.find_packages(where="MHCInc"),
+    python_requires=">=3.9",
+)
