83e69776c0b7cc017ba6c492cc1f8283de5f2c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:44 +0000

    Add package OpenMiChroM/1.0.8/OpenMiChroM-1.0.8.tar.gz

---
+++ packages/OpenMiChroM/OpenMiChroM-1.0.8.tar.gz/OpenMiChroM-1.0.8/OpenMiChroM/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.8'
+
+from .ChromDynamics import MiChroM
+from .Optimization import FullTraining, CustomMiChroMTraining
+from .CndbTools import cndbTools
+from .Integrators import ActiveBrownianIntegrator
