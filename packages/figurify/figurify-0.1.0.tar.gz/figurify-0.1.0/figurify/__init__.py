b791e4e158b5fe02205d1db2fb74e8e0214e0928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:04:55 +0000

    Add package figurify/0.1.0/figurify-0.1.0.tar.gz

---
+++ packages/figurify/figurify-0.1.0.tar.gz/figurify-0.1.0/figurify/__init__.py
@@ -0,0 +1,6 @@
+from figurify import plane_metrics
+from figurify import geometric_solids
+from figurify import figure_physics
+
+from figurify import units
+from figurify import material_properties
