f8aedaeca542845eb7573034ae7bfbd641bd673c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 06:04:46 +0000

    Add package keystone-api/0.3.7/keystone_api-0.3.7.tar.gz

---
+++ packages/keystone-api/keystone_api-0.3.7.tar.gz/keystone_api-0.3.7/keystone_api/apps/logging/tasks.py
@@ -0,0 +1,25 @@
+"""Scheduled tasks executed in parallel by Celery.
+
+Tasks are scheduled and executed in the background by Celery. They operate
+asynchronously from the rest of the application and log their results in the
+application database.
+"""
+
+from datetime import datetime, timedelta
+
+from celery import shared_task
+from django.conf import settings
+
+from .models import *
+
+
+@shared_task()
+def rotate_log_files() -> None:
+    """Delete old log files"""
+
+    if settings.LOG_RECORD_ROTATION == 0:
+        return
+
+    max_record_age = datetime.now() - timedelta(seconds=settings.LOG_RECORD_ROTATION)
+    AppLog.objects.filter(time__lt=max_record_age).delete()
+    RequestLog.objects.filter(time__lt=max_record_age).delete()
