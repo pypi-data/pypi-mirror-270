6ce22fa8df4111fa7dd289ad6769ac0b1fe82ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:07:09 +0000

    Add package tif-to-zarr/0.4/tif_to_zarr-0.4.tar.gz

---
+++ packages/tif-to-zarr/tif_to_zarr-0.4.tar.gz/tif_to_zarr-0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os import path
+working_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_directory, 'README.md'),
+          encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tif_to_zarr',
+    version=0.4,
+    url='https://github.com/yuriyzubov/tif-to-zarr.git',
+    author='Yurii Zubov',
+    description='Simple package to convert small tif files to zarr format.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'imagecodecs',
+        'click',
+        'zarr',
+        'dask', 
+        'tifffile',
+        ],
+)
