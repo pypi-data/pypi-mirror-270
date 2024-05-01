8c4bc9062cb4992f862be6e8e431c607c25fce32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 05:06:46 +0000

    Add package zpodcli/0.6.0/zpodcli-0.6.0-py3-none-any.whl

---
+++ packages/zpodcli/zpodcli-0.6.0-py3-none-any.whl/zpodcli/lib/utils.py
@@ -0,0 +1,17 @@
+import typer
+from rich import print
+
+
+def exit_with_error(txt, code=1, color="indian_red"):
+    print_errors(txt, color)
+    raise typer.Exit(code)
+
+
+def print_errors(txt, color="indian_red"):
+    print(f"[{color}]Error(s) Found:\n  {txt}[/{color}]")
+
+
+def get_boolean_markdown(boolean: bool):
+    if boolean:
+        return f"[dark_sea_green4]{boolean}[/dark_sea_green4]"
+    return f"[indian_red]{boolean}[/indian_red]"
