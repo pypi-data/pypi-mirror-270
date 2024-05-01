6d651650d4b299b791fe61d087df853147f47a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:05:19 +0000

    Add package layerborn.cdk-iam-policy-builder-helper/0.0.6/layerborn.cdk_iam_policy_builder_helper-0.0.6-py3-none-any.whl

---
+++ packages/layerborn.cdk-iam-policy-builder-helper/layerborn.cdk_iam_policy_builder_helper-0.0.6-py3-none-any.whl/layerborn/cdk_iam_policy_builder_helper/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@layerborn/cdk-iam-policy-builder-helper",
+    "0.0.6",
+    __name__[0:-6],
+    "cdk-iam-policy-builder-helper@0.0.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
