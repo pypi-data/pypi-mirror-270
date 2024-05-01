0fa9dfcbed0f4967f2c991378c6269e4b73879b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:04:50 +0000

    Add package testgunal123/0.0.2/testgunal123-0.0.2.tar.gz

---
+++ packages/testgunal123/testgunal123-0.0.2.tar.gz/testgunal123-0.0.2/gunaltest/app.py
@@ -0,0 +1,3 @@
+def gunal():
+
+    return('Hello Gunal')
---
+++ packages/testgunal123/testgunal123-0.0.2.tar.gz/testgunal123-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'gunal-testing'
+
+setup(
+    name="testgunal123",
+    version=VERSION,
+    author="Gunal",
+    author_email="gunalr00@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
