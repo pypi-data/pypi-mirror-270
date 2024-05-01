c1268320cd12c36481d4e93eb3e0cf49b0b241aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:04:40 +0000

    Add package pyvesc-fix/1.0.1/pyvesc_fix-1.0.1-py3-none-any.whl

---
+++ packages/pyvesc-fix/pyvesc_fix-1.0.1-py3-none-any.whl/pyvesc_fix/__main__.py
@@ -0,0 +1,6 @@
+from pyvesc_fix import PyvescFix
+
+if __name__ == "__main__":
+    print(f"Content of PyvescFix = {dir(PyvescFix)}")
+    print("Please follow the scripts in examples, ")
+    print("this library is meant to interract with a physical device.")
