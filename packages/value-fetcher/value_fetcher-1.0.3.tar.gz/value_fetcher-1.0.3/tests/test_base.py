1ab2aeb1b43d5abab76f3c02224eabaa80fc3bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 03:04:55 +0000

    Add package value-fetcher/1.0.3/value_fetcher-1.0.3.tar.gz

---
+++ packages/value-fetcher/value_fetcher-1.0.3.tar.gz/value_fetcher-1.0.3/tests/test_base.py
@@ -0,0 +1,41 @@
+"""
+Provider unit tests
+"""
+
+import os
+
+import pytest
+from value_fetcher import ValueFetcher
+
+# Set mocked boto (moto) client default values
+os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+fetcher = ValueFetcher()
+
+
+def test_invalid_names():
+    """
+    Assert invalid or empty names produce exceptions
+    """
+    # Test non-existent variable throws an exception
+    with pytest.raises(ValueError) as exception:
+        fetcher.get("")
+
+    with pytest.raises(ValueError) as env_exception:
+        fetcher.get_from_env("")
+
+    assert str(exception.value) == "Invalid key name provided"
+    assert str(env_exception.value) == "Missing or empty environment key"
+
+
+def test_unknown_source(monkeypatch):
+    """
+    Test unknown source raises an exception
+    """
+    monkeypatch.setenv("UNKNOWN", "test_value")
+    monkeypatch.setenv("UNKNOWN_SOURCE", "unknown")
+    # Test unknown source returns nothing
+    with pytest.raises(ValueError) as exception:
+        fetcher.get("UNKNOWN")
+
+    assert "Unknown source" in str(exception.value)
