803de729671e8a973dfcfcb78e82c966c0fb6dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 04:05:01 +0000

    Add package verifycredit/1.0.1/verifycredit-1.0.1.tar.gz

---
+++ packages/verifycredit/verifycredit-1.0.1.tar.gz/verifycredit-1.0.1/setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup
+setup(
+    name='verifycredit',
+    version='1.0.1',
+    description='verfication de credit',
+    py_modules=['verifycredit'],
+    install_requires=['requests'],
+    author='Mahdi Sahnoun',
+    author_email='mahdisahnoun31@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
