08fd0789edad4d8f6f799615c225fce52c9348d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-05-01 14:04:50 +0000

    Add package cometai-core/0.2/cometai_core-0.2-py3-none-any.whl

---
+++ packages/cometai-core/cometai_core-0.2-py3-none-any.whl/scraper/models/person/Person.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from Experience import Experience
+from typing import List
+from Education import Education
+from Post import Post
+from InterestInfo import InterestInfo
+
+@dataclass
+class PersonalLocation:
+    city: str
+    postal_code: str
+    country: str
+
+@dataclass
+class Name:
+    first: str
+    last: str
+    addiional: str
+
+@dataclass
+class BasicInfo:
+    name: Name
+    headline: str
+    # current_position: Experience #Not really sure what this dropdown is pulling from
+
+
+@dataclass
+class Person:
+    person_id: int
+    about: str;
+    basic: BasicInfo
+    industry: str
+    education: str
+    location: PersonalLocation
+    contact_info: str
+    interest_info: InterestInfo
+    posts: List[Post]
+    experiences: List[Experience]
+
